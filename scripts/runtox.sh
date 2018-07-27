995fe81f21b5f090fe8d3d253a5119de6c9ad028
Markus Unterwaditzer <markus@unterwaditzer.net>
2018-07-27 14:05:45 +0200

    feat: make runtox script more useful for local dev

--- scripts/runtox.sh
+++ scripts/runtox.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-pyversion="$(echo py$TRAVIS_PYTHON_VERSION | sed -e 's/pypypy/pypy/g' -e 's/-dev//g')"
-exec tox -e $(tox -l | grep $pyversion | tr '\n' ',')
+if [ -z "$1" ]; then
+    searchstring="$(echo py$TRAVIS_PYTHON_VERSION | sed -e 's/pypypy/pypy/g' -e 's/-dev//g')"
+else
+    searchstring="$1"
+fi
+
+exec tox -e $(tox -l | grep $searchstring | tr '\n' ',')
