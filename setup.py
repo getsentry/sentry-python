d3788da69bcae9e5e55a49375ade67c92e52c2f3
Markus Unterwaditzer <markus@unterwaditzer.net>
2018-07-31 23:46:47 +0200

    fix: genius packaging docs are only for py3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 # read the contents of your README file
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
 
 setup(
