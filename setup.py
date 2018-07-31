312ff20d52338512e78e907011debd3adafaacce
Markus Unterwaditzer <markus@unterwaditzer.net>
2018-07-31 23:39:04 +0200

    fix: fix markdown readme in setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,12 @@
 
 from setuptools import setup, find_packages
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name="sentry-sdk",
     version="0.1.0-preview3",
@@ -9,7 +15,8 @@ setup(
     author_email="hello@getsentry.com",
     url="https://github.com/getsentry/sentry-sdk",
     description="Python client for Sentry (https://getsentry.com)",
-    long_description=__doc__,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(exclude=("tests", "tests.*")),
     zip_safe=False,
     license="BSD",
