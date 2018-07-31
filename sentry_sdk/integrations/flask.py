574437be131d477911276fe4a102130e76f00add
Markus Unterwaditzer <markus@unterwaditzer.net>
2018-07-31 23:00:58 +0200

    fix: capture request in event processor

--- sentry_sdk/integrations/flask.py
+++ sentry_sdk/integrations/flask.py
@@ -10,7 +10,7 @@ try:
 except ImportError:
     current_user = None
 
-from flask import current_app, request
+from flask import _request_ctx_stack, _app_ctx_stack
 from flask.signals import (
     appcontext_pushed,
     appcontext_tearing_down,
@@ -32,7 +32,7 @@ class FlaskIntegration(Integration):
 
 def _push_appctx(*args, **kwargs):
     get_current_hub().push_scope()
-    get_current_hub().add_event_processor(lambda: _event_processor)
+    get_current_hub().add_event_processor(_make_event_processor)
 
 
 def _pop_appctx(*args, **kwargs):
@@ -43,24 +43,30 @@ def _capture_exception(sender, exception, **kwargs):
     capture_exception(exception)
 
 
-def _event_processor(event):
-    if request:
-        if "transaction" not in event:
+def _make_event_processor():
+    request = getattr(_request_ctx_stack.top, 'request', None)
+    app = getattr(_app_ctx_stack.top, 'app', None)
+
+    def event_processor(event):
+        if request:
+            if "transaction" not in event:
+                with _internal_exceptions():
+                    event["transaction"] = request.url_rule.endpoint
+
             with _internal_exceptions():
-                event["transaction"] = request.url_rule.endpoint
+                FlaskRequestExtractor(request).extract_into_event(event)
+
+        if _should_send_default_pii():
+            with _internal_exceptions():
+                _set_user_info(request, event)
 
         with _internal_exceptions():
-            FlaskRequestExtractor(request).extract_into_event(event)
+            _process_frames(app, event)
 
-    if _should_send_default_pii():
-        with _internal_exceptions():
-            _set_user_info(event)
-
-    with _internal_exceptions():
-        _process_frames(event)
+    return event_processor
 
 
-def _process_frames(event):
+def _process_frames(app, event):
     for frame in event.iter_frames():
         if "in_app" in frame:
             continue
@@ -70,9 +76,8 @@ def _process_frames(event):
 
         if module == "flask" or module.startswith("flask."):
             frame["in_app"] = False
-        elif current_app and (
-            module.startswith("%s." % current_app.import_name)
-            or module == current_app.import_name
+        elif app and (
+            module.startswith("%s." % app.import_name) or module == app.import_name
         ):
             frame["in_app"] = True
 
@@ -100,13 +105,13 @@ class FlaskRequestExtractor(RequestExtractor):
 
     @property
     def files(self):
-        return request.files
+        return self.request.files
 
     def size_of_file(self, file):
         return file.content_length
 
 
-def _set_user_info(event):
+def _set_user_info(request, event):
     if "user" in event:
         return
 
