4723dc250059d5c498f5200de654306ce275018a
Markus Unterwaditzer <markus@unterwaditzer.net>
2018-08-02 15:54:03 +0200

    feat: hardcode fingerprint for celery timeouts

--- sentry_sdk/integrations/celery.py
+++ sentry_sdk/integrations/celery.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 from celery.signals import task_failure, task_prerun, task_postrun
+from celery.exceptions import SoftTimeLimitExceeded
 
 from sentry_sdk import get_current_hub, configure_scope, capture_exception
 from sentry_sdk.hub import _internal_exceptions
@@ -23,7 +24,18 @@ class CeleryIntegration(Integration):
         if hasattr(sender, "throws") and isinstance(einfo.exception, sender.throws):
             return
 
-        capture_exception(einfo.exc_info)
+        if isinstance(einfo.exception, SoftTimeLimitExceeded):
+            with get_current_hub().push_scope():
+                with configure_scope() as scope:
+                    scope.fingerprint = [
+                        "celery",
+                        "SoftTimeLimitExceeded",
+                        getattr(sender, "name", sender),
+                    ]
+
+                capture_exception(einfo.exc_info)
+        else:
+            capture_exception(einfo.exc_info)
 
     def _handle_task_prerun(self, sender, task, **kw):
         with _internal_exceptions():
