76b0e1048308f9de6b681a1092decd3d39e2be63
Markus Unterwaditzer <markus@unterwaditzer.net>
2018-08-03 17:49:47 +0200

    chore: travis cache

--- scripts/checkout-semaphore.sh
+++ scripts/checkout-semaphore.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
+
+# This script is able to restore partially checked out repositories,
+# repositories that have all files but no git content, repositories checked out
+# on the wrong branch, etc.
+#
+# This is mainly useful because Travis creates an empty folder in the attempt
+# to restore the build cache.
+
 set -xe
 
-if [ ! -d checkouts/semaphore ]; then
-    mkdir -p checkouts
-    git clone https://github.com/getsentry/semaphore ./checkouts/semaphore
+if [ ! -d checkouts/semaphore/.git ]; then
+    mkdir -p checkouts/semaphore/
 fi
 
-(
-    cd ./checkouts/semaphore
-    git checkout -f master
-    git fetch
-    git reset --hard origin/master
-    cargo build
-)
+cd checkouts/semaphore/
+git init
+git remote remove origin || true
+git remote add origin https://github.com/getsentry/semaphore
+git fetch
+git reset --hard origin/master
+git clean -f
+cargo build
