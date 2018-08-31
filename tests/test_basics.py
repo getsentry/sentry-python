c94bb3057c50570fa3f34a7e65db6e6e3ba20243
Armin Ronacher <armin.ronacher@active-4.com>
2018-08-31 15:07:12 +0200

    feat: Addded before_breadcrumb

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1,4 +1,10 @@
-from sentry_sdk import configure_scope, capture_exception, last_event_id, Hub
+from sentry_sdk import (
+    configure_scope,
+    capture_exception,
+    add_breadcrumb,
+    last_event_id,
+    Hub,
+)
 
 
 def test_processors(sentry_init, capture_events):
@@ -38,3 +44,43 @@ def test_event_id(sentry_init, capture_events):
     assert event["event_id"] == event_id
     assert last_event_id() == event_id
     assert Hub.current.last_event_id() == event_id
+
+
+def test_option_callback(sentry_init, capture_events):
+    drop_events = False
+    drop_breadcrumbs = False
+
+    def before_send(event):
+        if not drop_events:
+            event["extra"] = {"foo": "bar"}
+            return event
+
+    def before_breadcrumb(crumb):
+        if not drop_breadcrumbs:
+            crumb["data"] = {"foo": "bar"}
+            return crumb
+
+    sentry_init(before_send=before_send, before_breadcrumb=before_breadcrumb)
+    events = capture_events()
+
+    def do_this():
+        add_breadcrumb(message="Hello")
+        try:
+            raise ValueError("aha!")
+        except Exception:
+            capture_exception()
+
+    do_this()
+    drop_breadcrumbs = True
+    do_this()
+    drop_events = True
+    do_this()
+
+    normal, no_crumbs = events
+
+    assert normal["exception"]["values"][0]["type"] == "ValueError"
+    crumb, = normal["breadcrumbs"]
+    assert "timestamp" in crumb
+    assert crumb["message"] == "Hello"
+    assert crumb["data"] == {"foo": "bar"}
+    assert crumb["type"] == "default"
