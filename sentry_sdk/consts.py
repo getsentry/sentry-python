33f4ee4eec2c2892dd0a5fb3ae5b23af5d2e7cb2
Markus Unterwaditzer <markus@unterwaditzer.net>
2018-09-05 11:06:58 +0200

    fix: Send correct packages and integrations

--- sentry_sdk/consts.py
+++ sentry_sdk/consts.py
@@ -1,4 +1,6 @@
+import sys
 import socket
+import platform
 
 
 VERSION = "0.1"
@@ -29,4 +31,26 @@ DEFAULT_OPTIONS = {
     "debug": False,
 }
 
-SDK_INFO = {"name": "sentry-python", "version": VERSION}
+
+# Modified by sentry_sdk.integrations
+INTEGRATIONS = []
+
+
+def _get_packages():
+    yield "python", ".".join(str(x) for x in sys.version_info[:3])
+    if hasattr(sys, "pypy_version_info"):
+        yield "pypy", ".".join(str(x) for x in sys.pypy_version_info[:3])
+
+
+SDK_INFO = {
+    "name": "sentry.python",
+    "version": VERSION,
+    "packages": [
+        {"package_name": package_name, "version": version}
+        for package_name, version in _get_packages()
+    ],
+    "integrations": INTEGRATIONS,
+}
+
+
+del _get_packages
