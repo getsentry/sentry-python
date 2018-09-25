767a212cd18d7f7c03c90549522930a519740596
Markus Unterwaditzer <markus@unterwaditzer.net>
2018-09-25 14:50:29 +0200

    fix(build): Merge coverage reports
    Don't overwrite coverage reports with each other

--- scripts/runtox.sh
+++ scripts/runtox.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
+
+# Usage: sh scripts/runtox.sh py3.7 <pytest-args>
+# Runs all environments with substring py3.7 and the given arguments for pytest
+set -xe
 if [ -z "$1" ]; then
     searchstring="$(echo py$TRAVIS_PYTHON_VERSION | sed -e 's/pypypy/pypy/g' -e 's/-dev//g')"
 else
     searchstring="$1"
 fi
 
-exec tox -e $(tox -l | grep $searchstring | tr '\n' ',')
+coverage erase
+exec tox -e $(tox -l | grep $searchstring | tr '\n' ',') -- "${@:2}"
