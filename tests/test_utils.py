71294f59c1ba30e1255611a6b6ad286cbe10d064
Markus Unterwaditzer <markus@unterwaditzer.net>
2018-09-29 22:51:42 +0200

    feat: Django sql queries (#94)
    * feat: Django sql queries
    
    * fix: Handle too large sql params better
    
    * fix: Fix test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,10 +2,17 @@
 import sys
 import os
 
+import pytest
+
 from hypothesis import given
 import hypothesis.strategies as st
 
-from sentry_sdk.utils import safe_repr, exceptions_from_error_tuple
+from sentry_sdk.utils import (
+    safe_repr,
+    exceptions_from_error_tuple,
+    format_and_strip,
+    strip_string,
+)
 from sentry_sdk._compat import text_type
 
 any_string = st.one_of(st.binary(), st.text())
@@ -56,3 +63,36 @@ def test_non_string_variables():
     assert exception["type"] == "ZeroDivisionError"
     frame, = exception["stacktrace"]["frames"]
     assert frame["vars"]["42"] == "True"
+
+
+def test_format_and_strip():
+    max_length = None
+
+    def x(template, params):
+        return format_and_strip(
+            template,
+            params,
+            strip_string=lambda x: strip_string(x, max_length=max_length),
+        )
+
+    max_length = 3
+
+    assert x("", []) == ""
+    assert x("f", []) == "f"
+    pytest.raises(ValueError, lambda: x("%s", []))
+
+    # Don't raise errors on leftover params, some django extensions send too
+    # many SQL parameters.
+    assert x("", [123]) == ""
+    assert x("foo%s", ["bar"]) == "foobar"
+
+    rv = x("foo%s", ["baer"])
+    assert rv.value == "foo..."
+    assert rv.metadata == {"len": 7, "rem": [["!limit", "x", 3, 6]]}
+
+    rv = x("foo%sbar%s", ["baer", "boor"])
+    assert rv.value == "foo...bar..."
+    assert rv.metadata == {
+        "len": 14,
+        "rem": [["!limit", "x", 3, 6], ["!limit", "x", 9, 12]],
+    }
