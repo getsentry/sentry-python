51e27c4309c2c8d96598a2064b9a2071509a6d89
Markus Unterwaditzer <markus@unterwaditzer.net>
2018-10-01 12:37:51 +0200

    fix: Dont rely on old_version in bump-version.sh

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -11,12 +11,10 @@ echo "Current version: $OLD_VERSION"
 echo "Bumping version: $NEW_VERSION"
 
 function replace() {
-    grep "$1" $3
     ! grep "$2" $3
-
     perl -i -pe "s/$1/$2/g" $3
     grep "$2" $3  # verify that replacement was successful
 }
 
-replace "version=\"$OLD_VERSION\"" "version=\"$NEW_VERSION\"" ./setup.py
-replace "VERSION = \"$OLD_VERSION\"" "VERSION = \"$NEW_VERSION\"" ./sentry_sdk/consts.py
+replace "version=\"[0-9.]+\"" "version=\"$NEW_VERSION\"" ./setup.py
+replace "VERSION = \"[0-9.]+\"" "VERSION = \"$NEW_VERSION\"" ./sentry_sdk/consts.py
