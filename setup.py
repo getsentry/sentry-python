c3beed89b86e181c880a99ac5a971441f8c5290e
Peter Demin <peterdemin@gmail.com>
2018-10-25 10:56:54 -0400

    Remove experimental disclaimer from PyPI

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 Sentry-Python - Sentry SDK for Python
 =====================================
 
-**Sentry-Python is an experimental SDK for Sentry.** Check out `GitHub
+**Sentry-Python is SDK for Sentry.** Check out `GitHub
 <https://github.com/getsentry/sentry-python>`_ to find out more.
 """
 
