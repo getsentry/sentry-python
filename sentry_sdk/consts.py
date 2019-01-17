6f37b22d6e46a2186f9308cc4c7622f554b11047
Markus Unterwaditzer <markus@unterwaditzer.net>
2019-01-17 11:08:15 +0100

    release: 0.6.7

--- sentry_sdk/consts.py
+++ sentry_sdk/consts.py
@@ -1,7 +1,7 @@
 import socket
 
 
-VERSION = "0.6.6"
+VERSION = "0.6.7"
 DEFAULT_SERVER_NAME = socket.gethostname() if hasattr(socket, "gethostname") else None
 DEFAULT_OPTIONS = {
     "dsn": None,
