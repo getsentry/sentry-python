3ac9097165180b4a53e5d483f73a3abfc2d239e6
Markus Unterwaditzer <markus@unterwaditzer.net>
2019-01-25 18:47:24 +0100

    fix: Fix pyramid tests

--- tests/integrations/pyramid/test_pyramid.py
+++ tests/integrations/pyramid/test_pyramid.py
@@ -240,7 +240,7 @@ def test_error_in_errorhandler(
     exception, = event1["exception"]["values"]
     assert exception["type"] == "ValueError"
 
-    exception, = event2["exception"]["values"]
+    exception = event2["exception"]["values"][0]
     assert exception["type"] == "ZeroDivisionError"
 
 
