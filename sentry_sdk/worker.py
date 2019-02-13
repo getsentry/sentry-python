34476e61c83b518c293447871f584b6e9d77a084
Markus Unterwaditzer <markus@unterwaditzer.net>
2019-02-13 15:53:43 +0100

    Revert "fix: Remove unused import"
    This reverts commit 783592c50e6701586c89e607e06fd9b13ea9a5f3.

--- sentry_sdk/worker.py
+++ sentry_sdk/worker.py
@@ -1,7 +1,7 @@
 import threading
 import os
 
-from time import sleep
+from time import sleep, time
 from sentry_sdk._compat import queue, check_thread_support
 from sentry_sdk.utils import logger
 
