97bd1cbf778b79f99a68a220651bb54f39127ccf
Markus Unterwaditzer <markus@unterwaditzer.net>
2019-02-21 08:35:11 +0100

    fix: Restore submodule link in apidocs

--- sentry_sdk/__init__.py
+++ sentry_sdk/__init__.py
@@ -32,7 +32,15 @@ from sentry_sdk.api import __all__ as api_all
 
 from sentry_sdk.consts import VERSION  # noqa
 
-__all__ = api_all + ["Hub", "Scope", "Client", "Transport", "HttpTransport", "init"]
+__all__ = api_all + [
+    "Hub",
+    "Scope",
+    "Client",
+    "Transport",
+    "HttpTransport",
+    "init",
+    "integrations",
+]
 
 # Initialize the debug support after everything is loaded
 from sentry_sdk.debug import init_debug_support
