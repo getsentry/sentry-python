2f451b8d365290bab5ce62e02745fdad6cd3e776
Markus Unterwaditzer <markus@unterwaditzer.net>
2019-03-30 15:10:55 +0100

    release: 0.7.9

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sentry-sdk",
-    version="0.7.8",
+    version="0.7.9",
     author="Sentry Team and Contributors",
     author_email="hello@getsentry.com",
     url="https://github.com/getsentry/sentry-python",
