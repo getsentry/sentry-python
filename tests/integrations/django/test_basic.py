46f8527e4f7bc013c5210fd385a7a18c036a7f7b
Markus Unterwaditzer <markus@unterwaditzer.net>
2019-04-06 01:11:47 +0200

    fix(django/sql): Run cursor.mogrify as fallback to regular formatting (#325)
    * fix(django/sql): Run cursor.mogrify as fallback to regular formatting
    
    * fix: Fix imports

--- tests/integrations/django/test_basic.py
+++ tests/integrations/django/test_basic.py
@@ -226,7 +226,7 @@ def test_sql_psycopg2_string_composition(sentry_init, capture_events, query):
     if "postgres" not in connections:
         pytest.skip("postgres tests disabled")
 
-    import psycopg2
+    import psycopg2.sql
 
     sql = connections["postgres"].cursor()
 
@@ -249,7 +249,7 @@ def test_sql_psycopg2_placeholders(sentry_init, capture_events):
     if "postgres" not in connections:
         pytest.skip("postgres tests disabled")
 
-    import psycopg2
+    import psycopg2.sql
 
     sql = connections["postgres"].cursor()
 
