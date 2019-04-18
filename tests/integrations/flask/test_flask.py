20462e4373bd6a001ddab351f06ae44d57ba1795
Markus Unterwaditzer <markus@unterwaditzer.net>
2019-04-18 13:08:40 +0200

    fix: Fix flask tests

--- tests/integrations/flask/test_flask.py
+++ tests/integrations/flask/test_flask.py
@@ -470,7 +470,7 @@ def test_error_in_errorhandler(sentry_init, capture_events, app):
     exception, = event1["exception"]["values"]
     assert exception["type"] == "ValueError"
 
-    exception = event2["exception"]["values"][0]
+    exception = event2["exception"]["values"][-1]
     assert exception["type"] == "ZeroDivisionError"
 
 
