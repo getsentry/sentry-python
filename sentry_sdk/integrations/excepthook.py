b8483d2ab27639ae37d9e8d991d451a91b1030ea
tolgahanuzun <mail@tolgahanuzun.com>
2019-05-10 09:38:35 +0300

    Added the always_run option in excepthook integration

--- sentry_sdk/integrations/excepthook.py
+++ sentry_sdk/integrations/excepthook.py
@@ -11,6 +11,18 @@ if False:
 class ExcepthookIntegration(Integration):
     identifier = "excepthook"
 
+    always_run = False
+
+    def __init__(self, always_run=False):
+        # type: (bool) -> None
+
+        if not isinstance(always_run, bool):
+            raise ValueError(
+                "Invalid value for always_run: %s (must be type boolean)"
+                % (always_run)
+            )
+        self.always_run = always_run
+
     @staticmethod
     def setup_once():
         # type: () -> None
@@ -23,7 +35,7 @@ def _make_excepthook(old_excepthook):
         hub = Hub.current
         integration = hub.get_integration(ExcepthookIntegration)
 
-        if integration is not None and _should_send():
+        if integration is not None and _should_send(integration.always_run):
             with capture_internal_exceptions():
                 event, hint = event_from_exception(
                     (exctype, value, traceback),
@@ -37,7 +49,10 @@ def _make_excepthook(old_excepthook):
     return sentry_sdk_excepthook
 
 
-def _should_send():
+def _should_send(always_run=False):
+    if always_run:
+        return True
+
     if hasattr(sys, "ps1"):
         # Disable the excepthook for interactive Python shells, otherwise
         # every typo gets sent to Sentry.
