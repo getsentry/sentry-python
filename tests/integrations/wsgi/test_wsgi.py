ddab2d4e628adb5de73b5dd8d62873a406cd362f
Saif Hakim <saif@benchling.com>
2019-06-01 08:02:42 -0700

    fix(wsgi): Log SystemExit (with non-zero exit code) and KeyboardInterrupt (#380)
    * Log SystemExit (with non-zero exit code) and KeyboardInterrupt in WSGI middleware
    
    Neither SystemExit nor KeyboardInterrupt subclass Exception, so we must
    explicitly handle these two exception classes in addition to Exception in the
    WSGI middleware.
    
    This is notably a direct port from raven-python:
     - https://github.com/getsentry/raven-python/blob/master/raven/middleware.py
     - https://github.com/getsentry/raven-python/blob/master/tests/middleware/tests.py
    
    Fixes: GH-379

--- tests/integrations/wsgi/test_wsgi.py
+++ tests/integrations/wsgi/test_wsgi.py
@@ -12,6 +12,28 @@ def crashing_app():
     return app
 
 
+class IterableApp(object):
+    def __init__(self, iterable):
+        self.iterable = iterable
+
+    def __call__(self, environ, start_response):
+        return self.iterable
+
+
+class ExitingIterable(object):
+    def __init__(self, exc_func):
+        self._exc_func = exc_func
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        raise self._exc_func()
+
+    def next(self):
+        return type(self).__next__(self)
+
+
 def test_basic(sentry_init, crashing_app, capture_events):
     sentry_init(send_default_pii=True)
     app = SentryWsgiMiddleware(crashing_app)
@@ -30,3 +52,58 @@ def test_basic(sentry_init, crashing_app, capture_events):
         "query_string": "",
         "url": "http://localhost/",
     }
+
+
+@pytest.fixture(params=[0, None])
+def test_systemexit_zero_is_ignored(sentry_init, capture_events, request):
+    zero_code = request.param
+    sentry_init(send_default_pii=True)
+    iterable = ExitingIterable(lambda: SystemExit(zero_code))
+    app = SentryWsgiMiddleware(IterableApp(iterable))
+    client = Client(app)
+    events = capture_events()
+
+    with pytest.raises(SystemExit):
+        client.get("/")
+
+    assert len(events) == 0
+
+
+@pytest.fixture(params=["", "foo", 1, 2])
+def test_systemexit_nonzero_is_captured(sentry_init, capture_events, request):
+    nonzero_code = request.param
+    sentry_init(send_default_pii=True)
+    iterable = ExitingIterable(lambda: SystemExit(nonzero_code))
+    app = SentryWsgiMiddleware(IterableApp(iterable))
+    client = Client(app)
+    events = capture_events()
+
+    with pytest.raises(SystemExit):
+        client.get("/")
+
+    event, = events
+
+    assert "exception" in event
+    exc = event["exception"]["values"][-1]
+    assert exc["type"] == "SystemExit"
+    assert exc["value"] == nonzero_code
+    assert event["level"] == "error"
+
+
+def test_keyboard_interrupt_is_captured(sentry_init, capture_events):
+    sentry_init(send_default_pii=True)
+    iterable = ExitingIterable(lambda: KeyboardInterrupt())
+    app = SentryWsgiMiddleware(IterableApp(iterable))
+    client = Client(app)
+    events = capture_events()
+
+    with pytest.raises(KeyboardInterrupt):
+        client.get("/")
+
+    event, = events
+
+    assert "exception" in event
+    exc = event["exception"]["values"][-1]
+    assert exc["type"] == "KeyboardInterrupt"
+    assert exc["value"] == ""
+    assert event["level"] == "error"
