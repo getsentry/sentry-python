3cd6b5261d48f988139b0ca55487b306e86a475e
Markus Unterwaditzer <markus@unterwaditzer.net>
2019-06-19 17:30:36 +0200

    Revert "fix: Fix broken sampling logic"
    This reverts commit ccec6cafe0e1908b6269d402e07d61625a2b2234.

--- sentry_sdk/hub.py
+++ sentry_sdk/hub.py
@@ -436,7 +436,7 @@ class Hub(with_metaclass(HubMeta)):  # type: ignore
         if span.sampled is None:
             # span.sampled = True -> Span forcibly sampled
             sample_rate = self.client.options["traces_sample_rate"]
-            if sample_rate < 1.0 and random.random() <= sample_rate:
+            if sample_rate < 1.0 and random.random() >= sample_rate:
                 return None
 
         return self.capture_event(
