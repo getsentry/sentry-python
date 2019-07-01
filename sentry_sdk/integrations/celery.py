d782942bb412df8cf45d5f015efb72b929263c77
Anthony Ricaud <anthony@ricaud.me>
2019-07-01 14:16:19 +0100

    Add typings for CeleryIntegration.__init__

--- sentry_sdk/integrations/celery.py
+++ sentry_sdk/integrations/celery.py
@@ -24,6 +24,7 @@ class CeleryIntegration(Integration):
     identifier = "celery"
 
     def __init__(self, propagate_traces=True):
+        # type: (bool) -> None
         self.propagate_traces = propagate_traces
 
     @staticmethod
