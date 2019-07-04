60ec0ccfeb1c1235b6f5304f65e5171fd0b59a46
Markus Unterwaditzer <markus@unterwaditzer.net>
2019-07-04 14:23:12 +0200

    fix: Use sys.executable

--- tests/integrations/stdlib/test_subprocess.py
+++ tests/integrations/stdlib/test_subprocess.py
@@ -1,4 +1,5 @@
 import subprocess
+import sys
 
 from sentry_sdk import Hub, capture_message
 from sentry_sdk.integrations.stdlib import StdlibIntegration
@@ -9,13 +10,14 @@ def test_subprocess_basic(sentry_init, capture_events):
 
     with Hub.current.trace(transaction="foo", op="foo") as span:
         output = subprocess.check_output(
-            "python -c '"
-            "import sentry_sdk; "
-            "from sentry_sdk.integrations.stdlib import get_subprocess_traceparent_headers; "
-            "sentry_sdk.init(); "
-            "print(dict(get_subprocess_traceparent_headers()))"
-            "'",
-            shell=True,
+            [
+                sys.executable,
+                "-c",
+                "import sentry_sdk; "
+                "from sentry_sdk.integrations.stdlib import get_subprocess_traceparent_headers; "
+                "sentry_sdk.init(); "
+                "print(dict(get_subprocess_traceparent_headers()))",
+            ]
         )
 
     assert span.trace_id in str(output)
