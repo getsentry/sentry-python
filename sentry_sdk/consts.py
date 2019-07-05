518a9660f7ab12029ed323aa2004e64cb9ce30eb
Markus Unterwaditzer <markus@unterwaditzer.net>
2019-07-05 15:21:37 +0200

    fix: Fix linters

--- sentry_sdk/consts.py
+++ sentry_sdk/consts.py
@@ -49,7 +49,6 @@ class ClientConstructor(object):
         attach_stacktrace=False,  # type: bool
         ca_certs=None,  # type: Optional[str]
         propagate_traces=True,  # type: bool
-
         # DO NOT ENABLE THIS RIGHT NOW UNLESS YOU WANT TO EXCEED YOUR EVENT QUOTA IMMEDIATELY
         traces_sample_rate=0.0,  # type: float
         traceparent_v2=False,  # type: bool
