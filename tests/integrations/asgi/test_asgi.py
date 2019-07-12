7454711a20c6ae113263cffc1c7fd8bac95038dd
Markus Unterwaditzer <markus@unterwaditzer.net>
2019-07-12 14:41:37 +0200

    feat: ASGI middleware
    Diff to sentry-asgi:
    
    ```
    diff --git a/../sentry-asgi/sentry_asgi/middleware.py b/sentry_sdk/integrations/asgi.py
    index 37d1117..4c40750 100644
    --- a/../sentry-asgi/sentry_asgi/middleware.py
    +++ b/sentry_sdk/integrations/asgi.py
    @@ -1,35 +1,44 @@
     import functools
     import urllib
    
    -import sentry_sdk
    -from sentry_sdk.utils import event_from_exception, exc_info_from_error
    +from sentry_sdk._types import MYPY
    +from sentry_sdk.hub import Hub, _should_send_default_pii
    +from sentry_sdk.integrations._wsgi_common import _filter_headers
    +from sentry_sdk.utils import transaction_from_function
    
    +if MYPY:
    +    from typing import Dict
    +
    +
    +class SentryAsgiMiddleware:
    +    __slots__ = ("app",)
    
    -class SentryMiddleware:
         def __init__(self, app):
             self.app = app
    
         async def __call__(self, scope, receive, send):
    -        hub = sentry_sdk.Hub.current
    -        with sentry_sdk.Hub(hub) as hub:
    +        hub = Hub.current
    +        with Hub(hub) as hub:
                 with hub.configure_scope() as sentry_scope:
    +                sentry_scope._name = "asgi"
                     processor = functools.partial(self.event_processor, asgi_scope=scope)
                     sentry_scope.add_event_processor(processor)
    -                try:
    -                    await self.app(scope, receive, send)
    -                except Exception as exc:
    -                    hub.capture_exception(exc)
    -                    raise exc from None
    +
    +            try:
    +                await self.app(scope, receive, send)
    +            except Exception as exc:
    +                hub.capture_exception(exc)
    +                raise exc from None
    
         def event_processor(self, event, hint, asgi_scope):
             if asgi_scope["type"] in ("http", "websocket"):
                 event["request"] = {
                     "url": self.get_url(asgi_scope),
                     "method": asgi_scope["method"],
    -                "headers": self.get_headers(asgi_scope),
    +                "headers": _filter_headers(self.get_headers(asgi_scope)),
                     "query_string": self.get_query(asgi_scope),
                 }
    -        if asgi_scope.get("client"):
    +        if asgi_scope.get("client") and _should_send_default_pii():
                 event["request"]["env"] = {"REMOTE_ADDR": asgi_scope["client"][0]}
             if asgi_scope.get("endpoint"):
                 event["transaction"] = self.get_transaction(asgi_scope)
    @@ -66,7 +75,7 @@ class SentryMiddleware:
             """
             Extract headers from the ASGI scope, in the format that the Sentry protocol expects.
             """
    -        headers = {}
    +        headers = {}  # type: Dict[str, str]
             for raw_key, raw_value in scope["headers"]:
                 key = raw_key.decode("latin-1")
                 value = raw_value.decode("latin-1")
    @@ -80,12 +89,4 @@ class SentryMiddleware:
             """
             Return a transaction string to identify the routed endpoint.
             """
    -        endpoint = scope["endpoint"]
    -        qualname = (
    -            getattr(endpoint, "__qualname__", None)
    -            or getattr(endpoint, "__name__", None)
    -            or None
    -        )
    -        if not qualname:
    -            return None
    -        return "%s.%s" % (endpoint.__module__, qualname)
    +        return transaction_from_function(scope["endpoint"])
    ```

---
+++ tests/integrations/asgi/test_asgi.py
@@ -0,0 +1,122 @@
+import asyncio
+import random
+import sys
+
+import pytest
+from sentry_sdk import capture_message
+from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
+from starlette.applications import Starlette
+from starlette.responses import PlainTextResponse
+from starlette.testclient import TestClient
+
+
+@pytest.fixture
+def app():
+    app = Starlette()
+
+    @app.route("/sync-message")
+    def hi(request):
+        capture_message("hi", level="error")
+        return PlainTextResponse("ok")
+
+    @app.route("/async-message")
+    async def hi(request):
+        capture_message("hi", level="error")
+        return PlainTextResponse("ok")
+
+    app.add_middleware(SentryAsgiMiddleware)
+
+    return app
+
+
+@pytest.mark.skipif(sys.version_info < (3, 7), reason="requires python3.7 or higher")
+def test_sync_request_data(sentry_init, app, capture_events):
+    sentry_init(send_default_pii=True)
+    events = capture_events()
+
+    client = TestClient(app)
+    response = client.get("/sync-message?foo=bar")
+
+    assert response.status_code == 200
+
+    event, = events
+    assert event["transaction"] == "tests.integrations.asgi.test_asgi.app.<locals>.hi"
+    assert event["request"]["env"] == {"REMOTE_ADDR": "testclient"}
+    assert set(event["request"]["headers"]) == {
+        "accept",
+        "accept-encoding",
+        "connection",
+        "host",
+        "user-agent",
+    }
+    assert event["request"]["query_string"] == "foo=bar"
+    assert event["request"]["url"].endswith("/sync-message")
+    assert event["request"]["method"] == "GET"
+
+    # Assert that state is not leaked
+    events.clear()
+    capture_message("foo")
+    event, = events
+
+    assert "request" not in event
+    assert "transaction" not in event
+
+
+def test_async_request_data(sentry_init, app, capture_events):
+    sentry_init(send_default_pii=True)
+    events = capture_events()
+
+    client = TestClient(app)
+    response = client.get("/async-message?foo=bar")
+
+    assert response.status_code == 200
+
+    event, = events
+    assert event["transaction"] == "tests.integrations.asgi.test_asgi.app.<locals>.hi"
+    assert event["request"]["env"] == {"REMOTE_ADDR": "testclient"}
+    assert set(event["request"]["headers"]) == {
+        "accept",
+        "accept-encoding",
+        "connection",
+        "host",
+        "user-agent",
+    }
+    assert event["request"]["query_string"] == "foo=bar"
+    assert event["request"]["url"].endswith("/async-message")
+    assert event["request"]["method"] == "GET"
+
+    # Assert that state is not leaked
+    events.clear()
+    capture_message("foo")
+    event, = events
+
+    assert "request" not in event
+    assert "transaction" not in event
+
+
+def test_errors(sentry_init, app, capture_events):
+    sentry_init(send_default_pii=True)
+    events = capture_events()
+
+    @app.route("/error")
+    def myerror(request):
+        raise ValueError("oh no")
+
+    client = TestClient(app, raise_server_exceptions=False)
+    response = client.get("/error")
+
+    assert response.status_code == 500
+
+    event, = events
+    assert (
+        event["transaction"]
+        == "tests.integrations.asgi.test_asgi.test_errors.<locals>.myerror"
+    )
+    exception, = event["exception"]["values"]
+
+    assert exception["type"] == "ValueError"
+    assert exception["value"] == "oh no"
+    assert any(
+        frame["filename"].endswith("tests/integrations/asgi/test_asgi.py")
+        for frame in exception["stacktrace"]["frames"]
+    )
