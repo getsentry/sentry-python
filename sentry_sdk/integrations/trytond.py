79a78ee433dce05c931a44624a32baf9a57d915b
Dani <daniel@calidae.com>
2019-07-30 13:26:39 +0200

    Simplify trytond integration

--- sentry_sdk/integrations/trytond.py
+++ sentry_sdk/integrations/trytond.py
@@ -1,13 +1,8 @@
 
-
-from sentry_sdk.integrations import Integration
-from sentry_sdk import capture_exception
-
 import sentry_sdk
 import sentry_sdk.integrations
 
-from werkzeug.wrappers import Response
-from trytond.application import app as application
+from trytond.wsgi import app
 
 
 def append_err_handler(self, handler):
@@ -15,20 +10,7 @@ def append_err_handler(self, handler):
     return handler
 
 
-def prepend_err_handler(self, handler):
-    self.error_handlers.insert(0, handler)
-    return handler
-
-
-# One would expect the TrytondWSGI class to implement such decorators
-# but it does not
-application.append_err_handler = append_err_handler.__get__(application)
-application.prepend_err_handler = prepend_err_handler.__get__(application)
-
-
-def error_handler(e):
-    event_id = sentry_sdk.capture_exception(e)
-    return e
+app.append_err_handler = append_err_handler.__get__(app)
 
 
 class TrytondIntegration(sentry_sdk.integrations.Integration):
@@ -39,10 +21,8 @@ class TrytondIntegration(sentry_sdk.integrations.Integration):
 
     @staticmethod
     def setup_once():
-        application.append_err_handler(error_handler)
 
-
-sentry_sdk.init(
-    "dsn",
-    integrations=[TrytondIntegration()]
-)
+        @app.append_err_handler
+        def error_handler(e):
+            event_id = sentry_sdk.capture_exception(e)
+            return e
