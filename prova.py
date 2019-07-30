036a1f3699380d073226fc3e1b6b4cfe60683ca2
Dani <daniel@calidae.com>
2019-07-30 13:41:43 +0200

    Fuzzy test script

---
+++ prova.py
@@ -0,0 +1,78 @@
+
+import os
+
+import sentry_sdk
+import sentry_sdk.integrations.trytond
+
+sentry_sdk.init(
+    os.environ['SENTRY_DSN'],
+    integrations=[
+        sentry_sdk.integrations.trytond.TrytondWSGIIntegration(),
+    ]
+)
+
+
+from trytond.application import app
+
+from trytond.pool import PoolMeta, Pool
+from trytond.model import Model
+from trytond.protocols.wrappers import user_application
+from trytond.protocols.wrappers import with_pool, with_transaction
+
+
+class MyModel(Model):
+    __name__ = 'mymodel'
+
+    @classmethod
+    def cronfail(cls):
+        raise Exception('cron')
+
+
+class Cron(metaclass=PoolMeta):
+    __name__ = 'ir.cron'
+
+    @classmethod
+    def __setup__(cls):
+        super(Cron, cls).__setup__()
+        cls.method.selection += [
+            ('mymodel|cronfail', 'Fail'),
+        ]
+
+
+class UserApplication(metaclass=PoolMeta):
+    __name__ = 'res.user.application'
+
+    @classmethod
+    def __setup__(cls):
+        super(UserApplication, cls).__setup__()
+        cls.application.selection.append(('webapp', 'WebApp'))
+
+
+Pool.register(
+    MyModel,
+    Cron,
+    UserApplication,
+    module='res',
+    type_='model'
+)
+
+
+@app.route('/myroute/fail', methods=['GET'])
+def _route_fail(request):
+    raise Exception('myroute')
+
+
+@app.route('/<string:database_name>/fail', methods=['GET'])
+@with_pool
+@with_transaction()
+def _pool_fail(request, pool):
+    import pudb; pudb.set_trace()
+    raise Exception('pool')
+
+
+@app.route('/<string:database_name>/webapp/fail', methods=['GET'])
+@with_pool
+@with_transaction()
+@user_application('webapp')
+def _userapp_fail(request, pool):
+    raise Exception('webapp')
