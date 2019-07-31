2bdfe53fc77ada9189b1ebd36d1b18882e783882
Dani <daniel@calidae.com>
2019-07-31 15:52:01 +0200

    Add a hacky logging handler for trytond-cron integration

--- sentry_sdk/integrations/trytond.py
+++ sentry_sdk/integrations/trytond.py
@@ -1,6 +1,8 @@
 
+import logging
 import sentry_sdk
 import sentry_sdk.integrations
+import sentry_sdk.integrations.logging
 
 from werkzeug.wrappers import Response
 from trytond.wsgi import app
@@ -41,24 +43,45 @@ def rpc_error_page(self, e):
     if isinstance(e, TrytondBaseException):
         return
     else:
-    _environ = dict()
-    request_stub = type('JSONRequestStub', (JSONRequest,), dict(
-        accept_mimetypes=[('json', None)],
-        parsed_data=dict(
-            id=None,  # FIXME: The client won't trust this response because
-                      # the parameter id won't match the one it crafted
-                      # for the request. We must gain access to the
-                      # original request for error handlers
-            method=None,
-            params=None
-        ),
-    ))(_environ)
-    event_id = sentry_sdk.last_event_id()
-    data = TrytondUserError(
-        str(event_id),
-        str(e)
-    )
-    return self.make_response(
-        request_stub,
-        data
-    )
+        _environ = dict()
+        request_stub = type('JSONRequestStub', (JSONRequest,), dict(
+            accept_mimetypes=[('json', None)],
+            parsed_data=dict(
+                id=None,  # FIXME: The client won't trust this response because
+                        # the parameter id won't match the one it crafted
+                        # for the request. We must gain access to the
+                        # original request for error handlers
+                method=None,
+                params=None
+            ),
+        ))(_environ)
+        event_id = sentry_sdk.last_event_id()
+        data = TrytondUserError(
+            str(event_id),
+            str(e)
+        )
+        return self.make_response(
+            request_stub,
+            data
+        )
+
+
+class TrytondSentryHandler(logging.NullHandler):
+
+    def __init__(self, dsn, ignore=tuple()):
+        super(TrytondSentryHandler, self).__init__()
+        # There is no clean way to run a static configuration code block
+        # when running the trytond-cron binary without crafting a whole
+        # new binary that wraps it. This is a workaround in the line of
+        # the old raven package
+        sentry_sdk.init(dsn)
+        # Also, there is no need to inherit
+        # sentry_sdk.integrations.logging.EventHandler because
+        # sentry_sdk.init will already install the default 
+        # sentry_sdk.integrations.logging.LoggingIntegration
+        # that will already patch any logger.error call
+        # except fo rthe following
+        for logger in ignore:
+            sentry_sdk.integrations.logging.ignore_logger(logger)
+        # That is: there is no actual TrytondCronIntegration to be written
+        # but we need this so as to inject a sentry_sdk.init call
