03f12c24e82b3018bf5f0b29494ec12d4ad6aa3f
Markus Unterwaditzer <markus@unterwaditzer.net>
2019-09-13 14:35:54 +0200

    test: Add tests for django middlewares

--- sentry_sdk/integrations/django/middleware.py
+++ sentry_sdk/integrations/django/middleware.py
@@ -1,3 +1,7 @@
+"""
+Create spans from Django middleware invocations
+"""
+
 from functools import wraps
 
 from django import VERSION as DJANGO_VERSION  # type: ignore
@@ -45,10 +49,37 @@ def patch_django_middlewares():
 def _wrap_middleware(middleware, middleware_name):
     from sentry_sdk.integrations.django import DjangoIntegration
 
+    def _get_wrapped_method(old_method):
+        @wraps(old_method)
+        def sentry_wrapped_method(*args, **kwargs):
+            hub = Hub.current
+            integration = hub.get_integration(DjangoIntegration)
+            if integration is None or not integration.middleware_spans:
+                return old_method(*args, **kwargs)
+
+            function_name = transaction_from_function(old_method)
+
+            description = middleware_name
+            function_basename = getattr(old_method, "__name__", None)
+            if function_basename:
+                description = "{}.{}".format(description, function_basename)
+
+            with hub.start_span(
+                op="django.middleware", description=description
+            ) as span:
+                span.set_tag("django.function_name", function_name)
+                span.set_tag("django.middleware_name", middleware_name)
+                return old_method(*args, **kwargs)
+
+        return sentry_wrapped_method
+
     class SentryWrappingMiddleware(object):
         def __init__(self, *args, **kwargs):
             self._inner = middleware(*args, **kwargs)
+            self._call_method = None
 
+        # We need correct behavior for `hasattr()`, which we can only determine
+        # when we have an instance of the middleware we're wrapping.
         def __getattr__(self, method_name):
             if method_name not in (
                 "process_request",
@@ -60,28 +91,14 @@ def _wrap_middleware(middleware, middleware_name):
                 raise AttributeError()
 
             old_method = getattr(self._inner, method_name)
-
-            @wraps(old_method)
-            def sentry_wrapped_method(*args, **kwargs):
-                hub = Hub.current
-                integration = hub.get_integration(DjangoIntegration)
-                if integration is None or not integration.middleware_spans:
-                    return old_method(*args, **kwargs)
-
-                function_name = transaction_from_function(old_method)
-
-                with hub.start_span(
-                    op="django.middleware", description=function_name
-                ) as span:
-                    span.set_tag("django.function_name", function_name)
-                    span.set_tag("django.middleware_name", middleware_name)
-                    return old_method(*args, **kwargs)
-
-            self.__dict__[method_name] = sentry_wrapped_method
-            return sentry_wrapped_method
+            rv = _get_wrapped_method(old_method)
+            self.__dict__[method_name] = rv
+            return rv
 
         def __call__(self, *args, **kwargs):
-            return self._inner(*args, **kwargs)
+            if self._call_method is None:
+                self._call_method = _get_wrapped_method(self._inner.__call__)
+            return self._call_method(*args, **kwargs)
 
     if hasattr(middleware, "__name__"):
         SentryWrappingMiddleware.__name__ = middleware.__name__
