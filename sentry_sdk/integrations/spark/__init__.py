8914f2c15718a4053ff8a86ff114c7eb509c1d12
Abhijeet Prasad <aprasad@sentry.io>
2019-10-01 13:44:55 -0700

    Fix imports

--- sentry_sdk/integrations/spark/__init__.py
+++ sentry_sdk/integrations/spark/__init__.py
@@ -1,4 +1,4 @@
-from spark_driver import SparkIntegration
-from spark_worker import sentry_worker_main
+from sentry_sdk.integrations.spark.spark_driver import SparkIntegration
+from sentry_sdk.integrations.spark.spark_worker import sentry_worker_main
 
 __all__ = ["SparkIntegration", "sentry_worker_main"]
