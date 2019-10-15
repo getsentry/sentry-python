d50c6fb5b2f17aee16737efea14704f7f15b50d0
Abhijeet Prasad <aprasad@sentry.io>
2019-10-15 11:29:26 -0700

    Add capture_internal_exceptions to event processor

--- sentry_sdk/integrations/spark/spark_driver.py
+++ sentry_sdk/integrations/spark/spark_driver.py
@@ -1,6 +1,7 @@
 from sentry_sdk import configure_scope
 from sentry_sdk.hub import Hub
 from sentry_sdk.integrations import Integration
+from sentry_sdk.utils import capture_internal_exceptions
 
 
 class SparkIntegration(Integration):
@@ -54,36 +55,38 @@ def patch_spark_context_init():
         _start_sentry_listener(self)
         _set_app_properties()
 
-        with configure_scope() as scope:
+        with capture_internal_exceptions():
+            with configure_scope() as scope:
 
-            @scope.add_event_processor
-            def process_event(event, hint):
-                if Hub.current.get_integration(SparkIntegration) is None:
+                @scope.add_event_processor
+                def process_event(event, hint):
+                    if Hub.current.get_integration(SparkIntegration) is None:
+                        return event
+
+                    event.setdefault("user", {}).setdefault("id", self.sparkUser())
+
+                    event.setdefault("tags", {}).setdefault(
+                        "executor.id", self._conf.get("spark.executor.id")
+                    )
+                    event["tags"].setdefault(
+                        "spark.submit.deployMode",
+                        self._conf.get("spark.submit.deployMode"),
+                    )
+                    event["tags"].setdefault(
+                        "driver.host", self._conf.get("spark.driver.host")
+                    )
+                    event["tags"].setdefault(
+                        "driver.port", self._conf.get("spark.driver.port")
+                    )
+                    event["tags"].setdefault("spark_version", self.version)
+                    event["tags"].setdefault("app_name", self.appName)
+                    event["tags"].setdefault("application_id", self.applicationId)
+                    event["tags"].setdefault("master", self.master)
+                    event["tags"].setdefault("spark.home", self.sparkHome)
+
+                    event.setdefault("extra", {}).setdefault("web_url", self.uiWebUrl)
+
                     return event
-
-                event.setdefault("user", {}).setdefault("id", self.sparkUser())
-
-                event.setdefault("tags", {}).setdefault(
-                    "executor.id", self._conf.get("spark.executor.id")
-                )
-                event["tags"].setdefault(
-                    "spark.submit.deployMode", self._conf.get("spark.submit.deployMode")
-                )
-                event["tags"].setdefault(
-                    "driver.host", self._conf.get("spark.driver.host")
-                )
-                event["tags"].setdefault(
-                    "driver.port", self._conf.get("spark.driver.port")
-                )
-                event["tags"].setdefault("spark_version", self.version)
-                event["tags"].setdefault("app_name", self.appName)
-                event["tags"].setdefault("application_id", self.applicationId)
-                event["tags"].setdefault("master", self.master)
-                event["tags"].setdefault("spark.home", self.sparkHome)
-
-                event.setdefault("extra", {}).setdefault("web_url", self.uiWebUrl)
-
-                return event
 
         return init
 
--- sentry_sdk/integrations/spark/spark_worker.py
+++ sentry_sdk/integrations/spark/spark_worker.py
@@ -58,37 +58,40 @@ def _capture_exception(exc_info, hub):
 def _tag_task_context():
     from pyspark.taskcontext import TaskContext
 
-    with configure_scope() as scope:
+    with capture_internal_exceptions():
+        with configure_scope() as scope:
 
-        @scope.add_event_processor
-        def process_event(event, hint):
-            integration = Hub.current.get_integration(SparkWorkerIntegration)
-            taskContext = TaskContext.get()
+            @scope.add_event_processor
+            def process_event(event, hint):
+                integration = Hub.current.get_integration(SparkWorkerIntegration)
+                taskContext = TaskContext.get()
 
-            if integration is None or taskContext is None:
+                if integration is None or taskContext is None:
+                    return event
+
+                event.setdefault("tags", {}).setdefault(
+                    "stageId", taskContext.stageId()
+                )
+                event["tags"].setdefault("partitionId", taskContext.partitionId())
+                event["tags"].setdefault("attemptNumber", taskContext.attemptNumber())
+                event["tags"].setdefault("taskAttemptId", taskContext.taskAttemptId())
+
+                if taskContext._localProperties:
+                    if "sentry_app_name" in taskContext._localProperties:
+                        event["tags"].setdefault(
+                            "app_name", taskContext._localProperties["sentry_app_name"]
+                        )
+                        event["tags"].setdefault(
+                            "application_id",
+                            taskContext._localProperties["sentry_application_id"],
+                        )
+
+                    if "callSite.short" in taskContext._localProperties:
+                        event.setdefault("extra", {}).setdefault(
+                            "callSite", taskContext._localProperties["callSite.short"]
+                        )
+
                 return event
-
-            event.setdefault("tags", {}).setdefault("stageId", taskContext.stageId())
-            event["tags"].setdefault("partitionId", taskContext.partitionId())
-            event["tags"].setdefault("attemptNumber", taskContext.attemptNumber())
-            event["tags"].setdefault("taskAttemptId", taskContext.taskAttemptId())
-
-            if taskContext._localProperties:
-                if "sentry_app_name" in taskContext._localProperties:
-                    event["tags"].setdefault(
-                        "app_name", taskContext._localProperties["sentry_app_name"]
-                    )
-                    event["tags"].setdefault(
-                        "application_id",
-                        taskContext._localProperties["sentry_application_id"],
-                    )
-
-                if "callSite.short" in taskContext._localProperties:
-                    event.setdefault("extra", {}).setdefault(
-                        "callSite", taskContext._localProperties["callSite.short"]
-                    )
-
-            return event
 
 
 def _sentry_worker_main(*args, **kwargs):
