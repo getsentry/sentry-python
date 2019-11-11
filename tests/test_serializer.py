61bfae1fa7ecf3aa6a68966d54cd1ef9f4896210
Xavier Villaneau <xvillaneau@pandora.com>
2019-11-11 13:24:32 -0500

    fix: bytes now correctly serialized as strings

--- tests/test_serializer.py
+++ tests/test_serializer.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-
+import sys
 
 import pytest
 
@@ -30,3 +30,39 @@ else:
         # Float glitches can happen, and more glitches can happen
         # because we try to work around some float glitches in semaphore
         assert (dt - dt2).total_seconds() < 1.0
+
+    @given(binary=st.binary(min_size=1))
+    def test_bytes_serialization_decode_many(binary, message_normalizer):
+        result = message_normalizer(binary, should_repr_strings=False)
+        assert result == binary.decode("utf-8", "replace")
+
+    @given(binary=st.binary(min_size=1))
+    def test_bytes_serialization_repr_many(binary, message_normalizer):
+        result = message_normalizer(binary, should_repr_strings=True)
+        assert result == repr(binary)
+
+
+@pytest.fixture
+def message_normalizer(semaphore_normalize):
+    if semaphore_normalize({"test": "test"}) is None:
+        pytest.skip("no semaphore available")
+
+    def inner(message, **kwargs):
+        event = serialize({"logentry": {"message": message}}, **kwargs)
+        normalized = semaphore_normalize(event)
+        return normalized["logentry"]["message"]
+
+    return inner
+
+
+def test_bytes_serialization_decode(message_normalizer):
+    binary = b"abc123\x80\xf0\x9f\x8d\x95"
+    result = message_normalizer(binary, should_repr_strings=False)
+    assert result == u"abc123\ufffd\U0001f355"
+
+
+@pytest.mark.xfail(sys.version_info < (3,), reason="Known safe_repr bugs in Py2.7")
+def test_bytes_serialization_repr(message_normalizer):
+    binary = b"abc123\x80\xf0\x9f\x8d\x95"
+    result = message_normalizer(binary, should_repr_strings=True)
+    assert result == r"b'abc123\x80\xf0\x9f\x8d\x95'"
