f9a74b02cc107774c6405a2dc752facc75c427d1
Dani <daniel@calidae.com>
2019-11-25 18:42:08 +0100

    Remove controversial sentrysdk init handler

--- sentry_sdk/integrations/trytond.py
+++ sentry_sdk/integrations/trytond.py
@@ -1,18 +1,11 @@
-import logging
 import sentry_sdk
 import sentry_sdk.integrations
-import sentry_sdk.integrations.logging
 
-from trytond.exceptions import TrytonException as TrytondBaseException  # type: ignore
+from trytond.exceptions import TrytonException  # type: ignore
 from trytond.wsgi import app  # type: ignore
 
 
-from sentry_sdk._types import MYPY
-
-if MYPY:
-    from typing import Tuple
-
-# TODO: trytond_worker intergation
+# TODO: trytond-worker, trytond-cron and trytond-admin intergations
 
 
 class TrytondWSGIIntegration(sentry_sdk.integrations.Integration):
@@ -24,7 +17,7 @@ class TrytondWSGIIntegration(sentry_sdk.integrations.Integration):
     @staticmethod
     def setup_once():  # type: () -> None
         def error_handler(e):  # type: (Exception) -> None
-            if isinstance(e, TrytondBaseException):
+            if isinstance(e, TrytonException):
                 return
             else:
                 sentry_sdk.capture_exception(e)
@@ -40,30 +33,3 @@ class TrytondWSGIIntegration(sentry_sdk.integrations.Integration):
 
         else:
             app.error_handlers.append(error_handler)
-
-
-class TrytondSentryHandler(logging.NullHandler):
-    def __init__(self, dsn, ignore=tuple()):
-        # type: (str, Tuple[str, ...]) -> None
-        super(TrytondSentryHandler, self).__init__()
-
-        # There is no clean way to run a static configuration code block
-        # when running the trytond-cron binary without crafting a whole
-        # new binary that wraps it.
-        # (See https://github.com/trytonus/trytond-sentry/blob/master/bin/trytond_sentry)
-        # This is a workaround to provide a behaviour
-        # similar to the old raven package
-
-        sentry_sdk.init(dsn)
-
-        # Also, there is no need to inherit
-        # sentry_sdk.integrations.logging.EventHandler because
-        # sentry_sdk.init will already install the default
-        # LoggingIntegration that will already spy on any
-        # logger.error call except for the following
-
-        for logger in ignore:
-            sentry_sdk.integrations.logging.ignore_logger(logger)
-
-        # That is: there is no actual TrytondCronIntegration to be written
-        # but this is needed so as to inject a sentry_sdk.init call
