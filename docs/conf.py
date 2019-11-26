19e38d0f0b36aea12e844c6ffda84ab11f1606e3
Markus Unterwaditzer <markus@unterwaditzer.net>
2019-11-26 17:21:52 +0100

    release: 0.13.4

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ project = u"sentry-python"
 copyright = u"2019, Sentry Team and Contributors"
 author = u"Sentry Team and Contributors"
 
-release = "0.13.3"
+release = "0.13.4"
 version = ".".join(release.split(".")[:2])  # The short X.Y version.
 
 
