858dcdfd9280660539b51cc6124545e58a831305
Nick Gashkov <nick@gashkov.com>
2019-12-03 17:54:18 +0300

    Fix 'asyncio.CancelledError' capturing

--- sentry_sdk/integrations/aiohttp.py
+++ sentry_sdk/integrations/aiohttp.py
@@ -82,6 +82,8 @@ class AioHttpIntegration(Integration):
                         except HTTPException as e:
                             span.set_http_status(e.status_code)
                             raise
+                        except asyncio.CancelledError:
+                            raise
                         except Exception:
                             # This will probably map to a 500 but seems like we
                             # have no way to tell. Do not set span status.
