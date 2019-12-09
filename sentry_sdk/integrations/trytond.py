5ad40c311bde4f7d755e58d7f8dfc4eed985e74a
Dani <daniel@calidae.com>
2019-12-09 16:24:59 +0100

    Forward client options to event_from_exception

--- sentry_sdk/integrations/trytond.py
+++ sentry_sdk/integrations/trytond.py
@@ -1,12 +1,14 @@
-import sys
-
 import sentry_sdk.hub
 import sentry_sdk.utils
 import sentry_sdk.integrations
 import sentry_sdk.integrations.wsgi
+from sentry_sdk._types import MYPY
 
 from trytond.exceptions import TrytonException  # type: ignore
 from trytond.wsgi import app  # type: ignore
+
+if MYPY:
+    from typing import Any
 
 
 # TODO: trytond-worker, trytond-cron and trytond-admin intergations
@@ -25,12 +27,19 @@ class TrytondWSGIIntegration(sentry_sdk.integrations.Integration):
 
         def error_handler(e):  # type: (Exception) -> None
             hub = sentry_sdk.hub.Hub.current
+
             if hub.get_integration(TrytondWSGIIntegration) is None:
                 return
             elif isinstance(e, TrytonException):
                 return
             else:
-                (event, hint) = sentry_sdk.utils.event_from_exception(sys.exc_info())
+                # If an integration is there, a client has to be there.
+                client = hub.client  # type: Any
+                event, hint = sentry_sdk.utils.event_from_exception(
+                    e,
+                    client_options=client.options,
+                    mechanism={"type": "trytond", "handled": False},
+                )
                 hub.capture_event(event, hint=hint)
 
         # Expected error handlers signature was changed
