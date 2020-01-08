d947f525a34ff3174043d99325053002c6104a2f
Markus Unterwaditzer <markus@unterwaditzer.net>
2020-01-08 10:46:29 +0100

    craft(gh-pages): update, version "0.14.0"

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,8 +1,9 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.13.5',
+    VERSION: '0.14.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
