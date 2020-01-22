dbce2190fb6e6ab1daf042ce917313d4dd45dffd
Markus Unterwaditzer <markus@unterwaditzer.net>
2020-01-22 18:00:13 +0100

    fix: Make patched channels asgi app awaitable, fix integraton-disabled codepath (#598)
    * fix: Make patched channels asgi app awaitable, fix integraton-disabled codepath
    
    * fix: Fix linters
    
    * fix: Reformatting

--- sentry_sdk/integrations/django/asgi.py
+++ sentry_sdk/integrations/django/asgi.py
@@ -29,3 +29,19 @@ def patch_django_asgi_handler_impl(cls):
         return await middleware(scope, receive, send)
 
     cls.__call__ = sentry_patched_asgi_handler
+
+
+def patch_channels_asgi_handler_impl(cls):
+    # type: (Any) -> None
+    old_app = cls.__call__
+
+    async def sentry_patched_asgi_handler(self, receive, send):
+        # type: (Any, Any, Any) -> Any
+        if Hub.current.get_integration(DjangoIntegration) is None:
+            return await old_app(self, receive, send)
+
+        middleware = SentryAsgiMiddleware(lambda _scope: old_app.__get__(self, cls))
+
+        return await middleware(self.scope)(receive, send)
+
+    cls.__call__ = sentry_patched_asgi_handler
