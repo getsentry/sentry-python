376ce9cc9ab7918ed5681037925492fc1649bf41
Dan Rosenbloom <drosenbloom@logicworks.net>
2020-02-13 15:59:50 -0500

    style: change url string generation

--- sentry_sdk/integrations/aws_lambda.py
+++ sentry_sdk/integrations/aws_lambda.py
@@ -238,19 +238,17 @@ def _get_cloudwatch_logs_url(context, start_time):
         str -- AWS Console URL to logs.
     """
     formatstring = "%Y-%m-%dT%H:%M:%S"
-    start_time_str = (start_time - timedelta(seconds=1)).strftime(formatstring)
-    end_time_str = (datetime.now() + timedelta(seconds=2)).strftime(formatstring)
-    url = ''.join([
-        "https://console.aws.amazon.com/cloudwatch/home?region=",
-        environ.get('AWS_REGION'),
-        "#logEventViewer:group=",
-        context.log_group_name,
-        ";stream=",
-        context.log_stream_name,
-        ";start=",
-        start_time_str,
-        ";end=",
-        end_time_str,
-    ])
+
+    url = (
+        "https://console.aws.amazon.com/cloudwatch/home?region={region}"
+        "#logEventViewer:group={log_group};stream={log_stream}"
+        ";start={start_time};end={end_time}"
+    ).format(
+        region=environ.get('AWS_REGION'),
+        log_group=context.log_group_name,
+        log_stream=context.log_stream_name,
+        start_time=(start_time - timedelta(seconds=1)).strftime(formatstring),
+        end_time=(datetime.now() + timedelta(seconds=2)).strftime(formatstring),
+    )
 
     return url
