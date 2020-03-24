5a0b702428d1ec23ca661568e306305b3e342762
Markus Unterwaditzer <markus@unterwaditzer.net>
2020-03-24 13:05:09 +0100

    fix: Fix typo in header name

--- tests/test_transport.py
+++ tests/test_transport.py
@@ -119,7 +119,7 @@ def test_data_category_limits(httpserver, capsys, caplog, response_code):
     httpserver.serve_content(
         "hm",
         response_code,
-        headers={"X-Sentry-Rate-Limit": "4711:transaction:organization"},
+        headers={"X-Sentry-Rate-Limits": "4711:transaction:organization"},
     )
 
     client.capture_event({"type": "transaction"})
@@ -150,7 +150,7 @@ def test_complex_limits_without_data_category(
         dict(dsn="http://foobar@{}/123".format(httpserver.url[len("http://") :]))
     )
     httpserver.serve_content(
-        "hm", response_code, headers={"X-Sentry-Rate-Limit": "4711::organization"},
+        "hm", response_code, headers={"X-Sentry-Rate-Limits": "4711::organization"},
     )
 
     client.capture_event({"type": "transaction"})
