0ee6a25d8dc4fa28f927ad70b9be166fa2dc91f3
Alex Hall <alex.mojaki@gmail.com>
2020-07-10 23:16:20 +0200

    Use sentry_init fixture in tests instead of using Hub directly (#759)

--- tests/conftest.py
+++ tests/conftest.py
@@ -186,7 +186,8 @@ def sentry_init(monkeypatch_test_transport, request):
         hub = sentry_sdk.Hub.current
         client = sentry_sdk.Client(*a, **kw)
         hub.bind_client(client)
-        monkeypatch_test_transport(sentry_sdk.Hub.current.client)
+        if "transport" not in kw:
+            monkeypatch_test_transport(sentry_sdk.Hub.current.client)
 
     if request.node.get_closest_marker("forked"):
         # Do not run isolation if the test is already running in
