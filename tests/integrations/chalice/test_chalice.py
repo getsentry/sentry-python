660eb8f4e22dafd01fdfe294dffdd7f2acee2aca
Glen <lccbrokha@gmail.com>
2020-08-11 16:43:35 -0500

    add-chalice-tox

--- tests/integrations/chalice/test_chalice.py
+++ tests/integrations/chalice/test_chalice.py
@@ -26,35 +26,6 @@ def app():
     return app
 
 
-@pytest.fixture
-def capture_events(monkeypatch):
-    def inner():
-        events = []
-        test_client = sentry_sdk.Hub.current.client
-        old_capture_event = test_client.transport.capture_event
-        old_capture_envelope = test_client.transport.capture_envelope
-
-        def append_event(event):
-            events.append(event)
-            return old_capture_event(event)
-
-        def append_envelope(envelope):
-            for item in envelope:
-                if item.headers.get("type") in ("event", "transaction"):
-                    events.append(item.payload.json)
-            return old_capture_envelope(envelope)
-
-        monkeypatch.setattr(
-            test_client.transport, "capture_event", append_event
-        )
-        monkeypatch.setattr(
-            test_client.transport, "capture_envelope", append_envelope
-        )
-        return events
-
-    return inner
-
-
 def test_exception_boom(app, client: RequestHandler) -> None:
     response = client.get('/boom')
     assert response.status_code == 500
