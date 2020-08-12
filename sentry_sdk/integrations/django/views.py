2a8880667960832a6bab6ff92b808a397530f13a
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2020-08-12 20:04:36 +0200

    fix linters

--- sentry_sdk/integrations/django/views.py
+++ sentry_sdk/integrations/django/views.py
@@ -1,9 +1,17 @@
 import functools
 
 from sentry_sdk.hub import Hub
+from sentry_sdk._types import MYPY
+
+if MYPY:
+    from typing import Any
+
+    from django.core.handlers.wsgi import WSGIRequest
+    from django.urls.resolvers import ResolverMatch
 
 
 def patch_resolve_request():
+    # type: () -> None
     from django.core.handlers.base import BaseHandler
     from sentry_sdk.integrations.django import DjangoIntegration
 
@@ -14,6 +22,7 @@ def patch_resolve_request():
         return
 
     def resolve_request(self, request):
+        # type: (BaseHandler, WSGIRequest) -> ResolverMatch
         hub = Hub.current
         integration = hub.get_integration(DjangoIntegration)
 
@@ -27,7 +36,7 @@ def patch_resolve_request():
 
 
 def _wrap_resolver_match(hub, resolver_match):
-    # type: (django.urls.resolvers.ResolverMatch) -> django.urls.resolvers.ResolverMatch
+    # type: (Hub, ResolverMatch) -> ResolverMatch
 
     # XXX: The wrapper function is created for every request. Find more
     # efficient way to wrap views (or build a cache?)
@@ -36,6 +45,7 @@ def _wrap_resolver_match(hub, resolver_match):
 
     @functools.wraps(old_callback)
     def callback(*args, **kwargs):
+        # type: (*Any, **Any) -> Any
         with hub.start_span(op="django.view", description=resolver_match.view_name):
             return old_callback(*args, **kwargs)
 
