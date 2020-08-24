f6f3525f8812f60911573a8b7f71807ecf2e1052
Markus Unterwaditzer <markus@unterwaditzer.net>
2020-08-24 13:54:53 +0200

    ref: Remove traceparent_v2 flag (#795)

--- sentry_sdk/tracing.py
+++ sentry_sdk/tracing.py
@@ -304,10 +304,6 @@ class Span(object):
             sampled = "0"
         return "%s-%s-%s" % (self.trace_id, self.span_id, sampled)
 
-    def to_legacy_traceparent(self):
-        # type: () -> str
-        return "00-%s-%s-00" % (self.trace_id, self.span_id)
-
     def set_tag(self, key, value):
         # type: (str, Any) -> None
         self._tags[key] = value
