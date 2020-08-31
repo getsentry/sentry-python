9a3c3b9f86494ff18bb05b5963176c24ed1120f8
Shantanu  Dhiman <shantanu.dhiman@calsoftinc.com>
2020-08-31 18:13:21 +0530

    Changes: 1) Modified local time to utc time in google cloud logs url. 2) Converted log range to include log values from after the completion of execution.

--- sentry_sdk/integrations/gcp.py
+++ sentry_sdk/integrations/gcp.py
@@ -51,7 +51,7 @@ def _wrap_func(func):
 
         configured_time = int(configured_time)
 
-        initial_time = datetime.now()
+        initial_time = datetime.utcnow()
 
         with hub.push_scope() as scope:
             with capture_internal_exceptions():
@@ -119,7 +119,7 @@ def _make_request_event_processor(configured_timeout, initial_time):
     def event_processor(event, hint):
         # type: (Event, Hint) -> Optional[Event]
 
-        final_time = datetime.now()
+        final_time = datetime.utcnow()
         time_diff = final_time - initial_time
 
         execution_duration_in_millis = time_diff.microseconds / MILLIS_TO_SECONDS
@@ -136,7 +136,7 @@ def _make_request_event_processor(configured_timeout, initial_time):
         }
 
         extra["google cloud logs"] = {
-            "url": _get_google_cloud_logs_url(initial_time),
+            "url": _get_google_cloud_logs_url(final_time),
         }
 
         request = event.get("request", {})
@@ -150,7 +150,7 @@ def _make_request_event_processor(configured_timeout, initial_time):
     return event_processor
 
 
-def _get_google_cloud_logs_url(initial_time):
+def _get_google_cloud_logs_url(final_time):
     # type: (datetime) -> str
     """
     Generates a Google Cloud Logs console URL based on the environment variables
@@ -159,22 +159,22 @@ def _get_google_cloud_logs_url(initial_time):
     Returns:
         str -- Google Cloud Logs Console URL to logs.
     """
-    hour_ago = initial_time - timedelta(hours=1)
+    hour_ago = final_time - timedelta(hours=1)
 
     url = (
         "https://console.cloud.google.com/logs/viewer?project={project}&resource=cloud_function"
         "%2Ffunction_name%2F{function_name}%2Fregion%2F{region}&minLogLevel=0&expandAll=false"
-        "&timestamp={initial_time}&customFacets=&limitCustomFacetWidth=true"
-        "&dateRangeStart={timestamp_start}&dateRangeEnd={timestamp_end}"
-        "&interval=PT1H&scrollTimestamp={timestamp_current}"
+        "&timestamp={initial_time}Z&customFacets=&limitCustomFacetWidth=true"
+        "&dateRangeStart={timestamp_start}Z&dateRangeEnd={timestamp_end}Z"
+        "&interval=PT1H&scrollTimestamp={timestamp_current}Z"
     ).format(
         project=environ.get("GCP_PROJECT"),
         function_name=environ.get("FUNCTION_NAME"),
         region=environ.get("FUNCTION_REGION"),
-        initial_time=initial_time,
+        initial_time=final_time,
         timestamp_start=hour_ago,
-        timestamp_end=initial_time,
-        timestamp_current=initial_time,
+        timestamp_end=final_time,
+        timestamp_current=final_time,
     )
 
     return url
