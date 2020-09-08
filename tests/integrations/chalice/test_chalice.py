89bf77c6b8fdc6758019bbc3ab911f8d176b2470
Glen <lccbrokha@gmail.com>
2020-09-08 12:23:29 -0500

    mock-test

--- tests/integrations/chalice/test_chalice.py
+++ tests/integrations/chalice/test_chalice.py
@@ -1,10 +1,27 @@
 import pytest
 
 from chalice import Chalice
+from chalice.local import LambdaContext, LocalGateway
 
 from sentry_sdk.integrations.chalice import ChaliceIntegration
 
 from pytest_chalice.handlers import RequestHandler
+
+
+def _generate_lambda_context(self):
+    # Monkeypatch of the function _generate_lambda_context
+    # from the class LocalGateway
+    # for mock the timeout
+    # type: () -> LambdaContext
+    if self._config.lambda_timeout is None:
+        timeout = 10 * 1000
+    else:
+        timeout = self._config.lambda_timeout * 1000
+    return LambdaContext(
+        function_name=self._config.function_name,
+        memory_size=self._config.lambda_memory_size,
+        max_runtime_ms=timeout
+    )
 
 
 @pytest.fixture
@@ -20,10 +37,11 @@ def app(sentry_init):
     def has_request():
         raise Exception("boom goes the dynamite!")
 
+    LocalGateway._generate_lambda_context = _generate_lambda_context
+
     return app
 
 
-@pytest.mark.skip(reason="self._max_runtime is none, the response is correct")
 def test_exception_boom(app, client: RequestHandler) -> None:
     response = client.get("/boom")
     assert response.status_code == 500
@@ -35,7 +53,6 @@ def test_exception_boom(app, client: RequestHandler) -> None:
     )
 
 
-@pytest.mark.skip(reason="self._max_runtime is none, the response is correct")
 def test_has_request(app, capture_events, client: RequestHandler):
     events = capture_events()
 
