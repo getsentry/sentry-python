9d1a43aff36ba280b59eb10196dd820387ff5c84
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2020-09-21 13:26:49 +0200

    Update sentry_sdk/envelope.py
    Co-authored-by: Rodolfo Carvalho <rodolfo.carvalho@sentry.io>

--- sentry_sdk/envelope.py
+++ sentry_sdk/envelope.py
@@ -242,7 +242,7 @@ class Item(object):
     def get_event(self):
         # type: (...) -> Optional[Event]
         """
-        Returns a regular (non-transaction) event if there is one.
+        Returns an error event if there is one.
         """
         if self.headers.get("type") == "event" and self.payload.json is not None:
             return self.payload.json
