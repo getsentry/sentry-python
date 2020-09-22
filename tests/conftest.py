c3655c26351112d08b99f88e8bfd087712ac3cc7
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2020-09-22 17:31:59 +0200

    wip

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import os
 import json
+import traceback
 
 import pytest
 import jsonschema
@@ -235,11 +236,7 @@ def capture_envelopes(monkeypatch):
 @pytest.fixture
 def capture_events_forksafe(monkeypatch, capture_events, request):
     def inner():
-        in_process_events = capture_events()
-
-        @request.addfinalizer
-        def _():
-            assert not in_process_events
+        capture_events()
 
         events_r, events_w = os.pipe()
         events_r = os.fdopen(events_r, "rb", 0)
