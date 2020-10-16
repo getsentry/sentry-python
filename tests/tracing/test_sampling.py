4b56ec74063aa7c565a2c63edfa6760aa4b114a8
Katie Byers <lobsterkatie@gmail.com>
2020-10-16 00:31:13 -0700

    add rate validation tests

--- tests/tracing/test_sampling.py
+++ tests/tracing/test_sampling.py
@@ -1,4 +1,13 @@
+import pytest
+
 from sentry_sdk import start_span, start_transaction
+from sentry_sdk.tracing import _is_valid_sample_rate
+from sentry_sdk.utils import logger
+
+try:
+    from unittest import mock  # python 3.3 and above
+except ImportError:
+    import mock  # python < 3.3
 
 
 def test_sampling_decided_only_for_transactions(sentry_init, capture_events):
@@ -32,3 +41,35 @@ def test_no_double_sampling(sentry_init, capture_events):
         pass
 
     assert len(events) == 1
+
+
+@pytest.mark.parametrize(
+    "rate",
+    [0.0, 0.1231, 1.0, True, False],
+)
+def test_accepts_valid_sample_rate(rate):
+    with mock.patch.object(logger, "warning", mock.Mock()):
+        result = _is_valid_sample_rate(rate)
+        assert logger.warning.called is False
+        assert result is True
+
+
+@pytest.mark.parametrize(
+    "rate",
+    [
+        "dogs are great",  # wrong type
+        (0, 1),  # wrong type
+        {"Maisey": "Charllie"},  # wrong type
+        [True, True],  # wrong type
+        {0.2012},  # wrong type
+        float("NaN"),  # wrong type
+        None,  # wrong type
+        -1.121,  # wrong value
+        1.231,  # wrong value
+    ],
+)
+def test_warns_on_invalid_sample_rate(rate, StringContaining):  # noqa: N803
+    with mock.patch.object(logger, "warning", mock.Mock()):
+        result = _is_valid_sample_rate(rate)
+        logger.warning.assert_any_call(StringContaining("Given sample rate is invalid"))
+        assert result is False
