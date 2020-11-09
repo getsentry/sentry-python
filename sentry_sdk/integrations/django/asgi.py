7fe9e06676ff3748f052c5f2dc0980655382415a
Luke Pomfrey <github@lukepomfrey.org>
2020-11-09 08:36:45 +0000

    Fix patching of AsgiHandler in Django Channels >= 3.0 (#912)

--- sentry_sdk/integrations/django/asgi.py
+++ sentry_sdk/integrations/django/asgi.py
@@ -53,22 +53,30 @@ def patch_get_response_async(cls, _before_get_response):
 def patch_channels_asgi_handler_impl(cls):
     # type: (Any) -> None
 
+    import channels  # type: ignore
     from sentry_sdk.integrations.django import DjangoIntegration
 
-    old_app = cls.__call__
+    if channels.__version__ < "3.0.0":
 
-    async def sentry_patched_asgi_handler(self, receive, send):
-        # type: (Any, Any, Any) -> Any
-        if Hub.current.get_integration(DjangoIntegration) is None:
-            return await old_app(self, receive, send)
+        old_app = cls.__call__
 
-        middleware = SentryAsgiMiddleware(
-            lambda _scope: old_app.__get__(self, cls), unsafe_context_data=True
-        )
+        async def sentry_patched_asgi_handler(self, receive, send):
+            # type: (Any, Any, Any) -> Any
+            if Hub.current.get_integration(DjangoIntegration) is None:
+                return await old_app(self, receive, send)
 
-        return await middleware(self.scope)(receive, send)
+            middleware = SentryAsgiMiddleware(
+                lambda _scope: old_app.__get__(self, cls), unsafe_context_data=True
+            )
 
-    cls.__call__ = sentry_patched_asgi_handler
+            return await middleware(self.scope)(receive, send)
+
+        cls.__call__ = sentry_patched_asgi_handler
+
+    else:
+        # The ASGI handler in Channels >= 3 has the same signature as
+        # the Django handler.
+        patch_django_asgi_handler_impl(cls)
 
 
 def wrap_async_view(hub, callback):
