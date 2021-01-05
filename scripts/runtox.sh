85f2d7ac0b5f26c5fdca4b72595b272b9b133fa7
Billy Vong <billy@sentry.io>
2021-01-04 19:11:15 -0800

    pypypy

--- scripts/runtox.sh
+++ scripts/runtox.sh
@@ -16,6 +16,9 @@ if [ -n "$1" ]; then
     searchstring="$1"
 elif [ -n "$CI_PYTHON_VERSION" ]; then
     searchstring="$(echo py$CI_PYTHON_VERSION | sed -e 's/pypypy/pypy/g' -e 's/-dev//g')"
+    if [ "$searchstring" = "pypy2.7" ]; then
+        searchstring=pypy
+    fi
 elif [ -n "$AZURE_PYTHON_VERSION" ]; then
     searchstring="$(echo py$AZURE_PYTHON_VERSION | sed -e 's/pypypy/pypy/g' -e 's/-dev//g')"
     if [ "$searchstring" = pypy2 ]; then
