8d69b07a156b27deb5fa1d33d11ac9e93c44f56e
Ahmed Etefy <ahmed.etefy12@gmail.com>
2021-02-05 13:51:17 +0100

    Fixed type error

--- sentry_sdk/sessions.py
+++ sentry_sdk/sessions.py
@@ -16,10 +16,11 @@ if MYPY:
     from typing import Dict
     from typing import List
     from typing import Generator
+    from typing import Union
 
 
 def is_auto_session_tracking_enabled(hub=None):
-    # type: (Optional[sentry_sdk.Hub]) -> bool
+    # type: (Optional[sentry_sdk.Hub]) -> Union[bool, None]
     """Utility function to find out if session tracking is enabled."""
     if hub is None:
         hub = sentry_sdk.Hub.current
