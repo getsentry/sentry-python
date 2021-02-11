989e01dbd424f8255ff2ab510f6b7519324518c2
iker barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2021-02-11 15:25:55 +0100

    ref: Change serverless dist destination path to `/dist-serverless` (#1012)

--- scripts/build-awslambda-layer.py
+++ scripts/build-awslambda-layer.py
@@ -6,7 +6,10 @@ from sentry_sdk.consts import VERSION as SDK_VERSION
 
 
 DIST_DIRNAME = "dist"
-DIST_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", DIST_DIRNAME))
+DEST_REL_PATH = "dist-serverless"
+DEST_ABS_PATH = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "..", DEST_REL_PATH)
+)
 DEST_ZIP_FILENAME = f"sentry-python-serverless-{SDK_VERSION}.zip"
 WHEELS_FILEPATH = os.path.join(
     DIST_DIRNAME, f"sentry_sdk-{SDK_VERSION}-py2.py3-none-any.whl"
@@ -65,7 +68,9 @@ def build_packaged_zip():
         package_builder.make_directories()
         package_builder.install_python_binaries()
         package_builder.zip(DEST_ZIP_FILENAME)
-        shutil.copy(package_builder.get_relative_path_of(DEST_ZIP_FILENAME), DIST_DIR)
+        shutil.copy(
+            package_builder.get_relative_path_of(DEST_ZIP_FILENAME), DEST_ABS_PATH
+        )
 
 
 build_packaged_zip()
