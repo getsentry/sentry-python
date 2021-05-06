85951c30fdef859b036c613037a4d9bbc9276d41
Ahmed Etefy <ahmed.etefy12@gmail.com>
2021-05-06 17:31:49 +0200

    Revert "fix(aws-lambda): Change function handler name to 'x.y' (#1107)"
    This reverts commit b7b5c03ef3263ff62ffe00d6319a4ace508a7a26.

--- tests/integrations/aws_lambda/client.py
+++ tests/integrations/aws_lambda/client.py
@@ -51,7 +51,7 @@ def build_no_code_serverless_function_and_layer(
                 }
             },
             Role=os.environ["SENTRY_PYTHON_TEST_AWS_IAM_ROLE"],
-            Handler="init_serverless_sdk.sentry_lambda_handler",
+            Handler="sentry_sdk.integrations.init_serverless_sdk.sentry_lambda_handler",
             Layers=[response["LayerVersionArn"]],
             Code={"ZipFile": zip.read()},
             Description="Created as part of testsuite for getsentry/sentry-python",
