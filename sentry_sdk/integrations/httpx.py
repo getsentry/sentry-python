43aee0f68b3539c57ce535848a091a979b51a65b
elonzh <elonzh@qq.com>
2021-05-28 11:43:06 +0800

    test: add  test_outgoing_trace_headers and some docs

--- sentry_sdk/integrations/httpx.py
+++ sentry_sdk/integrations/httpx.py
@@ -21,6 +21,10 @@ class HttpxIntegration(Integration):
     @staticmethod
     def setup_once():
         # type: () -> None
+        """
+        httpx has its own transport layer and can be customized when needed,
+        so patch Client.send and AsyncClient.send to support both synchronous and async interfaces.
+        """
         _install_httpx_client()
         _install_httpx_async_client()
 
--- tests/integrations/httpx/test_httpx.py
+++ tests/integrations/httpx/test_httpx.py
@@ -2,8 +2,7 @@ import asyncio
 
 import httpx
 
-import sentry_sdk
-from sentry_sdk import capture_message
+from sentry_sdk import capture_message, start_transaction
 from sentry_sdk.integrations.httpx import HttpxIntegration
 
 
@@ -13,11 +12,9 @@ def test_crumb_capture_and_hint(sentry_init, capture_events):
         return crumb
 
     sentry_init(integrations=[HttpxIntegration()], before_breadcrumb=before_breadcrumb)
-
     clients = (httpx.Client(), httpx.AsyncClient())
-
     for i, c in enumerate(clients):
-        with sentry_sdk.start_transaction():
+        with start_transaction():
             events = capture_events()
 
             url = "https://httpbin.org/status/200"
@@ -30,6 +27,7 @@ def test_crumb_capture_and_hint(sentry_init, capture_events):
             capture_message("Testing!")
 
             (event,) = events
+            # send request twice so we need get breadcrumb by index
             crumb = event["breadcrumbs"]["values"][i]
             assert crumb["type"] == "http"
             assert crumb["category"] == "httplib"
@@ -40,3 +38,29 @@ def test_crumb_capture_and_hint(sentry_init, capture_events):
                 "reason": "OK",
                 "extra": "foo",
             }
+
+
+def test_outgoing_trace_headers(sentry_init):
+    sentry_init(traces_sample_rate=1.0, integrations=[HttpxIntegration()])
+    clients = (httpx.Client(), httpx.AsyncClient())
+    for i, c in enumerate(clients):
+        with start_transaction(
+            name="/interactions/other-dogs/new-dog",
+            op="greeting.sniff",
+            # make trace_id difference between transactions
+            trace_id=f"012345678901234567890123456789{i}",
+        ) as transaction:
+            url = "https://httpbin.org/status/200"
+            if not asyncio.iscoroutinefunction(c.get):
+                response = c.get(url)
+            else:
+                response = asyncio.get_event_loop().run_until_complete(c.get(url))
+
+            request_span = transaction._span_recorder.spans[-1]
+            assert response.request.headers[
+                "sentry-trace"
+            ] == "{trace_id}-{parent_span_id}-{sampled}".format(
+                trace_id=transaction.trace_id,
+                parent_span_id=request_span.span_id,
+                sampled=1,
+            )
