832ce55afef0dd88ed5c47d14a396eb57c0d3142
Ahmed Etefy <ahmed.etefy12@gmail.com>
2021-06-01 18:30:55 +0200

    Converted string version into tuple for comparison

--- tests/integrations/django/test_basic.py
+++ tests/integrations/django/test_basic.py
@@ -26,8 +26,9 @@ from tests.integrations.django.myapp.wsgi import application
 # requires explicit database allow from failing the test
 pytest_mark_django_db_decorator = pytest.mark.django_db
 try:
-    if pytest_django.__version__ > (4, 2, 0):
-        pytest_mark_django_db_decorator = pytest.mark.django_db(database="__all__")
+    pytest_version = tuple(map(int, pytest_django.__version__.split('.')))
+    if pytest_version > (4, 2, 0):
+        pytest_mark_django_db_decorator = pytest.mark.django_db(databases="__all__")
 except AttributeError:
     pass
 
