9fe89d0322206d8d6dce86d9356a70ac87ae1595
Katie Byers <lobsterkatie@gmail.com>
2021-09-10 00:19:02 -0700

    fix sqlalchemy test

--- tests/integrations/sqlalchemy/test_sqlalchemy.py
+++ tests/integrations/sqlalchemy/test_sqlalchemy.py
@@ -189,7 +189,7 @@ def test_too_large_event_truncated(sentry_init, capture_events):
     assert len(json_dumps(event)) < max_bytes
 
     # Some spans are discarded.
-    assert len(event["spans"]) == 999
+    assert len(event["spans"]) == 1000
 
     # Some spans have their descriptions truncated. Because the test always
     # generates the same amount of descriptions and truncation is deterministic,
@@ -197,7 +197,7 @@ def test_too_large_event_truncated(sentry_init, capture_events):
     #
     # Which exact span descriptions are truncated depends on the span durations
     # of each SQL query and is non-deterministic.
-    assert len(event["_meta"]["spans"]) == 536
+    assert len(event["_meta"]["spans"]) == 537
 
     for i, span in enumerate(event["spans"]):
         description = span["description"]
