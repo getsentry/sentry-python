639c9411309f7cce232da91547a808fbff2567cf
Anton Pirker <anton.pirker@sentry.io>
2022-01-24 15:56:43 +0100

    build(tests): Python 3.10 support (#1309)
    Adding Python 3.10 to our test suite
    
    Refs GH-1273
    
    * Do not test Flask 0.11 and 0.12 in Python 3.10
    
    * fix(python): Capture only 5xx HTTP errors in Falcon Integration (#1314)
    
    * Write code that is actually somehow typed and can be linted.
    
    * Updated test matrix for Tornado and Asgi

--- setup.py
+++ setup.py
@@ -72,6 +72,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     options={"bdist_wheel": {"universal": "1"}},
