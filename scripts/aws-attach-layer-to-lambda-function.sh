a2d4ce452a9f79d3be61a8f1ac29f8246a30e2a5
Anton Pirker <anton.pirker@sentry.io>
2022-04-22 14:54:18 +0200

    Added script to build and attach lambda layer

---
+++ scripts/aws-attach-layer-to-lambda-function.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+FUNCTION_NAME=$1
+
+echo "Getting ARN of newest Sentry lambda layer..."
+LAYER_ARN=$(aws lambda list-layer-versions --layer-name SentryPythonServerlessSDKLocalDev --query "LayerVersions[0].LayerVersionArn" | tr -d '"')
+echo "Done getting ARN of newest Sentry lambda layer $LAYER_ARN."
+
+echo "Attaching Lamba layer to function $FUNCTION_NAME..."
+echo "Warning: This remove all other layers!"
+aws lambda update-function-configuration \
+    --function-name "$FUNCTION_NAME" \
+    --layers "$LAYER_ARN" \
+    --no-cli-pager
+echo "Done attaching Lamba layer to function '$FUNCTION_NAME'."
+
+echo "All done. Have a nice day!"
