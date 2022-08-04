d26f27fc2417395084db3f30bfdea099ca408791
Jens Langhammer <jens.langhammer@beryju.org>
2022-08-04 10:09:00 +0200

    refactor to patch _live_receivers instead

--- sentry_sdk/integrations/django/signals_handlers.py
+++ sentry_sdk/integrations/django/signals_handlers.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
-import logging
-
 from django.dispatch import Signal
-from django.dispatch.dispatcher import NO_RECEIVERS
 
 from sentry_sdk import Hub
 
@@ -12,66 +9,32 @@ from sentry_sdk import Hub
 def patch_signals():
     """Patch django signal receivers to create a span"""
 
-    logger = logging.getLogger("django.dispatch")
+    old_live_receivers = Signal._live_receivers
 
-    def send(self: Signal, sender, **kwargs):
-        if (
-            not self.receivers
-            or self.sender_receivers_cache.get(sender) is NO_RECEIVERS
-        ):
-            return []
-        responses = []
+    def _get_receiver_name(receiver):
+        name = receiver.__module__ + "."
+        if hasattr(receiver, "__name__"):
+            return name + receiver.__name__
+        return name + str(receiver)
+
+    def _sentry_live_receivers(self, sender):
         hub = Hub.current
-        for receiver in self._live_receivers(sender):
-            name = receiver.__module__ + "."
-            if hasattr(receiver, "__name__"):
-                name += receiver.__name__
-            else:
-                name += str(receiver)
-            with hub.start_span(
-                op="django.signals",
-                description=name,
-            ) as span:
-                span.set_data("signal", name)
-                responses.append(
-                    (receiver, receiver(signal=self, sender=sender, **kwargs))
-                )
-        return responses
+        receivers = old_live_receivers(self, sender)
 
-    def send_robust(self: Signal, sender, **kwargs):
-        if (
-            not self.receivers
-            or self.sender_receivers_cache.get(sender) is NO_RECEIVERS
-        ):
-            return []
-        hub = Hub.current
-        # Call each receiver with whatever arguments it can accept.
-        # Return a list of tuple pairs [(receiver, response), ... ].
-        responses = []
-        for receiver in self._live_receivers(sender):
-            name = receiver.__module__ + "."
-            if hasattr(receiver, "__name__"):
-                name += receiver.__name__
-            else:
-                name += str(receiver)
-            with hub.start_span(
-                op="django.signals",
-                description=name,
-            ) as span:
-                span.set_data("signal", name)
-                try:
-                    response = receiver(signal=self, sender=sender, **kwargs)
-                except Exception as err:
-                    logger.error(
-                        "Error calling %s in Signal.send_robust() (%s)",
-                        receiver.__qualname__,
-                        err,
-                        exc_info=err,
-                    )
-                    responses.append((receiver, err))
-                else:
-                    responses.append((receiver, response))
-        return responses
+        def sentry_receiver_wrapper(receiver):
+            def wrapper(*args, **kwargs):
+                with hub.start_span(
+                    op="django.signals",
+                    description=_get_receiver_name(receiver),
+                ) as span:
+                    span.set_data("signal", _get_receiver_name(receiver))
+                    return receiver(*args, **kwargs)
 
-    Signal.send = send
-    Signal.send_robust = send_robust
+            return wrapper
+
+        for idx, receiver in enumerate(receivers):
+            receivers[idx] = sentry_receiver_wrapper(receiver)
+
+        return receivers
+
+    Signal._live_receivers = _sentry_live_receivers
