854109d59ef14ce53d3af920b3a05f5e5e82c537
Evgeny Seregin <zhenya.seregin@outlook.com>
2022-08-09 21:09:33 +0300

    Added context_processor for tasks

--- sentry_sdk/integrations/huey.py
+++ sentry_sdk/integrations/huey.py
@@ -8,6 +8,7 @@ from sentry_sdk.utils import capture_internal_exceptions
 
 if MYPY:
     from typing import Any, Optional
+    from sentry_sdk._types import EventProcessor, Event, Hint
 
 try:
     from huey.api import Huey, Task
@@ -22,6 +23,28 @@ class HueyIntegration(Integration):
     def setup_once():
         # type: () -> None
         patch_execute()
+
+
+def _make_event_processor(task):
+    # type: (Any) -> EventProcessor
+    def event_processor(event, hint):
+        # type: (Event, Hint) -> Optional[Event]
+
+        with capture_internal_exceptions():
+            tags = event.setdefault("tags", {})
+            tags["huey_task_id"] = task.id
+            tags["huey_task_retry"] = task.default_retries > task.retries
+            extra = event.setdefault("extra", {})
+            extra["huey-job"] = {
+                "task": task.name,
+                "args": task.args,
+                "kwargs": task.kwargs,
+                "retry": (task.default_retries or 0) - task.retries,
+            }
+
+        return event
+
+    return event_processor
 
 
 def patch_execute():
@@ -39,6 +62,7 @@ def patch_execute():
             with capture_internal_exceptions():
                 scope._name = "huey"
                 scope.clear_breadcrumbs()
+                scope.add_event_processor(_make_event_processor(task))
 
             transaction = Transaction(
                 name=task.name,
--- tests/integrations/huey/test_huey.py
+++ tests/integrations/huey/test_huey.py
@@ -1,4 +1,5 @@
 import pytest
+from decimal import DivisionByZero
 
 from sentry_sdk.integrations.huey import HueyIntegration
 
@@ -26,6 +27,19 @@ def flush_huey_tasks(init_huey):
     huey.flush()
 
 
+def execute_huey_task(huey, func, *args, exceptions=None, **kwargs):
+    result = func(*args, **kwargs)
+    task = huey.dequeue()
+    if exceptions is not None:
+        try:
+            huey.execute(task)
+        except exceptions:
+            pass
+    else:
+        huey.execute(task)
+    return result
+
+
 def test_task_result(init_huey):
     huey = init_huey()
 
@@ -40,3 +54,29 @@ def test_task_result(init_huey):
     task = huey.dequeue()
     assert huey.execute(task) == 4
     assert result.get() == 4
+
+
+@pytest.mark.parametrize("task_fails", [True, False], ids=["error", "success"])
+def test_task_transaction(capture_events, init_huey, task_fails):
+    huey = init_huey()
+
+    @huey.task()
+    def division(a, b):
+        return a / b
+
+    events = capture_events()
+    execute_huey_task(
+        huey, division, 1, int(not task_fails), exceptions=(DivisionByZero,)
+    )
+
+    if task_fails:
+        error_event = events.pop(0)
+        assert error_event["exception"]["values"][0]["type"] == "ZeroDivisionError"
+
+    (event,) = events
+    assert event["type"] == "transaction"
+    assert event["transaction"] == "division"
+    assert event["transaction_info"] == {"source": "task"}
+
+    assert "huey_task_id" in event["tags"]
+    assert "huey_task_retry" in event["tags"]
