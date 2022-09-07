f0d94d5cb227d59454aad62b6156931744c720ee
Anton Pirker <anton.pirker@sentry.io>
2022-09-07 14:48:09 +0200

    Added even more tests

--- tests/integrations/asgi/test_asgi.py
+++ tests/integrations/asgi/test_asgi.py
@@ -1,6 +1,9 @@
 import sys
 
+from collections import Counter
+
 import pytest
+import sentry_sdk
 from sentry_sdk.integrations.asgi import SentryAsgiMiddleware, _looks_like_asgi3
 
 async_asgi_testclient = pytest.importorskip("async_asgi_testclient")
@@ -15,6 +18,9 @@ minimum_python_36 = pytest.mark.skipif(
 @pytest.fixture
 def asgi3_app():
     async def app(scope, receive, send):
+        if scope["type"] == "http" and scope["route"] == "/trigger/error":
+            division_by_zero = 1 / 0  # noqa
+
         await send(
             {
                 "type": "http.response.start",
@@ -137,6 +143,59 @@ async def test_capture_transaction_with_error(
 
 
 @minimum_python_36
+@pytest.mark.asyncio
+async def test_websocket(
+    sentry_init,
+    asgi3_app_with_error,
+    capture_events,
+    DictionaryContaining,  # noqa: N803
+):
+    sentry_init(send_default_pii=True, traces_sample_rate=1.0)
+    app = SentryAsgiMiddleware(asgi3_app_with_error)
+    print(app)
+    # TODO: implement
+
+
+@minimum_python_36
+@pytest.mark.asyncio
+async def test_auto_session_tracking_with_aggregates(
+    sentry_init, asgi3_app, capture_envelopes
+):
+    sentry_init(send_default_pii=True, traces_sample_rate=1.0)
+    app = SentryAsgiMiddleware(asgi3_app)
+
+    scope = {
+        "endpoint": asgi3_app,
+        "client": ("127.0.0.1", 60457),
+    }
+    with pytest.raises(ZeroDivisionError):
+        envelopes = capture_envelopes()
+        async with TestClient(app, scope=scope) as client:
+            scope["route"] = "/some/fine/url"
+            await client.get("/some/fine/url")
+            scope["route"] = "/some/fine/url"
+            await client.get("/some/fine/url")
+            scope["route"] = "/trigger/error"
+            await client.get("/trigger/error")
+
+    sentry_sdk.flush()
+
+    count_item_types = Counter()
+    for envelope in envelopes:
+        count_item_types[envelope.items[0].type] += 1
+
+    assert count_item_types["transaction"] == 4
+    assert count_item_types["event"] == 1
+    assert count_item_types["sessions"] == 1
+    assert len(envelopes) == 6
+
+    session_aggregates = envelopes[-1].items[0].payload.json["aggregates"]
+    assert session_aggregates[0]["exited"] == 3
+    assert session_aggregates[0]["crashed"] == 1
+    assert len(session_aggregates) == 1
+
+
+@minimum_python_36
 @pytest.mark.parametrize(
     "url,transaction_style,expected_transaction,expected_source",
     [
@@ -218,3 +277,127 @@ def test_looks_like_asgi3(asgi3_app):
     assert _looks_like_asgi3(asgi3)
     asgi2 = MockAsgi2App()
     assert not _looks_like_asgi3(asgi2)
+
+
+@minimum_python_36
+def test_get_ip_x_forwarded_for():
+    headers = [
+        (b"x-forwarded-for", b"8.8.8.8"),
+    ]
+    scope = {
+        "client": ("127.0.0.1", 60457),
+        "headers": headers,
+    }
+    middleware = SentryAsgiMiddleware({})
+    ip = middleware._get_ip(scope)
+    assert ip == "8.8.8.8"
+
+    # x-forwarded-for overrides x-real-ip
+    headers = [
+        (b"x-forwarded-for", b"8.8.8.8"),
+        (b"x-real-ip", b"10.10.10.10"),
+    ]
+    scope = {
+        "client": ("127.0.0.1", 60457),
+        "headers": headers,
+    }
+    middleware = SentryAsgiMiddleware({})
+    ip = middleware._get_ip(scope)
+    assert ip == "8.8.8.8"
+
+    # when multiple x-forwarded-for headers are, the first is taken
+    headers = [
+        (b"x-forwarded-for", b"5.5.5.5"),
+        (b"x-forwarded-for", b"6.6.6.6"),
+        (b"x-forwarded-for", b"7.7.7.7"),
+    ]
+    scope = {
+        "client": ("127.0.0.1", 60457),
+        "headers": headers,
+    }
+    middleware = SentryAsgiMiddleware({})
+    ip = middleware._get_ip(scope)
+    assert ip == "5.5.5.5"
+
+
+@minimum_python_36
+def test_get_ip_x_real_ip():
+    headers = [
+        (b"x-real-ip", b"10.10.10.10"),
+    ]
+    scope = {
+        "client": ("127.0.0.1", 60457),
+        "headers": headers,
+    }
+    middleware = SentryAsgiMiddleware({})
+    ip = middleware._get_ip(scope)
+    assert ip == "10.10.10.10"
+
+    # x-forwarded-for overrides x-real-ip
+    headers = [
+        (b"x-forwarded-for", b"8.8.8.8"),
+        (b"x-real-ip", b"10.10.10.10"),
+    ]
+    scope = {
+        "client": ("127.0.0.1", 60457),
+        "headers": headers,
+    }
+    middleware = SentryAsgiMiddleware({})
+    ip = middleware._get_ip(scope)
+    assert ip == "8.8.8.8"
+
+
+@minimum_python_36
+def test_get_ip():
+    # if now headers are provided the ip is taken from the client.
+    headers = []
+    scope = {
+        "client": ("127.0.0.1", 60457),
+        "headers": headers,
+    }
+    middleware = SentryAsgiMiddleware({})
+    ip = middleware._get_ip(scope)
+    assert ip == "127.0.0.1"
+
+    # x-forwarded-for header overides the ip from client
+    headers = [
+        (b"x-forwarded-for", b"8.8.8.8"),
+    ]
+    scope = {
+        "client": ("127.0.0.1", 60457),
+        "headers": headers,
+    }
+    middleware = SentryAsgiMiddleware({})
+    ip = middleware._get_ip(scope)
+    assert ip == "8.8.8.8"
+
+    # x-real-for header overides the ip from client
+    headers = [
+        (b"x-real-ip", b"10.10.10.10"),
+    ]
+    scope = {
+        "client": ("127.0.0.1", 60457),
+        "headers": headers,
+    }
+    middleware = SentryAsgiMiddleware({})
+    ip = middleware._get_ip(scope)
+    assert ip == "10.10.10.10"
+
+
+@minimum_python_36
+def test_get_headers():
+    headers = [
+        (b"x-real-ip", b"10.10.10.10"),
+        (b"some_header", b"123"),
+        (b"some_header", b"abc"),
+    ]
+    scope = {
+        "client": ("127.0.0.1", 60457),
+        "headers": headers,
+    }
+    middleware = SentryAsgiMiddleware({})
+    headers = middleware._get_headers(scope)
+    assert headers == {
+        "x-real-ip": "10.10.10.10",
+        "some_header": "123, abc",
+    }
