726e5c2de8d23fafa9b8637c869c43c528a2d7d5
Anton Pirker <anton.pirker@sentry.io>
2022-09-09 16:22:09 +0200

    Run general tests (not integration specific)

--- scripts/tox_env_name.py
+++ scripts/tox_env_name.py
@@ -19,7 +19,12 @@ def main():
     else:
         fr_version = "-" + framework_version
 
-    print(py_version + "-" + framework + fr_version)
+    if framework == "":
+        fr_name = ""
+    else:
+        fr_name = "-" + framework
+
+    print(py_version + fr_name + fr_version)
 
 
 if __name__ == "__main__":
