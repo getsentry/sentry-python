8d10f5e00e523efb31a876833abd3aed2e20837e
Anton Pirker <anton.pirker@sentry.io>
2022-09-15 16:11:45 +0200

    Cleanup

--- scripts/split-tox-gh-actions/split-tox-gh-actions.py
+++ scripts/split-tox-gh-actions/split-tox-gh-actions.py
@@ -15,8 +15,7 @@ FRAMEWORKS_NEEDING_POSTGRES = [
 MATRIX_DEFINITION = [
     "    strategy:",
     "\n      matrix:",
-    '\n        python-version: ["{{ python-version }}"]',
-    '\n        {{ framework }}-versions: ["{{ framework-version }}"]',
+    "\n        python-version: [{{ python-version }}]",
     "\n        os: [ubuntu-latest]",
 ]
 
@@ -45,9 +44,7 @@ SERVICE_POSTGRES = [
 def write_yaml_file(
     template,
     current_framework,
-    matrix_includes,
-    init_python_version,
-    init_framework_version,
+    python_versions,
 ):
     # render template for print
     out_lines = []
@@ -56,35 +53,17 @@ def write_yaml_file(
             h = MATRIX_DEFINITION.copy()
 
             for l in h:
-                l = (
-                    l.replace("{{ framework }}", current_framework)
-                    .replace(
-                        "{{ python-version }}", init_python_version.replace("py", "")
-                    )
-                    .replace("{{ framework-version }}", init_framework_version)
+                output_array = [f'"{py.replace("py", "")}"' for py in python_versions]
+                output_string = ",".join(output_array)
+
+                l = l.replace("{{ framework }}", current_framework).replace(
+                    "{{ python-version }}", output_string
                 )
                 out_lines.append(l)
 
-            # write matrix includes
-            if len(matrix_includes) > 0:
-                out_lines.append("\n        include:")
-
-            for matrix_include in matrix_includes:
-                out_lines.append(matrix_include)
         elif template_line == "{{ services }}\n":
             if current_framework in FRAMEWORKS_NEEDING_POSTGRES:
-                services = SERVICE_POSTGRES.copy()
-
-                for l in services:
-                    l = (
-                        l.replace("{{ framework }}", current_framework)
-                        .replace(
-                            "{{ python-version }}",
-                            init_python_version.replace("py", ""),
-                        )
-                        .replace("{{ framework-version }}", init_framework_version)
-                    )
-                    out_lines.append(l)
+                out_lines += SERVICE_POSTGRES
 
         else:
             out_lines.append(
@@ -110,7 +89,7 @@ def main():
     config.read(TOX_FILE)
     lines = [x for x in config["tox"]["envlist"].split("\n") if len(x) > 0]
 
-    everything = defaultdict(lambda: defaultdict(list))
+    python_versions = defaultdict(list)
 
     print("Parse tox.ini nevlist")
 
@@ -125,58 +104,26 @@ def main():
         try:
             # parse tox environment definition
             try:
-                (raw_python_versions, framework, raw_framework_versions) = line.split(
-                    "-"
-                )
+                (raw_python_versions, framework, _) = line.split("-")
             except ValueError:
-                (
-                    raw_python_versions,
-                    framework,
-                ) = line.split("-")
-                raw_framework_versions = ""
+                (raw_python_versions, framework) = line.split("-")
 
-            # collect framework versions per python version
+            # collect python versions to test the framework in
             for python_version in (
                 raw_python_versions.replace("{", "").replace("}", "").split(",")
             ):
-                if raw_framework_versions == "":
-                    everything[framework][python_version].append("latest")
-                    continue
-
-                for framework_version in (
-                    raw_framework_versions.replace("{", "").replace("}", "").split(",")
-                ):
-                    everything[framework][python_version].append(framework_version)
+                if python_version not in python_versions[framework]:
+                    python_versions[framework].append(python_version)
 
         except ValueError as err:
             print(f"ERROR reading line {line}")
 
-    matrix_includes = []
-    init_python_version = None
-    init_framework_version = None
-
-    for framework in everything:
-        for py in everything[framework]:
-            if not init_python_version and not init_framework_version:
-                init_python_version = py
-                init_framework_version = ",".join(everything[framework][py])
-
-            else:
-                fr_versions = ",".join(everything[framework][py])
-                s = f'\n          - {{ python-version: "{py.replace("py", "")}", {framework}-versions: "{fr_versions}", os: "ubuntu-latest" }}'
-                matrix_includes.append(s)
-
+    for framework in python_versions:
         write_yaml_file(
             template,
             framework,
-            matrix_includes,
-            init_python_version,
-            init_framework_version,
+            python_versions[framework],
         )
-
-        matrix_includes = []
-        init_python_version = None
-        init_framework_version = None
 
     print("All done. Have a nice day!")
 
