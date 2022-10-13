a6a1f1bba7c54fd8440b77a5a186c01791f1ede2
Tony Xiao <txiao@sentry.io>
2022-10-13 10:43:52 -0400

    move test

--- tests/test_profiler.py
+++ tests/test_profiler.py
@@ -17,35 +17,6 @@ unix_only = pytest.mark.skipif(
 )
 
 
-@pytest.mark.parametrize(
-    ("depth", "max_stack_depth", "actual_depth"),
-    [
-        pytest.param(1, 128, 1, id="less than"),
-        pytest.param(256, 128, 128, id="greater than"),
-        pytest.param(128, 128, 128, id="equals"),
-    ],
-)
-def test_extract_stack_with_max_depth(depth, max_stack_depth, actual_depth):
-    # introduce a lambda that we'll be looking for in the stack
-    frame = (lambda: get_frame(depth=depth))()
-
-    # plus 1 because we introduced a lambda intentionally that we'll
-    # look for in the final stack to make sure its in the right position
-    base_stack_depth = len(inspect.stack()) + 1
-
-    # increase the max_depth by the `base_stack_depth` to account
-    # for the extra frames pytest will add
-    stack = extract_stack(frame, max_stack_depth + base_stack_depth)
-    assert len(stack) == base_stack_depth + actual_depth
-
-    for i in range(actual_depth):
-        assert stack[i].name == "get_frame", i
-
-    # index 0 contains the inner most frame on the stack, so the lamdba
-    # should be at index `actual_depth`
-    assert stack[actual_depth].name == "<lambda>", actual_depth
-
-
 @minimum_python_33
 def test_profiler_invalid_mode(teardown_profiling):
     with pytest.raises(ValueError):
@@ -150,3 +121,32 @@ class GetFrame:
 )
 def test_get_frame_name(frame, frame_name):
     assert get_frame_name(frame) == frame_name
+
+
+@pytest.mark.parametrize(
+    ("depth", "max_stack_depth", "actual_depth"),
+    [
+        pytest.param(1, 128, 1, id="less than"),
+        pytest.param(256, 128, 128, id="greater than"),
+        pytest.param(128, 128, 128, id="equals"),
+    ],
+)
+def test_extract_stack_with_max_depth(depth, max_stack_depth, actual_depth):
+    # introduce a lambda that we'll be looking for in the stack
+    frame = (lambda: get_frame(depth=depth))()
+
+    # plus 1 because we introduced a lambda intentionally that we'll
+    # look for in the final stack to make sure its in the right position
+    base_stack_depth = len(inspect.stack()) + 1
+
+    # increase the max_depth by the `base_stack_depth` to account
+    # for the extra frames pytest will add
+    stack = extract_stack(frame, max_stack_depth + base_stack_depth)
+    assert len(stack) == base_stack_depth + actual_depth
+
+    for i in range(actual_depth):
+        assert stack[i].name == "get_frame", i
+
+    # index 0 contains the inner most frame on the stack, so the lamdba
+    # should be at index `actual_depth`
+    assert stack[actual_depth].name == "<lambda>", actual_depth
