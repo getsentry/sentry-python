b575bfcf72e01fb3d34391e270ab6253cf36b184
Anton Pirker <anton.pirker@sentry.io>
2022-10-21 09:02:46 +0200

    Added first version of a span processor

--- sentry_sdk/integrations/opentelemetry/span_processor.py
+++ sentry_sdk/integrations/opentelemetry/span_processor.py
@@ -1,9 +1,50 @@
 from opentelemetry.sdk.trace import SpanProcessor
 
+from sentry_sdk.hub import Hub
+from sentry_sdk.tracing import Transaction
+
 
 class SentrySpanProcessor(SpanProcessor):
-    def on_start(self, span, parent_context=None):
-        print(f"[] in on_start {span}")
+    otel_span_map = {}
 
-    def on_end(self, span):
-        print(f"~ in on_end {span}")
+    def on_start(self, otel_span, parent_context=None):
+        print(f"[SENTRY/OTEL] in on_start {otel_span}")
+        hub = Hub.current
+        sentry_span = None
+        parent_sentry_span = hub.scope.span
+
+        if parent_sentry_span:
+            sentry_span = parent_sentry_span.start_child(op=otel_span.name)
+        else:
+            # TODO: add the baggagae sentry-trace stuff to transaction
+            sentry_span = hub.start_transaction(name=otel_span.name)
+
+        hub.scope.span = sentry_span
+        self.otel_span_map[otel_span.context.span_id] = (
+            sentry_span,
+            parent_sentry_span,
+        )
+
+    def on_end(self, otel_span):
+        print(f"[SENTRY/OTEL] in on_end {otel_span}")
+        hub = Hub.current
+        sentry_span, parent_sentry_span = self.otel_span_map[otel_span.context.span_id]
+
+        if not sentry_span:
+            return
+
+        sentry_span.op = otel_span.name
+        if isinstance(sentry_span, Transaction):
+            hub.scope.set_transaction_name(otel_span.name)
+
+        for key in otel_span.attributes:
+            val = otel_span.attributes[key]
+            sentry_span.set_data(key, val)
+
+            if key == "db.statement":
+                sentry_span.description = val
+
+        sentry_span.finish()
+
+        if parent_sentry_span:
+            hub.scope.span = parent_sentry_span
