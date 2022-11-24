73307ec781bf5dddd7076cef743b16fbe9cf5350
Anton Pirker <anton.pirker@sentry.io>
2022-11-24 15:50:53 +0100

    Test extract_sentry_trace

--- tests/test_tracing.py
+++ tests/test_tracing.py
@@ -1,9 +1,19 @@
-# @classmethod
-# def extract_sentry_trace(cls, sentry_trace):
-#     # type: (str) -> Tuple[str, str, bool]
-#     trace_id, parent_span_id, parent_sampled = sentry_trace.split("-")
-#     parent_sampled = True if parent_sampled == "1" else False
-#     return (trace_id, parent_span_id, parent_sampled)
+from sentry_sdk.tracing import Transaction
 
 
-# Transaction.extract_sentry_trace(sentry_trace[0])
+def test_extract_sentry_trace():
+    # type: () -> None
+
+    trace_id, parent_span_id, parent_sampled = Transaction.extract_sentry_trace(
+        "4c79f60c11214eb38604f4ae0781bfb2-fa90fdead5f74052-1"
+    )
+    assert trace_id == "4c79f60c11214eb38604f4ae0781bfb2"
+    assert parent_span_id == "fa90fdead5f74052"
+    assert parent_sampled
+
+    trace_id, parent_span_id, parent_sampled = Transaction.extract_sentry_trace(
+        "5e79f60c11214eb38604f4ae0781bfb2-0390fdead5f74052-0"
+    )
+    assert trace_id == "5e79f60c11214eb38604f4ae0781bfb2"
+    assert parent_span_id == "0390fdead5f74052"
+    assert not parent_sampled
