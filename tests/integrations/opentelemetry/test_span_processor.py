17f0346b1ef596b8f4f99da2e4c8b0b53081376f
Anton Pirker <anton.pirker@sentry.io>
2022-11-28 10:34:18 +0100

    Updated assert commands

--- tests/integrations/opentelemetry/test_span_processor.py
+++ tests/integrations/opentelemetry/test_span_processor.py
@@ -298,11 +298,7 @@ def test_on_end_sentry_transaction():
 
     fake_sentry_span.set_context.assert_called_once()
     span_processor._update_span_with_otel_data.assert_not_called()
-    fake_sentry_span.finish.assert_called_once_with(
-        end_timestamp=datetime.fromtimestamp(
-            otel_span.entest_on_end_sentry_Span_time / 1e9
-        )
-    )
+    fake_sentry_span.finish.assert_called_once_with()
 
 
 def test_on_end_sentry_span():
@@ -330,6 +326,4 @@ def test_on_end_sentry_span():
     span_processor._update_span_with_otel_data.assert_called_once_with(
         fake_sentry_span, otel_span
     )
-    fake_sentry_span.finish.assert_called_once_with(
-        end_timestamp=datetime.fromtimestamp(otel_span.end_time / 1e9)
-    )
+    fake_sentry_span.finish.assert_called_once()
