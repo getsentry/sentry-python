1d3d61e335ee2493cf82c7fc19ff94eb8d289f96
Anton Pirker <anton.pirker@sentry.io>
2022-11-30 14:48:52 +0100

    Added extract_sentry_trace to parse traceparent string into trace information

--- sentry_sdk/tracing.py
+++ sentry_sdk/tracing.py
@@ -372,6 +372,13 @@ class Span(object):
             sampled = "0"
         return "%s-%s-%s" % (self.trace_id, self.span_id, sampled)
 
+    @classmethod
+    def extract_sentry_trace(cls, sentry_trace):
+        # type: (str) -> Tuple[str, str, bool]
+        trace_id, parent_span_id, parent_sampled = sentry_trace.split("-")
+        parent_sampled = True if parent_sampled == "1" else False
+        return (trace_id, parent_span_id, parent_sampled)
+
     def to_tracestate(self):
         # type: () -> Optional[str]
         """
