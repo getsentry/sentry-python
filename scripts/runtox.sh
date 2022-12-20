d6d04adcfabfee61a7cc7fae48e179f59f9dfe14
Anton Pirker <anton.pirker@sentry.io>
2022-12-20 21:20:46 +0100

    Print tox version in use

--- scripts/runtox.sh
+++ scripts/runtox.sh
@@ -15,7 +15,7 @@ fi
 
 searchstring="$1"
 
-echo "Using tox executable: $TOXPATH"
+$TOXPATH --version
 
 export TOX_PARALLEL_NO_SPINNER=1
 exec $TOXPATH -p auto -e "$($TOXPATH -l | grep "$searchstring" | tr $'\n' ',')" -- "${@:2}"
