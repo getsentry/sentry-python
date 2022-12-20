e093ea37545d2a0f1a7ccfb73802dfb8c04c011d
Anton Pirker <anton.pirker@sentry.io>
2022-12-20 13:51:43 +0100

    Trying to run Python 3.11 tests in ubuntu-latest

--- scripts/split-tox-gh-actions/split-tox-gh-actions.py
+++ scripts/split-tox-gh-actions/split-tox-gh-actions.py
@@ -40,6 +40,9 @@ MATRIX_DEFINITION = """
         # ubuntu-20.04 is the last version that supported python3.6
         # see https://github.com/actions/setup-python/issues/544#issuecomment-1332535877
         os: [ubuntu-20.04]
+        include:
+            - python-version: 3.11
+              os: ubuntu-latest
 """
 
 
