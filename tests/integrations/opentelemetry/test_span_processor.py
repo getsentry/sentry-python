2bbfeb8bcae65650eb19796b65eaabfb2ab9bc52
Abhijeet Prasad <aprasad@sentry.io>
2023-01-18 13:07:00 +0100

    adjust other tests

--- tests/integrations/opentelemetry/test_span_processor.py
+++ tests/integrations/opentelemetry/test_span_processor.py
@@ -121,7 +121,9 @@ def test_get_trace_data_with_sentry_trace():
     with mock.patch(
         "sentry_sdk.integrations.opentelemetry.span_processor.get_value",
         side_effect=[
-            ("1234567890abcdef1234567890abcdef", "1234567890abcdef", False),
+            extract_sentrytrace_data(
+                "1234567890abcdef1234567890abcdef-1234567890abcdef-0"
+            ),
             None,
         ],
     ):
@@ -153,7 +155,9 @@ def test_get_trace_data_with_sentry_trace_and_baggage():
     with mock.patch(
         "sentry_sdk.integrations.opentelemetry.span_processor.get_value",
         side_effect=[
-            ("1234567890abcdef1234567890abcdef", "1234567890abcdef", True),
+            extract_sentrytrace_data(
+                "1234567890abcdef1234567890abcdef-1234567890abcdef-1"
+            ),
             baggage,
         ],
     ):
