51ab32d37483ef2dcd4121dfe5be00d3a69b9384
Anton Pirker <anton.pirker@sentry.io>
2023-02-15 13:40:18 +0100

    Moved import to the bottom of file to prevent circular import

--- sentry_sdk/utils.py
+++ sentry_sdk/utils.py
@@ -392,7 +392,6 @@ class AnnotatedValue(object):
     def substituted_because_contains_sensitive_data(cls):
         # type: () -> AnnotatedValue
         """The actual value was removed because it contained sensitive information."""
-        from sentry_sdk.consts import SENSITIVE_DATA_SUBSTITUTE
 
         return AnnotatedValue(
             value=SENSITIVE_DATA_SUBSTITUTE,
@@ -1192,8 +1191,6 @@ def sanitize_url(url, remove_authority=True, remove_query_values=True):
     parsed_url = urlsplit(url)
     query_params = parse_qs(parsed_url.query, keep_blank_values=True)
 
-    from sentry_sdk.consts import SENSITIVE_DATA_SUBSTITUTE
-
     # strip username:password (netloc can be usr:pwd@example.com)
     if remove_authority:
         netloc_parts = parsed_url.netloc.split("@")
@@ -1273,3 +1270,7 @@ else:
     def nanosecond_time():
         # type: () -> int
         raise AttributeError
+
+
+# prevent circular import
+from sentry_sdk.consts import SENSITIVE_DATA_SUBSTITUTE
