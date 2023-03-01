696ac7182cd8b837192f771d33b726a3174540e0
Anton Pirker <anton.pirker@sentry.io>
2023-03-01 17:47:10 +0100

    Tinkering around

---
+++ sentry_sdk/crons.py
@@ -0,0 +1,59 @@
+from functools import wraps
+import sys
+import uuid
+
+from sentry_sdk import Hub
+from sentry_sdk._compat import reraise
+
+
+class MONITOR_STATUS:
+    IN_PROGRESS = "in_progress"
+    OK = "ok"
+    ERROR = "error"
+
+
+def _create_checkin_event(monitor_id=None, status=None):
+    event = {
+        "type": "check_in",
+        "monitor_id": monitor_id,
+        "check_in_id": uuid.uuid4().hex,
+        "status": status,
+    }
+
+    return event
+
+
+def _capture_monitor_event(monitor_id=None, status=None):
+    hub = Hub.current
+    monitor_event = _create_checkin_event(monitor_id, status)
+    hub.capture_event(monitor_event)
+
+    # self.capture_envelope(Envelope(items=[client_report]))
+
+
+def monitor(monitor_id=None):
+    def decorate(func):
+        if not monitor_id:
+            return func
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            _capture_monitor_event(
+                monitor_id=monitor_id, status=MONITOR_STATUS.IN_PROGRESS
+            )
+
+            try:
+                result = func(*args, **kwargs)
+            except Exception:
+                _capture_monitor_event(
+                    monitor_id=monitor_id, status=MONITOR_STATUS.ERROR
+                )
+                exc_info = sys.exc_info()
+                reraise(*exc_info)
+
+            _capture_monitor_event(monitor_id=monitor_id, status=MONITOR_STATUS.OK)
+            return result
+
+        return wrapper
+
+    return decorate
