3e0e7698148dd287f0e3dfb6cb6aa27cd91a1250
Anton Pirker <anton.pirker@sentry.io>
2023-03-08 12:44:11 +0100

    Fixed typing

--- sentry_sdk/tracing.py
+++ sentry_sdk/tracing.py
@@ -823,9 +823,9 @@ def trace(func=None):
             ...
     """
     if PY2:
-        from sentry_sdk.tracing_utils_python2 import start_child_span_decorator
+        from sentry_sdk.tracing_utils_python2 import start_child_span_decorator  # type: ignore
     else:
-        from sentry_sdk.tracing_utils_python3 import start_child_span_decorator
+        from sentry_sdk.tracing_utils_python3 import start_child_span_decorator  # type: ignore
 
     # This patterns allows usage of both @sentry_traced and @sentry_traced(...)
     # See https://stackoverflow.com/questions/52126071/decorator-with-arguments-avoid-parenthesis-when-no-arguments/52126278
