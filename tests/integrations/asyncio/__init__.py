cefd2bc4b21e5413e46682ffe4daf549561894e5
Anton Pirker <anton.pirker@sentry.io>
2023-03-15 13:28:09 +0100

    Run asyncio tests only in python >3.6

---
+++ tests/integrations/asyncio/__init__.py
@@ -0,0 +1,7 @@
+# import os
+# import pytest 
+
+# test_path = os.environ.get("TESTPATH")
+
+# if "asyncio" not in test_path:
+#     pytest.skip()
--- tests/integrations/asyncio/test_asyncio_py3.py
+++ tests/integrations/asyncio/test_asyncio_py3.py
@@ -2,16 +2,14 @@ import asyncio
 import sys
 
 import pytest
-import pytest_asyncio
 
 import sentry_sdk
 from sentry_sdk.consts import OP
 from sentry_sdk.integrations.asyncio import AsyncioIntegration
 
 
-
-minimum_python_36 = pytest.mark.skipif(
-    sys.version_info < (3, 6), reason="Asyncio tests need Python >= 3.6"
+minimum_python_37 = pytest.mark.skipif(
+    sys.version_info < (3, 7), reason="Asyncio tests need Python >= 3.7"
 )
 
 
@@ -35,7 +33,7 @@ def event_loop(request):
     loop.close()
 
 
-@minimum_python_36
+@minimum_python_37
 @pytest.mark.asyncio
 async def test_create_task(
     sentry_init,
@@ -80,7 +78,7 @@ async def test_create_task(
     )
 
 
-@minimum_python_36
+@minimum_python_37
 @pytest.mark.asyncio
 async def test_gather(
     sentry_init,
@@ -123,7 +121,7 @@ async def test_gather(
     )
 
 
-@minimum_python_36
+@minimum_python_37
 @pytest.mark.asyncio
 async def test_exception(
     sentry_init,
@@ -158,7 +156,7 @@ async def test_exception(
     assert error_event["exception"]["values"][0]["mechanism"]["type"] == "asyncio"
 
 
-@minimum_python_36
+@minimum_python_37
 @pytest.mark.asyncio
 async def test_task_result(sentry_init):
     sentry_init(
