2308400e236b1e093179591f068abdf73bac6663
Anton Pirker <anton.pirker@sentry.io>
2023-03-17 08:43:39 +0100

    Better naming

--- sentry_sdk/tracing_utils_py3.py
+++ sentry_sdk/tracing_utils_py3.py
@@ -30,17 +30,17 @@ def start_child_span_decorator(func):
         async def func_with_tracing(*args, **kwargs):
             # type: (*Any, **Any) -> Any
 
-            span_or_trx = get_current_span(sentry_sdk.Hub.current)
+            span = get_current_span(sentry_sdk.Hub.current)
 
-            if span_or_trx is None:
+            if span is None:
                 logger.warning(
-                    "No transaction found. Not creating a child span for %s. "
+                    "Can not create a child span for %s. "
                     "Please start a Sentry transaction before calling this function.",
                     qualname_from_function(func),
                 )
                 return await func(*args, **kwargs)
 
-            with span_or_trx.start_child(
+            with span.start_child(
                 op=OP.FUNCTION,
                 description=qualname_from_function(func),
             ):
@@ -53,17 +53,17 @@ def start_child_span_decorator(func):
         def func_with_tracing(*args, **kwargs):
             # type: (*Any, **Any) -> Any
 
-            span_or_trx = get_current_span(sentry_sdk.Hub.current)
+            span = get_current_span(sentry_sdk.Hub.current)
 
-            if span_or_trx is None:
+            if span is None:
                 logger.warning(
-                    "No transaction found. Not creating a child span for %s. "
+                    "Can not create a child span for %s. "
                     "Please start a Sentry transaction before calling this function.",
                     qualname_from_function(func),
                 )
                 return func(*args, **kwargs)
 
-            with span_or_trx.start_child(
+            with span.start_child(
                 op=OP.FUNCTION,
                 description=qualname_from_function(func),
             ):
