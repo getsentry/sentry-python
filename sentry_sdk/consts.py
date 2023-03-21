b339d838223ad179dbaf6ddbd979e482bfa73023
Neel Shah <neel.shah@sentry.io>
2023-03-21 11:07:51 +0100

    Implement EventScrubber (#1943)
    As outlined in https://github.com/getsentry/rfcs/blob/main/text/0062-controlling-pii-and-credentials-in-sd-ks.md
    
    Co-authored-by: Anton Pirker <anton.pirker@sentry.io>

--- sentry_sdk/consts.py
+++ sentry_sdk/consts.py
@@ -133,6 +133,7 @@ class ClientConstructor(object):
         trace_propagation_targets=[  # noqa: B006
             MATCH_ALL
         ],  # type: Optional[Sequence[str]]
+        event_scrubber=None,  # type: Optional[sentry_sdk.scrubber.EventScrubber]
     ):
         # type: (...) -> None
         pass
