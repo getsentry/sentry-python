dc730ed953ffe00ad72e0a1c29e11b2caf4afe7f
Anton Pirker <anton.pirker@sentry.io>
2023-03-28 08:33:30 +0200

    Added new functions_to_trace option for celtral way of performance instrumentation (#1960)
    Have a list of functions that can be passed to "sentry_sdk.init()". When the SDK starts it goes through the list and instruments all the functions in the list.
    
    functions_to_trace = [
        {"qualified_name": "tests.test_basics._hello_world_counter"},
        {"qualified_name": "time.sleep"},
        {"qualified_name": "collections.Counter.most_common"},
    ]
    
    sentry_sdk.init(
        dsn="...",
        traces_sample_rate=1.0,
        functions_to_trace=functions_to_trace,
    )

--- sentry_sdk/consts.py
+++ sentry_sdk/consts.py
@@ -133,6 +133,7 @@ class ClientConstructor(object):
         trace_propagation_targets=[  # noqa: B006
             MATCH_ALL
         ],  # type: Optional[Sequence[str]]
+        functions_to_trace=[],  # type: Sequence[str]  # noqa: B006
         event_scrubber=None,  # type: Optional[sentry_sdk.scrubber.EventScrubber]
     ):
         # type: (...) -> None
