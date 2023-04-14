bbba136e8e1a59ec66a998d90001d62b13cdb75e
Anton Pirker <anton.pirker@sentry.io>
2023-04-14 14:25:54 +0200

    Removed check for errors are not allowed to be in envelopes

--- tests/conftest.py
+++ tests/conftest.py
@@ -157,11 +157,15 @@ def monkeypatch_test_transport(monkeypatch, validate_event_schema):
 
     def check_envelope(envelope):
         with capture_internal_exceptions():
-            # Assert error events are sent without envelope to server, for compat.
-            # This does not apply if any item in the envelope is an attachment.
-            if not any(x.type == "attachment" for x in envelope.items):
-                assert not any(item.data_category == "error" for item in envelope.items)
-                assert not any(item.get_event() is not None for item in envelope.items)
+            pass
+            # # Assert error events are sent without envelope to server, for compat.
+            # # This does not apply if any item in the envelope is an attachment.
+            # envelope_contains_attachments = any(x.type == "attachment" for x in envelope.items)
+            # if not envelope_contains_attachments:
+            #     import ipdb
+            #     ipdb.set_trace()
+            #     assert not any(item.data_category == "error" for item in envelope.items)
+            #     assert not any(item.get_event() is not None for item in envelope.items)
 
     def inner(client):
         monkeypatch.setattr(
