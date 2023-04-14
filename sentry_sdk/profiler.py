5fdfa824e0e79775c7c1784c3c11504eba0f0506
Tony Xiao <txiao@sentry.io>
2023-04-14 15:30:01 -0400

    chore(profiling): Remove profile context from sdk
    The profile context can be populated by the relay automatically. No need to do
    this in the SDK. This also means that if the profile has to be dropped by relay
    due to rate limits or any reason, we won't render a bad link on the transaction
    to a non-existent profile.

--- sentry_sdk/profiler.py
+++ sentry_sdk/profiler.py
@@ -575,10 +575,6 @@ class Profile(object):
                 )
             )
 
-    def get_profile_context(self):
-        # type: () -> ProfileContext
-        return {"profile_id": self.event_id}
-
     def start(self):
         # type: () -> None
         if not self.sampled or self.active:
