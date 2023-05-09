86b6f7d500682a7dd92c5a1cc55e27529ef592ca
Ivana Kellyerova <ivana.kellyerova@sentry.io>
2023-05-09 14:45:37 +0200

    add test

--- tests/integrations/modules/test_modules.py
+++ tests/integrations/modules/test_modules.py
@@ -1,6 +1,6 @@
 import sentry_sdk
 
-from sentry_sdk.integrations.modules import ModulesIntegration
+from sentry_sdk.integrations.modules import ModulesIntegration, _get_installed_modules
 
 
 def test_basic(sentry_init, capture_events):
@@ -12,3 +12,30 @@ def test_basic(sentry_init, capture_events):
     (event,) = events
     assert "sentry-sdk" in event["modules"]
     assert "pytest" in event["modules"]
+
+
+def test_installed_modules():
+    try:
+        from importlib import distributions, version
+
+        importlib_available = True
+    except ImportError:
+        importlib_available = False
+
+    try:
+        import pkg_resources
+
+        pkg_resources_available = True
+    except ImportError:
+        pkg_resources_available = False
+
+    installed_modules = _get_installed_modules()
+    if importlib_available:
+        assert installed_modules == [
+            (dist.metadata["Name"].lower(), version(dist.metadata["Name"]))
+            for dist in distributions()
+        ]
+    if pkg_resources_available:
+        assert installed_modules == [
+            (dist.key, dist.version) for dist in pkg_resources.working_set
+        ]
