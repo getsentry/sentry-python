ac850ecc82dd7ed98ce3a20ff7f677a56d320b88
Anton Pirker <anton.pirker@sentry.io>
2023-05-17 11:58:56 +0200

    Fixed tests

--- tests/test_exceptiongroup.py
+++ tests/test_exceptiongroup.py
@@ -190,7 +190,7 @@ def test_exceptiongroup_simple():
     }
     frame = exception_values[1]["stacktrace"]["frames"][0]
     assert frame["module"] == "tests.test_exceptiongroup"
-    assert frame["lineno"] == 150
+    assert frame["lineno"] == 151
     assert frame["context_line"] == "        raise ExceptionGroup("
 
 
