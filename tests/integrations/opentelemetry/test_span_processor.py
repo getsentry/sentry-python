6a3da24aa9d814da3bb81d408010394376c85fad
Anton Pirker <anton.pirker@sentry.io>
2023-06-05 14:48:18 +0200

    Fixed tests

--- tests/integrations/opentelemetry/test_span_processor.py
+++ tests/integrations/opentelemetry/test_span_processor.py
@@ -178,7 +178,7 @@ def test_update_span_with_otel_data_http_method():
     otel_span.kind = SpanKind.CLIENT
     otel_span.attributes = {
         "http.method": "GET",
-        "http.response.status_code": 429,
+        "http.status_code": 429,
         "http.status_text": "xxx",
         "http.user_agent": "curl/7.64.1",
         "net.peer.name": "example.com",
@@ -208,7 +208,7 @@ def test_update_span_with_otel_data_http_method2():
     otel_span.kind = SpanKind.SERVER
     otel_span.attributes = {
         "http.method": "GET",
-        "http.response.status_code": 429,
+        "http.status_code": 429,
         "http.status_text": "xxx",
         "http.user_agent": "curl/7.64.1",
         "http.url": "https://example.com/status/403?password=123&username=test@example.com&author=User123&auth=1234567890abcdef",
