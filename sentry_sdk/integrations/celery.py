127c2369d89612b3f202a9df7932d715a949b17c
yuganshnetsolutions <120713747+yuganshnetsolutions@users.noreply.github.com>
2023-07-14 16:47:31 +0530

    Update celery.py

--- sentry_sdk/integrations/celery.py
+++ sentry_sdk/integrations/celery.py
@@ -1,34 +1,21 @@
 from __future__ import absolute_import
 
 import sys
-import time
 
-from sentry_sdk.api import continue_trace
-from sentry_sdk.consts import OP
-from sentry_sdk._compat import reraise
-from sentry_sdk._functools import wraps
-from sentry_sdk.crons import capture_checkin, MonitorStatus
 from sentry_sdk.hub import Hub
+from sentry_sdk.utils import capture_internal_exceptions, event_from_exception
+from sentry_sdk.tracing import Transaction
+from sentry_sdk._compat import reraise
 from sentry_sdk.integrations import Integration, DidNotEnable
 from sentry_sdk.integrations.logging import ignore_logger
-from sentry_sdk.tracing import BAGGAGE_HEADER_NAME, TRANSACTION_SOURCE_TASK
-from sentry_sdk._types import TYPE_CHECKING
-from sentry_sdk.utils import (
-    capture_internal_exceptions,
-    event_from_exception,
-    logger,
-    match_regex_list,
-)
+from sentry_sdk._types import MYPY
+from sentry_sdk._functools import wraps
 
-if TYPE_CHECKING:
+if MYPY:
     from typing import Any
-    from typing import Callable
-    from typing import Dict
-    from typing import List
-    from typing import Optional
-    from typing import Tuple
     from typing import TypeVar
-    from typing import Union
+    from typing import Callable
+    from typing import Optional
 
     from sentry_sdk._types import EventProcessor, Event, Hint, ExcInfo
 
@@ -37,20 +24,11 @@ if TYPE_CHECKING:
 
 try:
     from celery import VERSION as CELERY_VERSION  # type: ignore
-    from celery import Task, Celery
-    from celery.app.trace import task_has_custom
-    from celery.beat import Scheduler  # type: ignore
     from celery.exceptions import (  # type: ignore
+        SoftTimeLimitExceeded,
+        Retry,
         Ignore,
         Reject,
-        Retry,
-        SoftTimeLimitExceeded,
-    )
-    from celery.schedules import crontab, schedule  # type: ignore
-    from celery.signals import (  # type: ignore
-        task_failure,
-        task_success,
-        task_retry,
     )
 except ImportError:
     raise DidNotEnable("Celery not installed")
@@ -62,20 +40,9 @@ CELERY_CONTROL_FLOW_EXCEPTIONS = (Retry, Ignore, Reject)
 class CeleryIntegration(Integration):
     identifier = "celery"
 
-    def __init__(
-        self,
-        propagate_traces=True,
-        monitor_beat_tasks=False,
-        exclude_beat_tasks=None,
-    ):
-        # type: (bool, bool, Optional[List[str]]) -> None
+    def __init__(self, propagate_traces=True):
+        # type: (bool) -> None
         self.propagate_traces = propagate_traces
-        self.monitor_beat_tasks = monitor_beat_tasks
-        self.exclude_beat_tasks = exclude_beat_tasks
-
-        if monitor_beat_tasks:
-            _patch_beat_apply_entry()
-            _setup_celery_beat_signals()
 
     @staticmethod
     def setup_once():
@@ -90,12 +57,11 @@ class CeleryIntegration(Integration):
         def sentry_build_tracer(name, task, *args, **kwargs):
             # type: (Any, Any, *Any, **Any) -> Any
             if not getattr(task, "_sentry_is_patched", False):
-                # determine whether Celery will use __call__ or run and patch
-                # accordingly
-                if task_has_custom(task, "__call__"):
-                    type(task).__call__ = _wrap_task_call(task, type(task).__call__)
-                else:
-                    task.run = _wrap_task_call(task, task.run)
+                # Need to patch both methods because older celery sometimes
+                # short-circuits to task.run if it thinks it's safe.
+                task.__call__ = _wrap_task_call(task, task.__call__)
+                task.run = _wrap_task_call(task, task.run)
+                task.apply_async = _wrap_apply_async(task, task.apply_async)
 
                 # `build_tracer` is apparently called for every task
                 # invocation. Can't wrap every celery task for every invocation
@@ -105,10 +71,6 @@ class CeleryIntegration(Integration):
             return _wrap_tracer(task, old_build_tracer(name, task, *args, **kwargs))
 
         trace.build_tracer = sentry_build_tracer
-
-        from celery.app.task import Task  # type: ignore
-
-        Task.apply_async = _wrap_apply_async(Task.apply_async)
 
         _patch_worker_exit()
 
@@ -123,74 +85,30 @@ class CeleryIntegration(Integration):
         ignore_logger("celery.redirected")
 
 
-def _now_seconds_since_epoch():
-    # type: () -> float
-    # We cannot use `time.perf_counter()` when dealing with the duration
-    # of a Celery task, because the start of a Celery task and
-    # the end are recorded in different processes.
-    # Start happens in the Celery Beat process,
-    # the end in a Celery Worker process.
-    return time.time()
-
-
-def _wrap_apply_async(f):
-    # type: (F) -> F
+def _wrap_apply_async(task, f):
+    # type: (Any, F) -> F
     @wraps(f)
     def apply_async(*args, **kwargs):
         # type: (*Any, **Any) -> Any
         hub = Hub.current
         integration = hub.get_integration(CeleryIntegration)
         if integration is not None and integration.propagate_traces:
-            with hub.start_span(
-                op=OP.QUEUE_SUBMIT_CELERY, description=args[0].name
-            ) as span:
+            with hub.start_span(op="celery.submit", description=task.name):
                 with capture_internal_exceptions():
-                    headers = dict(hub.iter_trace_propagation_headers(span))
-                    if integration.monitor_beat_tasks:
-                        headers.update(
-                            {
-                                "sentry-monitor-start-timestamp-s": "%.9f"
-                                % _now_seconds_since_epoch(),
-                            }
-                        )
-
+                    headers = dict(hub.iter_trace_propagation_headers())
                     if headers:
-                        # Note: kwargs can contain headers=None, so no setdefault!
-                        # Unsure which backend though.
-                        kwarg_headers = kwargs.get("headers") or {}
-
-                        existing_baggage = kwarg_headers.get(BAGGAGE_HEADER_NAME)
-                        sentry_baggage = headers.get(BAGGAGE_HEADER_NAME)
-
-                        combined_baggage = sentry_baggage or existing_baggage
-                        if sentry_baggage and existing_baggage:
-                            combined_baggage = "{},{}".format(
-                                existing_baggage,
-                                sentry_baggage,
-                            )
-
+                        kwarg_headers = kwargs.setdefault("headers", {})
                         kwarg_headers.update(headers)
-                        if combined_baggage:
-                            kwarg_headers[BAGGAGE_HEADER_NAME] = combined_baggage
 
                         # https://github.com/celery/celery/issues/4875
                         #
                         # Need to setdefault the inner headers too since other
                         # tracing tools (dd-trace-py) also employ this exact
                         # workaround and we don't want to break them.
+                        #
+                        # This is not reproducible outside of AMQP, therefore no
+                        # tests!
                         kwarg_headers.setdefault("headers", {}).update(headers)
-                        if combined_baggage:
-                            kwarg_headers["headers"][
-                                BAGGAGE_HEADER_NAME
-                            ] = combined_baggage
-
-                        # Add the Sentry options potentially added in `sentry_apply_entry`
-                        # to the headers (done when auto-instrumenting Celery Beat tasks)
-                        for key, value in kwarg_headers.items():
-                            if key.startswith("sentry-"):
-                                kwarg_headers["headers"][key] = value
-
-                        kwargs["headers"] = kwarg_headers
 
                 return f(*args, **kwargs)
         else:
@@ -225,30 +143,19 @@ def _wrap_tracer(task, f):
             # Celery task objects are not a thing to be trusted. Even
             # something such as attribute access can fail.
             with capture_internal_exceptions():
-                transaction = continue_trace(
+                transaction = Transaction.continue_from_headers(
                     args[3].get("headers") or {},
-                    op=OP.QUEUE_TASK_CELERY,
+                    op="celery.task",
                     name="unknown celery task",
-                    source=TRANSACTION_SOURCE_TASK,
                 )
+
                 transaction.name = task.name
                 transaction.set_status("ok")
 
             if transaction is None:
                 return f(*args, **kwargs)
 
-            with hub.start_transaction(
-                transaction,
-                custom_sampling_context={
-                    "celery_job": {
-                        "task": task.name,
-                        # for some reason, args[1] is a list if non-empty but a
-                        # tuple if empty
-                        "args": list(args[1]),
-                        "kwargs": args[2],
-                    }
-                },
-            ):
+            with hub.start_transaction(transaction):
                 return f(*args, **kwargs)
 
     return _inner  # type: ignore
@@ -362,203 +269,3 @@ def _patch_worker_exit():
                     hub.flush()
 
     Worker.workloop = sentry_workloop
-
-
-def _get_headers(task):
-    # type: (Task) -> Dict[str, Any]
-    headers = task.request.get("headers") or {}
-
-    # flatten nested headers
-    if "headers" in headers:
-        headers.update(headers["headers"])
-        del headers["headers"]
-
-    headers.update(task.request.get("properties") or {})
-
-    return headers
-
-
-def _get_humanized_interval(seconds):
-    # type: (float) -> Tuple[int, str]
-    TIME_UNITS = (  # noqa: N806
-        ("day", 60 * 60 * 24.0),
-        ("hour", 60 * 60.0),
-        ("minute", 60.0),
-    )
-
-    seconds = float(seconds)
-    for unit, divider in TIME_UNITS:
-        if seconds >= divider:
-            interval = int(seconds / divider)
-            return (interval, unit)
-
-    return (int(seconds), "second")
-
-
-def _get_monitor_config(celery_schedule, app):
-    # type: (Any, Celery) -> Dict[str, Any]
-    monitor_config = {}  # type: Dict[str, Any]
-    schedule_type = None  # type: Optional[str]
-    schedule_value = None  # type: Optional[Union[str, int]]
-    schedule_unit = None  # type: Optional[str]
-
-    if isinstance(celery_schedule, crontab):
-        schedule_type = "crontab"
-        schedule_value = (
-            "{0._orig_minute} "
-            "{0._orig_hour} "
-            "{0._orig_day_of_month} "
-            "{0._orig_month_of_year} "
-            "{0._orig_day_of_week}".format(celery_schedule)
-        )
-    elif isinstance(celery_schedule, schedule):
-        schedule_type = "interval"
-        (schedule_value, schedule_unit) = _get_humanized_interval(
-            celery_schedule.seconds
-        )
-
-        if schedule_unit == "second":
-            logger.warning(
-                "Intervals shorter than one minute are not supported by Sentry Crons."
-            )
-            return {}
-
-    else:
-        logger.warning(
-            "Celery schedule type '%s' not supported by Sentry Crons.",
-            type(celery_schedule),
-        )
-        return {}
-
-    monitor_config["schedule"] = {}
-    monitor_config["schedule"]["type"] = schedule_type
-    monitor_config["schedule"]["value"] = schedule_value
-
-    if schedule_unit is not None:
-        monitor_config["schedule"]["unit"] = schedule_unit
-
-    monitor_config["timezone"] = app.conf.timezone or "UTC"
-
-    return monitor_config
-
-
-def _patch_beat_apply_entry():
-    # type: () -> None
-    original_apply_entry = Scheduler.apply_entry
-
-    def sentry_apply_entry(*args, **kwargs):
-        # type: (*Any, **Any) -> None
-        scheduler, schedule_entry = args
-        app = scheduler.app
-
-        celery_schedule = schedule_entry.schedule
-        monitor_name = schedule_entry.name
-
-        hub = Hub.current
-        integration = hub.get_integration(CeleryIntegration)
-        if integration is None:
-            return original_apply_entry(*args, **kwargs)
-
-        if match_regex_list(monitor_name, integration.exclude_beat_tasks):
-            return original_apply_entry(*args, **kwargs)
-
-        with hub.configure_scope() as scope:
-            # When tasks are started from Celery Beat, make sure each task has its own trace.
-            scope.set_new_propagation_context()
-
-            monitor_config = _get_monitor_config(celery_schedule, app)
-
-            is_supported_schedule = bool(monitor_config)
-            if is_supported_schedule:
-                headers = schedule_entry.options.pop("headers", {})
-                headers.update(
-                    {
-                        "sentry-monitor-slug": monitor_name,
-                        "sentry-monitor-config": monitor_config,
-                    }
-                )
-
-                check_in_id = capture_checkin(
-                    monitor_slug=monitor_name,
-                    monitor_config=monitor_config,
-                    status=MonitorStatus.IN_PROGRESS,
-                )
-                headers.update({"sentry-monitor-check-in-id": check_in_id})
-
-                # Set the Sentry configuration in the options of the ScheduleEntry.
-                # Those will be picked up in `apply_async` and added to the headers.
-                schedule_entry.options["headers"] = headers
-
-            return original_apply_entry(*args, **kwargs)
-
-    Scheduler.apply_entry = sentry_apply_entry
-
-
-def _setup_celery_beat_signals():
-    # type: () -> None
-    task_success.connect(crons_task_success)
-    task_failure.connect(crons_task_failure)
-    task_retry.connect(crons_task_retry)
-
-
-def crons_task_success(sender, **kwargs):
-    # type: (Task, Dict[Any, Any]) -> None
-    logger.debug("celery_task_success %s", sender)
-    headers = _get_headers(sender)
-
-    if "sentry-monitor-slug" not in headers:
-        return
-
-    monitor_config = headers.get("sentry-monitor-config", {})
-
-    start_timestamp_s = float(headers["sentry-monitor-start-timestamp-s"])
-
-    capture_checkin(
-        monitor_slug=headers["sentry-monitor-slug"],
-        monitor_config=monitor_config,
-        check_in_id=headers["sentry-monitor-check-in-id"],
-        duration=_now_seconds_since_epoch() - start_timestamp_s,
-        status=MonitorStatus.OK,
-    )
-
-
-def crons_task_failure(sender, **kwargs):
-    # type: (Task, Dict[Any, Any]) -> None
-    logger.debug("celery_task_failure %s", sender)
-    headers = _get_headers(sender)
-
-    if "sentry-monitor-slug" not in headers:
-        return
-
-    monitor_config = headers.get("sentry-monitor-config", {})
-
-    start_timestamp_s = float(headers["sentry-monitor-start-timestamp-s"])
-
-    capture_checkin(
-        monitor_slug=headers["sentry-monitor-slug"],
-        monitor_config=monitor_config,
-        check_in_id=headers["sentry-monitor-check-in-id"],
-        duration=_now_seconds_since_epoch() - start_timestamp_s,
-        status=MonitorStatus.ERROR,
-    )
-
-
-def crons_task_retry(sender, **kwargs):
-    # type: (Task, Dict[Any, Any]) -> None
-    logger.debug("celery_task_retry %s", sender)
-    headers = _get_headers(sender)
-
-    if "sentry-monitor-slug" not in headers:
-        return
-
-    monitor_config = headers.get("sentry-monitor-config", {})
-
-    start_timestamp_s = float(headers["sentry-monitor-start-timestamp-s"])
-
-    capture_checkin(
-        monitor_slug=headers["sentry-monitor-slug"],
-        monitor_config=monitor_config,
-        check_in_id=headers["sentry-monitor-check-in-id"],
-        duration=_now_seconds_since_epoch() - start_timestamp_s,
-        status=MonitorStatus.ERROR,
-    )
