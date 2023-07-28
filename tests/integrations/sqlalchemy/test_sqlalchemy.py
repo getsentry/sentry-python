a47b99364831a02969df4f1d841ea40ad9ef15e3
Anton Pirker <anton.pirker@sentry.io>
2023-07-28 08:27:39 +0200

    Fixing hosts in tests

--- tests/integrations/sqlalchemy/test_sqlalchemy.py
+++ tests/integrations/sqlalchemy/test_sqlalchemy.py
@@ -122,7 +122,7 @@ def test_transactions(sentry_init, capture_events, render_span_tree):
 
     for span in event["spans"]:
         assert span["data"][SPANDATA.DB_SYSTEM] == "sqlite"
-        assert span["data"][SPANDATA.DB_NAME] == "sqlite"
+        assert span["data"][SPANDATA.DB_NAME] == ":memory:"
         assert span["data"][SPANDATA.SERVER_ADDRESS] == "sqlite"
         assert span["data"][SPANDATA.SERVER_PORT] == "sqlite"
 
