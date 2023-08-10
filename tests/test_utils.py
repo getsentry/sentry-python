2f14816933c36aa2510a688b625bf3763290122c
Nicolás San Martín <57573579+nicolassanmar@users.noreply.github.com>
2023-08-10 07:51:54 -0300

    fix: Exceptions include detail property for their value  (#2193)
    ---------
    
    Co-authored-by: Anton Pirker <anton.pirker@sentry.io>
    Co-authored-by: Ivana Kellyerova <ivana.kellyerova@sentry.io>
    Co-authored-by: Gohar Shoukat <25367760+goharShoukat@users.noreply.github.com>

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,11 +4,13 @@ import sys
 
 from sentry_sdk.utils import (
     Components,
+    get_error_message,
     is_valid_sample_rate,
     logger,
     match_regex_list,
     parse_url,
     parse_version,
+    safe_str,
     sanitize_url,
     serialize_frame,
 )
@@ -423,3 +425,22 @@ def test_match_regex_list(item, regex_list, expected_result):
 )
 def test_parse_version(version, expected_result):
     assert parse_version(version) == expected_result
+
+
+@pytest.mark.parametrize(
+    "error,expected_result",
+    [
+        ["", lambda x: safe_str(x)],
+        ["some-string", lambda _: "some-string"],
+    ],
+)
+def test_get_error_message(error, expected_result):
+    with pytest.raises(BaseException) as exc_value:
+        exc_value.message = error
+        raise Exception
+    assert get_error_message(exc_value) == expected_result(exc_value)
+
+    with pytest.raises(BaseException) as exc_value:
+        exc_value.detail = error
+        raise Exception
+    assert get_error_message(exc_value) == expected_result(exc_value)
