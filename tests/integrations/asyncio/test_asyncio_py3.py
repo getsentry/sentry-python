60cac8839c5a2c168047c5bcfb9300f340d799ce
Daniel Szoke <daniel.szoke@sentry.io>
2023-08-17 15:42:31 +0200

    Suppress linter error

--- tests/integrations/asyncio/test_asyncio_py3.py
+++ tests/integrations/asyncio/test_asyncio_py3.py
@@ -246,7 +246,7 @@ def test_patch_asyncio(mock_get_running_loop):
 @minimum_python_37
 @patch("asyncio.get_running_loop")
 @patch("sentry_sdk.integrations.asyncio.Task")
-def test_sentry_task_factory_no_factory(MockTask, mock_get_running_loop):
+def test_sentry_task_factory_no_factory(MockTask, mock_get_running_loop):  # noqa: N803
     mock_loop = mock_get_running_loop.return_value
     mock_coro = MagicMock()
 
@@ -298,7 +298,9 @@ def test_sentry_task_factory_with_factory(mock_get_running_loop):
 @minimum_python_311
 @patch("asyncio.get_running_loop")
 @patch("sentry_sdk.integrations.asyncio.Task")
-def test_sentry_task_factory_context_no_factory(MockTask, mock_get_running_loop):
+def test_sentry_task_factory_context_no_factory(
+    MockTask, mock_get_running_loop
+):  # noqa: N803
     mock_loop = mock_get_running_loop.return_value
     mock_coro = MagicMock()
     mock_context = MagicMock()
