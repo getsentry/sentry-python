00d6ad919c38b03754e3c98ec90fc718e1f65b4b
Martin Imre <martinimre25@gmail.com>
2023-08-19 15:36:42 +0200

    feat(integrations): Add integration for asyncpg >= 0.23.0 (WIP)

---
+++ sentry_sdk/integrations/asyncpg.py
@@ -0,0 +1,62 @@
+from typing import ParamSpec, TypeVar, Callable
+
+from sentry_sdk import Hub
+from sentry_sdk.integrations import Integration, DidNotEnable
+from sentry_sdk.tracing_utils import record_sql_queries
+from sentry_sdk.utils import parse_version
+
+try:
+    import asyncpg  # type: ignore[import]
+
+except ImportError:
+    raise DidNotEnable("asyncpg not installed.")
+
+# asyncpg.__version__ is a string containing the semantic version in the form of "<major>.<minor>.<patch>"
+asyncpg_version = parse_version(asyncpg.__version__)
+
+if asyncpg_version < (0, 23, 0):
+    raise DidNotEnable("asyncpg >= 0.23.0 required")
+
+
+class AsyncPGIntegration(Integration):
+    identifier = "asyncpg"
+
+    def __init__(self, *, record_params=False):
+        AsyncPGIntegration._record_params = record_params
+
+    @staticmethod
+    def setup_once() -> None:
+        asyncpg.Connection._execute = _wrap_connection_method(
+            asyncpg.Connection._execute
+        )
+        asyncpg.Connection._executemany = _wrap_connection_method(
+            asyncpg.Connection._executemany
+        )
+        asyncpg.Connection.cursor = _wrap_connection_method(asyncpg.Connection.cursor)
+        asyncpg.Connection.prepare = _wrap_connection_method(asyncpg.Connection.prepare)
+
+
+P = ParamSpec("P")
+T = TypeVar("T")
+
+
+def _wrap_connection_method(f: Callable[P, T]) -> Callable[P, T]:
+    def _inner(*args: P.args, **kwargs: P.kwargs) -> T:
+        hub = Hub.current
+        integration = hub.get_integration(AsyncPGIntegration)
+
+        if integration is None:
+            return f(*args, **kwargs)
+
+        record_params = integration._record_params
+
+        query = args[1]
+        params_list = args[2] if record_params else None
+        param_style = "pyformat" if params_list else None
+        with record_sql_queries(
+            hub, None, query, params_list, param_style, executemany=False
+        ):
+            res = f(*args, **kwargs)
+        return res
+
+    return _inner
---
+++ tests/integrations/asyncpg/test_asyncpg.py
@@ -0,0 +1,318 @@
+"""
+Tests need pytest-asyncio installed.
+
+Tests need a local postgresql instance running, this can best be done using
+```sh
+docker run --rm --name some-postgres -e POSTGRES_USER=foo -e POSTGRES_PASSWORD=bar -d -p 5432:5432 postgres
+```
+
+The tests use the following credentials to establish a database connection.
+"""
+PG_USER = "foo"
+PG_PASSWORD = "bar"
+
+
+import datetime
+
+import asyncpg
+import pytest
+from asyncpg import connect, Connection
+
+from sentry_sdk import capture_message
+from sentry_sdk.integrations.asyncpg import AsyncPGIntegration
+from tests.integrations.asgi import pytest_asyncio
+
+PG_CONNECTION_URI = f"postgresql://{PG_USER}:{PG_PASSWORD}@localhost/"
+
+
+@pytest_asyncio.fixture(autouse=True)
+async def _clean_pg():
+    conn = await connect(PG_CONNECTION_URI)
+    await conn.execute("DROP TABLE IF EXISTS users")
+    await conn.execute(
+        """
+            CREATE TABLE users(
+                id serial PRIMARY KEY,
+                name text,
+                password text,
+                dob date
+            )
+        """
+    )
+    await conn.close()
+
+
+@pytest.mark.asyncio
+async def test_execute(sentry_init, capture_events) -> None:
+    sentry_init(
+        integrations=[AsyncPGIntegration()],
+        _experiments={"record_sql_params": True},
+    )
+    events = capture_events()
+
+    conn: Connection = await connect("postgresql://foo:bar@localhost/")
+
+    await conn.execute(
+        "INSERT INTO users(name, password, dob) VALUES($1, $2, $3)",
+        "Bob",
+        "secret_pw",
+        datetime.date(1984, 3, 1),
+    )
+
+    row = await conn.fetchrow("SELECT * FROM users WHERE name = $1", "Bob")
+    assert row == (1, "Bob", "secret_pw", datetime.date(1984, 3, 1))
+
+    row = await conn.fetchrow("SELECT * FROM users WHERE name = 'Bob'")
+    assert row == (1, "Bob", "secret_pw", datetime.date(1984, 3, 1))
+
+    await conn.close()
+
+    capture_message("hi")
+
+    (event,) = events
+
+    for crumb in event["breadcrumbs"]["values"]:
+        del crumb["timestamp"]
+
+    assert event["breadcrumbs"]["values"] == [
+        {
+            "category": "query",
+            "data": {},
+            "message": "INSERT INTO users(name, password, dob) VALUES($1, $2, $3)",
+            "type": "default",
+        },
+        {
+            "category": "query",
+            "data": {},
+            "message": "SELECT * FROM users WHERE name = $1",
+            "type": "default",
+        },
+        {
+            "category": "query",
+            "data": {},
+            "message": "SELECT * FROM users WHERE name = 'Bob'",
+            "type": "default",
+        },
+    ]
+
+
+@pytest.mark.asyncio
+async def test_execute_many(sentry_init, capture_events) -> None:
+    sentry_init(
+        integrations=[AsyncPGIntegration()],
+        _experiments={"record_sql_params": True},
+    )
+    events = capture_events()
+
+    conn: Connection = await connect("postgresql://foo:bar@localhost/")
+
+    await conn.executemany(
+        "INSERT INTO users(name, password, dob) VALUES($1, $2, $3)",
+        [
+            ("Bob", "secret_pw", datetime.date(1984, 3, 1)),
+            ("Alice", "pw", datetime.date(1990, 12, 25)),
+        ],
+    )
+
+    await conn.close()
+
+    capture_message("hi")
+
+    (event,) = events
+
+    for crumb in event["breadcrumbs"]["values"]:
+        del crumb["timestamp"]
+
+    assert event["breadcrumbs"]["values"] == [
+        {
+            "category": "query",
+            "data": {},
+            "message": "INSERT INTO users(name, password, dob) VALUES($1, $2, $3)",
+            "type": "default",
+        }
+    ]
+
+
+@pytest.mark.asyncio
+async def test_record_params(sentry_init, capture_events) -> None:
+    sentry_init(
+        integrations=[AsyncPGIntegration(record_params=True)],
+        _experiments={"record_sql_params": True},
+    )
+    events = capture_events()
+
+    conn: Connection = await connect("postgresql://foo:bar@localhost/")
+
+    await conn.execute(
+        "INSERT INTO users(name, password, dob) VALUES($1, $2, $3)",
+        "Bob",
+        "secret_pw",
+        datetime.date(1984, 3, 1),
+    )
+
+    await conn.close()
+
+    capture_message("hi")
+
+    (event,) = events
+
+    for crumb in event["breadcrumbs"]["values"]:
+        del crumb["timestamp"]
+
+    assert event["breadcrumbs"]["values"] == [
+        {
+            "category": "query",
+            "data": {
+                "db.params": ["Bob", "secret_pw", "datetime.date(1984, 3, 1)"],
+                "db.paramstyle": "format",
+            },
+            "message": "INSERT INTO users(name, password, dob) VALUES($1, $2, $3)",
+            "type": "default",
+        }
+    ]
+
+
+@pytest.mark.asyncio
+async def test_cursor(sentry_init, capture_events) -> None:
+    sentry_init(
+        integrations=[AsyncPGIntegration()],
+        _experiments={"record_sql_params": True},
+    )
+    events = capture_events()
+
+    conn: Connection = await connect("postgresql://foo:bar@localhost/")
+
+    await conn.executemany(
+        "INSERT INTO users(name, password, dob) VALUES($1, $2, $3)",
+        [
+            ("Bob", "secret_pw", datetime.date(1984, 3, 1)),
+            ("Alice", "pw", datetime.date(1990, 12, 25)),
+        ],
+    )
+
+    async with conn.transaction():
+        # Postgres requires non-scrollable cursors to be created
+        # and used in a transaction.
+        async for record in conn.cursor(
+            "SELECT * FROM users WHERE dob > $1", datetime.date(1970, 1, 1)
+        ):
+            print(record)
+
+    await conn.close()
+
+    capture_message("hi")
+
+    (event,) = events
+
+    for crumb in event["breadcrumbs"]["values"]:
+        del crumb["timestamp"]
+
+    assert event["breadcrumbs"]["values"] == [
+        {
+            "category": "query",
+            "data": {},
+            "message": "INSERT INTO users(name, password, dob) VALUES($1, $2, $3)",
+            "type": "default",
+        },
+        {
+            "category": "query",
+            "data": {},
+            "message": "SELECT * FROM users WHERE dob > $1",
+            "type": "default",
+        },
+    ]
+
+
+@pytest.mark.asyncio
+async def test_prepared_stmt(sentry_init, capture_events) -> None:
+    sentry_init(
+        integrations=[AsyncPGIntegration()],
+        _experiments={"record_sql_params": True},
+    )
+    events = capture_events()
+
+    conn: Connection = await connect("postgresql://foo:bar@localhost/")
+
+    await conn.executemany(
+        "INSERT INTO users(name, password, dob) VALUES($1, $2, $3)",
+        [
+            ("Bob", "secret_pw", datetime.date(1984, 3, 1)),
+            ("Alice", "pw", datetime.date(1990, 12, 25)),
+        ],
+    )
+
+    stmt = await conn.prepare("SELECT * FROM users WHERE name = $1")
+
+    print(await stmt.fetchval("Bob"))
+    print(await stmt.fetchval("Alice"))
+
+    await conn.close()
+
+    capture_message("hi")
+
+    (event,) = events
+
+    for crumb in event["breadcrumbs"]["values"]:
+        del crumb["timestamp"]
+
+    assert event["breadcrumbs"]["values"] == [
+        {
+            "category": "query",
+            "data": {},
+            "message": "INSERT INTO users(name, password, dob) VALUES($1, $2, $3)",
+            "type": "default",
+        },
+        {
+            "category": "query",
+            "data": {},
+            "message": "SELECT * FROM users WHERE name = $1",
+            "type": "default",
+        },
+    ]
+
+
+@pytest.mark.asyncio
+async def test_connection_pool(sentry_init, capture_events) -> None:
+    sentry_init(
+        integrations=[AsyncPGIntegration()],
+        _experiments={"record_sql_params": True},
+    )
+    events = capture_events()
+
+    pool = await asyncpg.create_pool("postgresql://foo:bar@localhost/")
+
+    async with pool.acquire() as conn:
+        await conn.execute(
+            "INSERT INTO users(name, password, dob) VALUES($1, $2, $3)",
+            "Bob",
+            "secret_pw",
+            datetime.date(1984, 3, 1),
+        )
+
+    async with pool.acquire() as conn:
+        row = await conn.fetchrow("SELECT * FROM users WHERE name = $1", "Bob")
+        assert row == (1, "Bob", "secret_pw", datetime.date(1984, 3, 1))
+
+    await pool.close()
+
+    capture_message("hi")
+
+    (event,) = events
+
+    for crumb in event["breadcrumbs"]["values"]:
+        del crumb["timestamp"]
+
+    assert event["breadcrumbs"]["values"] == [
+        {
+            "category": "query",
+            "data": {},
+            "message": "INSERT INTO users(name, password, dob) VALUES($1, $2, $3)",
+            "type": "default",
+        },
+        {
+            "category": "query",
+            "data": {},
+            "message": "SELECT * FROM users WHERE name = $1",
+            "type": "default",
+        },
+    ]
