0891fe28cd64dac7e8fc1e94a36b7a5754f87017
Vageeshan <mankalavageeshan1@gmail.com>
2023-09-04 01:22:05 -0700

    getting flask version from _get_installed_modules

--- sentry_sdk/integrations/flask.py
+++ sentry_sdk/integrations/flask.py
@@ -1,11 +1,11 @@
 from __future__ import absolute_import
-from importlib.metadata import version
 
 from sentry_sdk._types import TYPE_CHECKING
 from sentry_sdk.hub import Hub, _should_send_default_pii
 from sentry_sdk.integrations import DidNotEnable, Integration
 from sentry_sdk.integrations._wsgi_common import RequestExtractor
 from sentry_sdk.integrations.wsgi import SentryWsgiMiddleware
+from sentry_sdk.integrations.modules import _get_installed_modules
 from sentry_sdk.scope import Scope
 from sentry_sdk.tracing import SOURCE_FOR_STYLE
 from sentry_sdk.utils import (
@@ -44,7 +44,8 @@ try:
 except ImportError:
     raise DidNotEnable("blinker is not installed")
 
-FLASK_VERSION = version("flask")
+installed_packages = _get_installed_modules()
+FLASK_VERSION = installed_packages("flask")
 TRANSACTION_STYLE_VALUES = ("endpoint", "url")
 
 
