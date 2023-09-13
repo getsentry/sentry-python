a8ddcf647bf6674ad9d476730a084d69eeb6b293
Florian Dellekart <florian.dellekart@hotmail.de>
2023-09-13 13:59:56 +0200

    fix: gRPC async metadata can be tuple although differently typed
    Opened an issue in grpc repo to ask if this is inteded behaviour
    If it should be changed one day the the .add method of the metadata
    object would avoid reconstructing the whole client call details.
    
    Link to issue:
    https://github.com/grpc/grpc/issues/34298

--- sentry_sdk/integrations/grpc/aio/client.py
+++ sentry_sdk/integrations/grpc/aio/client.py
@@ -25,14 +25,32 @@ class AsyncClientInterceptor(UnaryUnaryClientInterceptor):
             span.set_data("type", "unary unary")
             span.set_data("method", method)
 
-            for key, value in hub.iter_trace_propagation_headers():
-                # Currently broken
-                # Waiting for response here
-                # https://github.com/grpc/grpc/issues/34298
-                client_call_details.metadata.add(key, value)
+            client_call_details = self._update_client_call_details_metadata_from_hub(
+                client_call_details, hub
+            )
 
             response = await continuation(client_call_details, request)
             status_code = await response.code()
             span.set_data("code", status_code.name)
 
             return response
+
+    @staticmethod
+    def _update_client_call_details_metadata_from_hub(
+        client_call_details: ClientCallDetails, hub: Hub
+    ):
+        metadata = (
+            list(client_call_details.metadata) if client_call_details.metadata else []
+        )
+        for key, value in hub.iter_trace_propagation_headers():
+            metadata.append((key, value))
+
+        client_call_details = ClientCallDetails(
+            method=client_call_details.method,
+            timeout=client_call_details.timeout,
+            metadata=metadata,
+            credentials=client_call_details.credentials,
+            wait_for_ready=client_call_details.wait_for_ready,
+        )
+
+        return client_call_details
