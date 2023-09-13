6d06fdc489abe681c5bc8f928976aba4533a61da
Anton Pirker <anton.pirker@sentry.io>
2023-09-13 12:15:39 +0200

    Make ParamSpec work in older Python (hopefully)

--- sentry_sdk/integrations/clickhouse_driver.py
+++ sentry_sdk/integrations/clickhouse_driver.py
@@ -1,11 +1,21 @@
-from typing import ParamSpec, TypeVar, Callable
-
 from sentry_sdk import Hub
 from sentry_sdk.consts import OP, SPANDATA
 from sentry_sdk.hub import _should_send_default_pii
 from sentry_sdk.integrations import Integration, DidNotEnable
 from sentry_sdk.tracing import Span
+from sentry_sdk._types import TYPE_CHECKING
 from sentry_sdk.utils import capture_internal_exceptions
+
+from typing import TypeVar, Callable
+
+if TYPE_CHECKING:
+    from typing import ParamSpec
+else:
+    # Fake ParamSpec
+    class ParamSpec:
+        def __init__(self, _):
+            self.args = None
+            self.kwargs = None
 
 
 try:
