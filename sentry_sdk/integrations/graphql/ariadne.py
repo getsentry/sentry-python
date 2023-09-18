c3191508dfb22c330b7e371e74d9982bb12e1557
Ivana Kellyerova <ivana.kellyerova@sentry.io>
2023-09-18 16:06:12 +0200

    clean up ariadne

--- sentry_sdk/integrations/graphql/ariadne.py
+++ sentry_sdk/integrations/graphql/ariadne.py
@@ -1,24 +1,10 @@
 from importlib import import_module
 
-from sentry_sdk.consts import OP
 from sentry_sdk.hub import Hub, _should_send_default_pii
 from sentry_sdk.integrations import DidNotEnable, Integration
-from sentry_sdk.integrations.graphql._common import (
-    OPERATION_TYPE_TO_OP,
-    get_operation_type_and_name_from_operation_definition,
-)
-from sentry_sdk.utils import (
-    capture_internal_exceptions,
-    event_from_exception,
-    parse_version,
-)
+from sentry_sdk.integrations.modules import _get_installed_modules
+from sentry_sdk.utils import event_from_exception, parse_version
 from sentry_sdk._types import TYPE_CHECKING
-
-try:
-    from graphql import version as GRAPHQL_CORE_VERSION
-    from graphql.language import DocumentNode
-except ImportError:
-    raise DidNotEnable("graphql-core not installed")
 
 try:
     ariadne_graphql = import_module(
@@ -39,29 +25,26 @@ if TYPE_CHECKING:
 class AriadneIntegration(Integration):
     identifier = "ariadne"
 
-    # XXX add an option to turn on error monitoring so that people who are instrumenting
-    # both the server and the client side can turn one of them off if they want
     # XXX double patching
+    # XXX if we end up patching graphql-core, we need guards to prevent re-patching
     # XXX capture internal exceptions
     # XXX capture request/response? to override pii?
     # XXX max_request_body_size? see request_body_within_bounds
-    # XXX version guard for ariadne
-    # XXX if we end up patching graphql-core, we need guards to prevent re-patching
     # XXX subscriptions
     # XXX ws
 
     @staticmethod
     def setup_once():
         # type: () -> None
-        version = parse_version(GRAPHQL_CORE_VERSION)
+        installed_packages = _get_installed_modules()
 
-        if version is None:
-            raise DidNotEnable(
-                "Unparsable graphql-core version: {}".format(GRAPHQL_CORE_VERSION)
-            )
+        ariadne_version = parse_version(installed_packages["ariadne"])
 
-        if version < (3, 2):
-            raise DidNotEnable("graphql-core 3.2 or newer required.")
+        if ariadne_version is None:
+            raise DidNotEnable("Unparsable ariadne version: {}".format(ariadne_version))
+
+        if ariadne_version < (0, 20):
+            raise DidNotEnable("ariadne 0.20 or newer required.")
 
         _patch_graphql()
 
@@ -73,10 +56,6 @@ def _patch_graphql():
     old_handle_errors = ariadne_graphql.handle_graphql_errors
     old_handle_query_result = ariadne_graphql.handle_query_result
 
-    old_execute_async = ariadne_graphql.execute
-    old_execute_sync = ariadne_graphql.execute_sync
-    old_parse_query = ariadne_graphql.parse_query
-
     def _sentry_patched_graphql_sync(schema, data, *args, **kwargs):
         # type: (GraphQLSchema, Any, Any, Any) -> GraphQLResult
         hub = Hub.current
@@ -84,8 +63,7 @@ def _patch_graphql():
         if integration is None:
             return old_graphql_sync(schema, data, *args, **kwargs)
 
-        scope = hub.scope  # XXX configure?
-        if scope is not None:
+        with hub.configure_scope() as scope:
             event_processor = _make_request_event_processor(schema, data)
             scope.add_event_processor(event_processor)
 
@@ -99,8 +77,7 @@ def _patch_graphql():
         if integration is None:
             return await old_graphql_async(schema, data, *args, **kwargs)
 
-        scope = hub.scope  # XXX configure?
-        if scope is not None:
+        with hub.configure_scope() as scope:
             event_processor = _make_request_event_processor(data)
             scope.add_event_processor(event_processor)
 
@@ -116,8 +93,7 @@ def _patch_graphql():
 
         result = old_handle_errors(errors, *args, **kwargs)
 
-        scope = hub.scope  # XXX configure?
-        if scope is not None:
+        with hub.configure_scope() as scope:
             event_processor = _make_response_event_processor(result[1])
             scope.add_event_processor(event_processor)
 
@@ -143,8 +119,7 @@ def _patch_graphql():
 
         response = old_handle_query_result(result, *args, **kwargs)
 
-        scope = hub.scope  # XXX configure?
-        if scope is not None:
+        with hub.configure_scope() as scope:
             event_processor = _make_response_event_processor(response[1])
             scope.add_event_processor(event_processor)
 
@@ -161,57 +136,11 @@ def _patch_graphql():
 
         return response
 
-    def _sentry_patched_execute_sync(schema, document, *args, **kwargs):
-        # XXX
-        hub = Hub.current
-        integration = hub.get_integration(AriadneIntegration)
-        if integration is None:
-            return old_execute_sync(schema, document, *args, **kwargs)
-
-        print(type(document))
-        print("execute_sync operation", extract_operation_type_and_name(document))
-
-        result = old_execute_sync(schema, document, *args, **kwargs)
-
-        return result
-
-    def _sentry_patched_execute_async(schema, document, *args, **kwargs):
-        # XXX
-        hub = Hub.current
-        integration = hub.get_integration(AriadneIntegration)
-        if integration is None:
-            return old_execute_async(schema, document, *args, **kwargs)
-
-        print(type(document))
-        print("execute_async operation", extract_operation_type_and_name(document))
-
-        result = old_execute_async(schema, document, *args, **kwargs)
-
-        return result
-
-    def _sentry_patched_parse_query(*args, **kwargs):
-        # XXX
-        hub = Hub.current
-        integration = hub.get_integration(AriadneIntegration)
-        if integration is None:
-            return old_parse_query(*args, **kwargs)
-
-        document = old_parse_query(*args, **kwargs)
-        scope = hub.scope  # XXX configure?
-        if scope is not None:
-            event_processor = _make_operation_event_processor(document)
-            scope.add_event_processor(event_processor)
-
-        return document
-
     ariadne_graphql.graphql_sync = _sentry_patched_graphql_sync
     ariadne_graphql.graphql = _sentry_patched_graphql_async
     ariadne_http.graphql = _sentry_patched_graphql_async
     ariadne_graphql.handle_graphql_errors = _sentry_patched_handle_graphql_errors
     ariadne_graphql.handle_query_result = _sentry_patched_handle_query_result
-    # ariadne_graphql.execute = _sentry_patched_execute_async
-    # ariadne_graphql.execute_sync = _sentry_patched_execute_sync
-    # ariadne_graphql.parse_query = _sentry_patched_parse_query
 
 
 def _make_request_event_processor(data):
@@ -220,11 +149,10 @@ def _make_request_event_processor(data):
 
     def inner(event, hint):
         # type: (Dict[str, Any], Dict[str, Any]) -> Dict[str, Any]
-        if _should_send_default_pii():
-            if isinstance(data, dict) and data.get("query"):
-                request_info = event.setdefault("request", {})
-                request_info["api_target"] = "graphql"
-                request_info["data"] = str(data["query"])
+        if _should_send_default_pii() and isinstance(data, dict) and data.get("query"):
+            request_info = event.setdefault("request", {})
+            request_info["api_target"] = "graphql"
+            request_info["data"] = data
 
         return event
 
@@ -242,29 +170,6 @@ def _make_response_event_processor(response):
             request_info["response"] = {
                 "data": response,
             }
-
-        return event
-
-    return inner
-
-
-def _make_operation_event_processor(document_node):
-    # type: (DocumentNode) -> EventProcessor
-    def inner(event, hint):
-        # type: (Dict[str, Any], Dict[str, Any]) -> Dict[str, Any]
-        # If the operation type is not known yet, set it to the first operation
-        # in the document
-        print(document_node.to_dict())
-        operation_type, operation_name = extract_operation_type_and_name(document_node)
-
-        if operation_type is not None:
-            type_to_op = {
-                "query": OP.HTTP_GRAPHQL_QUERY,
-                "mutation": OP.HTTP_GRAPHQL_MUTATION,
-                "subscription": OP.HTTP_GRAPHQL_SUBSCRIPTION,
-            }
-            print("OPERATION TYPE", type_to_op.get(operation_type))
-        print("OPERATION NAME", operation_name)
 
         return event
 
