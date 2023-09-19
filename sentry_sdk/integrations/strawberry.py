cf6c44790f7323f9155c30ee19749f0318832773
Ivana Kellyerova <ivana.kellyerova@sentry.io>
2023-09-19 14:49:40 +0200

    some ariadne tests

--- sentry_sdk/integrations/strawberry.py
+++ sentry_sdk/integrations/strawberry.py
@@ -1,10 +1,9 @@
-import importlib.metadata
-
-from sentry_sdk._types import TYPE_CHECKING
+from sentry_sdk import configure_scope, start_span
 from sentry_sdk.integrations import Integration, DidNotEnable
 from sentry_sdk.integrations.modules import _get_installed_modules
 from sentry_sdk.hub import Hub
 from sentry_sdk.utils import logger, parse_version
+from sentry_sdk._types import TYPE_CHECKING
 
 try:
     from strawberry import Schema
@@ -22,8 +21,6 @@ import hashlib
 from inspect import isawaitable
 from typing import Any, Callable, Generator, Optional
 
-from sentry_sdk import configure_scope, start_span
-
 
 if TYPE_CHECKING:
     from graphql import GraphQLResolveInfo
@@ -32,6 +29,15 @@ if TYPE_CHECKING:
 
 class StrawberryIntegration(Integration):
     identifier = "strawberry"
+
+    def __init__(self, async_execution=None):
+        # type: (Optional[bool]) -> None
+        if async_execution not in (None, False, True):
+            raise ValueError(
+                "Invalid value for async_execution: %s (must be bool)"
+                % (async_execution)
+            )
+        self.async_execution = async_execution
 
     @staticmethod
     def setup_once():
@@ -47,3 +53,201 @@ class StrawberryIntegration(Integration):
 
         if version < (0, 208):
             raise DidNotEnable("strawberry-graphql 0.208 or newer required.")
+
+        patch_schema_init()
+
+
+def patch_schema_init():
+    # type: () -> None
+    old_schema = Schema.__init__
+
+    def _sentry_patched_schema_init(self, *args, **kwargs):
+        integration = Hub.current.get_integration(StrawberryIntegration)
+        if integration is None:
+            return old_schema(self, *args, **kwargs)
+
+        extensions = kwargs.get("extensions") or []
+
+        if integration.async_execution is not None:
+            should_use_async_extension = integration.async_execution
+        else:
+            # try to figure it out ourselves
+            should_use_async_extension = bool(
+                {"starlette", "starlite", "litestar", "fastapi"}
+                & set(_get_installed_modules())
+            )
+
+            logger.info(
+                "Assuming strawberry is running in %s context. If not, initialize the integration with async_execution=%s.",
+                "async" if should_use_async_extension else "sync",
+                "False" if should_use_async_extension else "True",
+            )
+
+        # remove the strawberry sentry extension, if present, to avoid double
+        # tracing
+        extensions = [
+            extension
+            for extension in extensions
+            if extension
+            not in (StrawberrySentryAsyncExtension, StrawberrySentrySyncExtension)
+        ]
+
+        # add our extension
+        extensions.append(
+            SentryAsyncExtension if should_use_async_extension else SentrySyncExtension
+        )
+
+        kwargs["extensions"] = extensions
+
+        return old_schema(self, *args, **kwargs)
+
+    # XXX
+    not_yet_patched = old_schema.__name__ != "_sentry_patched_schema_init"
+    if not_yet_patched:
+        Schema.__init__ = _sentry_patched_schema_init
+
+
+class SentryAsyncExtension(SchemaExtension):
+    def __init__(
+        self,
+        *,
+        execution_context=None,  # type: Optional[ExecutionContext]
+    ):
+        if execution_context:
+            self.execution_context = execution_context
+
+    @cached_property
+    def _resource_name(self):
+        assert self.execution_context.query
+
+        query_hash = self.hash_query(self.execution_context.query)
+
+        if self.execution_context.operation_name:
+            return "{}:{}".format(self.execution_context.operation_name, query_hash)
+
+        return query_hash
+
+    def hash_query(self, query):
+        # type: (str) -> str
+        return hashlib.md5(query.encode("utf-8")).hexdigest()
+
+    def on_operation(self):
+        # type: () -> Generator[None, None, None]
+        self._operation_name = self.execution_context.operation_name
+        name = self._operation_name if self._operation_name else "anonymous query"
+
+        self.operation_type = "query"
+
+        assert self.execution_context.query
+
+        if self.execution_context.query.strip().startswith("mutation"):
+            self.operation_type = "mutation"
+        if self.execution_context.query.strip().startswith("subscription"):
+            self.operation_type = "subscription"
+
+        with configure_scope() as scope:
+            if scope.span:
+                self.graphql_span = scope.span.start_child(
+                    op="graphlql.{}".format(self.operation_type),
+                    description=name,
+                )
+            else:
+                self.graphql_span = start_span(
+                    op="graphql.{}".format(self.operation_type),
+                    description=name,
+                )
+
+        self.graphql_span.set_tag("graphql.operation_type", self.operation_type)
+        self.graphql_span.set_tag("graphql.resource_name", self._resource_name)
+        self.graphql_span.set_data("graphql.query", self.execution_context.query)
+
+        yield
+
+        self.graphql_span.finish()
+
+    def on_validate(self):
+        # type: () -> Generator[None, None, None]
+        validation_span = self.graphql_span.start_child(
+            op="graphql.validate", description="validation"
+        )
+
+        yield
+
+        validation_span.finish()
+
+    def on_parse(self):
+        # type: () -> Generator[None, None, None]
+        parsing_span = self.graphql_span.start_child(
+            op="graphql.parse", description="parsing"
+        )
+
+        yield
+
+        parsing_span.finish()
+
+    def should_skip_tracing(self, _next, info):
+        # type: (Callable, GraphQLResolveInfo) -> bool
+        return should_skip_tracing(_next, info)
+
+    async def resolve(
+        self,
+        _next,  # type: Callable
+        root,  # type: Any
+        info,  # type: GraphQLResolveInfo
+        *args,  # type: str
+        **kwargs,  # type: Any
+    ):
+        # type: (...) -> Any
+        if self.should_skip_tracing(_next, info):
+            result = _next(root, info, *args, **kwargs)
+
+            if isawaitable(result):
+                result = await result
+
+            return result
+
+        field_path = f"{info.parent_type}.{info.field_name}"
+
+        with self.graphql_span.start_child(
+            op="graphql.resolve", description="resolving: {}".format(field_path)
+        ) as span:
+            span.set_tag("graphql.field_name", info.field_name)
+            span.set_tag("graphql.parent_type", info.parent_type.name)
+            span.set_tag("graphql.field_path", field_path)
+            span.set_tag("graphql.path", ".".join(map(str, info.path.as_list())))
+
+            result = _next(root, info, *args, **kwargs)
+
+            if isawaitable(result):
+                result = await result
+
+            return result
+
+
+class SentrySyncExtension(SentryAsyncExtension):
+    def resolve(
+        self,
+        _next,  # type: Callable
+        root,  # type: Any
+        info,  # type: GraphQLResolveInfo
+        *args,  # type: str
+        **kwargs,  # type: Any
+    ):
+        # type: (...) -> Any
+        if self.should_skip_tracing(_next, info):
+            result = _next(root, info, *args, **kwargs)
+            return result
+
+        field_path = f"{info.parent_type}.{info.field_name}"
+
+        with self.graphql_span.start_child(
+            op="graphql.resolve", description="resolving: {}".format(field_path)
+        ) as span:
+            span.set_tag("graphql.field_name", info.field_name)
+            span.set_tag("graphql.parent_type", info.parent_type.name)
+            span.set_tag("graphql.field_path", field_path)
+            span.set_tag("graphql.path", ".".join(map(str, info.path.as_list())))
+
+            result = _next(root, info, *args, **kwargs)
+
+            return result
--- tests/integrations/ariadne/test_ariadne.py
+++ tests/integrations/ariadne/test_ariadne.py
@@ -1,8 +1,125 @@
 import pytest
 
-import sentry_sdk
+from ariadne import ObjectType, QueryType, gql, make_executable_schema
+from ariadne.asgi import GraphQL
+from ariadne.graphql import graphql as graphql_async, graphql_sync
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+from flask import Flask, request, jsonify
+
 from sentry_sdk.integrations.ariadne import AriadneIntegration
+from sentry_sdk.integrations.logging import ignore_logger
 
 
-def test_something(sentry_init, capture_events):
-    assert False
+# to prevent the logging integration from capturing the exception first
+ignore_logger("ariadne")
+
+
+def schema_factory():
+    type_defs = gql(
+        """
+        type Query {
+            greeting(name: String): Greeting
+            error: String
+        }
+
+        type Greeting {
+            name: String
+        }
+    """
+    )
+
+    query = QueryType()
+    greeting = ObjectType("Greeting")
+
+    @query.field("greeting")
+    def resolve_greeting(*_, name=None):
+        return {"name": name}
+
+    @query.field("error")
+    def resolve_error(obj, *_):
+        raise RuntimeError("resolver failed")
+
+    @greeting.field("name")
+    def resolve_name(obj, *_):
+        return f"Hello, {obj['name']}!"
+
+    return make_executable_schema(type_defs, query)
+
+
+schema = schema_factory()
+
+async_app = FastAPI()
+async_app.mount("/graphql/", GraphQL(schema))
+
+sync_app = Flask(__name__)
+
+
+@sync_app.route("/graphql", methods=["POST"])
+def graphql_server():
+    data = request.get_json()
+    success, result = graphql_sync(schema, data)
+    return jsonify(result), 200
+
+
+@pytest.mark.parametrize(
+    "client",
+    [TestClient(async_app), sync_app.test_client()],
+)
+def test_capture_request_and_response_if_send_pii_is_on(
+    sentry_init, capture_events, client
+):
+    sentry_init(
+        send_default_pii=True,
+        integrations=[AriadneIntegration()],
+    )
+    events = capture_events()
+
+    query = {"query": "query ErrorQuery {error}"}
+    client.post("/graphql", json=query)
+
+    assert len(events) == 1
+
+    (event,) = events
+    assert event["exception"]["values"][0]["mechanism"]["type"] == "ariadne"
+    assert event["contexts"]["response"] == {
+        "data": {
+            "data": {"error": None},
+            "errors": [
+                {
+                    "locations": [{"column": 19, "line": 1}],
+                    "message": "resolver failed",
+                    "path": ["error"],
+                }
+            ],
+        }
+    }
+    from pprint import pprint
+
+    pprint(event["request"])
+    assert event["request"]["api_target"] == "graphql"
+    assert event["request"]["data"] == query
+
+
+@pytest.mark.parametrize(
+    "graphql_run",
+    [graphql_async, graphql_sync],
+)
+def test_nothing_captured_on_success(sentry_init, capture_events, graphql_run):
+    sentry_init(
+        send_default_pii=True,
+        integrations=[AriadneIntegration()],
+    )
+    events = capture_events()
+
+    schema = schema_factory()
+
+    graphql_run(
+        schema,
+        data={
+            "query": "query GreetingQuery($name: String) { greeting(name: $name) {name}}",
+            "variables": {"name": "some name"},
+        },
+    )
+
+    assert len(events) == 0
