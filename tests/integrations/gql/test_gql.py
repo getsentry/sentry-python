dc676fcbce3365faf944fbc25f452415900ec853
Daniel Szoke <szokeasaurusrex@users.noreply.github.com>
2023-09-19 17:08:55 +0200

    More unit tests

--- tests/integrations/gql/test_gql.py
+++ tests/integrations/gql/test_gql.py
@@ -2,8 +2,9 @@ import pytest
 
 gql = pytest.importorskip("gql")
 
+from gql.transport.exceptions import TransportQueryError
 from sentry_sdk.integrations.gql import GQLIntegration
-from unittest.mock import Mock, patch
+from unittest.mock import MagicMock, Mock, patch
 
 
 def test_gql_init(sentry_init):
@@ -11,13 +12,60 @@ def test_gql_init(sentry_init):
 
 
 @patch("sentry_sdk.integrations.gql.gql")
-def test_setup_once_patches_gql_execute(patched_gql):
+def test_setup_once_patches_gql_execute(mock_gql):
     GQLIntegration.setup_once()
 
     assert not isinstance(
-        patched_gql.Client.execute, Mock
+        mock_gql.Client.execute, Mock
     ), "setup_once did not patch gql.Client.execute"
 
     assert callable(
-        patched_gql.Client.execute
+        mock_gql.Client.execute
     ), "the patched gql.Client.execute is not a function"
+
+
+@patch("sentry_sdk.integrations.gql.gql")
+def test_patched_gql_execute_still_calls_real_execute(mock_gql):
+    class MockClient(MagicMock):
+        def execute(self, _, x, y):
+            return 2 * x + y
+
+    mock_gql.Client = MockClient
+    call_args = (None, 3, 4)
+
+    client_before_patch = MockClient()
+    result_before_patch = client_before_patch.execute(*call_args)
+
+    GQLIntegration.setup_once()
+
+    client_after_patch = mock_gql.Client()
+    result_after_patch = client_after_patch.execute(*call_args)
+
+    assert (
+        result_after_patch == result_before_patch
+    ), "the patched execute returned a different result than the real execute"
+
+
+@patch("sentry_sdk.integrations.gql.print_ast")
+@patch("sentry_sdk.integrations.gql.event_from_exception")
+@patch("sentry_sdk.integrations.gql.Hub")
+@patch("sentry_sdk.integrations.gql.gql")
+def test_patched_gql_execute_captures_and_reraises_graphql_exception(
+    mock_gql, mock_hub, mock_event_from_exception, _
+):
+    mock_event_from_exception.return_value = (MagicMock(), MagicMock())
+
+    class MockClient(MagicMock):
+        def execute(self, _):
+            raise TransportQueryError("the query failed")
+
+    mock_gql.Client = MockClient
+
+    GQLIntegration.setup_once()
+
+    mock_client = mock_gql.Client()
+
+    with pytest.raises(TransportQueryError):
+        mock_client.execute(MagicMock())
+
+    assert mock_hub.current.capture_event.called, "event was not captured"
