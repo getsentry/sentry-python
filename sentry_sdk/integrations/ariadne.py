9c36137f4756eff87ea0c76cbf91ebdcaad20dda
Ivana Kellyerova <ivana.kellyerova@sentry.io>
2023-09-21 12:27:26 +0200

    Add ariadne error integration

---
+++ sentry_sdk/integrations/ariadne.py
@@ -0,0 +1,163 @@
+from importlib import import_module
+
+from sentry_sdk.hub import Hub, _should_send_default_pii
+from sentry_sdk.integrations import DidNotEnable, Integration
+from sentry_sdk.integrations.modules import _get_installed_modules
+from sentry_sdk.integrations._wsgi_common import request_body_within_bounds
+from sentry_sdk.utils import event_from_exception, parse_version
+from sentry_sdk._types import TYPE_CHECKING
+
+try:
+    # importing like this is necessary due to name shadowing in ariadne
+    # (ariadne.graphql is also a function)
+    ariadne_graphql = import_module("ariadne.graphql")
+except ImportError:
+    raise DidNotEnable("ariadne is not installed")
+
+
+if TYPE_CHECKING:
+    from typing import Any, Dict, List, Optional
+    from ariadne.types import GraphQLError, GraphQLResult, GraphQLSchema, QueryParser
+    from graphql.language.ast import DocumentNode
+    from sentry_sdk._types import EventProcessor
+
+
+class AriadneIntegration(Integration):
+    identifier = "ariadne"
+
+    @staticmethod
+    def setup_once():
+        # type: () -> None
+        installed_packages = _get_installed_modules()
+        version = parse_version(installed_packages["ariadne"])
+
+        if version is None:
+            raise DidNotEnable("Unparsable ariadne version: {}".format(version))
+
+        if version < (0, 20):
+            raise DidNotEnable("ariadne 0.20 or newer required.")
+
+        _patch_graphql()
+
+
+def _patch_graphql():
+    # type: () -> None
+    old_parse_query = ariadne_graphql.parse_query
+    old_handle_errors = ariadne_graphql.handle_graphql_errors
+    old_handle_query_result = ariadne_graphql.handle_query_result
+
+    def _sentry_patched_parse_query(context_value, query_parser, data):
+        # type: (Optional[Any], Optional[QueryParser], Any) -> DocumentNode
+        hub = Hub.current
+        integration = hub.get_integration(AriadneIntegration)
+        if integration is None:
+            return old_parse_query(context_value, query_parser, data)
+
+        with hub.configure_scope() as scope:
+            event_processor = _make_request_event_processor(data)
+            scope.add_event_processor(event_processor)
+
+        result = old_parse_query(context_value, query_parser, data)
+        return result
+
+    def _sentry_patched_handle_graphql_errors(errors, *args, **kwargs):
+        # type: (List[GraphQLError], Any, Any) -> GraphQLResult
+        hub = Hub.current
+        integration = hub.get_integration(AriadneIntegration)
+        if integration is None:
+            return old_handle_errors(errors, *args, **kwargs)
+
+        result = old_handle_errors(errors, *args, **kwargs)
+
+        with hub.configure_scope() as scope:
+            event_processor = _make_response_event_processor(result[1])
+            scope.add_event_processor(event_processor)
+
+        for error in errors:
+            event, hint = event_from_exception(
+                error,
+                client_options=hub.client.options,
+                mechanism={
+                    "type": hub.get_integration(AriadneIntegration).identifier,
+                    "handled": False,
+                },
+            )
+            hub.capture_event(event, hint=hint)
+
+        return result
+
+    def _sentry_patched_handle_query_result(result, *args, **kwargs):
+        # type: (Any, Any, Any) -> GraphQLResult
+        hub = Hub.current
+        integration = hub.get_integration(AriadneIntegration)
+        if integration is None:
+            return old_handle_query_result(result, *args, **kwargs)
+
+        response = old_handle_query_result(result, *args, **kwargs)
+
+        with hub.configure_scope() as scope:
+            event_processor = _make_response_event_processor(response[1])
+            scope.add_event_processor(event_processor)
+
+        for error in result.errors or []:
+            event, hint = event_from_exception(
+                error,
+                client_options=hub.client.options,
+                mechanism={
+                    "type": hub.get_integration(AriadneIntegration).identifier,
+                    "handled": False,
+                },
+            )
+            hub.capture_event(event, hint=hint)
+
+        return response
+
+    ariadne_graphql.parse_query = _sentry_patched_parse_query
+    ariadne_graphql.handle_graphql_errors = _sentry_patched_handle_graphql_errors
+    ariadne_graphql.handle_query_result = _sentry_patched_handle_query_result
+
+
+def _make_request_event_processor(data):
+    # type: (GraphQLSchema) -> EventProcessor
+    """Add request data and api_target to events."""
+
+    def inner(event, hint):
+        # type: (Dict[str, Any], Dict[str, Any]) -> Dict[str, Any]
+        if not isinstance(data, dict):
+            return event
+
+        try:
+            content_length = int((data.get("headers") or {}).get("Content-Length", 0))
+        except (TypeError, ValueError):
+            return event
+
+        if _should_send_default_pii() and request_body_within_bounds(
+            Hub.current.client, content_length
+        ):
+            request_info = event.setdefault("request", {})
+            request_info["api_target"] = "graphql"
+            request_info["data"] = data
+
+        elif event.get("request", {}).get("data"):
+            del event["request"]["data"]
+
+        return event
+
+    return inner
+
+
+def _make_response_event_processor(response):
+    # type: (dict) -> EventProcessor
+    """Add response data to the event's response context."""
+
+    def inner(event, hint):
+        # type: (Dict[str, Any], Dict[str, Any]) -> Dict[str, Any]
+        if _should_send_default_pii() and response.get("errors"):
+            request_info = event.setdefault("contexts", {})
+            request_info["response"] = {
+                "data": response,
+            }
+
+        return event
+
+    return inner
---
+++ tests/integrations/ariadne/test_ariadne.py
@@ -0,0 +1,141 @@
+import pytest
+
+from ariadne import gql, graphql_sync, ObjectType, QueryType, make_executable_schema
+from ariadne.asgi import GraphQL
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+from flask import Flask, request, jsonify
+
+from sentry_sdk.integrations.ariadne import AriadneIntegration
+from sentry_sdk.integrations.fastapi import FastApiIntegration
+from sentry_sdk.integrations.flask import FlaskIntegration
+from sentry_sdk.integrations.logging import ignore_logger
+
+
+# to prevent the logging integration from capturing the exception first
+ignore_logger("ariadne")
+
+
+def schema_factory():
+    type_defs = gql(
+        """
+        type Query {
+            greeting(name: String): Greeting
+            error: String
+        }
+
+        type Greeting {
+            name: String
+        }
+    """
+    )
+
+    query = QueryType()
+    greeting = ObjectType("Greeting")
+
+    @query.field("greeting")
+    def resolve_greeting(*_, name=None):
+        return {"name": name}
+
+    @query.field("error")
+    def resolve_error(obj, *_):
+        raise RuntimeError("resolver failed")
+
+    @greeting.field("name")
+    def resolve_name(obj, *_):
+        return f"Hello, {obj['name']}!"
+
+    return make_executable_schema(type_defs, query)
+
+
+schema = schema_factory()
+
+async_app = FastAPI()
+async_app.mount("/graphql/", GraphQL(schema))
+
+sync_app = Flask(__name__)
+
+
+@sync_app.route("/graphql", methods=["POST"])
+def graphql_server():
+    data = request.get_json()
+    success, result = graphql_sync(schema, data)
+    return jsonify(result), 200
+
+
+@pytest.mark.parametrize(
+    "client",
+    [TestClient(async_app), sync_app.test_client()],
+)
+def test_capture_request_and_response_if_send_pii_is_on(
+    sentry_init, capture_events, client
+):
+    sentry_init(
+        send_default_pii=True,
+        integrations=[AriadneIntegration(), FastApiIntegration(), FlaskIntegration()],
+    )
+    events = capture_events()
+
+    query = {"query": "query ErrorQuery {error}"}
+    client.post("/graphql", json=query)
+
+    assert len(events) == 1
+
+    (event,) = events
+    assert event["exception"]["values"][0]["mechanism"]["type"] == "ariadne"
+    assert event["contexts"]["response"] == {
+        "data": {
+            "data": {"error": None},
+            "errors": [
+                {
+                    "locations": [{"column": 19, "line": 1}],
+                    "message": "resolver failed",
+                    "path": ["error"],
+                }
+            ],
+        }
+    }
+    assert event["request"]["api_target"] == "graphql"
+    assert event["request"]["data"] == query
+
+
+@pytest.mark.parametrize(
+    "client",
+    [TestClient(async_app), sync_app.test_client()],
+)
+def test_do_not_capture_request_and_response_if_send_pii_is_off(
+    sentry_init, capture_events, client
+):
+    sentry_init(
+        integrations=[AriadneIntegration(), FastApiIntegration(), FlaskIntegration()],
+    )
+    events = capture_events()
+
+    query = {"query": "query ErrorQuery {error}"}
+    client.post("/graphql", json=query)
+
+    assert len(events) == 1
+
+    (event,) = events
+    assert event["exception"]["values"][0]["mechanism"]["type"] == "ariadne"
+    assert "data" not in event["request"]
+    assert "response" not in event["contexts"]
+
+
+@pytest.mark.parametrize(
+    "client",
+    [TestClient(async_app), sync_app.test_client()],
+)
+def test_no_event_if_no_errors(sentry_init, capture_events, client):
+    sentry_init(
+        integrations=[AriadneIntegration(), FastApiIntegration(), FlaskIntegration()],
+    )
+    events = capture_events()
+
+    query = {
+        "query": "query GreetingQuery($name: String) { greeting(name: $name) {name} }",
+        "variables": {"name": "some name"},
+    }
+    client.post("/graphql", json=query)
+
+    assert len(events) == 0
