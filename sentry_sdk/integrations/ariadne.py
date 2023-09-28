25495d565b8d4b12733d4f0cbf0a33233b0f6a28
Ivana Kellyerova <ivana.kellyerova@sentry.io>
2023-09-28 13:37:29 +0200

    ignore logger

--- sentry_sdk/integrations/ariadne.py
+++ sentry_sdk/integrations/ariadne.py
@@ -2,6 +2,7 @@ from importlib import import_module
 
 from sentry_sdk.hub import Hub, _should_send_default_pii
 from sentry_sdk.integrations import DidNotEnable, Integration
+from sentry_sdk.integrations.logging import ignore_logger
 from sentry_sdk.integrations.modules import _get_installed_modules
 from sentry_sdk.integrations._wsgi_common import request_body_within_bounds
 from sentry_sdk.utils import (
@@ -40,6 +41,8 @@ class AriadneIntegration(Integration):
 
         if version < (0, 20):
             raise DidNotEnable("ariadne 0.20 or newer required.")
+
+        ignore_logger("ariadne")
 
         _patch_graphql()
 
