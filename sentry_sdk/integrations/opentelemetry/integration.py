aed4cc92134f88b18c601b6c7c889d6eddb63dba
Ivana Kellyerova <ivana.kellyerova@sentry.io>
2023-10-10 14:10:47 +0200

    introduce package_version helper

--- sentry_sdk/integrations/opentelemetry/integration.py
+++ sentry_sdk/integrations/opentelemetry/integration.py
@@ -9,8 +9,7 @@ from importlib import import_module
 from sentry_sdk.integrations import DidNotEnable, Integration
 from sentry_sdk.integrations.opentelemetry.span_processor import SentrySpanProcessor
 from sentry_sdk.integrations.opentelemetry.propagator import SentryPropagator
-from sentry_sdk.integrations.modules import _get_installed_modules
-from sentry_sdk.utils import logger
+from sentry_sdk.utils import logger, _get_installed_modules
 from sentry_sdk._types import TYPE_CHECKING
 
 try:
