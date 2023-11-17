d9bd1fde093cbd065bd48f591881d9390da21f39
Daniel Szoke <szokeasaurusrex@users.noreply.github.com>
2023-11-17 10:22:04 -0500

    Add unit test to check multiple `setup_integrations` calls

---
+++ tests/integrations/test_init.py
@@ -0,0 +1,26 @@
+from sentry_sdk.integrations import Integration, setup_integrations
+
+
+class NoOpIntegration(Integration):
+    """
+    A simple no-op integration for testing purposes.
+    """
+
+    identifier = "noop"
+
+    def setup_once():  # type: () -> None
+        pass
+
+    def __eq__(self, __value: object) -> bool:
+        """
+        All instances of NoOpIntegration should be considered equal to each other.
+        """
+        return type(__value) == type(self)
+
+
+def test_multiple_setup_integrations_calls():
+    first_call_return = setup_integrations([NoOpIntegration()], with_defaults=False)
+    assert first_call_return == {NoOpIntegration.identifier: NoOpIntegration()}
+
+    second_call_return = setup_integrations([NoOpIntegration()], with_defaults=False)
+    assert second_call_return == {NoOpIntegration.identifier: NoOpIntegration()}
