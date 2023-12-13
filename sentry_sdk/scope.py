4e78d1f5bb93e52390797b6fc8ca3877573a694a
Anton Pirker <anton.pirker@sentry.io>
2023-12-13 16:53:20 +0100

    Fixed test

--- sentry_sdk/scope.py
+++ sentry_sdk/scope.py
@@ -143,6 +143,7 @@ class Scope(object):
         "_force_auto_session_tracking",
         "_profile",
         "_propagation_context",
+        "_capture_internal_exception",
     )
 
     def __init__(self):
