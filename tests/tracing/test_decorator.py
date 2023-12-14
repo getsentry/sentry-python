779e90f5dbd5efac635c2dd790c6213074c543e9
Ivana Kellyerova <ivana.kellyerova@sentry.io>
2023-12-14 13:08:57 +0100

    Fix decorator tests

--- tests/tracing/test_decorator.py
+++ tests/tracing/test_decorator.py
@@ -2,9 +2,7 @@ from unittest import mock
 
 import pytest
 
-from sentry_sdk.tracing_utils import (
-    start_child_span_decorator as start_child_span_decorator_py3,
-)
+from sentry_sdk.tracing_utils import start_child_span_decorator
 from sentry_sdk.utils import logger
 
 
@@ -16,31 +14,31 @@ async def my_async_example_function():
     return "return_of_async_function"
 
 
-def test_trace_decorator_sync_py3():
+def test_trace_decorator_sync():
     fake_start_child = mock.MagicMock()
     fake_transaction = mock.MagicMock()
     fake_transaction.start_child = fake_start_child
 
     with mock.patch(
-        "sentry_sdk.tracing_utils.get_current_span",
+        "sentry_sdk.get_current_span",
         return_value=fake_transaction,
     ):
         result = my_example_function()
         fake_start_child.assert_not_called()
         assert result == "return_of_sync_function"
 
-        result2 = start_child_span_decorator_py3(my_example_function)()
+        result2 = start_child_span_decorator(my_example_function)()
         fake_start_child.assert_called_once_with(
-            op="function", description="test_decorator_py3.my_example_function"
+            op="function", description="test_decorator.my_example_function"
         )
         assert result2 == "return_of_sync_function"
 
 
-def test_trace_decorator_sync_py3_no_trx():
+def test_trace_decorator_sync_no_trx():
     fake_transaction = None
 
     with mock.patch(
-        "sentry_sdk.tracing_utils.get_current_span",
+        "sentry_sdk.get_current_span",
         return_value=fake_transaction,
     ):
         with mock.patch.object(logger, "warning", mock.Mock()) as fake_warning:
@@ -48,42 +46,42 @@ def test_trace_decorator_sync_py3_no_trx():
             fake_warning.assert_not_called()
             assert result == "return_of_sync_function"
 
-            result2 = start_child_span_decorator_py3(my_example_function)()
+            result2 = start_child_span_decorator(my_example_function)()
             fake_warning.assert_called_once_with(
                 "Can not create a child span for %s. "
                 "Please start a Sentry transaction before calling this function.",
-                "test_decorator_py3.my_example_function",
+                "test_decorator.my_example_function",
             )
             assert result2 == "return_of_sync_function"
 
 
 @pytest.mark.asyncio
-async def test_trace_decorator_async_py3():
+async def test_trace_decorator_async():
     fake_start_child = mock.MagicMock()
     fake_transaction = mock.MagicMock()
     fake_transaction.start_child = fake_start_child
 
     with mock.patch(
-        "sentry_sdk.tracing_utils.get_current_span",
+        "sentry_sdk.get_current_span",
         return_value=fake_transaction,
     ):
         result = await my_async_example_function()
         fake_start_child.assert_not_called()
         assert result == "return_of_async_function"
 
-        result2 = await start_child_span_decorator_py3(my_async_example_function)()
+        result2 = await start_child_span_decorator(my_async_example_function)()
         fake_start_child.assert_called_once_with(
-            op="function", description="test_decorator_py3.my_async_example_function"
+            op="function", description="test_decorator.my_async_example_function"
         )
         assert result2 == "return_of_async_function"
 
 
 @pytest.mark.asyncio
-async def test_trace_decorator_async_py3_no_trx():
+async def test_trace_decorator_async_no_trx():
     fake_transaction = None
 
     with mock.patch(
-        "sentry_sdk.tracing_utils.get_current_span",
+        "sentry_sdk.get_current_span",
         return_value=fake_transaction,
     ):
         with mock.patch.object(logger, "warning", mock.Mock()) as fake_warning:
@@ -91,10 +89,10 @@ async def test_trace_decorator_async_py3_no_trx():
             fake_warning.assert_not_called()
             assert result == "return_of_async_function"
 
-            result2 = await start_child_span_decorator_py3(my_async_example_function)()
+            result2 = await start_child_span_decorator(my_async_example_function)()
             fake_warning.assert_called_once_with(
                 "Can not create a child span for %s. "
                 "Please start a Sentry transaction before calling this function.",
-                "test_decorator_py3.my_async_example_function",
+                "test_decorator.my_async_example_function",
             )
             assert result2 == "return_of_async_function"
