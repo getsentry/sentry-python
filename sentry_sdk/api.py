8c5d3bbb660588070565cab310dbe71072d39c31
Anton Pirker <anton.pirker@sentry.io>
2023-12-19 11:11:53 +0100

    Cleanup

--- sentry_sdk/api.py
+++ sentry_sdk/api.py
@@ -2,7 +2,6 @@ import inspect
 
 from sentry_sdk import scope
 from sentry_sdk._types import TYPE_CHECKING
-from sentry_sdk.client import Client, NoopClient
 from sentry_sdk.hub import Hub
 from sentry_sdk.scope import Scope
 from sentry_sdk.tracing import NoOpSpan, Transaction
