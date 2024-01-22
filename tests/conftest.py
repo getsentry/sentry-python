65d830b6449510a8a1483f1e74f0876a2001fcc1
Daniel Szoke <szokeasaurusrex@users.noreply.github.com>
2024-01-22 11:17:54 +0100

    Manually set TestTransport

--- tests/conftest.py
+++ tests/conftest.py
@@ -154,14 +154,6 @@ def _capture_internal_warnings():
 
 
 @pytest.fixture
-def monkeypatch_test_transport(monkeypatch):
-    def inner(client):
-        monkeypatch.setattr(client, "transport", TestTransport())
-
-    return inner
-
-
-@pytest.fixture
 def validate_event_schema(tmpdir):
     def inner(event):
         if SENTRY_EVENT_SCHEMA:
@@ -182,14 +174,12 @@ def reset_integrations():
 
 
 @pytest.fixture
-def sentry_init(monkeypatch_test_transport, request):
+def sentry_init(request):
     def inner(*a, **kw):
         hub = sentry_sdk.Hub.current
+        kw.setdefault("transport", TestTransport())
         client = sentry_sdk.Client(*a, **kw)
         hub.bind_client(client)
-        if "transport" not in kw:
-            # TODO: Why do we even monkeypatch the transport, when we could just set it on the kw before constructing the Client?
-            monkeypatch_test_transport(sentry_sdk.Hub.current.client)
 
     if request.node.get_closest_marker("forked"):
         # Do not run isolation if the test is already running in
