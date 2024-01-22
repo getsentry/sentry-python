e681bdb7967d7d66da949a5c484f3b7d861ad304
Daniel Szoke <szokeasaurusrex@users.noreply.github.com>
2024-01-22 11:35:35 +0100

    Fix circular import

--- sentry_sdk/utils.py
+++ sentry_sdk/utils.py
@@ -25,9 +25,10 @@ except ImportError:
     BaseExceptionGroup = None  # type: ignore
 
 import sentry_sdk
+import sentry_sdk.consts as consts
 from sentry_sdk._compat import PY37
 from sentry_sdk._types import TYPE_CHECKING
-from sentry_sdk.consts import DEFAULT_MAX_VALUE_LENGTH, EndpointType
+from sentry_sdk.consts import DEFAULT_MAX_VALUE_LENGTH
 
 if TYPE_CHECKING:
     from types import FrameType, TracebackType
@@ -306,7 +307,7 @@ class Auth:
         self.client = client
 
     def get_api_url(
-        self, type=EndpointType.ENVELOPE  # type: EndpointType
+        self, type=consts.EndpointType.ENVELOPE  # type: consts.EndpointType
     ):
         # type: (...) -> str
         """Returns the API url for storing events."""
