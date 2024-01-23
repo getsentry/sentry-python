553cbbc768ba8f1ad747dc4ee2ba36ca7e971ff4
Anton Pirker <anton.pirker@sentry.io>
2024-01-23 09:41:25 +0100

    Bring back old _merge_scopes

--- sentry_sdk/scope.py
+++ sentry_sdk/scope.py
@@ -98,6 +98,28 @@ def _disable_capture(fn):
     return wrapper  # type: ignore
 
 
+def _merge_scopes(base, scope_change, scope_kwargs):
+    # type: (Scope, Optional[Any], Dict[str, Any]) -> Scope
+    if scope_change and scope_kwargs:
+        raise TypeError("cannot provide scope and kwargs")
+
+    if scope_change is not None:
+        final_scope = copy(base)
+        if callable(scope_change):
+            scope_change(final_scope)
+        else:
+            final_scope.update_from_scope(scope_change)
+
+    elif scope_kwargs:
+        final_scope = copy(base)
+        final_scope.update_from_kwargs(**scope_kwargs)
+
+    else:
+        final_scope = base
+
+    return final_scope
+
+
 def _copy_on_write(property_name):
     # type: (str) -> Callable[[Any], Any]
     """
@@ -1019,7 +1041,7 @@ class Scope(object):
         if client is None:
             return None
 
-        scope = Scope._merge_scopes(scope, scope_kwargs)
+        scope = _merge_scopes(self, scope, scope_kwargs)
 
         return client.capture_event(event=event, hint=hint, scope=scope)
 
