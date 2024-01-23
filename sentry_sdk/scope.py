18b13ca3a9e25c952454979ff00ea54cb1c90617
Daniel Szoke <szokeasaurusrex@users.noreply.github.com>
2024-01-23 11:11:26 +0100

    Update

--- sentry_sdk/scope.py
+++ sentry_sdk/scope.py
@@ -9,7 +9,6 @@ from sentry_sdk.attachments import Attachment
 from sentry_sdk.tracing_utils import (
     Baggage,
     extract_sentrytrace_data,
-    has_tracing_enabled,
     normalize_incoming_data,
 )
 from sentry_sdk.tracing import (
@@ -612,21 +611,10 @@ class Scope:
 
         # Add "trace" context
         if contexts.get("trace") is None:
-            if has_tracing_enabled(options) and self._span is not None:
+            if self._span is not None:
                 contexts["trace"] = self._span.get_trace_context()
             else:
                 contexts["trace"] = self.get_trace_context()
-
-        # Add "reply_id" context
-        try:
-            replay_id = contexts["trace"]["dynamic_sampling_context"]["replay_id"]
-        except (KeyError, TypeError):
-            replay_id = None
-
-        if replay_id is not None:
-            contexts["replay"] = {
-                "replay_id": replay_id,
-            }
 
     @_disable_capture
     def apply_to_event(
