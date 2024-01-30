771791bcf7e27f3736f53b0c473b91d31996cbb4
Ivana Kellyerova <ivana.kellyerova@sentry.io>
2024-01-30 15:35:44 +0100

    Remove is_gevent() check

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -954,26 +954,3 @@ def test_flush_recursion_protection_background_flush(
     m = parse_metrics(envelope.items[0].payload.get_bytes())
     assert len(m) == 1
     assert m[0][1] == "counter@none"
-
-
-@pytest.mark.forked
-@requires_gevent
-def test_no_metrics_with_gevent(sentry_init, capture_envelopes):
-    from gevent import monkey
-
-    monkey.patch_all()
-
-    sentry_init(
-        release="fun-release",
-        environment="not-fun-env",
-        _experiments={"enable_metrics": True, "metric_code_locations": True},
-    )
-    ts = time.time()
-    envelopes = capture_envelopes()
-
-    metrics.incr("foobar", 1.0, tags={"foo": "bar", "blub": "blah"}, timestamp=ts)
-    metrics.incr("foobar", 2.0, tags={"foo": "bar", "blub": "blah"}, timestamp=ts)
-    Hub.current.flush()
-
-    assert Hub.current.client.metrics_aggregator is None
-    assert len(envelopes) == 0
