19af46fdb843627ee3026e1dcfd5319a3e46fa86
Anton Pirker <anton.pirker@sentry.io>
2024-02-06 22:22:36 +0100

    Trying something

--- tests/integrations/threading/test_threading.py
+++ tests/integrations/threading/test_threading.py
@@ -115,6 +115,7 @@ def test_propagates_threadpool_hub(sentry_init, capture_events, propagate_hub):
         assert len(event["spans"]) == 0
 
 
+@pytest.mark.forked
 def test_circular_references(sentry_init, request):
     sentry_init(default_integrations=False, integrations=[ThreadingIntegration()])
 
