8bbcd269eca16ec9a8f44ac99a199add3ceec060
Anton Pirker <anton.pirker@sentry.io>
2024-02-13 16:31:13 +0100

    Fixed capture_event parameters in conftest

--- tests/integrations/conftest.py
+++ tests/integrations/conftest.py
@@ -8,12 +8,12 @@ def capture_exceptions(monkeypatch):
         errors = set()
         old_capture_event = sentry_sdk.Hub.capture_event
 
-        def capture_event(self, event, hint=None):
+        def capture_event(self, event, hint=None, scope=None):
             if hint:
                 if "exc_info" in hint:
                     error = hint["exc_info"][1]
                     errors.add(error)
-            return old_capture_event(self, event, hint=hint)
+            return old_capture_event(self, event, hint=hint, scope=scope)
 
         monkeypatch.setattr(sentry_sdk.Hub, "capture_event", capture_event)
         return errors
