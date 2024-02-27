524312aa5b89b4db0344bab59b9b1590bafc498e
Daniel Szoke <szokeasaurusrex@users.noreply.github.com>
2024-02-27 11:43:22 +0100

    Add some more keys, switch to class syntax

--- sentry_sdk/event_types.py
+++ sentry_sdk/event_types.py
@@ -5,21 +5,20 @@ if TYPE_CHECKING:
     from typing import Literal, Union
     from typing_extensions import TypedDict, Required
 
-    Event = TypedDict(
-        "Event",
-        {
-            "event_id": Required[str],
-            "platform": Required[Literal["python"]],
-            "timestamp": Required[Union[str, int, float]],
-            "dist": str,
-            "environment": str,
-            "extra": dict[object, object],
-            "fingerprint": list[str],
-            "level": Literal["fatal", "error", "warning", "info", "debug"],
-            "modules": dict[str, str],
-            "release": str,
-            "server_name": str,
-            "transaction": str,
-        },
-        total=False,
-    )
+    class Event(TypedDict, total=False):
+        event_id: Required[str]
+        platform: Required[Literal["python"]]
+        timestamp: Required[Union[str, int, float]]
+        dist: str
+        environment: str
+        extra: dict[object, object]
+        fingerprint: list[str]
+        level: Literal["fatal", "error", "warning", "info", "debug"]
+        logger: str
+        modules: dict[str, str]
+        release: str
+        server_name: str
+        tags: Union[
+            list[str], dict[str, object]
+        ]  # Tags must be less than 200 characters each
+        transaction: str
