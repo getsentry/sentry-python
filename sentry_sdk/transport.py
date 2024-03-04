c9c01c749f39b7503c25941cbc8181c0c473c36e
Ivana Kellyerova <ivana.kellyerova@sentry.io>
2024-03-04 17:31:11 +0100

    feat(transport): Expose socket_options

--- sentry_sdk/transport.py
+++ sentry_sdk/transport.py
@@ -1,18 +1,18 @@
 from __future__ import print_function
 
 import io
-import urllib3
-import certifi
 import gzip
 import time
-
 from datetime import timedelta
 from collections import defaultdict
+
+import urllib3
+import certifi
+from urllib3.connection import HTTPConnection
 
 from sentry_sdk.utils import Dsn, logger, capture_internal_exceptions, json_dumps
 from sentry_sdk.worker import BackgroundWorker
 from sentry_sdk.envelope import Envelope, Item, PayloadRef
-
 from sentry_sdk._compat import datetime_utcnow
 from sentry_sdk._types import TYPE_CHECKING
 
@@ -441,11 +441,18 @@ class HttpTransport(Transport):
 
     def _get_pool_options(self, ca_certs):
         # type: (Optional[Any]) -> Dict[str, Any]
-        return {
+        options = {
             "num_pools": self._num_pools,
             "cert_reqs": "CERT_REQUIRED",
             "ca_certs": ca_certs or certifi.where(),
         }
+
+        if self.options["socket_options"]:
+            options["socket_options"] = (
+                HTTPConnection.default_socket_options + self.options["socket_options"]
+            )
+
+        return options
 
     def _in_no_proxy(self, parsed_dsn):
         # type: (Dsn) -> bool
