7eb109f16220fe4532f111e3e7584948bca0635e
Ivana Kellyerova <ivana.kellyerova@sentry.io>
2024-03-07 13:48:38 +0100

    ref(awslambda): xfail broken tests for now

--- tests/integrations/aws_lambda/test_aws.py
+++ tests/integrations/aws_lambda/test_aws.py
@@ -661,6 +661,9 @@ def test_traces_sampler_gets_correct_values_in_sampling_context(
     assert response["Payload"]["AssertionError raised"] is False
 
 
+@pytest.mark.xfail(
+    "The limited log output we depend on is being clogged by a new warning"
+)
 def test_serverless_no_code_instrumentation(run_lambda_function):
     """
     Test that ensures that just by adding a lambda layer containing the
@@ -705,6 +708,9 @@ def test_serverless_no_code_instrumentation(run_lambda_function):
         assert "sentry_handler" in response["LogResult"][3].decode("utf-8")
 
 
+@pytest.mark.xfail(
+    "The limited log output we depend on is being clogged by a new warning"
+)
 def test_error_has_new_trace_context_performance_enabled(run_lambda_function):
     envelopes, _, _ = run_lambda_function(
         LAMBDA_PRELUDE
@@ -767,6 +773,9 @@ def test_error_has_new_trace_context_performance_disabled(run_lambda_function):
     )
 
 
+@pytest.mark.xfail(
+    "The limited log output we depend on is being clogged by a new warning"
+)
 def test_error_has_existing_trace_context_performance_enabled(run_lambda_function):
     trace_id = "471a43a4192642f0b136d5159a501701"
     parent_span_id = "6e8f22c393e68f19"
