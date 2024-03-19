41b433e0e41841ffe0c0b3f54b5cbbbe68e07455
Anton Pirker <anton.pirker@sentry.io>
2024-03-19 09:26:13 +0100

    fix?

--- tests/integrations/cloud_resource_context/test_cloud_resource_context.py
+++ tests/integrations/cloud_resource_context/test_cloud_resource_context.py
@@ -395,7 +395,12 @@ def test_setup_once(
                 fake_set_context.assert_not_called()
 
             if warning_called:
-                print(fake_warning.call_args_list)
-                assert fake_warning.call_count == 1
+                correct_warning_found = False
+                for call in fake_warning.call_args_list:
+                    if call[0][0].startswith('Invalid value for cloud_provider:')
+                        correct_warning_found = True
+                        break
+
+                assert correct_warning_found
             else:
                 fake_warning.assert_not_called()
