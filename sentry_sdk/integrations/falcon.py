b1774c6ae8d427eb536b5e57e0e775bb8ec38479
Ivana Kellyerova <ivana.kellyerova@sentry.io>
2024-03-21 13:06:54 +0100

    fix

--- sentry_sdk/integrations/falcon.py
+++ sentry_sdk/integrations/falcon.py
@@ -6,7 +6,6 @@ from sentry_sdk.scope import Scope
 from sentry_sdk.tracing import SOURCE_FOR_STYLE
 from sentry_sdk.utils import (
     capture_internal_exceptions,
-    ensure_integration_enabled,
     event_from_exception,
     parse_version,
 )
@@ -149,9 +148,12 @@ def _patch_wsgi_app():
     # type: () -> None
     original_wsgi_app = falcon_app_class.__call__
 
-    @ensure_integration_enabled(FalconIntegration, original_wsgi_app)
     def sentry_patched_wsgi_app(self, env, start_response):
         # type: (falcon.API, Any, Any) -> Any
+        integration = sentry_sdk.get_client().get_integration(FalconIntegration)
+        if integration is None:
+            return original_wsgi_app(self, env, start_response)
+
         sentry_wrapped = SentryWsgiMiddleware(
             lambda envi, start_resp: original_wsgi_app(self, envi, start_resp)
         )
