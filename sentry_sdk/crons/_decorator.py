3dc5bc2379a126dc187de8a6be76163f8a981e54
Ivana Kellyerova <ivana.kellyerova@sentry.io>
2024-03-28 11:49:52 +0100

    fix mypy

--- sentry_sdk/crons/_decorator.py
+++ sentry_sdk/crons/_decorator.py
@@ -4,7 +4,13 @@ from inspect import iscoroutinefunction
 from sentry_sdk._types import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from typing import Any, Callable, ParamSpec, TypeVar
+    from typing import (
+        Awaitable,
+        Callable,
+        ParamSpec,
+        TypeVar,
+        Union,
+    )
 
     P = ParamSpec("P")
     R = TypeVar("R")
@@ -12,20 +18,18 @@ if TYPE_CHECKING:
 
 class MonitorMixin:
     def __call__(self, fn):
-        # type: (Callable[P, R]) -> Callable[P, R]
+        # type: (Callable[P, R]) -> Callable[P, Union[R, Awaitable[R]]]
         if iscoroutinefunction(fn):
 
             @wraps(fn)
-            async def inner(*args, **kwargs):
-                # type: (Any, Any) -> Any
+            async def inner(*args: "P.args", **kwargs: "P.kwargs") -> R:
                 with self:  # type: ignore[attr-defined]
                     return await fn(*args, **kwargs)
 
         else:
 
             @wraps(fn)
-            def inner(*args, **kwargs):
-                # type: (Any, Any) -> Any
+            def inner(*args: "P.args", **kwargs: "P.kwargs") -> R:
                 with self:  # type: ignore[attr-defined]
                     return fn(*args, **kwargs)
 
