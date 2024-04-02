725a80012682106b0d297680851d2de0717cbaa0
Daniel Szoke <szokeasaurusrex@users.noreply.github.com>
2024-04-02 12:41:01 +0200

    Skip tests below 3.10

--- tests/test_types.py
+++ tests/test_types.py
@@ -1,6 +1,13 @@
+import sys
+
+import pytest
 from sentry_sdk.types import Event, Hint
 
 
+@pytest.mark.skipif(
+    sys.version_info < (3, 10),
+    reason="Type hinting with `|` is available in Python 3.10+",
+)
 def test_event_or_none_runtime():
     """
     Ensures that the `Event` type's runtime value supports the `|` operation with `None`.
@@ -10,6 +17,10 @@ def test_event_or_none_runtime():
     Event | None
 
 
+@pytest.mark.skipif(
+    sys.version_info < (3, 10),
+    reason="Type hinting with `|` is available in Python 3.10+",
+)
 def test_hint_or_none_runtime():
     """
     Analogue to `test_event_or_none_runtime`, but for the `Hint` type.
