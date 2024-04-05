7dfbaca782b24ec6ab8851268a5e61101c252325
Anton Pirker <anton.pirker@sentry.io>
2024-04-05 14:16:42 +0200

    use a noop db setup fixed to prevent db creation

--- tests/integrations/conftest.py
+++ tests/integrations/conftest.py
@@ -29,3 +29,13 @@ def capture_exceptions(monkeypatch):
         return errors
 
     return inner
+
+
+@pytest.fixture(scope='session')
+def django_db_setup():
+    print("Setting up Django database #################################################")
+    # settings.DATABASES['default'] = {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'HOST': 'db.example.com',
+    #     'NAME': 'external_db',
+    # }
