5520bdb05eacc69c78713557dae5788549ece052
Anton Pirker <anton.pirker@sentry.io>
2024-05-06 13:32:43 +0200

    Fix DSC in celery tasks started by Celery Beat. (#3047)
    Only create the span for enqueuing the task when we are not currently running in a celery beat task. (because then there is only the span without a transaction and thus the baggage header can not be given to the child celery task.) Without the span the child celery task creates its own trace, this is what we want.

--- scripts/aws_lambda_functions/sentryPythonDeleteTestFunctions/lambda_function.py
+++ scripts/aws_lambda_functions/sentryPythonDeleteTestFunctions/lambda_function.py
@@ -1,12 +1,12 @@
 import boto3
-import sentry_sdk 
+import sentry_sdk
 
 
 monitor_slug = "python-sdk-aws-lambda-tests-cleanup"
 monitor_config = {
     "schedule": {
         "type": "crontab",
-        "value": "0 12 * * 0", # 12 o'clock on Sunday
+        "value": "0 12 * * 0",  # 12 o'clock on Sunday
     },
     "timezone": "UTC",
     "checkin_margin": 2,
@@ -24,7 +24,7 @@ def delete_lambda_functions(prefix="test_"):
     """
     client = boto3.client("lambda", region_name="us-east-1")
     functions_deleted = 0
-    
+
     functions_paginator = client.get_paginator("list_functions")
     for functions_page in functions_paginator.paginate():
         for func in functions_page["Functions"]:
@@ -39,17 +39,17 @@ def delete_lambda_functions(prefix="test_"):
                     print(f"Got exception: {ex}")
 
     return functions_deleted
-    
+
 
 def lambda_handler(event, context):
     functions_deleted = delete_lambda_functions()
-    
+
     sentry_sdk.metrics.gauge(
-        key="num_aws_functions_deleted", 
+        key="num_aws_functions_deleted",
         value=functions_deleted,
     )
-    
+
     return {
-        'statusCode': 200,
-        'body': f"{functions_deleted} AWS Lambda functions deleted successfully."
+        "statusCode": 200,
+        "body": f"{functions_deleted} AWS Lambda functions deleted successfully.",
     }
