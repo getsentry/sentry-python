9a04d67d242d71750bec5b100a255cbda4be440b
Arjen Nienhuis <arjen@zorgdoc.nl>
2024-05-08 10:02:29 +0200

    Add support for add_note()

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -999,3 +999,47 @@ def test_hub_current_deprecation_warning():
 def test_hub_main_deprecation_warnings():
     with pytest.warns(sentry_sdk.hub.SentryHubDeprecationWarning):
         Hub.main
+
+
+@pytest.mark.skipif(sys.version_info < (3, 11), reason="add_note() not supported")
+def test_notes(sentry_init, capture_events):
+    sentry_init()
+    events = capture_events()
+    try:
+        e = ValueError("aha!")
+        e.add_note("Test 123")
+        raise e
+    except Exception:
+        capture_exception()
+
+    (event,) = events
+
+    assert event["exception"]["values"][0]["value"] == "aha!\nTest 123"
+
+
+@pytest.mark.skipif(sys.version_info < (3, 11), reason="add_note() not supported")
+def test_notes_safe_str(sentry_init, capture_events):
+    class Note2:
+        def __repr__(self):
+            raise TypeError
+
+        def __str__(self):
+            raise TypeError
+
+    sentry_init()
+    events = capture_events()
+    try:
+        e = ValueError("aha!")
+        e.add_note("note 1")
+        e.__notes__.append(Note2())  # type: ignore
+        e.add_note("note 3")
+        raise e
+    except Exception:
+        capture_exception()
+
+    (event,) = events
+
+    assert (
+        event["exception"]["values"][0]["value"]
+        == "aha!\nnote 1\n<broken repr>\nnote 3"
+    )
