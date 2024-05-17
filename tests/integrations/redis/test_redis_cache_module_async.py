f603fb7be3c23979e6117c41f8dd1e84ee8d5b64
Anton Pirker <anton.pirker@sentry.io>
2024-05-17 15:24:58 +0200

    Added async tests

---
+++ tests/integrations/redis/test_redis_cache_module_async.py
@@ -0,0 +1,171 @@
+import fakeredis
+from fakeredis.aioredis import FakeRedis as FakeRedisAsync
+
+import pytest
+
+from sentry_sdk.integrations.redis import RedisIntegration
+from sentry_sdk.utils import parse_version
+import sentry_sdk
+
+
+FAKEREDIS_VERSION = parse_version(fakeredis.__version__)
+
+
+@pytest.mark.asyncio
+async def test_no_cache_basic(sentry_init, capture_events):
+    sentry_init(
+        integrations=[
+            RedisIntegration(),
+        ],
+        traces_sample_rate=1.0,
+    )
+    events = capture_events()
+
+    connection = FakeRedisAsync()
+    with sentry_sdk.start_transaction():
+        await connection.get("mycachekey")
+
+    (event,) = events
+    spans = event["spans"]
+    assert len(spans) == 1
+    assert spans[0]["op"] == "db.redis"
+
+
+@pytest.mark.asyncio
+async def test_cache_basic(sentry_init, capture_events):
+    sentry_init(
+        integrations=[
+            RedisIntegration(
+                cache_prefixes=["mycache"],
+            ),
+        ],
+        traces_sample_rate=1.0,
+    )
+    events = capture_events()
+
+    connection = FakeRedisAsync()
+    with sentry_sdk.start_transaction():
+        await connection.get("mycachekey")
+
+    (event,) = events
+    spans = event["spans"]
+    assert len(spans) == 2
+    assert spans[0]["op"] == "cache.get_item"
+    assert spans[1]["op"] == "db.redis"
+
+
+@pytest.mark.asyncio
+async def test_cache_keys(sentry_init, capture_events):
+    sentry_init(
+        integrations=[
+            RedisIntegration(
+                cache_prefixes=["bla", "blub"],
+            ),
+        ],
+        traces_sample_rate=1.0,
+    )
+    events = capture_events()
+
+    connection = FakeRedisAsync()
+    with sentry_sdk.start_transaction():
+        await connection.get("somethingelse")
+        await connection.get("blub")
+        await connection.get("blubkeything")
+        await connection.get("bl")
+
+    (event,) = events
+    spans = event["spans"]
+    assert len(spans) == 6
+    assert spans[0]["op"] == "db.redis"
+    assert spans[0]["description"] == "GET 'somethingelse'"
+
+    assert spans[1]["op"] == "cache.get_item"
+    assert spans[1]["description"] == "blub"
+    assert spans[2]["op"] == "db.redis"
+    assert spans[2]["description"] == "GET 'blub'"
+
+    assert spans[3]["op"] == "cache.get_item"
+    assert spans[3]["description"] == "blubkeything"
+    assert spans[4]["op"] == "db.redis"
+    assert spans[4]["description"] == "GET 'blubkeything'"
+
+    assert spans[5]["op"] == "db.redis"
+    assert spans[5]["description"] == "GET 'bl'"
+
+
+@pytest.mark.asyncio
+async def test_cache_data(sentry_init, capture_events):
+    sentry_init(
+        integrations=[
+            RedisIntegration(
+                cache_prefixes=["mycache"],
+            ),
+        ],
+        traces_sample_rate=1.0,
+    )
+    events = capture_events()
+
+    connection = FakeRedisAsync(host="mycacheserver.io", port=6378)
+    with sentry_sdk.start_transaction():
+        await connection.get("mycachekey")
+        await connection.set("mycachekey", "事实胜于雄辩")
+        await connection.get("mycachekey")
+
+    (event,) = events
+    spans = event["spans"]
+
+    assert len(spans) == 6
+
+    assert spans[0]["op"] == "cache.get_item"
+    assert spans[0]["description"] == "mycachekey"
+    assert spans[0]["data"]["cache.key"] == "mycachekey"
+    assert spans[0]["data"]["cache.hit"] == False  # noqa: E712
+    assert "cache.item_size" not in spans[0]["data"]
+    # very old fakeredis can not handle port and/or host.
+    # only applicable for Redis v3
+    if FAKEREDIS_VERSION <= (2, 7, 1):
+        assert "network.peer.port" not in spans[0]["data"]
+    else:
+        assert spans[0]["data"]["network.peer.port"] == 6378
+    if FAKEREDIS_VERSION <= (1, 7, 1):
+        assert "network.peer.address" not in spans[0]["data"]
+    else:
+        assert spans[0]["data"]["network.peer.address"] == "mycacheserver.io"
+
+    assert spans[1]["op"] == "db.redis"  # we ignore db spans in this test.
+
+    assert spans[2]["op"] == "cache.set_item"
+    assert spans[2]["description"] == "mycachekey"
+    assert spans[2]["data"]["cache.key"] == "mycachekey"
+    assert "cache.hit" not in spans[1]["data"]
+    assert spans[2]["data"]["cache.item_size"] == 18
+    # very old fakeredis can not handle port.
+    # only used with redis v3
+    if FAKEREDIS_VERSION <= (2, 7, 1):
+        assert "network.peer.port" not in spans[2]["data"]
+    else:
+        assert spans[2]["data"]["network.peer.port"] == 6378
+    if FAKEREDIS_VERSION <= (1, 7, 1):
+        assert "network.peer.address" not in spans[2]["data"]
+    else:
+        assert spans[2]["data"]["network.peer.address"] == "mycacheserver.io"
+
+    assert spans[3]["op"] == "db.redis"  # we ignore db spans in this test.
+
+    assert spans[4]["op"] == "cache.get_item"
+    assert spans[4]["description"] == "mycachekey"
+    assert spans[4]["data"]["cache.key"] == "mycachekey"
+    assert spans[4]["data"]["cache.hit"] == True  # noqa: E712
+    assert spans[4]["data"]["cache.item_size"] == 18
+    # very old fakeredis can not handle port.
+    # only used with redis v3
+    if FAKEREDIS_VERSION <= (2, 7, 1):
+        assert "network.peer.port" not in spans[4]["data"]
+    else:
+        assert spans[4]["data"]["network.peer.port"] == 6378
+    if FAKEREDIS_VERSION <= (1, 7, 1):
+        assert "network.peer.address" not in spans[4]["data"]
+    else:
+        assert spans[4]["data"]["network.peer.address"] == "mycacheserver.io"
+
+    assert spans[5]["op"] == "db.redis"  # we ignore db spans in this test.
