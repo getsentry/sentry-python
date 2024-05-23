8c33a760c6db8b91db4fb0c3b2e273410c39c696
Anton Pirker <anton.pirker@sentry.io>
2024-05-23 09:58:54 +0200

    Redis Cache Module - 2 - Add Cache Spans (#3075)
    * Some work to use same function for queries and caches module
    
    * Moved functions to better place
    
    * Added tests
    
    * Fix
    
    * Tests and linting
    
    * Thats important for Python 3.6
    
    * Fixed some tests
    
    * Removed ipdb
    
    * more fixing
    
    * Cleanup
    
    * Async cache spans
    
    * Added async tests
    
    * Fixed async tests
    
    * Guard for not running async tests when there is no async fakeredis for that python version
    
    * linting
    
    * Use new names for ops
    
    * Renamed for consistency
    
    * fix _get_op()
    
    * Cleaning up unused properties/parameters
    
    * Use _get_safe_key in Django integration
    
    * Fixed typing
    
    * More tests
    
    * Only return the keys in set_many, makes more sense
    
    * Linting
    
    * Cleanup
    
    * fix(clickhouse): `_sentry_span` might be missing (#3096)
    
    We started auto-enabling the ClickHouse integration in 2.0+. This led to it getting auto-enabled also for folks using ClickHouse with Django via `django-clickhouse-backend`, but it turns out that the integration doesn't work properly with `django-clickhouse-backend` and leads to `AttributeError: 'Connection' object has no attribute '_sentry_span'`.
    
    * Make _get_safe_key work for all multi key methods in django and redis
    
    * Fixed kwargs case and updated tests
    
    * Updated tests
    
    * cache.set should be cache.put
    
    * Fix `cohere` testsuite for new release of `cohere`. (#3098)
    
    * Check for new class to signal end of stream
    
    ---------
    
    Co-authored-by: Ivana Kellyerova <ivana.kellyerova@sentry.io>

--- sentry_sdk/integrations/redis/__init__.py
+++ sentry_sdk/integrations/redis/__init__.py
@@ -1,3 +1,4 @@
+from sentry_sdk._types import TYPE_CHECKING
 from sentry_sdk.integrations import Integration, DidNotEnable
 from sentry_sdk.integrations.redis.consts import _DEFAULT_MAX_DATA_SIZE
 from sentry_sdk.integrations.redis.rb import _patch_rb
@@ -6,13 +7,17 @@ from sentry_sdk.integrations.redis.redis_cluster import _patch_redis_cluster
 from sentry_sdk.integrations.redis.redis_py_cluster_legacy import _patch_rediscluster
 from sentry_sdk.utils import logger
 
+if TYPE_CHECKING:
+    from typing import Optional
+
 
 class RedisIntegration(Integration):
     identifier = "redis"
 
-    def __init__(self, max_data_size=_DEFAULT_MAX_DATA_SIZE):
-        # type: (int) -> None
+    def __init__(self, max_data_size=_DEFAULT_MAX_DATA_SIZE, cache_prefixes=None):
+        # type: (int, Optional[list[str]]) -> None
         self.max_data_size = max_data_size
+        self.cache_prefixes = cache_prefixes if cache_prefixes is not None else []
 
     @staticmethod
     def setup_once():
--- sentry_sdk/integrations/redis/modules/queries.py
+++ sentry_sdk/integrations/redis/modules/queries.py
@@ -3,13 +3,44 @@ Code used for the Queries module in Sentry
 """
 
 from sentry_sdk._types import TYPE_CHECKING
-from sentry_sdk.consts import SPANDATA
+from sentry_sdk.consts import OP, SPANDATA
+from sentry_sdk.integrations.redis.utils import _get_safe_command
+from sentry_sdk.utils import capture_internal_exceptions
 
 
 if TYPE_CHECKING:
     from redis import Redis
+    from sentry_sdk.integrations.redis import RedisIntegration
     from sentry_sdk.tracing import Span
     from typing import Any
+
+
+def _compile_db_span_properties(integration, redis_command, args):
+    # type: (RedisIntegration, str, tuple[Any, ...]) -> dict[str, Any]
+    description = _get_db_span_description(integration, redis_command, args)
+
+    properties = {
+        "op": OP.DB_REDIS,
+        "description": description,
+    }
+
+    return properties
+
+
+def _get_db_span_description(integration, command_name, args):
+    # type: (RedisIntegration, str, tuple[Any, ...]) -> str
+    description = command_name
+
+    with capture_internal_exceptions():
+        description = _get_safe_command(command_name, args)
+
+    data_should_be_truncated = (
+        integration.max_data_size and len(description) > integration.max_data_size
+    )
+    if data_should_be_truncated:
+        description = description[: integration.max_data_size - len("...")] + "..."
+
+    return description
 
 
 def _set_db_data_on_span(span, connection_params):
