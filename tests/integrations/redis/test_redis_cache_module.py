ac4d657a88a74c8a0e0d963457fccc0bb4164fa7
Ivana Kellyerova <ivana.kellyerova@sentry.io>
2024-06-05 16:50:16 +0200

    fix(redis): Support multiple keys with cache_prefixes (#3136)

--- tests/integrations/redis/test_redis_cache_module.py
+++ tests/integrations/redis/test_redis_cache_module.py
@@ -198,6 +198,43 @@ def test_cache_data(sentry_init, capture_events):
     assert spans[5]["op"] == "db.redis"  # we ignore db spans in this test.
 
 
+def test_cache_prefixes(sentry_init, capture_events):
+    sentry_init(
+        integrations=[
+            RedisIntegration(
+                cache_prefixes=["yes"],
+            ),
+        ],
+        traces_sample_rate=1.0,
+    )
+    events = capture_events()
+
+    connection = FakeStrictRedis()
+    with sentry_sdk.start_transaction():
+        connection.mget("yes", "no")
+        connection.mget("no", 1, "yes")
+        connection.mget("no", "yes.1", "yes.2")
+        connection.mget("no.1", "no.2", "no.3")
+        connection.mget("no.1", "no.2", "no.actually.yes")
+        connection.mget(b"no.3", b"yes.5")
+        connection.mget(uuid.uuid4().bytes)
+        connection.mget(uuid.uuid4().bytes, "yes")
+
+    (event,) = events
+
+    spans = event["spans"]
+    assert len(spans) == 13  # 8 db spans + 5 cache spans
+
+    cache_spans = [span for span in spans if span["op"] == "cache.get"]
+    assert len(cache_spans) == 5
+
+    assert cache_spans[0]["description"] == "yes, no"
+    assert cache_spans[1]["description"] == "no, 1, yes"
+    assert cache_spans[2]["description"] == "no, yes.1, yes.2"
+    assert cache_spans[3]["description"] == "no.3, yes.5"
+    assert cache_spans[4]["description"] == ", yes"
+
+
 @pytest.mark.parametrize(
     "method_name,args,kwargs,expected_key",
     [
