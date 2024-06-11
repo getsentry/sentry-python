2b2084b3a9256a748dfdb34fdd50e06de063907d
Tony Xiao <txiao@sentry.io>
2024-06-11 15:41:36 -0400

    fix tests

--- tests/test_new_scopes_compat_event.py
+++ tests/test_new_scopes_compat_event.py
@@ -36,7 +36,7 @@ def expected_error():
                                     "abs_path": mock.ANY,
                                     "function": "_faulty_function",
                                     "module": "tests.test_new_scopes_compat_event",
-                                    "lineno": 240,
+                                    "lineno": 244,
                                     "pre_context": [
                                         "    return create_expected_transaction_event",
                                         "",
