d4db67d6a17bc2bbde4ef01a28e0866025308502
Anton Pirker <anton.pirker@sentry.io>
2024-06-19 13:42:56 +0200

    Added origin to celery integration

--- sentry_sdk/integrations/celery/__init__.py
+++ sentry_sdk/integrations/celery/__init__.py
@@ -58,6 +58,7 @@ CELERY_CONTROL_FLOW_EXCEPTIONS = (Retry, Ignore, Reject)
 
 class CeleryIntegration(Integration):
     identifier = "celery"
+    origin = f"auto.queue.{identifier}"
 
     def __init__(
         self,
@@ -261,7 +262,11 @@ def _wrap_apply_async(f):
         )
 
         span_mgr = (
-            sentry_sdk.start_span(op=OP.QUEUE_SUBMIT_CELERY, description=task.name)
+            sentry_sdk.start_span(
+                op=OP.QUEUE_SUBMIT_CELERY,
+                description=task.name,
+                origin=CeleryIntegration.origin,
+            )
             if not task_started_from_beat
             else NoOpMgr()
         )  # type: Union[Span, NoOpMgr]
@@ -304,6 +309,7 @@ def _wrap_tracer(task, f):
                     op=OP.QUEUE_TASK_CELERY,
                     name="unknown celery task",
                     source=TRANSACTION_SOURCE_TASK,
+                    origin=CeleryIntegration.origin,
                 )
                 transaction.name = task.name
                 transaction.set_status("ok")
@@ -357,7 +363,9 @@ def _wrap_task_call(task, f):
         # type: (*Any, **Any) -> Any
         try:
             with sentry_sdk.start_span(
-                op=OP.QUEUE_PROCESS, description=task.name
+                op=OP.QUEUE_PROCESS,
+                description=task.name,
+                origin=CeleryIntegration.origin,
             ) as span:
                 _set_messaging_destination_name(task, span)
                 with capture_internal_exceptions():
@@ -462,7 +470,11 @@ def _patch_producer_publish():
         routing_key = kwargs.get("routing_key")
         exchange = kwargs.get("exchange")
 
-        with sentry_sdk.start_span(op=OP.QUEUE_PUBLISH, description=task_name) as span:
+        with sentry_sdk.start_span(
+            op=OP.QUEUE_PUBLISH,
+            description=task_name,
+            origin=CeleryIntegration.origin,
+        ) as span:
             if task_id is not None:
                 span.set_data(SPANDATA.MESSAGING_MESSAGE_ID, task_id)
 
--- tests/integrations/celery/test_celery.py
+++ tests/integrations/celery/test_celery.py
@@ -226,7 +226,7 @@ def test_transaction_events(capture_events, init_celery, celery_invocation, task
             "data": ApproxDict(),
             "description": "dummy_task",
             "op": "queue.submit.celery",
-            "origin": "manual",
+            "origin": "auto.queue.celery",
             "parent_span_id": submission_event["contexts"]["trace"]["span_id"],
             "same_process_as_parent": True,
             "span_id": submission_event["spans"][0]["span_id"],
@@ -753,5 +753,51 @@ def test_producer_span_data(system, monkeypatch, sentry_init, capture_events):
     assert span["data"]["messaging.destination.name"] == "celery"
     assert "messaging.message.id" in span["data"]
     assert span["data"]["messaging.message.retry.count"] == 0
+
+    monkeypatch.setattr(kombu.messaging.Producer, "_publish", old_publish)
+
+
+def tests_span_origin_consumer(init_celery, capture_events):
+    celery = init_celery(enable_tracing=True)
+    celery.conf.broker_url = "redis://example.com"  # noqa: E231
+
+    events = capture_events()
+
+    @celery.task()
+    def task(): ...
+
+    task.apply_async()
+
+    (event,) = events
+
+    assert event["contexts"]["trace"]["origin"] == "auto.queue.celery"
+    assert event["spans"][0]["origin"] == "auto.queue.celery"
+
+
+def tests_span_origin_producer(monkeypatch, sentry_init, capture_events):
+    old_publish = kombu.messaging.Producer._publish
+
+    def publish(*args, **kwargs):
+        pass
+
+    monkeypatch.setattr(kombu.messaging.Producer, "_publish", publish)
+
+    sentry_init(integrations=[CeleryIntegration()], enable_tracing=True)
+    celery = Celery(__name__, broker="redis://example.com")  # noqa: E231
+
+    events = capture_events()
+
+    @celery.task()
+    def task(): ...
+
+    with start_transaction(name="custom_transaction"):
+        task.apply_async()
+
+    (event,) = events
+
+    assert event["contexts"]["trace"]["origin"] == "manual"
+
+    for span in event["spans"]:
+        assert span["origin"] == "auto.queue.celery"
 
     monkeypatch.setattr(kombu.messaging.Producer, "_publish", old_publish)
