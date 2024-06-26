c8fc77d385ef455728fd288988bd043b3bc30a30
Florian Dellekart <florian.dellekart@hotmail.de>
2024-06-26 21:57:17 +0200

    fix(grpc): Return propagate proper metadata object instead of list in client interceptor
    Fixes #2509

--- sentry_sdk/integrations/grpc/aio/client.py
+++ sentry_sdk/integrations/grpc/aio/client.py
@@ -6,6 +6,7 @@ from grpc.aio import (
     ClientCallDetails,
     UnaryUnaryCall,
     UnaryStreamCall,
+    Metadata,
 )
 from google.protobuf.message import Message
 
@@ -20,20 +21,10 @@ class ClientInterceptor:
     def _update_client_call_details_metadata_from_scope(
         client_call_details: ClientCallDetails,
     ) -> ClientCallDetails:
-        metadata = (
-            list(client_call_details.metadata) if client_call_details.metadata else []
-        )
+        if client_call_details.metadata is None:
+            client_call_details = client_call_details._replace(metadata=Metadata())
         for key, value in Scope.get_current_scope().iter_trace_propagation_headers():
-            metadata.append((key, value))
-
-        client_call_details = ClientCallDetails(
-            method=client_call_details.method,
-            timeout=client_call_details.timeout,
-            metadata=metadata,
-            credentials=client_call_details.credentials,
-            wait_for_ready=client_call_details.wait_for_ready,
-        )
-
+            client_call_details.metadata.add(key, value)
         return client_call_details
 
 
