ef1bc38cd807d6e314bc0f9c19bed6f43be00825
Daniel Szoke <daniel.szoke@sentry.io>
2024-08-09 11:14:41 +0200

    feat(sessions): Remove deprecated functions
    Remove the deprecated functions from `sessions.py` (this includes all Hub-based logic) and all tests for these functions.
    
    BREAKING CHANGE: Remove `sentry_sdk.sessions.auto_session_tracking`.
    
    BREAKING CHANGE: Remove `sentry_sdk.sessions.is_auto_session_tracking_enabled`.
    
    BREAKING CHANGE: Remove `sentry_sdk.sessions.is_auto_session_tracking_enabled_scope`.
    
    BREAKING CHANGE: Remove `sentry_sdk.sessions.is_auto_session_tracking_enabled_scope`.
    
    Closes: #3416

--- tests/test_sessions.py
+++ tests/test_sessions.py
@@ -1,7 +1,7 @@
 from unittest import mock
 
 import sentry_sdk
-from sentry_sdk.sessions import auto_session_tracking, track_session
+from sentry_sdk.sessions import track_session
 
 
 def sorted_aggregates(item):
@@ -83,47 +83,6 @@ def test_aggregates(sentry_init, capture_envelopes):
     assert aggregates[0]["errored"] == 1
 
 
-def test_aggregates_deprecated(
-    sentry_init, capture_envelopes, suppress_deprecation_warnings
-):
-    sentry_init(
-        release="fun-release",
-        environment="not-fun-env",
-    )
-    envelopes = capture_envelopes()
-
-    with auto_session_tracking(session_mode="request"):
-        with sentry_sdk.new_scope() as scope:
-            try:
-                scope.set_user({"id": "42"})
-                raise Exception("all is wrong")
-            except Exception:
-                sentry_sdk.capture_exception()
-
-    with auto_session_tracking(session_mode="request"):
-        pass
-
-    sentry_sdk.get_isolation_scope().start_session(session_mode="request")
-    sentry_sdk.get_isolation_scope().end_session()
-    sentry_sdk.flush()
-
-    assert len(envelopes) == 2
-    assert envelopes[0].get_event() is not None
-
-    sess = envelopes[1]
-    assert len(sess.items) == 1
-    sess_event = sess.items[0].payload.json
-    assert sess_event["attrs"] == {
-        "release": "fun-release",
-        "environment": "not-fun-env",
-    }
-
-    aggregates = sorted_aggregates(sess_event)
-    assert len(aggregates) == 1
-    assert aggregates[0]["exited"] == 2
-    assert aggregates[0]["errored"] == 1
-
-
 def test_aggregates_explicitly_disabled_session_tracking_request_mode(
     sentry_init, capture_envelopes
 ):
@@ -142,38 +101,6 @@ def test_aggregates_explicitly_disabled_session_tracking_request_mode(
     with sentry_sdk.isolation_scope() as scope:
         with track_session(scope, session_mode="request"):
             pass
-
-    sentry_sdk.get_isolation_scope().start_session(session_mode="request")
-    sentry_sdk.get_isolation_scope().end_session()
-    sentry_sdk.flush()
-
-    sess = envelopes[1]
-    assert len(sess.items) == 1
-    sess_event = sess.items[0].payload.json
-
-    aggregates = sorted_aggregates(sess_event)
-    assert len(aggregates) == 1
-    assert aggregates[0]["exited"] == 1
-    assert "errored" not in aggregates[0]
-
-
-def test_aggregates_explicitly_disabled_session_tracking_request_mode_deprecated(
-    sentry_init, capture_envelopes, suppress_deprecation_warnings
-):
-    sentry_init(
-        release="fun-release", environment="not-fun-env", auto_session_tracking=False
-    )
-    envelopes = capture_envelopes()
-
-    with auto_session_tracking(session_mode="request"):
-        with sentry_sdk.new_scope():
-            try:
-                raise Exception("all is wrong")
-            except Exception:
-                sentry_sdk.capture_exception()
-
-    with auto_session_tracking(session_mode="request"):
-        pass
 
     sentry_sdk.get_isolation_scope().start_session(session_mode="request")
     sentry_sdk.get_isolation_scope().end_session()
@@ -210,34 +137,6 @@ def test_no_thread_on_shutdown_no_errors(sentry_init):
         with sentry_sdk.isolation_scope() as scope:
             with track_session(scope, session_mode="request"):
                 pass
-
-        sentry_sdk.get_isolation_scope().start_session(session_mode="request")
-        sentry_sdk.get_isolation_scope().end_session()
-        sentry_sdk.flush()
-
-
-def test_no_thread_on_shutdown_no_errors_deprecated(
-    sentry_init, suppress_deprecation_warnings
-):
-    sentry_init(
-        release="fun-release",
-        environment="not-fun-env",
-    )
-
-    # make it seem like the interpreter is shutting down
-    with mock.patch(
-        "threading.Thread.start",
-        side_effect=RuntimeError("can't create new thread at interpreter shutdown"),
-    ):
-        with auto_session_tracking(session_mode="request"):
-            with sentry_sdk.new_scope():
-                try:
-                    raise Exception("all is wrong")
-                except Exception:
-                    sentry_sdk.capture_exception()
-
-        with auto_session_tracking(session_mode="request"):
-            pass
 
         sentry_sdk.get_isolation_scope().start_session(session_mode="request")
         sentry_sdk.get_isolation_scope().end_session()
