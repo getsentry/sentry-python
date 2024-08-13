67a5823eca559bad51f86adfa0653f659adcb6e8
Ivana Kellyer <ivana.kellyer@sentry.io>
2024-08-13 12:44:22 +0200

    Tweak OTel timestamp utils (#3436)

--- sentry_sdk/integrations/opentelemetry/utils.py
+++ sentry_sdk/integrations/opentelemetry/utils.py
@@ -14,7 +14,7 @@ from sentry_sdk.utils import Dsn
 from sentry_sdk._types import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from typing import Optional, Mapping, Sequence
+    from typing import Optional, Mapping, Sequence, Union
 
 
 GRPC_ERROR_MAP = {
@@ -72,9 +72,18 @@ def is_sentry_span(span):
     return False
 
 
-def convert_otel_timestamp(time):
+def convert_from_otel_timestamp(time):
     # type: (int) -> datetime
+    """Convert an OTel nanosecond-level timestamp to a datetime."""
     return datetime.fromtimestamp(time / 1e9, timezone.utc)
+
+
+def convert_to_otel_timestamp(time):
+    # type: (Union[datetime.datetime, float]) -> int
+    """Convert a datetime to an OTel timestamp (with nanosecond precision)."""
+    if isinstance(time, datetime):
+        return int(time.timestamp() * 1e9)
+    return int(time * 1e9)
 
 
 def extract_span_data(span):
