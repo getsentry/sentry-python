a8fbb9da8f5275a7dd9aa7ef6683fb9241a54463
Anton Pirker <anton.pirker@sentry.io>
2024-09-09 14:54:43 +0200

    Cleanup

--- sentry_sdk/integrations/opentelemetry/sampler.py
+++ sentry_sdk/integrations/opentelemetry/sampler.py
@@ -6,6 +6,7 @@ from opentelemetry.trace import SpanKind
 from opentelemetry.util.types import Attributes
 
 import sentry_sdk
+from sentry_sdk.integrations.opentelemetry.consts import SENTRY_TRACE_STATE_DROPPED
 from sentry_sdk.tracing_utils import has_tracing_enabled
 from sentry_sdk.utils import logger
 
@@ -20,7 +21,7 @@ def get_parent_sampled(parent_context, trace_id):
     if is_span_context_valid and parent_context.trace_id == trace_id:
         if parent_context.is_remote:
             dropped = (
-                parent_context.trace_state.get("SENTRY_TRACE_STATE_DROPPED") == "true"
+                parent_context.trace_state.get(SENTRY_TRACE_STATE_DROPPED) == "true"
             )
             if parent_context.trace_flags.sampled:
                 return True
@@ -44,44 +45,6 @@ class SentrySampler(Sampler):
         links: Optional[Sequence["Link"]] = None,
         trace_state: Optional["TraceState"] = None,
     ) -> "SamplingResult":
-        """
-        Decisions:
-        # IsRecording() == false, span will not be recorded and all events and attributes will be dropped.
-        DROP = 0
-        # IsRecording() == true, but Sampled flag MUST NOT be set.
-        RECORD_ONLY = 1
-        # IsRecording() == true AND Sampled flag` MUST be set.
-        RECORD_AND_SAMPLE = 2
-
-        Recorded:
-            sent to sentry
-        Sampled:
-            it is not dropped, but could be NonRecordingSpan that is never sent to Sentry (for trace propagation)
-        """
-        """
-            SamplingResult
-                decision: A sampling decision based off of whether the span is recorded
-                    and the sampled flag in trace flags in the span context.
-                attributes: Attributes to add to the `opentelemetry.trace.Span`.
-                trace_state: The tracestate used for the `opentelemetry.trace.Span`.
-                    Could possibly have been modified by the sampler.
-        """
-        """
-            1. If a sampling decision is passed to `start_transaction`
-            (`start_transaction(name: "my transaction", sampled: True)`), that
-            decision will be used, regardless of anything else
-
-            2. If `traces_sampler` is defined, its decision will be used. It can
-            choose to keep or ignore any parent sampling decision, or use the
-            sampling context data to make its own decision or to choose a sample
-            rate for the transaction.
-
-            3. If `traces_sampler` is not defined, but there's a parent sampling
-            decision, the parent sampling decision will be used.
-
-            4. If `traces_sampler` is not defined and there's no parent sampling
-            decision, `traces_sample_rate` will be used.
-        """
         client = sentry_sdk.get_client()
 
         # No tracing enabled, thus no sampling
@@ -133,14 +96,12 @@ class SentrySampler(Sampler):
 
         # Roll the dice on sample rate
         sampled = random() < float(sample_rate)
-        print(f"- name: {name} / sample_rate: {sample_rate} / sampled: {sampled}")
+
         if sampled:
             return SamplingResult(Decision.RECORD_AND_SAMPLE)
         else:
-            # TODO-anton: set span_context.trace_state.
-            parent_context.trace_state.update("SENTRY_TRACE_STATE_DROPPED", "true")
+            parent_context.trace_state.update(SENTRY_TRACE_STATE_DROPPED, "true")
             return SamplingResult(Decision.DROP)
 
     def get_description(self) -> str:
-        print("YYYYYYYYYYYYYYYY")
         return self.__class__.__name__
