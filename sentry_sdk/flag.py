f7a722c8e246c9e1149a144b689f47f8cda7174f
Colton Allen <colton.allen@sentry.io>
2024-09-12 10:43:28 -0500

    Implement using LRU cache

--- sentry_sdk/flag.py
+++ sentry_sdk/flag.py
@@ -1,6 +1,5 @@
-import itertools
-import time
 from typing import TYPE_CHECKING
+from sentry_sdk._lru_cache import LRUCache, KEY, NEXT, VALUE
 
 
 if TYPE_CHECKING:
@@ -9,13 +8,11 @@ if TYPE_CHECKING:
 
 class Flag:
 
-    __slots__ = ("call_count", "last_call_time", "name", "last_result")
+    __slots__ = ("name", "result")
 
     def __init__(self, name, result):
         # type: (str, bool) -> None
-        self.call_count = 1
-        self.last_call_time = time.time()
-        self.last_result = result
+        self.result = result
         self.name = name
 
     @property
@@ -23,62 +20,20 @@ class Flag:
         # type: () -> FType
         return {
             "flag": self.name,
-            "result": self.last_result,
+            "result": self.result,
         }
-
-    def called(self, result):
-        # type: (bool) -> None
-        self.call_count += 1
-        self.last_call_time = time.time()
-        self.last_result = result
-        return None
 
 
 class FlagManager:
 
     def __init__(self, capacity):
         # type: (int) -> None
-        self._buffer = []  # type: list[Flag]
-        self._capacity = capacity
-        self._flag_idx_map = {}  # type: dict[str, int]
-        self._pointer = 0
-
-    @property
-    def index(self):
-        return self._pointer % self._capacity
+        self._cache = LRUCache(max_size=capacity)
 
     def add(self, name, result):
         # type: (str, bool) -> None
-        if name in self._flag_idx_map:
-            # If the flag exists in the set record that the flag was
-            # called again. Repeated calls to a flag does not alter its
-            # ordering in the buffer. This is not an LRU cache. It is a
-            # FIFO queue.
-            self._buffer[self._flag_idx_map[name]].called(result)
-            return None
-
-        flag = Flag(name, result)
-
-        if self._pointer >= self._capacity:
-            # The old flag is fetched and removed from the mapping.
-            old = self._buffer[self.index]
-            self._flag_idx_map.pop(old.name)
-
-            # The new flag overwrites the old flag in the buffer.
-            self._buffer[self.index] = flag
-        else:
-            # Because the slots have not been populated yet we append
-            # to the buffer until it fills up.
-            self._buffer.append(flag)
-
-        # The flag's name is added to the map with a copy of its index
-        # position. Subsequent calls to this flag will be de-duplicated
-        # unless the flag is removed from the buffer. In which case a
-        # new entry for the flag will be created.
-        self._flag_idx_map[flag.name] = self.index
-
-        # The pointer always points to the next slot written to.
-        self._pointer += 1
+        assert name is not None
+        self._cache.set(name, Flag(name, result))
 
     def serialize(self):
         # type: () -> Flags
@@ -86,16 +41,14 @@ class FlagManager:
 
         Flags are serialized in order of first-evaluation.
         """
-        if self._pointer >= self._capacity:
-            # We've wrapped around the list and now our evaluation results
-            # are unordered. Starting from the pointer (which always
-            # points to the next slot) iterate to the end of the list.
-            # Then starting from 0 iterate to the pointer.
-            iterator = itertools.chain(
-                range(self.index, self._capacity), range(0, self.index)
-            )
-            return [self._buffer[i].as_dict for i in iterator]
-        else:
-            # The buffer has not been filled. We can iterate up to the
-            # index position (which always points to the next slot).
-            return [self._buffer[i].as_dict for i in range(0, self.index)]
+
+        def iter_flags():
+            # This only works if you know the root node is the only
+            # item with a null key. The list is circularly linked so we
+            # need a termination condition.
+            node = self._cache.root[NEXT]
+            while node[KEY] is not None:
+                yield node[VALUE].as_dict
+                node = node[NEXT]
+
+        return [f for f in iter_flags()]
--- tests/test_flag.py
+++ tests/test_flag.py
@@ -1,20 +1,6 @@
 from sentry_sdk.flag import Flag, FlagManager
 
 
-def test_calling_flag():
-    flag = Flag("k", True)
-    last_call_time = flag.last_call_time
-
-    flag.called(False)
-    assert flag.name == "k"
-    assert flag.last_result is False
-    assert flag.last_call_time > last_call_time
-    assert flag.call_count == 2
-
-    flag.called(False)
-    assert flag.call_count == 3
-
-
 def test_serializing_flag():
     flag = Flag("k", True)
     assert flag.as_dict == {"flag": "k", "result": True}
