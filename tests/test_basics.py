857a9d304364679755653bcd29a7872400fef1b0
Anton Pirker <anton.pirker@sentry.io>
2024-09-13 14:42:04 +0200

    Also in other test

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -326,9 +326,9 @@ def test_breadcrumb_ordering(sentry_init, capture_events):
     events = capture_events()
 
     timestamps = [
-        datetime.datetime.now() - datetime.timedelta(days=10),
-        datetime.datetime.now() - datetime.timedelta(days=8),
-        datetime.datetime.now() - datetime.timedelta(days=12),
+        datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=10),
+        datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=8),
+        datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=12),
     ]
 
     for timestamp in timestamps:
@@ -344,8 +344,8 @@ def test_breadcrumb_ordering(sentry_init, capture_events):
 
     assert len(event["breadcrumbs"]["values"]) == len(timestamps)
     timestamps_from_event = [
-        datetime.datetime.strptime(
-            x["timestamp"].replace("Z", ""), "%Y-%m-%dT%H:%M:%S.%f"
+        datetime.datetime.strptime(x["timestamp"], "%Y-%m-%dT%H:%M:%S.%fZ").replace(
+            tzinfo=datetime.timezone.utc
         )
         for x in event["breadcrumbs"]["values"]
     ]
