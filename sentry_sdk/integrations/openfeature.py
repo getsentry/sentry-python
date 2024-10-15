928350a88e45b8a9112c44097bbd7dff601bda2f
Colton Allen <colton.allen@sentry.io>
2024-10-15 09:25:06 -0500

    Add type hints

--- sentry_sdk/integrations/openfeature.py
+++ sentry_sdk/integrations/openfeature.py
@@ -6,6 +6,8 @@ from sentry_sdk.integrations import DidNotEnable, Integration
 try:
     from openfeature import api
     from openfeature.hook import Hook
+    from sentry_sdk._types import Event, ExcInfo
+    from typing import Optional
 
     if TYPE_CHECKING:
         from openfeature.flag_evaluation import FlagEvaluationDetails
@@ -19,7 +21,9 @@ class OpenFeatureIntegration(Integration):
 
     @staticmethod
     def setup_once():
+        # type: () -> None
         def error_processor(event, exc_info):
+            # type: (Event, ExcInfo) -> Optional[Event]
             scope = sentry_sdk.get_current_scope()
             event["contexts"]["flags"] = {"values": scope.flags.get()}
             return event
