e762ef943a93f43674f1ce5be26fe0f53b091f77
Anton Pirker <anton.pirker@sentry.io>
2024-11-19 16:23:33 +0100

    .

--- sentry_sdk/tracing_utils.py
+++ sentry_sdk/tracing_utils.py
@@ -726,7 +726,7 @@ if TYPE_CHECKING:
     from sentry_sdk.tracing import Span
 
 
-def finish_running_transaction(scope):
+def finish_running_transaction(scope=None):
     # type: (Optional[sentry_sdk.Scope]) -> None
     current_scope = scope or sentry_sdk.get_current_scope()
     if current_scope.transaction is not None and hasattr(
