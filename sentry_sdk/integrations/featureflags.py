36bc8698dfbae98effae627cb9312eca228a64c6
Andrew Liu <159852527+aliu39@users.noreply.github.com>
2024-12-06 11:19:45 -0800

    Add docstrs

--- sentry_sdk/integrations/featureflags.py
+++ sentry_sdk/integrations/featureflags.py
@@ -5,6 +5,26 @@ from sentry_sdk.integrations import Integration
 
 
 class FeatureFlagsIntegration(Integration):
+    """
+    Sentry integration for buffering feature flags manually with an API and capturing them on
+    error events. We recommend you do this on each flag evaluation. Flags are buffered per Sentry
+    scope and limited to 100 per event.
+
+    See the [feature flag documentation](https://develop.sentry.dev/sdk/expected-features/#feature-flags)
+    for more information.
+
+    @example
+    ```
+    import sentry_sdk
+    from sentry_sdk.integrations.featureflags import FeatureFlagsIntegration, add_flag
+
+    sentry_sdk.init(dsn="my_dsn", integrations=[FeatureFlagsIntegration()]);
+
+    add_flag('my-flag', true);
+    sentry_sdk.capture_exception(Exception('broke')); // 'my-flag' should be captured on this Sentry event.
+    ```
+    """
+
     identifier = "featureflags"
 
     @staticmethod
@@ -14,5 +34,9 @@ class FeatureFlagsIntegration(Integration):
 
 
 def add_flag(flag: str, result: bool):
+    """
+    Records a flag and its value to be sent on subsequent error events. We recommend you do this
+    on flag evaluations. Flags are buffered per Sentry scope and limited to 100 per event.
+    """
     flags = sentry_sdk.get_current_scope().flags
     flags.set(flag, result)
