0fff0201004fcd16f7feb5cccec499c50131da95
Anton Pirker <anton.pirker@sentry.io>
2024-12-20 10:33:02 +0100

    Cleanup

--- tests/integrations/aws_lambda/test_aws.py
+++ tests/integrations/aws_lambda/test_aws.py
@@ -37,10 +37,10 @@ from textwrap import dedent
 import pytest
 
 RUNTIMES_TO_TEST = [
-    # "python3.8",
-    # "python3.9",
-    # "python3.10",
-    # "python3.11",
+    "python3.8",
+    "python3.9",
+    "python3.10",
+    "python3.11",
     "python3.12",
 ]
 
