da54e4981cb31e1d6e76071922144de717d4e738
Andrew Liu <159852527+aliu39@users.noreply.github.com>
2024-12-22 17:05:47 -0800

    wraps_original test

--- tests/integrations/unleash/test_unleash.py
+++ tests/integrations/unleash/test_unleash.py
@@ -1,8 +1,9 @@
+from random import random
 from unittest.mock import patch
 
 import sentry_sdk
 from sentry_sdk.integrations.unleash import UnleashIntegration
-from tests.integrations.unleash import MockUnleashClient
+from tests.integrations.unleash.testutils import MockUnleashClient
 
 original_is_enabled = MockUnleashClient.is_enabled
 original_get_variant = MockUnleashClient.get_variant
@@ -10,13 +11,13 @@ original_get_variant = MockUnleashClient.get_variant
 
 @patch("sentry_sdk.integrations.unleash.UnleashClient", MockUnleashClient)
 def test_is_enabled(sentry_init, capture_events, reset_integration):
-    mock_unleash_client = MockUnleashClient()
+    client = MockUnleashClient()
     reset_integration(UnleashIntegration.identifier)
     sentry_init(integrations=[UnleashIntegration()])
 
-    mock_unleash_client.is_enabled("hello")
-    mock_unleash_client.is_enabled("world")
-    mock_unleash_client.is_enabled("other")
+    client.is_enabled("hello")
+    client.is_enabled("world")
+    client.is_enabled("other")
 
     events = capture_events()
     sentry_sdk.capture_exception(Exception("something wrong!"))
@@ -33,16 +34,16 @@ def test_is_enabled(sentry_init, capture_events, reset_integration):
 
 @patch("sentry_sdk.integrations.unleash.UnleashClient", MockUnleashClient)
 def test_get_variant(sentry_init, capture_events, reset_integration):
-    mock_unleash_client = MockUnleashClient()
+    client = MockUnleashClient()
     reset_integration(UnleashIntegration.identifier)
     sentry_init(integrations=[UnleashIntegration()])
 
-    mock_unleash_client.get_variant("toggle_feature")
-    mock_unleash_client.get_variant("string_feature")
-    mock_unleash_client.get_variant("json_feature")
-    mock_unleash_client.get_variant("csv_feature")
-    mock_unleash_client.get_variant("number_feature")
-    mock_unleash_client.get_variant("unknown_feature")
+    client.get_variant("toggle_feature")
+    client.get_variant("string_feature")
+    client.get_variant("json_feature")
+    client.get_variant("csv_feature")
+    client.get_variant("number_feature")
+    client.get_variant("unknown_feature")
 
     events = capture_events()
     sentry_sdk.capture_exception(Exception("something wrong!"))
@@ -56,6 +57,37 @@ def test_get_variant(sentry_init, capture_events, reset_integration):
     }
 
 
+@patch("sentry_sdk.integrations.unleash.UnleashClient", MockUnleashClient)
+def test_wraps_original(sentry_init, reset_integration):
+    reset_integration(UnleashIntegration.identifier)
+
+    with patch(
+        "sentry_sdk.integrations.unleash.UnleashClient.is_enabled"
+    ) as mock_is_enabled:
+        with patch(
+            "sentry_sdk.integrations.unleash.UnleashClient.get_variant"
+        ) as mock_get_variant:
+            mock_is_enabled.return_value = random() < 0.5
+            mock_get_variant.return_value = {"enabled": random() < 0.5}
+            sentry_init(integrations=[UnleashIntegration()])
+            client = MockUnleashClient()
+
+            res = client.is_enabled("test-flag", "arg", kwarg=1)
+            assert res == mock_is_enabled.return_value
+            assert mock_is_enabled.call_args == (
+                (client, "test-flag", "arg"),
+                {"kwarg": 1},
+            )
+
+            res = client.get_variant("test-flag", "arg", kwarg=1)
+            assert res == mock_get_variant.return_value
+            assert mock_get_variant.call_args == (
+                (client, "test-flag", "arg"),
+                {"kwarg": 1},
+            )
+
+
+@patch("sentry_sdk.integrations.unleash.UnleashClient", MockUnleashClient)
 def test_wrapper_attributes(sentry_init, reset_integration):
     reset_integration(UnleashIntegration.identifier)
     sentry_init(integrations=[UnleashIntegration()])
