f54a0180089214a2cb86d9893f54377947d31c9e
Ivana Kellyer <ivana.kellyer@sentry.io>
2025-01-13 16:03:20 +0100

    Use a script to populate parts of tox.ini

---
+++ scripts/populate_tox/dependencies.py
@@ -0,0 +1,406 @@
+# The DEPENDENCIES dictionary defines the test dependencies of each integration
+# test suite.
+#
+# The format is:
+# ```
+# integration_name: {
+#     "package": name_of_main_package_on_pypi,
+#     "deps": {
+#          rule1: [package1, package2, ...],
+#          rule2: [package3, package4, ...],
+#      }
+# }
+# ```
+#
+# The following can be set as a rule:
+# - `*`: packages will be always installed
+# - a version bound on the main package (e.g. `<=0.32`): packages will only be installed if the main package falls into the version bounds specified
+# - specific Python version(s) in the form `py3.8,py3.9`: packages will only be installed if the Python version matches one from the list
+#
+# Rules can be used to specify version bounds on older versions of the main
+# package's dependencies, for example. If e.g. Flask tests generally need
+# Werkzeug and don't care about its version, but Flask older than 3.0 needs
+# a specific Werkzeug version to work, you can say:
+#
+# ```
+#     "flask": {
+#         "deps": {
+#             "*": ["Werkzeug"],
+#             "<3.0": ["Werkzeug<2.1.0"],
+#         }
+#     }
+# ````
+
+
+DEPENDENCIES = {
+    "aiohttp": {
+        "package": "aiohttp",
+        "deps": {"*": ["pytest-aiohttp", "pytest-asyncio"]},
+    },
+    "anthropic": {
+        "package": "anthropic",
+        "deps": {
+            "*": ["pytest-asyncio"],
+            "<=0.32": ["httpx<0.28.0"],
+        },
+    },
+    "ariadne": {
+        "package": "ariadne",
+        "deps": {
+            "*": ["fastapi", "flask", "httpx"],
+        },
+    },
+    "arq": {
+        "package": "arq",
+        "deps": {
+            "*": ["fakeredis>=2.2.0,<2.8", "pytest-asyncio", "async-timeout"],
+            "<=0.25": ["pydantic<2"],
+        },
+    },
+    "asyncpg": {
+        "package": "asyncpg",
+        "deps": {
+            "*": ["pytest-asyncio"],
+        },
+    },
+    "beam": {
+        "package": "apache-beam",
+        "deps": {
+            "*": [],
+        },
+    },
+    "boto3": {
+        "package": "boto3",
+        "deps": {
+            "*": [],
+        },
+    },
+    "bottle": {
+        "package": "bottle",
+        "deps": {
+            "*": ["werkzeug<2.1.0"],
+        },
+    },
+    "celery": {
+        "package": "celery",
+        "deps": {
+            "*": ["newrelic", "redis"],
+            "py3.7": ["importlib-metata<5.0"],
+        },
+    },
+    "chalice": {
+        "package": "chalice",
+        "deps": {
+            "*": ["pytest-chalice==0.0.5"],
+        },
+    },
+    "clickhouse_driver": {
+        "package": "clickhouse-driver",
+        "deps": {
+            "*": [],
+        },
+    },
+    "cohere": {
+        "package": "cohere",
+        "deps": {
+            "*": ["httpx"],
+        },
+    },
+    "django": {
+        "package": "django",
+        "deps": {
+            "*": [
+                "psycopg2-binary",
+                "werkzeug",
+            ],
+            ">=2.0,<3.0": ["six"],
+            "<=3.2": [
+                "werkzeug<2.1.0",
+                "djangorestframework>=3.0.0,<4.0.0",
+                "pytest-django",
+            ],
+            ">=2.0": ["channels[daphne]"],
+            "<=3.0": ["pytest-django<4.0"],
+            ">=4.0": ["djangorestframework", "pytest-asyncio"],
+        },
+    },
+    "dramatiq": {
+        "package": "dramatiq",
+        "deps": {},
+    },
+    "falcon": {
+        "package": "falcon",
+        "deps": {},
+    },
+    "fastapi": {
+        "package": "fastapi",
+        "deps": {
+            "*": [
+                "httpx",
+                "anyio<4.0.0",
+                "python-multipart",
+                "pytest-asyncio",
+                "requests",
+            ]
+        },
+    },
+    "flask": {
+        "package": "flask",
+        "deps": {
+            "*": ["flask-login", "werkzeug"],
+            "<3.0": ["werkzeug<2.1.0", "markupsafe<2.1.0"],
+        },
+    },
+    "gql": {
+        "package": "gql[all]",
+        "deps": {},
+    },
+    "graphene": {
+        "package": "graphene",
+        "deps": {"*": ["blinker", "fastapi", "flask", "httpx"]},
+    },
+    "grpc": {
+        "package": "grpcio",
+        "deps": {
+            "*": ["protobuf", "mypy-protobuf", "types-protobuf", "pytest-asyncio"]
+        },
+    },
+    "httpx": {
+        "package": "httpx",
+        "deps": {
+            "*": ["anyio<4.0.0", "pytest-httpx"],
+            "==0.16": ["pytest-httpx==0.10.0"],
+            "==0.18": ["pytest-httpx==0.12.0"],
+            "==0.20": ["pytest-httpx==0.14.0"],
+            "==0.22": ["pytest-httpx==0.19.0"],
+            "==0.23": ["pytest-httpx==0.21.0"],
+            "==0.24": ["pytest-httpx==0.22.0"],
+            "==0.25": ["pytest-httpx==0.25.0"],
+        },
+    },
+    "huey": {
+        "package": "huey",
+        "deps": {
+            "*": [],
+        },
+    },
+    "huggingface_hub": {
+        "package": "huggingface_hub",
+        "deps": {"*": []},
+    },
+    # XXX
+    # langchain-v0.1: openai~=1.0.0
+    # langchain-v0.1: tiktoken~=0.6.0
+    # langchain-v0.1: httpx<0.28.0
+    # langchain-v0.3: langchain-community
+    # langchain-v0.3: tiktoken
+    # langchain-v0.3: openai
+    # langchain-{latest,notiktoken}: langchain
+    # langchain-{latest,notiktoken}: langchain-openai
+    # langchain-{latest,notiktoken}: openai>=1.6.1
+    # langchain-latest: tiktoken~=0.6.0
+    "langchain": {
+        "package": "langchain",
+        "deps": {
+            "*": ["openai", "tiktoken", "httpx"],
+            ">=0.3": ["langchain-community"],
+        },
+    },
+    "langchain_notiktoken": {
+        "package": "langchain",
+        "deps": {
+            "*": ["openai", "httpx"],
+            ">=0.3": ["langchain-community"],
+        },
+    },
+    "litestar": {
+        "package": "litestar",
+        "deps": {
+            "*": ["pytest-asyncio", "python-multipart", "requests", "cryptography"],
+            "<=2.6": ["httpx<0.28"],
+        },
+    },
+    "loguru": {
+        "package": "loguru",
+        "deps": {
+            "*": [],
+        },
+    },
+    # XXX
+    # openai-latest: tiktoken~=0.6.0
+    "openai": {
+        "package": "openai",
+        "deps": {
+            "*": ["pytest-asyncio", "tiktoken", "httpx"],
+            "<=1.22": ["httpx<0.28.0"],
+        },
+    },
+    "openai_notiktoken": {
+        "package": "openai",
+        "deps": {
+            "*": ["pytest-asyncio", "httpx"],
+            "<=1.22": ["httpx<0.28.0"],
+        },
+    },
+    "openfeature": {
+        "package": "openfeature-sdk",
+        "deps": {
+            "*": [],
+        },
+    },
+    "launchdarkly": {
+        "package": "launchdarkly-server-sdk",
+        "deps": {
+            "*": [],
+        },
+    },
+    "opentelemetry": {
+        "package": "opentelemetry-distro",
+        "deps": {
+            "*": [],
+        },
+    },
+    "pure_eval": {
+        "package": "pure_eval",
+        "deps": {
+            "*": [],
+        },
+    },
+    "pymongo": {
+        "package": "pymongo",
+        "deps": {
+            "*": ["mockupdb"],
+        },
+    },
+    "pyramid": {
+        "package": "pyramid",
+        "deps": {
+            "*": ["werkzeug<2.1.0"],
+        },
+    },
+    "quart": {
+        "package": "quart",
+        "deps": {
+            "*": [
+                "quart-auth",
+                "pytest-asyncio",
+                "werkzeug",
+            ],
+            "<=0.16": [
+                "blinker<1.6",
+                "jinja2<3.1.0",
+                "Werkzeug<2.1.0",
+                "hypercorn<0.15.0",
+            ],
+            "py3.8": ["taskgroup==0.0.0a4"],
+        },
+    },
+    "ray": {
+        "package": "ray",
+        "deps": {},
+    },
+    "redis": {
+        "package": "redis",
+        "deps": {
+            "*": ["fakeredis!=1.7.4", "pytest<8.0.0", "pytest-asyncio"],
+            "py3.6,py3.7": [
+                "fakeredis!=2.26.0"
+            ],  # https://github.com/cunla/fakeredis-py/issues/341
+        },
+    },
+    "redis_py_cluster_legacy": {
+        "package": "redis-py-cluster",
+        "deps": {},
+    },
+    "requests": {
+        "package": "requests",
+        "deps": {},
+    },
+    "rq": {
+        "package": "rq",
+        "deps": {
+            "*": ["fakeredis"],
+            "<0.13": [
+                "fakeredis<1.0",
+                "redis<3.2.2",
+            ],  # https://github.com/jamesls/fakeredis/issues/245
+            ">=0.13,<=1.10": ["fakeredis>=1.0,<1.7.4"],
+            "py3.6,py3.7": [
+                "fakeredis!=2.26.0"
+            ],  # https://github.com/cunla/fakeredis-py/issues/341
+        },
+    },
+    "sanic": {
+        "package": "sanic",
+        "deps": {
+            "*": ["websockets<11.0", "aiohttp", "sanic_testing", "aiocontextvars"],
+            ">=22.0": ["sanic_testing"],
+            "py3.6": ["aiocontextvars==0.2.1"],
+        },
+    },
+    "spark": {
+        "package": "pyspark",
+        "deps": {},
+    },
+    "starlette": {
+        "package": "starlette",
+        "deps": {
+            "*": [
+                "pytest-asyncio",
+                "python-multipart",
+                "requests",
+                "anyio<4.0.0",
+                "jinja2",
+                "httpx",
+            ],
+            "<=0.36": ["httpx<0.28.0"],
+        },
+    },
+    "starlite": {
+        "package": "starlite",
+        "deps": {
+            "*": [
+                "pytest-asyncio",
+                "pytest-multipart",
+                "requests",
+                "cryptography",
+                "pydantic<2.0.0",
+                "httpx<0.28",
+            ]
+        },
+    },
+    "sqlalchemy": {
+        "package": "sqlalchemy",
+        "deps": {},
+    },
+    "strawberry": {
+        "package": "strawberry-graphql[fastapi,flask]",
+        "deps": {
+            "*": ["fastapi", "flask", "httpx"],
+        },
+    },
+    "tornado": {
+        "package": "tornado",
+        "deps": {
+            "*": ["pytest"],
+            "<=6.4.1": [
+                "pytest<8.2"
+            ],  # https://github.com/tornadoweb/tornado/pull/3382
+        },
+    },
+    "trytond": {
+        "package": "trytond",
+        "deps": {
+            "*": ["werkzeug"],
+            "<=5.0": ["werkzeug<1.0"],
+        },
+    },
+    "typer": {
+        "package": "typer",
+        "deps": {},
+    },
+    "unleash": {
+        "package": "UnleashClient",
+        "deps": {},
+    },
+}
---
+++ scripts/populate_tox/populate_tox.py
@@ -0,0 +1,459 @@
+"""
+This scripts populates tox.ini automatically using release data from PYPI.
+
+To add a new test target:
+- add it to a group in GROUPs
+- add it to DEPENDENCIES
+"""
+
+import functools
+import time
+from collections import defaultdict
+from datetime import datetime, timedelta
+from packaging.specifiers import InvalidSpecifier, SpecifierSet
+from packaging.version import Version
+from pathlib import Path
+from typing import Union
+
+import requests
+from jinja2 import Environment, FileSystemLoader
+
+from sentry_sdk.integrations import _MIN_VERSIONS
+
+from dependencies import DEPENDENCIES
+
+# TODO:
+# - put GROUPS someplace where both this script and split_tox_actions can use it
+# - (optional) order by alphabet, not group then alphabet
+
+# Only consider package versions going back this far
+CUTOFF = datetime.now() - timedelta(days=365 * 5)
+
+TOX_FILE = Path(__file__).resolve().parent.parent.parent / "tox.ini"
+ENV = Environment(
+    loader=FileSystemLoader(Path(__file__).resolve().parent),
+    trim_blocks=True,
+    lstrip_blocks=True,
+)
+
+PYPI_PROJECT_URL = "https://pypi.python.org/pypi/{project}/json"
+PYPI_VERSION_URL = "https://pypi.python.org/pypi/{project}/{version}/json"
+CLASSIFIER_PREFIX = "Programming Language :: Python :: "
+
+GROUPS = {
+    "Common": [
+        "common",
+    ],
+    "AI": [
+        "anthropic",
+        "cohere",
+        "langchain",
+        "langchain_notiktoken",
+        "openai",
+        "openai_notiktoken",
+        "huggingface_hub",
+    ],
+    "AWS": [
+        # this is separate from Cloud Computing because only this one test suite
+        # needs to run with access to GitHub secrets
+        "aws_lambda",
+    ],
+    "Cloud": [
+        "boto3",
+        "chalice",
+        "cloud_resource_context",
+        "gcp",
+    ],
+    "DBs": [
+        "asyncpg",
+        "clickhouse_driver",
+        "pymongo",
+        "redis",
+        "redis_py_cluster_legacy",
+        "sqlalchemy",
+    ],
+    "Flags": [
+        "launchdarkly",
+        "openfeature",
+        "unleash",
+    ],
+    "GraphQL": [
+        "ariadne",
+        "gql",
+        "graphene",
+        "strawberry",
+    ],
+    "Network": [
+        "gevent",
+        "grpc",
+        "httpx",
+        "requests",
+    ],
+    "Tasks": [
+        "arq",
+        "beam",
+        "celery",
+        "dramatiq",
+        "huey",
+        "ray",
+        "rq",
+        "spark",
+    ],
+    "Web 1": [
+        "django",
+        "flask",
+        "starlette",
+        "fastapi",
+    ],
+    "Web 2": [
+        "aiohttp",
+        "asgi",
+        "bottle",
+        "falcon",
+        "litestar",
+        "pyramid",
+        "quart",
+        "sanic",
+        "starlite",
+        "tornado",
+    ],
+    "Misc": [
+        "loguru",
+        "opentelemetry",
+        "potel",
+        "pure_eval",
+        "trytond",
+        "typer",
+    ],
+}
+
+IGNORE = {
+    # Do not try auto-generating the tox entries for these. They will be
+    # hardcoded in tox.ini.
+    "asgi",
+    "aws_lambda",
+    "cloud_resource_context",
+    "common",
+    "gcp",
+    "gevent",
+    "potel",
+}
+
+
+@functools.cache
+def fetch_package(package: str) -> dict:
+    """Fetch package metadata from PYPI."""
+    url = PYPI_PROJECT_URL.format(project=package)
+    pypi_data = requests.get(url)
+
+    if pypi_data.status_code != 200:
+        print(f"{package} not found")
+
+    return pypi_data.json()
+
+
+@functools.cache
+def fetch_release(package: str, version: Version) -> dict:
+    url = PYPI_VERSION_URL.format(project=package, version=version)
+    pypi_data = requests.get(url)
+
+    if pypi_data.status_code != 200:
+        print(f"{package} not found")
+
+    return pypi_data.json()
+
+
+def get_supported_releases(integration: str, pypi_data: dict) -> list[Version]:
+    min_supported = _MIN_VERSIONS.get(integration)
+    if min_supported:
+        min_supported = Version(".".join(map(str, min_supported)))
+        print(f"  Minimum supported version for {integration} is {min_supported}.")
+    else:
+        print(
+            f"  {integration} doesn't have a minimum version. Maybe we should define one?"
+        )
+
+    releases = []
+
+    for release, metadata in pypi_data["releases"].items():
+        if not metadata:
+            continue
+
+        meta = metadata[0]
+        if datetime.fromisoformat(meta["upload_time"]) < CUTOFF:
+            continue
+
+        if meta["yanked"]:
+            continue
+
+        version = Version(release)
+
+        if min_supported and version < min_supported:
+            continue
+
+        if version.is_prerelease or version.is_postrelease:
+            # TODO: consider the newest prerelease unless obsolete
+            continue
+
+        # The release listing that you get via the package endpoint doesn't
+        # contain all metadata for a release. `requires_python` is included,
+        # but classifiers are not (they require a separate call to the release
+        # endpoint).
+        # Some packages don't use `requires_python`, they supply classifiers
+        # instead.
+        version.python_versions = None
+        requires_python = meta.get("requires_python")
+        if requires_python:
+            try:
+                version.python_versions = supported_python_versions(
+                    SpecifierSet(requires_python)
+                )
+            except InvalidSpecifier:
+                continue
+        else:
+            # No `requires_python``. Let's fetch the metadata to see
+            # the classifiers.
+            # XXX do something with this. no need to fetch every release ever
+            release_metadata = fetch_release(package, version)
+            version.python_versions = supported_python_versions(
+                determine_python_versions(release_metadata)
+            )
+            time.sleep(0.1)
+
+        if not version.python_versions:
+            continue
+
+        for i, saved_version in enumerate(releases):
+            if (
+                version.major == saved_version.major
+                and version.minor == saved_version.minor
+                and version.micro > saved_version.micro
+            ):
+                # don't save all patch versions of a release, just the newest one
+                releases[i] = version
+                break
+        else:
+            releases.append(version)
+
+    return sorted(releases)
+
+
+def pick_releases_to_test(releases: list[Version]) -> list[Version]:
+    """Pick a handful of releases from a list of supported releases."""
+    # If the package has majors (or major-like releases, even if they don't do
+    # semver), we want to make sure we're testing them all. If not, we just pick
+    # the oldest, the newest, and a couple in between.
+    has_majors = len(set([v.major for v in releases])) > 1
+    filtered_releases = set()
+
+    if has_majors:
+        # Find out the min and max release by each major
+        releases_by_major = {}
+        for release in releases:
+            if release.major not in releases_by_major:
+                releases_by_major[release.major] = [release, release]
+            if release < releases_by_major[release.major][0]:
+                releases_by_major[release.major][0] = release
+            if release > releases_by_major[release.major][1]:
+                releases_by_major[release.major][1] = release
+        for min_version, max_version in releases_by_major.values():
+            filtered_releases.add(min_version)
+            filtered_releases.add(max_version)
+
+    else:
+        indexes = [
+            0,  # oldest version supported
+            len(releases) // 3,
+            len(releases) // 3 * 2,
+            -1,  # latest
+        ]
+
+        for i in indexes:
+            try:
+                filtered_releases.add(releases[i])
+            except IndexError:
+                pass
+
+    return sorted(filtered_releases)
+
+
+def supported_python_versions(python_versions: SpecifierSet) -> list[Version]:
+    """Get an intersection of python_versions and Python versions supported in the SDK."""
+    supported = []
+
+    curr = MIN_PYTHON_VERSION
+    while curr <= MAX_PYTHON_VERSION:
+        if curr in python_versions:
+            supported.append(curr)
+        next = [int(v) for v in str(curr).split(".")]
+        next[1] += 1
+        curr = Version(".".join(map(str, next)))
+
+    return supported
+
+
+def pick_python_versions_to_test(python_versions: list[Version]) -> list[Version]:
+    filtered_python_versions = {
+        python_versions[0],
+    }
+
+    filtered_python_versions.add(python_versions[-1])
+    try:
+        filtered_python_versions.add(python_versions[-2])
+    except IndexError:
+        pass
+
+    return sorted(filtered_python_versions)
+
+
+def determine_python_versions(pypi_data: dict) -> Union[SpecifierSet, list[Version]]:
+    try:
+        classifiers = pypi_data["info"]["classifiers"]
+    except (AttributeError, KeyError):
+        return []
+
+    python_versions = []
+    for classifier in classifiers:
+        if classifier.startswith(CLASSIFIER_PREFIX):
+            python_version = classifier[len(CLASSIFIER_PREFIX) :]
+            if "." in python_version:
+                # We don't care about stuff like
+                # Programming Language :: Python :: 3 :: Only,
+                # Programming Language :: Python :: 3,
+                # etc., we're only interested in specific versions, like 3.13
+                python_versions.append(Version(python_version))
+
+    if python_versions:
+        python_versions.sort()
+        return python_versions
+
+    try:
+        requires_python = pypi_data["info"]["requires_python"]
+    except (AttributeError, KeyError):
+        pass
+
+    if requires_python:
+        return SpecifierSet(requires_python)
+
+    return []
+
+
+def _render_python_versions(python_versions: list[Version]) -> str:
+    return (
+        "{"
+        + ",".join(f"py{version.major}.{version.minor}" for version in python_versions)
+        + "}"
+    )
+
+
+def _render_dependencies(integration: str, releases: list[Version]) -> list[str]:
+    rendered = []
+    for constraint, deps in DEPENDENCIES[integration]["deps"].items():
+        if constraint == "*":
+            for dep in deps:
+                rendered.append(f"{integration}: {dep}")
+        elif constraint.startswith("py3"):
+            for dep in deps:
+                rendered.append(f"{constraint}-{integration}: {dep}")
+        else:
+            restriction = SpecifierSet(constraint)
+            for release in releases:
+                if release in restriction:
+                    for dep in deps:
+                        rendered.append(f"{integration}-v{release}: {dep}")
+
+    return rendered
+
+
+def write_tox_file(packages: dict) -> None:
+    template = ENV.get_template("tox.jinja")
+
+    context = {"groups": {}}
+    for group, integrations in packages.items():
+        context["groups"][group] = []
+        for integration in integrations:
+            context["groups"][group].append(
+                {
+                    "name": integration["name"],
+                    "package": integration["package"],
+                    "extra": integration["extra"],
+                    "releases": integration["releases"],
+                    "dependencies": _render_dependencies(
+                        integration["name"], integration["releases"]
+                    ),
+                }
+            )
+
+    rendered = template.render(context)
+
+    with open(TOX_FILE, "w") as file:
+        file.write(rendered)
+
+
+if __name__ == "__main__":
+    print("Finding out the lowest and highest Python version supported by the SDK...")
+    sdk_python_versions = determine_python_versions(fetch_package("sentry_sdk"))
+    MIN_PYTHON_VERSION = sdk_python_versions[0]
+    MAX_PYTHON_VERSION = sdk_python_versions[-1]
+    print(
+        f"The SDK supports Python versions {MIN_PYTHON_VERSION} - {MAX_PYTHON_VERSION}."
+    )
+
+    packages = defaultdict(list)
+
+    for group, integrations in GROUPS.items():
+        for integration in integrations:
+            if integration in IGNORE:
+                continue
+
+            print(f"Processing {integration}...")
+
+            # Figure out the actual main package
+            package = DEPENDENCIES[integration]["package"]
+            extra = None
+            if "[" in package:
+                extra = package[package.find("[") + 1 : package.find("]")]
+                package = package[: package.find("[")]
+
+            # Fetch data for the main package
+            pypi_data = fetch_package(package)
+
+            # Get the list of all supported releases
+            releases = get_supported_releases(integration, pypi_data)
+            if not releases:
+                print("  Found no supported releases.")
+                continue
+
+            # Pick a handful of the supported releases to actually test against
+            # and fetch the PYPI data for each to determine which Python versions
+            # to test it on
+            test_releases = pick_releases_to_test(releases)
+
+            for release in test_releases:
+                release_pypi_data = fetch_release(package, release)
+                release.python_versions = pick_python_versions_to_test(
+                    release.python_versions
+                )
+                if not release.python_versions:
+                    print(f"  Release {release} has no Python versions, skipping.")
+                release.rendered_python_versions = _render_python_versions(
+                    release.python_versions
+                )
+
+                # Give PYPI some breathing room
+                time.sleep(0.25)
+
+            test_releases = [
+                release for release in test_releases if release.python_versions
+            ]
+            if test_releases:
+                packages[group].append(
+                    {
+                        "name": integration,
+                        "package": package,
+                        "extra": extra,
+                        "releases": test_releases,
+                    }
+                )
+
+    write_tox_file(packages)
