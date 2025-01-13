25014f9f456eca48193031d503f0aede56cc135c
Ivana Kellyer <ivana.kellyer@sentry.io>
2025-01-13 10:47:13 +0100

    add doc

--- scripts/populate_tox/dependencies.py
+++ scripts/populate_tox/dependencies.py
@@ -1,3 +1,37 @@
+# The DEPENDENCIES dictionary defines the test dependencies of each integration
+# test suite.
+#
+# The format is:
+# ```
+# integration_name: {
+#     "package": name_of_main_package_on_pypi,
+#     "deps": {
+#          rule1: [package1, package2, ...],
+#          rule2: [package3, package4, ...],
+#      }
+# }
+# ```
+#
+# The following can be set as a rule:
+# - `*`: packages will be always installed
+# - a version bound on the main package (e.g. `<=0.32`): packages will only be installed if the main package falls into the version bounds specified
+# - specific Python version(s) in the form `py3.8,py3.9`: packages will only be installed if the Python version matches one from the list
+#
+# Rules can be used to specify version bounds on older versions of the main
+# package's dependencies, for example. If e.g. Flask tests generally need
+# Werkzeug and don't care about its version, but Flask older than 3.0 needs
+# a specific Werkzeug version to work, you can say:
+#
+# ```
+#     "flask": {
+#         "deps": {
+#             "*": ["Werkzeug"],
+#             "<3.0": ["Werkzeug<2.1.0"],
+#         }
+#     }
+# ````
+
+
 DEPENDENCIES = {
     "aiohttp": {
         "package": "aiohttp",
