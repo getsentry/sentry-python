c227e11460a9cde0562ea660fdd6de8942485e83
Matt Purnell <65473602+mpurnell1@users.noreply.github.com>
2025-02-11 09:44:28 -0600

    ref(utils): Explicitly use None default when checking metadata (#4039)
    Fixes #4035
    
    As described in the above issue, starting in Python 3.14
    importlib_metadata 8 provides the desired behavior, raising KeyError on
    a missing key. In preparation for this change, and to remove a
    DeprecationWarning, we should explicitly default to None when getting
    metadata.

--- sentry_sdk/utils.py
+++ sentry_sdk/utils.py
@@ -1665,7 +1665,7 @@ def _generate_installed_modules():
 
         yielded = set()
         for dist in metadata.distributions():
-            name = dist.metadata["Name"]
+            name = dist.metadata.get("Name", None)  # type: ignore[attr-defined]
             # `metadata` values may be `None`, see:
             # https://github.com/python/cpython/issues/91216
             # and
