f995d8c191667380c41f339544b40443c0ee4453
Ivana Kellyer <ivana.kellyer@sentry.io>
2025-02-11 11:13:18 +0100

    [1] Add tox generation script, but don't use it yet (#3971)
    Add:
    * tox generation script
    * tox template
    * script for generating tox and CI yamls in one go
    * readme for the script
    
    In this PR, the script is set to ignore all integrations, so no tox
    configuration is actually added. However, it's still the script actually
    generating the real `tox.ini` from the `tox.jinja` template.
    
    See follow-up PRs for more.
    
    ---------
    
    Co-authored-by: Daniel Szoke <7881302+szokeasaurusrex@users.noreply.github.com>

---
+++ scripts/populate_tox/config.py
@@ -0,0 +1,8 @@
+# The TEST_SUITE_CONFIG dictionary defines, for each integration test suite,
+# the main package (framework, library) to test with; any additional test
+# dependencies, optionally gated behind specific conditions; and optionally
+# the Python versions to test on.
+#
+# See scripts/populate_tox/README.md for more info on the format and examples.
+
+TEST_SUITE_CONFIG = {}
