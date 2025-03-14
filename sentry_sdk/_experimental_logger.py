d90a2dab3ab9a2c3ac621af7605dd1abd0e8a6e3
Anton Pirker <anton.pirker@sentry.io>
2025-03-14 13:33:00 +0100

    Mark capture_log as private

--- sentry_sdk/_experimental_logger.py
+++ sentry_sdk/_experimental_logger.py
@@ -5,16 +5,16 @@ from typing import Any
 from sentry_sdk import get_client, get_current_scope
 
 
-def capture_log(severity_text, severity_number, template, **kwargs):
+def _capture_log(severity_text, severity_number, template, **kwargs):
     # type: (str, int, str, **Any) -> None
     client = get_client()
     scope = get_current_scope()
     client.capture_log(scope, severity_text, severity_number, template, **kwargs)
 
 
-trace = functools.partial(capture_log, "trace", 1)
-debug = functools.partial(capture_log, "debug", 5)
-info = functools.partial(capture_log, "info", 9)
-warn = functools.partial(capture_log, "warn", 13)
-error = functools.partial(capture_log, "error", 17)
-fatal = functools.partial(capture_log, "fatal", 21)
+trace = functools.partial(_capture_log, "trace", 1)
+debug = functools.partial(_capture_log, "debug", 5)
+info = functools.partial(_capture_log, "info", 9)
+warn = functools.partial(_capture_log, "warn", 13)
+error = functools.partial(_capture_log, "error", 17)
+fatal = functools.partial(_capture_log, "fatal", 21)
