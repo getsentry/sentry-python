12b3ca39ca48dc611207a77c63659b3a93d88445
Daniel Szoke <7881302+szokeasaurusrex@users.noreply.github.com>
2025-03-20 17:31:21 +0100

    fix(tracing): Fix `InvalidOperation` (#4179)
    `InvalidOperation` can occur when using tracing if the `Decimal` class's
    global context has been modified to set the precision below 6. This
    change fixes this bug by setting a custom context for our `quantize`
    call.
    
    Fixes #4177

--- tests/tracing/test_sample_rand.py
+++ tests/tracing/test_sample_rand.py
@@ -1,3 +1,4 @@
+import decimal
 from unittest import mock
 
 import pytest
@@ -53,3 +54,28 @@ def test_transaction_uses_incoming_sample_rand(
     # Transaction event captured if sample_rand < sample_rate, indicating that
     # sample_rand is used to make the sampling decision.
     assert len(events) == int(sample_rand < sample_rate)
+
+
+def test_decimal_context(sentry_init, capture_events):
+    """
+    Ensure that having a decimal context with a precision below 6
+    does not cause an InvalidOperation exception.
+    """
+    sentry_init(traces_sample_rate=1.0)
+    events = capture_events()
+
+    old_prec = decimal.getcontext().prec
+    decimal.getcontext().prec = 2
+
+    try:
+        with mock.patch(
+            "sentry_sdk.tracing_utils.Random.uniform", return_value=0.123456789
+        ):
+            with sentry_sdk.start_transaction() as transaction:
+                assert (
+                    transaction.get_baggage().sentry_items["sample_rand"] == "0.123456"
+                )
+    finally:
+        decimal.getcontext().prec = old_prec
+
+    assert len(events) == 1
