96d8460f1960fa023f6559f6f7f81440dc23de8c
Ivana Kellyer <ivana.kellyer@sentry.io>
2025-03-20 15:25:04 +0100

    slim down profiler init

--- sentry_sdk/profiler/__init__.py
+++ sentry_sdk/profiler/__init__.py
@@ -2,38 +2,8 @@ from sentry_sdk.profiler.continuous_profiler import (
     start_profiler,
     stop_profiler,
 )
-from sentry_sdk.profiler.transaction_profiler import (
-    Profile,
-    Scheduler,
-    ThreadScheduler,
-    GeventScheduler,
-    has_profiling_enabled,
-    setup_profiler,
-    teardown_profiler,
-)
-from sentry_sdk.profiler.utils import (
-    DEFAULT_SAMPLING_FREQUENCY,
-    MAX_STACK_DEPTH,
-    get_frame_name,
-    extract_frame,
-    extract_stack,
-    frame_id,
-)
 
 __all__ = [
     "start_profiler",
     "stop_profiler",
-    "Profile",
-    "Scheduler",
-    "ThreadScheduler",
-    "GeventScheduler",
-    "has_profiling_enabled",
-    "setup_profiler",
-    "teardown_profiler",
-    "DEFAULT_SAMPLING_FREQUENCY",
-    "MAX_STACK_DEPTH",
-    "get_frame_name",
-    "extract_frame",
-    "extract_stack",
-    "frame_id",
 ]
