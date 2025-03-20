168be8c8b2e1ea9a525cd8cfcd38917723ab9ac2
Ivana Kellyer <ivana.kellyer@sentry.io>
2025-03-20 15:38:22 +0100

    remove enable_tracing

--- sentry_sdk/tracing_utils.py
+++ sentry_sdk/tracing_utils.py
@@ -93,17 +93,14 @@ def has_tracing_enabled(options):
     # type: (Optional[Dict[str, Any]]) -> bool
     """
     Returns True if either traces_sample_rate or traces_sampler is
-    defined and enable_tracing is set and not false.
+    defined.
     """
     if options is None:
         return False
 
     return bool(
-        options.get("enable_tracing") is not False
-        and (
-            options.get("traces_sample_rate") is not None
-            or options.get("traces_sampler") is not None
-        )
+        options.get("traces_sample_rate") is not None
+        or options.get("traces_sampler") is not None
     )
 
 
