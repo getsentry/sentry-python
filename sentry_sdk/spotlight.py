6b6ac37acd1499e132956a7d4ab76b5d57b9967b
Burak Yigit Kaya <byk@sentry.io>
2025-03-21 15:37:26 +0000

    lol

--- sentry_sdk/spotlight.py
+++ sentry_sdk/spotlight.py
@@ -86,7 +86,7 @@ try:
         def __init__(self, get_response):
             # type: (Self, Callable[..., HttpResponse]) -> None
             super().__init__(get_response)
-            _spotlight_script = None
+            self._spotlight_script = None
 
             import sentry_sdk.api
 
