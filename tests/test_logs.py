4b01d3c64b1f6dcc5b94101c7a1ea5b4ea6428fc
Colin Chartier <colin.chartier@sentry.io>
2025-04-01 15:44:00 -0400

    feat(ourlogs): Batch logs into a single envelope

--- tests/test_logs.py
+++ tests/test_logs.py
@@ -1,11 +1,16 @@
+import json
 import logging
 import sys
+import time
 from typing import List, Any
-from unittest import mock
 import pytest
 
 import sentry_sdk
+import sentry_sdk.logger
+from sentry_sdk import get_client
+from sentry_sdk.envelope import Envelope
 from sentry_sdk.integrations.logging import LoggingIntegration
+from sentry_sdk.types import Log
 
 minimum_python_37 = pytest.mark.skipif(
     sys.version_info < (3, 7), reason="Asyncio tests need Python >= 3.7"
@@ -13,7 +18,41 @@ minimum_python_37 = pytest.mark.skipif(
 
 
 def otel_attributes_to_dict(otel_attrs: List[Any]):
-    return {item["key"]: item["value"] for item in otel_attrs}
+    def _convert_attr(attr: dict[str, str | int | bool]):
+        if "boolValue" in attr:
+            return bool(attr["boolValue"])
+        if "doubleValue" in attr:
+            return float(attr["doubleValue"])
+        if "intValue" in attr:
+            return int(attr["intValue"])
+        if attr["stringValue"].startswith("{"):
+            try:
+                return json.loads(attr["stringValue"])
+            except ValueError:
+                pass
+        return str(attr["stringValue"])
+
+    return {item["key"]: _convert_attr(item["value"]) for item in otel_attrs}
+
+
+def envelopes_to_logs(envelopes: List[Envelope]) -> List[Log]:
+    res = []  # type: List[Log]
+    for envelope in envelopes:
+        for item in envelope.items:
+            if item.type == "otel_log":
+                log_json = item.payload.json
+                log = {
+                    "severity_text": log_json["severityText"],
+                    "severity_number": log_json["severityNumber"],
+                    "body": log_json["body"]["stringValue"],
+                    "attributes": otel_attributes_to_dict(log_json["attributes"]),
+                    "time_unix_nano": int(log_json["timeUnixNano"]),
+                    "trace_id": None,
+                }  # type: Log
+                if "traceId" in log_json:
+                    log["trace_id"] = log_json["traceId"]
+                res.append(log)
+    return res
 
 
 @minimum_python_37
@@ -37,7 +76,7 @@ def test_logs_disabled_by_default(sentry_init, capture_envelopes):
 
 @minimum_python_37
 def test_logs_basics(sentry_init, capture_envelopes):
-    sentry_init(_experiments={"enable_sentry_logs": True})
+    sentry_init(_experiments={"enable_logs": True})
     envelopes = capture_envelopes()
 
     sentry_sdk.logger.trace("This is a 'trace' log...")
@@ -47,31 +86,31 @@ def test_logs_basics(sentry_init, capture_envelopes):
     sentry_sdk.logger.error("This is a 'error' log...")
     sentry_sdk.logger.fatal("This is a 'fatal' log...")
 
-    assert (
-        len(envelopes) == 6
-    )  # We will batch those log items into a single envelope at some point
+    get_client().flush()
+    logs = envelopes_to_logs(envelopes)
+    assert logs[0].get("severity_text") == "trace"
+    assert logs[0].get("severity_number") == 1
 
-    assert envelopes[0].items[0].payload.json["severityText"] == "trace"
-    assert envelopes[0].items[0].payload.json["severityNumber"] == 1
+    assert logs[1].get("severity_text") == "debug"
+    assert logs[1].get("severity_number") == 5
 
-    assert envelopes[1].items[0].payload.json["severityText"] == "debug"
-    assert envelopes[1].items[0].payload.json["severityNumber"] == 5
+    assert logs[2].get("severity_text") == "info"
+    assert logs[2].get("severity_number") == 9
 
-    assert envelopes[2].items[0].payload.json["severityText"] == "info"
-    assert envelopes[2].items[0].payload.json["severityNumber"] == 9
+    assert logs[3].get("severity_text") == "warning"
+    assert logs[3].get("severity_number") == 13
 
-    assert envelopes[3].items[0].payload.json["severityText"] == "warning"
-    assert envelopes[3].items[0].payload.json["severityNumber"] == 13
+    assert logs[4].get("severity_text") == "error"
+    assert logs[4].get("severity_number") == 17
 
-    assert envelopes[4].items[0].payload.json["severityText"] == "error"
-    assert envelopes[4].items[0].payload.json["severityNumber"] == 17
-
-    assert envelopes[5].items[0].payload.json["severityText"] == "fatal"
-    assert envelopes[5].items[0].payload.json["severityNumber"] == 21
+    assert logs[5].get("severity_text") == "fatal"
+    assert logs[5].get("severity_number") == 21
 
 
 @minimum_python_37
 def test_logs_before_emit_log(sentry_init, capture_envelopes):
+    before_log_called = [False]
+
     def _before_log(record, hint):
         assert set(record.keys()) == {
             "severity_text",
@@ -85,11 +124,13 @@ def test_logs_before_emit_log(sentry_init, capture_envelopes):
         if record["severity_text"] in ["fatal", "error"]:
             return None
 
+        before_log_called[0] = True
+
         return record
 
     sentry_init(
         _experiments={
-            "enable_sentry_logs": True,
+            "enable_logs": True,
             "before_emit_log": _before_log,
         }
     )
@@ -102,12 +143,15 @@ def test_logs_before_emit_log(sentry_init, capture_envelopes):
     sentry_sdk.logger.error("This is a 'error' log...")
     sentry_sdk.logger.fatal("This is a 'fatal' log...")
 
-    assert len(envelopes) == 4
+    get_client().flush()
+    logs = envelopes_to_logs(envelopes)
+    assert len(logs) == 4
 
-    assert envelopes[0].items[0].payload.json["severityText"] == "trace"
-    assert envelopes[1].items[0].payload.json["severityText"] == "debug"
-    assert envelopes[2].items[0].payload.json["severityText"] == "info"
-    assert envelopes[3].items[0].payload.json["severityText"] == "warning"
+    assert logs[0]["severity_text"] == "trace"
+    assert logs[1]["severity_text"] == "debug"
+    assert logs[2]["severity_text"] == "info"
+    assert logs[3]["severity_text"] == "warning"
+    assert before_log_called[0]
 
 
 @minimum_python_37
@@ -115,7 +159,7 @@ def test_logs_attributes(sentry_init, capture_envelopes):
     """
     Passing arbitrary attributes to log messages.
     """
-    sentry_init(_experiments={"enable_sentry_logs": True})
+    sentry_init(_experiments={"enable_logs": True})
     envelopes = capture_envelopes()
 
     attrs = {
@@ -129,17 +173,15 @@ def test_logs_attributes(sentry_init, capture_envelopes):
         "The recorded value was '{my_var}'", my_var="some value", attributes=attrs
     )
 
-    log_item = envelopes[0].items[0].payload.json
-    assert log_item["body"]["stringValue"] == "The recorded value was 'some value'"
+    get_client().flush()
+    logs = envelopes_to_logs(envelopes)
+    assert logs[0]["body"] == "The recorded value was 'some value'"
 
-    attrs = otel_attributes_to_dict(log_item["attributes"])
-    assert attrs["attr_int"] == {"intValue": "1"}
-    assert attrs["attr_float"] == {"doubleValue": 2.0}
-    assert attrs["attr_bool"] == {"boolValue": True}
-    assert attrs["attr_string"] == {"stringValue": "string attribute"}
-    assert attrs["sentry.environment"] == {"stringValue": "production"}
-    assert attrs["sentry.release"] == {"stringValue": mock.ANY}
-    assert attrs["sentry.message.parameters.my_var"] == {"stringValue": "some value"}
+    for k, v in attrs.items():
+        assert logs[0]["attributes"][k] == v
+    assert logs[0]["attributes"]["sentry.environment"] == "production"
+    assert "sentry.release" in logs[0]["attributes"]
+    assert logs[0]["attributes"]["sentry.message.parameters.my_var"] == "some value"
 
 
 @minimum_python_37
@@ -147,7 +189,7 @@ def test_logs_message_params(sentry_init, capture_envelopes):
     """
     This is the official way of how to pass vars to log messages.
     """
-    sentry_init(_experiments={"enable_sentry_logs": True})
+    sentry_init(_experiments={"enable_logs": True})
     envelopes = capture_envelopes()
 
     sentry_sdk.logger.warning("The recorded value was '{int_var}'", int_var=1)
@@ -156,38 +198,33 @@ def test_logs_message_params(sentry_init, capture_envelopes):
     sentry_sdk.logger.warning(
         "The recorded value was '{string_var}'", string_var="some string value"
     )
-
-    assert (
-        envelopes[0].items[0].payload.json["body"]["stringValue"]
-        == "The recorded value was '1'"
+    sentry_sdk.logger.error(
+        "The recorded error was '{error}'", error=Exception("some error")
     )
-    assert otel_attributes_to_dict(envelopes[0].items[0].payload.json["attributes"])[
-        "sentry.message.parameters.int_var"
-    ] == {"intValue": "1"}
 
-    assert (
-        envelopes[1].items[0].payload.json["body"]["stringValue"]
-        == "The recorded value was '2.0'"
-    )
-    assert otel_attributes_to_dict(envelopes[1].items[0].payload.json["attributes"])[
-        "sentry.message.parameters.float_var"
-    ] == {"doubleValue": 2.0}
+    get_client().flush()
+    logs = envelopes_to_logs(envelopes)
 
-    assert (
-        envelopes[2].items[0].payload.json["body"]["stringValue"]
-        == "The recorded value was 'False'"
-    )
-    assert otel_attributes_to_dict(envelopes[2].items[0].payload.json["attributes"])[
-        "sentry.message.parameters.bool_var"
-    ] == {"boolValue": False}
+    assert logs[0]["body"] == "The recorded value was '1'"
+    assert logs[0]["attributes"]["sentry.message.parameters.int_var"] == 1
 
+    assert logs[1]["body"] == "The recorded value was '2.0'"
+    assert logs[1]["attributes"]["sentry.message.parameters.float_var"] == 2.0
+
+    assert logs[2]["body"] == "The recorded value was 'False'"
+    assert logs[2]["attributes"]["sentry.message.parameters.bool_var"] is False
+
+    assert logs[3]["body"] == "The recorded value was 'some string value'"
     assert (
-        envelopes[3].items[0].payload.json["body"]["stringValue"]
-        == "The recorded value was 'some string value'"
+        logs[3]["attributes"]["sentry.message.parameters.string_var"]
+        == "some string value"
     )
-    assert otel_attributes_to_dict(envelopes[3].items[0].payload.json["attributes"])[
-        "sentry.message.parameters.string_var"
-    ] == {"stringValue": "some string value"}
+
+    assert logs[4]["body"] == "The recorded error was 'some error'"
+    assert (
+        logs[4]["attributes"]["sentry.message.parameters.error"]
+        == "Exception('some error')"
+    )
 
 
 @minimum_python_37
@@ -195,17 +232,15 @@ def test_logs_tied_to_transactions(sentry_init, capture_envelopes):
     """
     Log messages are also tied to transactions.
     """
-    sentry_init(_experiments={"enable_sentry_logs": True})
+    sentry_init(_experiments={"enable_logs": True})
     envelopes = capture_envelopes()
 
     with sentry_sdk.start_transaction(name="test-transaction") as trx:
         sentry_sdk.logger.warning("This is a log tied to a transaction")
 
-    log_entry = envelopes[0].items[0].payload.json
-    assert log_entry["attributes"][-1] == {
-        "key": "sentry.trace.parent_span_id",
-        "value": {"stringValue": trx.span_id},
-    }
+    get_client().flush()
+    logs = envelopes_to_logs(envelopes)
+    assert logs[0]["attributes"]["sentry.trace.parent_span_id"] == trx.span_id
 
 
 @minimum_python_37
@@ -213,15 +248,16 @@ def test_logs_tied_to_spans(sentry_init, capture_envelopes):
     """
     Log messages are also tied to spans.
     """
-    sentry_init(_experiments={"enable_sentry_logs": True})
+    sentry_init(_experiments={"enable_logs": True})
     envelopes = capture_envelopes()
 
     with sentry_sdk.start_transaction(name="test-transaction"):
         with sentry_sdk.start_span(description="test-span") as span:
             sentry_sdk.logger.warning("This is a log tied to a span")
 
-    attrs = otel_attributes_to_dict(envelopes[0].items[0].payload.json["attributes"])
-    assert attrs["sentry.trace.parent_span_id"] == {"stringValue": span.span_id}
+    get_client().flush()
+    logs = envelopes_to_logs(envelopes)
+    assert logs[0]["attributes"]["sentry.trace.parent_span_id"] == span.span_id
 
 
 @minimum_python_37
@@ -229,25 +265,24 @@ def test_logger_integration_warning(sentry_init, capture_envelopes):
     """
     The python logger module should create 'warn' sentry logs if the flag is on.
     """
-    sentry_init(_experiments={"enable_sentry_logs": True})
+    sentry_init(_experiments={"enable_logs": True})
     envelopes = capture_envelopes()
 
     python_logger = logging.Logger("test-logger")
     python_logger.warning("this is %s a template %s", "1", "2")
 
-    log_entry = envelopes[0].items[0].payload.json
-    attrs = otel_attributes_to_dict(log_entry["attributes"])
-    assert attrs["sentry.message.template"] == {
-        "stringValue": "this is %s a template %s"
-    }
+    get_client().flush()
+    logs = envelopes_to_logs(envelopes)
+    attrs = logs[0]["attributes"]
+    assert attrs["sentry.message.template"] == "this is %s a template %s"
     assert "code.file.path" in attrs
     assert "code.line.number" in attrs
-    assert attrs["logger.name"] == {"stringValue": "test-logger"}
-    assert attrs["sentry.environment"] == {"stringValue": "production"}
-    assert attrs["sentry.message.parameters.0"] == {"stringValue": "1"}
-    assert attrs["sentry.message.parameters.1"]
-    assert log_entry["severityNumber"] == 13
-    assert log_entry["severityText"] == "warning"
+    assert attrs["logger.name"] == "test-logger"
+    assert attrs["sentry.environment"] == "production"
+    assert attrs["sentry.message.parameters.0"] == "1"
+    assert attrs["sentry.message.parameters.1"] == "2"
+    assert logs[0]["severity_number"] == 13
+    assert logs[0]["severity_text"] == "warn"
 
 
 @minimum_python_37
@@ -255,11 +290,12 @@ def test_logger_integration_debug(sentry_init, capture_envelopes):
     """
     The python logger module should not create 'debug' sentry logs if the flag is on by default
     """
-    sentry_init(_experiments={"enable_sentry_logs": True})
+    sentry_init(_experiments={"enable_logs": True})
     envelopes = capture_envelopes()
 
     python_logger = logging.Logger("test-logger")
     python_logger.debug("this is %s a template %s", "1", "2")
+    get_client().flush()
 
     assert len(envelopes) == 0
 
@@ -270,7 +306,7 @@ def test_no_log_infinite_loop(sentry_init, capture_envelopes):
     If 'debug' mode is true, and you set a low log level in the logging integration, there should be no infinite loops.
     """
     sentry_init(
-        _experiments={"enable_sentry_logs": True},
+        _experiments={"enable_logs": True},
         integrations=[LoggingIntegration(sentry_logs_level=logging.DEBUG)],
         debug=True,
     )
@@ -278,6 +314,7 @@ def test_no_log_infinite_loop(sentry_init, capture_envelopes):
 
     python_logger = logging.Logger("test-logger")
     python_logger.debug("this is %s a template %s", "1", "2")
+    get_client().flush()
 
     assert len(envelopes) == 1
 
@@ -287,34 +324,67 @@ def test_logging_errors(sentry_init, capture_envelopes):
     """
     The python logger module should be able to log errors without erroring
     """
-    sentry_init(_experiments={"enable_sentry_logs": True})
+    sentry_init(_experiments={"enable_logs": True})
     envelopes = capture_envelopes()
 
     python_logger = logging.Logger("test-logger")
     python_logger.error(Exception("test exc 1"))
     python_logger.error("error is %s", Exception("test exc 2"))
+    get_client().flush()
 
     error_event_1 = envelopes[0].items[0].payload.json
     assert error_event_1["level"] == "error"
-
-    log_event_1 = envelopes[1].items[0].payload.json
-    assert log_event_1["severityText"] == "error"
-    # When only logging an exception, there is no "sentry.message.template" or "sentry.message.parameters.0"
-    assert len(log_event_1["attributes"]) == 10
-    assert log_event_1["attributes"][0]["key"] == "code.line.number"
-
-    error_event_2 = envelopes[2].items[0].payload.json
+    error_event_2 = envelopes[1].items[0].payload.json
     assert error_event_2["level"] == "error"
 
-    log_event_2 = envelopes[3].items[0].payload.json
-    assert log_event_2["severityText"] == "error"
-    assert len(log_event_2["attributes"]) == 12
-    assert log_event_2["attributes"][0]["key"] == "sentry.message.template"
-    assert log_event_2["attributes"][0]["value"] == {"stringValue": "error is %s"}
-    assert log_event_2["attributes"][1]["key"] == "sentry.message.parameters.0"
-    assert log_event_2["attributes"][1]["value"] == {
-        "stringValue": "Exception('test exc 2')"
-    }
-    assert log_event_2["attributes"][2]["key"] == "code.line.number"
+    print(envelopes)
+    logs = envelopes_to_logs(envelopes)
+    assert logs[0]["severity_text"] == "error"
+    assert "sentry.message.template" not in logs[0]["attributes"]
+    assert "sentry.message.parameters.0" not in logs[0]["attributes"]
+    assert "code.line.number" in logs[0]["attributes"]
 
-    assert len(envelopes) == 4
+    assert logs[1]["severity_text"] == "error"
+    assert logs[1]["attributes"]["sentry.message.template"] == "error is %s"
+    assert logs[1]["attributes"]["sentry.message.parameters.0"] == "Exception('test exc 2')"
+    assert "code.line.number" in logs[1]["attributes"]
+
+    assert len(logs) == 2
+
+
+def test_auto_flush_logs_after_100(sentry_init, capture_envelopes):
+    """
+    If you log >100 logs, it should automatically trigger a flush.
+    """
+    sentry_init(_experiments={"enable_logs": True})
+    envelopes = capture_envelopes()
+
+    python_logger = logging.Logger("test-logger")
+    for i in range(200):
+        python_logger.warning("log #%d", i)
+
+    for _ in range(500):
+        time.sleep(1.0 / 100.0)
+        if len(envelopes) > 0:
+            return
+
+    raise AssertionError("200 logs were never flushed after five seconds")
+
+
+@minimum_python_37
+def test_auto_flush_logs_after_5s(sentry_init, capture_envelopes):
+    """
+    If you log a single log, it should automatically flush after 5 seconds, at most 10 seconds.
+    """
+    sentry_init(_experiments={"enable_logs": True})
+    envelopes = capture_envelopes()
+
+    python_logger = logging.Logger("test-logger")
+    python_logger.warning("log #%d", 1)
+
+    for _ in range(100):
+        time.sleep(1.0 / 10.0)
+        if len(envelopes) > 0:
+            return
+
+    raise AssertionError("1 logs was never flushed after 10 seconds")
