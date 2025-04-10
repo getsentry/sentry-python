7adcd1b6e2a51b06abfcddf9a12952a6777c6e82
Ivana Kellyer <ivana.kellyer@sentry.io>
2025-04-10 16:30:31 +0200

    fix import

--- sentry_sdk/integrations/huey.py
+++ sentry_sdk/integrations/huey.py
@@ -8,12 +8,10 @@ from sentry_sdk.consts import (
     SPANSTATUS,
     BAGGAGE_HEADER_NAME,
     SENTRY_TRACE_HEADER_NAME,
+    TransactionSource,
 )
 from sentry_sdk.integrations import DidNotEnable, Integration
 from sentry_sdk.scope import should_send_default_pii
-from sentry_sdk.tracing import (
-    TransactionSource,
-)
 from sentry_sdk.utils import (
     capture_internal_exceptions,
     ensure_integration_enabled,
