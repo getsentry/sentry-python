a2f365568c2727a369bb29fa8362ec69abf5af63
Ivana Kellyer <ivana.kellyer@sentry.io>
2025-04-14 15:43:21 +0200

    newline

--- sentry_sdk/scope.py
+++ sentry_sdk/scope.py
@@ -8,6 +8,7 @@ from enum import Enum
 from datetime import datetime, timezone
 from functools import wraps
 from itertools import chain
+
 from opentelemetry.trace import Span as OTelSpan
 
 from sentry_sdk._types import AnnotatedValue
