957d7bb4c17b38dccae3be4f92c1ded395c5bfab
Anton Pirker <anton.pirker@sentry.io>
2025-04-14 12:48:34 +0200

    does it work in django 3.0?

--- sentry_sdk/integrations/__init__.py
+++ sentry_sdk/integrations/__init__.py
@@ -131,7 +131,7 @@ _MIN_VERSIONS = {
     "celery": (4, 4, 7),
     "chalice": (1, 16, 0),
     "clickhouse_driver": (0, 2, 0),
-    "django": (1, 8),
+    "django": (3, 0),
     "dramatiq": (1, 9),
     "falcon": (1, 4),
     "fastapi": (0, 79, 0),
