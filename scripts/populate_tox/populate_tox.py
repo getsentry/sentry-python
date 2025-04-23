2c3776c582a23b6936c76ef53008bf63f861b6fd
Ivana Kellyer <ivana.kellyer@sentry.io>
2025-04-23 11:03:10 +0200

    tests: Move aiohttp under toxgen (#4319)
    Depends on https://github.com/getsentry/sentry-python/pull/4323

--- scripts/populate_tox/populate_tox.py
+++ scripts/populate_tox/populate_tox.py
@@ -67,7 +67,6 @@ IGNORE = {
     "potel",
     # Integrations that can be migrated -- we should eventually remove all
     # of these from the IGNORE list
-    "aiohttp",
     "anthropic",
     "arq",
     "asyncpg",
