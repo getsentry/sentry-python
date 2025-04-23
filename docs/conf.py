d53090eebd2f3c6642a676db206211f25c9c9023
Anton Pirker <anton.pirker@sentry.io>
2025-04-23 08:54:55 +0200

    Set major version

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ project = "sentry-python"
 copyright = "2019-{}, Sentry Team and Contributors".format(datetime.now().year)
 author = "Sentry Team and Contributors"
 
-release = "2.26.1"
+release = "3.0.0"
 version = ".".join(release.split(".")[:2])  # The short X.Y version.
 
 
