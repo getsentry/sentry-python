db482ca25a93cce8473f4bd9f01670237f5e42cc
Anton Pirker <anton.pirker@sentry.io>
2025-04-29 11:06:35 +0200

    updated test

--- tests/tracing/test_misc.py
+++ tests/tracing/test_misc.py
@@ -340,8 +340,8 @@ def test_set_measurement_deprecated(sentry_init):
 
 def test_set_meaurement_compared_to_set_data(sentry_init, capture_events):
     """
-    This is just a test to see if we can use set_data() instead of set_measurement() with
-    the same result in the backend.
+    This is just a test to see the difference
+    between measurements and data in the resulting event payload.
     """
     sentry_init(traces_sample_rate=1.0)
 
@@ -350,15 +350,19 @@ def test_set_meaurement_compared_to_set_data(sentry_init, capture_events):
     with start_transaction(name="measuring stuff") as transaction:
         transaction.set_measurement("metric.foo", 123)
         transaction.set_data("metric.bar", 456)
+
         with start_span(op="measuring span") as span:
             span.set_measurement("metric.baz", 420.69, unit="custom")
             span.set_data("metric.qux", 789)
 
     (event,) = events
-    from pprint import pprint
-
-    pprint(event)
-    raise AssertionError("This test is not finished yet.")
+    assert event["measurements"]["metric.foo"] == {"value": 123, "unit": ""}
+    assert event["contexts"]["trace"]["data"]["metric.bar"] == 456
+    assert event["spans"][0]["measurements"]["metric.baz"] == {
+        "value": 420.69,
+        "unit": "custom",
+    }
+    assert event["spans"][0]["data"]["metric.qux"] == 789
 
 
 @pytest.mark.parametrize(
