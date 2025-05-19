f5727572281578a73b21cd757288b820932eded6
Daniel Szoke <7881302+szokeasaurusrex@users.noreply.github.com>
2025-05-19 08:42:22 -0400

    feat: Allow configuring `keep_alive` via environment variable (#4366)
    This commit enables the `keep_alive` option to be set via the
    `SENTRY_KEEP_ALIVE` environment variable. When both the environment
    variable and the argument are provided, the argument takes precedence.
    
    Closes #4354

--- sentry_sdk/consts.py
+++ sentry_sdk/consts.py
@@ -578,7 +578,7 @@ class ClientConstructor:
         ignore_errors=[],  # type: Sequence[Union[type, str]]  # noqa: B006
         max_request_body_size="medium",  # type: str
         socket_options=None,  # type: Optional[List[Tuple[int, int, int | bytes]]]
-        keep_alive=False,  # type: bool
+        keep_alive=None,  # type: Optional[bool]
         before_send=None,  # type: Optional[EventProcessor]
         before_breadcrumb=None,  # type: Optional[BreadcrumbProcessor]
         debug=None,  # type: Optional[bool]
