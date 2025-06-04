bba3ef24ae2ee8cac134d6467efcc2bf99525d0d
Anton Pirker <anton.pirker@sentry.io>
2025-06-04 10:53:53 +0200

    organize code

--- sentry_sdk/integrations/openai_agents/run_hooks.py
+++ sentry_sdk/integrations/openai_agents/run_hooks.py
@@ -1,15 +1,14 @@
-import sentry_sdk
 from sentry_sdk.integrations import DidNotEnable
 
 from typing import TYPE_CHECKING
 
 from sentry_sdk.integrations.openai_agents.spans import (
     execute_tool_span,
-    invoke_agent_span,
     handoff_span,
+    invoke_agent_span,
+    update_execute_tool_span,
+    update_invoke_agent_span,
 )
-
-from .utils import _usage_to_str
 
 
 if TYPE_CHECKING:
@@ -33,13 +32,7 @@ class SentryRunHooks(RunHooks):
 
     async def on_agent_end(self, context, agent, output):
         # type: (RunContextWrapper, Agent, Any) -> None
-        print(
-            f"### Agent '{agent.name}' ended with output {output}. "
-            f"Usage: {_usage_to_str(context.usage)}"
-        )
-        current_span = sentry_sdk.get_current_span()
-        if current_span:
-            current_span.__exit__(None, None, None)
+        update_invoke_agent_span(context, agent, output)
 
     async def on_tool_start(self, context, agent, tool):
         # type: (RunContextWrapper, Agent, Tool) -> None
@@ -47,16 +40,7 @@ class SentryRunHooks(RunHooks):
 
     async def on_tool_end(self, context, agent, tool, result):
         # type: (RunContextWrapper, Agent, Tool, str) -> None
-        print(
-            f"### Tool {tool.name} ended with result {result}. "
-            f"Usage: {_usage_to_str(context.usage)}"
-        )
-        current_span = sentry_sdk.get_current_span()
-        if current_span:
-            import ipdb
-
-            ipdb.set_trace()
-            current_span.__exit__(None, None, None)
+        update_execute_tool_span(context, agent, tool, result)
 
     async def on_handoff(
         self, context: RunContextWrapper, from_agent: Agent, to_agent: Agent
