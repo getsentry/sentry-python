cf06427dd0152502c35273e1c54f0329f2632b49
Anton Pirker <anton.pirker@sentry.io>
2025-06-04 11:01:13 +0200

    organize code

--- sentry_sdk/integrations/openai_agents/run_hooks.py
+++ sentry_sdk/integrations/openai_agents/run_hooks.py
@@ -6,8 +6,8 @@ from sentry_sdk.integrations.openai_agents.spans import (
     execute_tool_span,
     handoff_span,
     invoke_agent_span,
-    update_execute_tool_span,
-    update_invoke_agent_span,
+    finish_execute_tool_span,
+    finish_invoke_agent_span,
 )
 
 
@@ -32,7 +32,7 @@ class SentryRunHooks(RunHooks):
 
     async def on_agent_end(self, context, agent, output):
         # type: (RunContextWrapper, Agent, Any) -> None
-        update_invoke_agent_span(context, agent, output)
+        finish_invoke_agent_span(context, agent, output)
 
     async def on_tool_start(self, context, agent, tool):
         # type: (RunContextWrapper, Agent, Tool) -> None
@@ -40,7 +40,7 @@ class SentryRunHooks(RunHooks):
 
     async def on_tool_end(self, context, agent, tool, result):
         # type: (RunContextWrapper, Agent, Tool, str) -> None
-        update_execute_tool_span(context, agent, tool, result)
+        finish_execute_tool_span(context, agent, tool, result)
 
     async def on_handoff(
         self, context: RunContextWrapper, from_agent: Agent, to_agent: Agent
