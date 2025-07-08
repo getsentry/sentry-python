8d0692ddf5e48ee4abf13da0a597baa348933826
Ivana Kellyer <ivana.kellyer@sentry.io>
2025-07-08 12:02:03 +0200

    .

--- scripts/generate-test-files.sh
+++ scripts/generate-test-files.sh
@@ -6,6 +6,7 @@ set -xe
 
 cd "$(dirname "$0")"
 
+rm -rf toxgen.venv
 python -m venv toxgen.venv
 . toxgen.venv/bin/activate
 
@@ -13,5 +14,5 @@ pip install -e ..
 pip install -r populate_tox/requirements.txt
 pip install -r split_tox_gh_actions/requirements.txt
 
-python populate_tox/populate_tox.py
-python split_tox_gh_actions/split_tox_gh_actions.py
+toxgen.venv/bin/python populate_tox/populate_tox.py
+toxgen.venv/bin/python split_tox_gh_actions/split_tox_gh_actions.py
