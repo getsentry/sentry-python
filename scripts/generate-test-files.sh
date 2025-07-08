4d7fd62178444c6ba1439f3ae54c051f93535a4c
Ivana Kellyer <ivana.kellyer@sentry.io>
2025-07-08 12:09:47 +0200

    make everything use venv stuff explicitly

--- scripts/generate-test-files.sh
+++ scripts/generate-test-files.sh
@@ -10,9 +10,9 @@ rm -rf toxgen.venv
 python -m venv toxgen.venv
 . toxgen.venv/bin/activate
 
-pip install -e ..
-pip install -r populate_tox/requirements.txt
-pip install -r split_tox_gh_actions/requirements.txt
+toxgen.venv/bin/pip install -e ..
+toxgen.venv/bin/pip install -r populate_tox/requirements.txt
+toxgen.venv/bin/pip install -r split_tox_gh_actions/requirements.txt
 
 toxgen.venv/bin/python populate_tox/populate_tox.py
 toxgen.venv/bin/python split_tox_gh_actions/split_tox_gh_actions.py
