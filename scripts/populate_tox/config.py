f21e2ea1c03835ab06580b28c52316c769dd2a39
srothh <simon.roth@sentry.io>
2025-07-29 10:47:03 +0200

    fix(test): Add fastapi anyio pin for <0.8
    Anyio >=4.0 breaks fastapi for versions <0.8
    
    GH-4601

--- scripts/populate_tox/config.py
+++ scripts/populate_tox/config.py
@@ -106,6 +106,7 @@ TEST_SUITE_CONFIG = {
             # FastAPI versions we use older httpx which still supports the
             # deprecated argument.
             "<0.110.1": ["httpx<0.28.0"],
+            "<0.80": ["anyio<4"],
             "py3.6": ["aiocontextvars"],
         },
     },
