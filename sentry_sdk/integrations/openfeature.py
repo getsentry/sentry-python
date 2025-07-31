c1861a3ca963512f1609cc125dd2648dc029b64b
Ivana Kellyer <ivana.kellyer@sentry.io>
2025-07-31 12:01:59 +0200

    Fix mypy (#4649)

--- sentry_sdk/integrations/openfeature.py
+++ sentry_sdk/integrations/openfeature.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 from sentry_sdk.feature_flags import add_feature_flag
 from sentry_sdk.integrations import DidNotEnable, Integration
@@ -8,7 +8,6 @@ try:
     from openfeature.hook import Hook
 
     if TYPE_CHECKING:
-        from openfeature.flag_evaluation import FlagEvaluationDetails
         from openfeature.hook import HookContext, HookHints
 except ImportError:
     raise DidNotEnable("OpenFeature is not installed")
@@ -25,9 +24,8 @@ class OpenFeatureIntegration(Integration):
 
 
 class OpenFeatureHook(Hook):
-
     def after(self, hook_context, details, hints):
-        # type: (HookContext, FlagEvaluationDetails[bool], HookHints) -> None
+        # type: (Any, Any, Any) -> None
         if isinstance(details.value, bool):
             add_feature_flag(details.flag_key, details.value)
 
