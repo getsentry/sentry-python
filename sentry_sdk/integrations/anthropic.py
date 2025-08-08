9c52cd6e45c427e8263860030cff392eb9f4d021
Fabian Schindler <fabian.schindler@sentry.io>
2025-08-08 11:46:25 +0200

    fix(anthropic) make result content blocks more resilient

--- sentry_sdk/integrations/anthropic.py
+++ sentry_sdk/integrations/anthropic.py
@@ -215,12 +215,12 @@ def _sentry_patched_create_common(f, *args, **kwargs):
                 input_tokens=input_tokens,
                 output_tokens=output_tokens,
                 content_blocks=[
-                    (
-                        content_block.to_dict()
-                        if hasattr(content_block, "to_dict")
-                        else content_block.model_dump()
-                    )
+                    {
+                        "type": "text",
+                        "text": content_block.text,
+                    }
                     for content_block in result.content
+                    if hasattr(content_block, "text")
                 ],
                 finish_span=True,
             )
