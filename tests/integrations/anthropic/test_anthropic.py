b0b2331aac09214c7ec2c997c8d2ffeee3481711
Anton Pirker <anton.pirker@sentry.io>
2025-08-08 12:35:55 +0200

    fixed tests

--- tests/integrations/anthropic/test_anthropic.py
+++ tests/integrations/anthropic/test_anthropic.py
@@ -123,7 +123,7 @@ def test_nonstreaming_create_message(
         )
         assert (
             span["data"][SPANDATA.GEN_AI_RESPONSE_TEXT]
-            == '[{"text": "Hi, I\'m Claude.", "type": "text"}]'
+            == '[{"type": "text", "text": "Hi, I\'m Claude."}]'
         )
     else:
         assert SPANDATA.GEN_AI_REQUEST_MESSAGES not in span["data"]
@@ -195,7 +195,7 @@ async def test_nonstreaming_create_message_async(
         )
         assert (
             span["data"][SPANDATA.GEN_AI_RESPONSE_TEXT]
-            == '[{"text": "Hi, I\'m Claude.", "type": "text"}]'
+            == '[{"type": "text", "text": "Hi, I\'m Claude."}]'
         )
     else:
         assert SPANDATA.GEN_AI_REQUEST_MESSAGES not in span["data"]
