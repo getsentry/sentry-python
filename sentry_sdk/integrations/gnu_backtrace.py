4e2c1c096f6bdd6ae42b11fa660228c73a4bb4f8
Meredith Heller <meredith@sentry.io>
2025-08-11 15:31:51 -0700

    split regex

--- sentry_sdk/integrations/gnu_backtrace.py
+++ sentry_sdk/integrations/gnu_backtrace.py
@@ -13,12 +13,14 @@ if TYPE_CHECKING:
 
 # function is everything between index at @
 # and then we match on the @ plus the hex val
-FUNCTION_RE = r"[^@]+?)\s+@\s+0x[0-9a-fA-F]+"
+FUNCTION_RE = r"[^@]+?"
+HEX_ADDRESS = r"\s+@\s+0x[0-9a-fA-F]+"
 
 FRAME_RE = r"""
-^(?P<index>\d+)\.\s+(?P<function>{FUNCTION_RE}(?:\s+in\s+(?P<package>.+))?$
+^(?P<index>\d+)\.\s+(?P<function>{FUNCTION_RE}){HEX_ADDRESS}(?:\s+in\s+(?P<package>.+))?$
 """.format(
     FUNCTION_RE=FUNCTION_RE,
+    HEX_ADDRESS=HEX_ADDRESS,
 )
 
 FRAME_RE = re.compile(FRAME_RE, re.MULTILINE | re.VERBOSE)
