65755f95351581bd89101ce8eba9ff4768c9474e
Ivana Kellyer <ivana.kellyer@sentry.io>
2025-09-02 13:42:55 +0200

    tests: Move langchain under toxgen (#4734)
    - move the langchain test suites to be governed by toxgen
    - this indirectly results in removing the `-latest` tests in the AI
    group 🎉
    - `-latest` tests predate toxgen and langchain was the last non-toxgen
    test suite (the rest was just skipped) -- all AI tests are now pinned
    - updated the naming scheme to use dashes instead of underscores for
    variants so that it's clearer if something is part of the name of the
    integration or if it denotes a variant
    - for instance, `openai-base` means this is the `base` variant of the
    `openai` test suite, but `openai_agents` means this is the
    `openai_agents` test suite (no variant)
    
    I'm explicitly ignoring the two alpha versions of 1.0 since adapting the
    integration to work with those is out of scope: [dedicated
    issue](https://github.com/getsentry/sentry-python/issues/4735)
      
    Part of https://github.com/getsentry/sentry-python/issues/4506

--- scripts/populate_tox/config.py
+++ scripts/populate_tox/config.py
@@ -126,6 +126,26 @@ TEST_SUITE_CONFIG = {
     "huggingface_hub": {
         "package": "huggingface_hub",
     },
+    "langchain-base": {
+        "package": "langchain",
+        "integration_name": "langchain",
+        "deps": {
+            "*": ["openai", "tiktoken", "langchain-openai"],
+            "<=0.1": ["httpx<0.28.0"],
+            ">=0.3": ["langchain-community"],
+        },
+        "include": "<1.0",
+    },
+    "langchain-notiktoken": {
+        "package": "langchain",
+        "integration_name": "langchain",
+        "deps": {
+            "*": ["openai", "langchain-openai"],
+            "<=0.1": ["httpx<0.28.0"],
+            ">=0.3": ["langchain-community"],
+        },
+        "include": "<1.0",
+    },
     "launchdarkly": {
         "package": "launchdarkly-server-sdk",
     },
@@ -139,7 +159,7 @@ TEST_SUITE_CONFIG = {
     "loguru": {
         "package": "loguru",
     },
-    "openai_base": {
+    "openai-base": {
         "package": "openai",
         "integration_name": "openai",
         "deps": {
@@ -148,7 +168,7 @@ TEST_SUITE_CONFIG = {
         },
         "python": ">=3.8",
     },
-    "openai_notiktoken": {
+    "openai-notiktoken": {
         "package": "openai",
         "integration_name": "openai",
         "deps": {
