afa687cfc08e6bba52a1b481d954e2a53db15084
Anton Pirker <anton.pirker@sentry.io>
2025-09-03 15:04:38 +0200

    attributes for huggingface requests/responses

--- sentry_sdk/integrations/huggingface_hub.py
+++ sentry_sdk/integrations/huggingface_hub.py
@@ -62,7 +62,7 @@ def _capture_exception(exc):
 def _wrap_huggingface_task(f, op):
     # type: (Callable[..., Any], str) -> Callable[..., Any]
     @wraps(f)
-    def new_text_generation(*args, **kwargs):
+    def new_huggingface_task(*args, **kwargs):
         # type: (*Any, **Any) -> Any
         integration = sentry_sdk.get_client().get_integration(HuggingfaceHubIntegration)
         if integration is None:
@@ -82,7 +82,6 @@ def _wrap_huggingface_task(f, op):
 
         client = args[0]
         model = client.model or kwargs.get("model") or ""
-        streaming = kwargs.get("stream")
         operation_name = op.split(".")[-1]
 
         span = sentry_sdk.start_span(
@@ -93,8 +92,28 @@ def _wrap_huggingface_task(f, op):
         span.__enter__()
 
         span.set_data(SPANDATA.GEN_AI_OPERATION_NAME, operation_name)
+
         if model:
             span.set_data(SPANDATA.GEN_AI_REQUEST_MODEL, model)
+
+        # Input attributes
+        attribute_mapping = {
+            "tools": SPANDATA.GEN_AI_REQUEST_AVAILABLE_TOOLS,
+            "frequency_penalty": SPANDATA.GEN_AI_REQUEST_FREQUENCY_PENALTY,
+            "max_tokens": SPANDATA.GEN_AI_REQUEST_MAX_TOKENS,
+            "presence_penalty": SPANDATA.GEN_AI_REQUEST_PRESENCE_PENALTY,
+            "temperature": SPANDATA.GEN_AI_REQUEST_TEMPERATURE,
+            "top_p": SPANDATA.GEN_AI_REQUEST_TOP_P,
+            "top_k": SPANDATA.GEN_AI_REQUEST_TOP_K,
+            "stream": SPANDATA.GEN_AI_RESPONSE_STREAMING,
+        }
+        for attribute, span_attribute in attribute_mapping.items():
+            value = kwargs.get(attribute, None)
+            if value is not None:
+                if isinstance(value, (int, float, bool, str)):
+                    span.set_data(span_attribute, value)
+                else:
+                    set_data_normalized(span, span_attribute, value, unpack=False)
 
         try:
             res = f(*args, **kwargs)
@@ -105,30 +124,56 @@ def _wrap_huggingface_task(f, op):
             raise e from None
 
         with capture_internal_exceptions():
+            # Output attributes
+            if hasattr(res, "model"):
+                model = res.model
+                if model:
+                    span.set_data(SPANDATA.GEN_AI_RESPONSE_MODEL, model)
+
+            if hasattr(res, "details") and res.details is not None:
+                finish_reason = getattr(res.details, "finish_reason", None)
+                if finish_reason:
+                    span.set_data(
+                        SPANDATA.GEN_AI_RESPONSE_FINISH_REASONS, finish_reason
+                    )
+
+            try:
+                tool_calls = res.choices[0].message.tool_calls
+            except Exception:
+                tool_calls = []
+
+            if len(tool_calls) > 0:
+                set_data_normalized(
+                    span,
+                    SPANDATA.GEN_AI_RESPONSE_TOOL_CALLS,
+                    tool_calls,
+                    unpack=False,
+                )
+
             if should_send_default_pii() and integration.include_prompts:
                 set_data_normalized(
                     span, SPANDATA.GEN_AI_REQUEST_MESSAGES, prompt, unpack=False
                 )
 
-            span.set_data(SPANDATA.GEN_AI_RESPONSE_STREAMING, streaming)
-
             if isinstance(res, str):
                 if should_send_default_pii() and integration.include_prompts:
-                    set_data_normalized(
-                        span,
-                        SPANDATA.GEN_AI_RESPONSE_TEXT,
-                        res,
-                    )
+                    if res:
+                        set_data_normalized(
+                            span,
+                            SPANDATA.GEN_AI_RESPONSE_TEXT,
+                            res,
+                        )
                 span.__exit__(None, None, None)
                 return res
 
             if isinstance(res, TextGenerationOutput):
                 if should_send_default_pii() and integration.include_prompts:
-                    set_data_normalized(
-                        span,
-                        SPANDATA.GEN_AI_RESPONSE_TEXT,
-                        res.generated_text,
-                    )
+                    if res.generated_text:
+                        set_data_normalized(
+                            span,
+                            SPANDATA.GEN_AI_RESPONSE_TEXT,
+                            res.generated_text,
+                        )
                 if res.details is not None and res.details.generated_tokens > 0:
                     record_token_usage(
                         span,
@@ -140,13 +185,17 @@ def _wrap_huggingface_task(f, op):
             if isinstance(res, ChatCompletionOutput):
                 if should_send_default_pii() and integration.include_prompts:
                     text_response = "".join(
-                        [x.get("message", {}).get("content") for x in res.choices]
+                        [
+                            x.get("message", {}).get("content", None) or ""
+                            for x in res.choices
+                        ]
                     )
-                    set_data_normalized(
-                        span,
-                        SPANDATA.GEN_AI_RESPONSE_TEXT,
-                        text_response,
-                    )
+                    if text_response:
+                        set_data_normalized(
+                            span,
+                            SPANDATA.GEN_AI_RESPONSE_TEXT,
+                            text_response,
+                        )
                 if hasattr(res, "usage") and res.usage is not None:
                     record_token_usage(
                         span,
@@ -182,11 +231,13 @@ def _wrap_huggingface_task(f, op):
                             and should_send_default_pii()
                             and integration.include_prompts
                         ):
-                            set_data_normalized(
-                                span,
-                                SPANDATA.GEN_AI_RESPONSE_TEXT,
-                                "".join(data_buf),
-                            )
+                            text_response = "".join(data_buf)
+                            if text_response:
+                                set_data_normalized(
+                                    span,
+                                    SPANDATA.GEN_AI_RESPONSE_TEXT,
+                                    text_response,
+                                )
                         if tokens_used > 0:
                             record_token_usage(
                                 span,
@@ -211,13 +262,15 @@ def _wrap_huggingface_task(f, op):
                             and should_send_default_pii()
                             and integration.include_prompts
                         ):
-                            set_data_normalized(
-                                span,
-                                SPANDATA.GEN_AI_RESPONSE_TEXT,
-                                "".join(data_buf),
-                            )
+                            text_response = "".join(data_buf)
+                            if text_response:
+                                set_data_normalized(
+                                    span,
+                                    SPANDATA.GEN_AI_RESPONSE_TEXT,
+                                    text_response,
+                                )
                         span.__exit__(None, None, None)
 
                 return new_iterator()
 
-    return new_text_generation
+    return new_huggingface_task
