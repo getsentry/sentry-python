5b3c754fcde3d04b8240f786c42d56340e7ce86d
Anton Pirker <anton.pirker@sentry.io>
2025-09-04 17:01:08 +0200

    Make _normalize_data deal with model_dump() returning functions or classes

--- sentry_sdk/ai/utils.py
+++ sentry_sdk/ai/utils.py
@@ -11,14 +11,13 @@ from sentry_sdk.utils import logger
 
 def _normalize_data(data, unpack=True):
     # type: (Any, bool) -> Any
-
     # convert pydantic data (e.g. OpenAI v1+) to json compatible format
     if hasattr(data, "model_dump"):
         try:
-            return data.model_dump()
+            return _normalize_data(data.model_dump(), unpack=unpack)
         except Exception as e:
             logger.warning("Could not convert pydantic data to JSON: %s", e)
-            return data
+            return data if isinstance(data, (int, float, bool, str)) else str(data)
     if isinstance(data, list):
         if unpack and len(data) == 1:
             return _normalize_data(data[0], unpack=unpack)  # remove empty dimensions
@@ -26,7 +25,7 @@ def _normalize_data(data, unpack=True):
     if isinstance(data, dict):
         return {k: _normalize_data(v, unpack=unpack) for (k, v) in data.items()}
 
-    return data
+    return data if isinstance(data, (int, float, bool, str)) else str(data)
 
 
 def set_data_normalized(span, key, value, unpack=True):
