ff575b288467efc659d89e491135ac861e0d5fba
Anton Pirker <anton.pirker@sentry.io>
2025-09-04 15:13:00 +0200

    auto enable

--- sentry_sdk/integrations/__init__.py
+++ sentry_sdk/integrations/__init__.py
@@ -95,6 +95,7 @@ _AUTO_ENABLING_INTEGRATIONS = [
     "sentry_sdk.integrations.huey.HueyIntegration",
     "sentry_sdk.integrations.huggingface_hub.HuggingfaceHubIntegration",
     "sentry_sdk.integrations.langchain.LangchainIntegration",
+    "sentry_sdk.integrations.langgraph.LanggraphIntegration",
     "sentry_sdk.integrations.litestar.LitestarIntegration",
     "sentry_sdk.integrations.loguru.LoguruIntegration",
     "sentry_sdk.integrations.openai.OpenAIIntegration",
