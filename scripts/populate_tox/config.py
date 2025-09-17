e8ebbda58022357375af31418d365cdf13691419
Ivana Kellyer <ivana.kellyer@sentry.io>
2025-09-17 14:36:49 +0200

    .

--- scripts/populate_tox/config.py
+++ scripts/populate_tox/config.py
@@ -252,6 +252,7 @@ TEST_SUITE_CONFIG = {
     },
     "ray": {
         "package": "ray",
+        "python": "<=3.11",
     },
     "redis_py_cluster_legacy": {
         "package": "redis-py-cluster",
