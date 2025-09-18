c10fb5c8b71d72c661c0a03999636ec967def616
Anton Pirker <anton.pirker@sentry.io>
2025-09-18 14:45:44 +0200

    Revert "updated test"
    This reverts commit 98c0bf6635f1e336fa9e3b5e352e4bea41793ff6.

--- tests/test_feature_flags.py
+++ tests/test_feature_flags.py
@@ -104,9 +104,7 @@ def test_featureflags_integration_threaded(
             add_feature_flag(flag_key, False)
             # use a tag to identify to identify events later on
             sentry_sdk.set_tag("task_id", flag_key)
-            sentry_sdk.capture_exception(
-                Exception(f"something wrong with task {flag_key}!")
-            )
+            sentry_sdk.capture_exception(Exception("something wrong!"))
 
     # Run tasks in separate threads
     with cf.ThreadPoolExecutor(max_workers=2) as pool:
