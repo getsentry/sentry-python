a535ab829d4e2d72578320914fbc2e75f5c68a11
Ivana Kellyer <ivana.kellyer@sentry.io>
2025-09-18 13:56:41 +0200

    restrict to py37+

--- scripts/populate_tox/config.py
+++ scripts/populate_tox/config.py
@@ -291,8 +291,9 @@ TEST_SUITE_CONFIG = {
             "*": ["fakeredis<2.28.0"],
             "<0.9": ["fakeredis<1.0", "redis<3.2.2"],
             ">=1.10": ["fakeredis>=1.0,<1.7.4"],
-            "py3.6,py3.7": ["fakeredis!=2.26.0"],
+            "py3.7": ["fakeredis!=2.26.0"],
         },
+        "python": ">=3.7",
     },
     "sanic": {
         "package": "sanic",
