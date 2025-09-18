f32df6da386d5529f2679b5c1da8a434edf6d01c
Ivana Kellyer <ivana.kellyer@sentry.io>
2025-09-18 17:33:36 +0200

    .

--- scripts/populate_tox/config.py
+++ scripts/populate_tox/config.py
@@ -291,8 +291,9 @@ TEST_SUITE_CONFIG = {
             "*": ["fakeredis<2.28.0"],
             "<0.7": ["fakeredis<1.0", "redis<3.2.2"],
             ">=0.7,<1.11": ["fakeredis>=1.0,<1.7.4"],
-            "py3.6,py3.7": ["fakeredis!=2.26.0"],
+            "py3.7": ["fakeredis!=2.26.0"],
         },
+        "python": ">=3.7",
     },
     "sanic": {
         "package": "sanic",
