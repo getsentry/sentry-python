222badbbf6549c97dfe23ec655594e502d4cabbf
Ivana Kellyer <ivana.kellyer@sentry.io>
2025-09-19 13:50:15 +0200

    tests: Move rq under toxgen (#4818)
    ### Description
    - moved rq under toxgen 🎉 it needed quite some fiddling around with
    fakeredis versions but seems to work now
    
    #### Issues
    
    Ref https://github.com/getsentry/sentry-python/issues/4506
    
    #### Reminders
    - Please add tests to validate your changes, and lint your code using
    `tox -e linters`.
    - Add GH Issue ID _&_ Linear ID (if applicable)
    - PR title should use [conventional
    commit](https://develop.sentry.dev/engineering-practices/commit-messages/#type)
    style (`feat:`, `fix:`, `ref:`, `meta:`)
    - For external contributors:
    [CONTRIBUTING.md](https://github.com/getsentry/sentry-python/blob/master/CONTRIBUTING.md),
    [Sentry SDK development docs](https://develop.sentry.dev/sdk/), [Discord
    community](https://discord.gg/Ww9hbqr)

--- scripts/populate_tox/config.py
+++ scripts/populate_tox/config.py
@@ -292,6 +292,17 @@ TEST_SUITE_CONFIG = {
     "requests": {
         "package": "requests",
     },
+    "rq": {
+        "package": "rq",
+        "deps": {
+            # https://github.com/jamesls/fakeredis/issues/245
+            # https://github.com/cunla/fakeredis-py/issues/341
+            "*": ["fakeredis<2.28.0"],
+            "<0.9": ["fakeredis<1.0", "redis<3.2.2"],
+            ">=0.9,<0.14": ["fakeredis>=1.0,<1.7.4"],
+            "py3.6,py3.7": ["fakeredis!=2.26.0"],
+        },
+    },
     "sanic": {
         "package": "sanic",
         "deps": {
