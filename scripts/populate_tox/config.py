60db8c2aa0419073338c62e5642914d3c27f9864
Ivana Kellyer <ivana.kellyer@sentry.io>
2025-09-19 11:09:31 +0200

    trying smth

--- scripts/populate_tox/config.py
+++ scripts/populate_tox/config.py
@@ -289,11 +289,10 @@ TEST_SUITE_CONFIG = {
             # https://github.com/jamesls/fakeredis/issues/245
             # https://github.com/cunla/fakeredis-py/issues/341
             "*": ["fakeredis<2.28.0"],
-            "<0.7": ["fakeredis<1.0", "redis<3.2.2"],
-            ">=0.7,<1.11": ["fakeredis>=1.0,<1.7.4"],
-            "py3.7": ["fakeredis!=2.26.0"],
+            "<0.9": ["fakeredis<1.0", "redis<3.2.2"],
+            # ">=0.7,<1.11": ["fakeredis>=1.0,<1.7.4"],
+            "py3.6,py3.7": ["fakeredis!=2.26.0"],
         },
-        "python": ">=3.7",
     },
     "sanic": {
         "package": "sanic",
