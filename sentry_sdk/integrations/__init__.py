fea80090461f2a07ea789d98d56371c4d70d6349
Fabian Schindler <fabian.schindler@sentry.io>
2025-09-30 14:09:28 +0200

    fix(litellm): removing LiteLLM from default enabled integration list

--- sentry_sdk/integrations/__init__.py
+++ sentry_sdk/integrations/__init__.py
@@ -97,7 +97,6 @@ _AUTO_ENABLING_INTEGRATIONS = [
     "sentry_sdk.integrations.langchain.LangchainIntegration",
     "sentry_sdk.integrations.langgraph.LanggraphIntegration",
     "sentry_sdk.integrations.litestar.LitestarIntegration",
-    "sentry_sdk.integrations.litellm.LiteLLMIntegration",
     "sentry_sdk.integrations.loguru.LoguruIntegration",
     "sentry_sdk.integrations.openai.OpenAIIntegration",
     "sentry_sdk.integrations.pymongo.PyMongoIntegration",
