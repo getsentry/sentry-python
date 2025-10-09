27951251e96a650f1449b4e7536ea1cbef28425b
Alexander Alderman Webb <alexander.webb@sentry.io>
2025-10-09 14:03:09 +0200

    change comment

--- tests/integrations/stdlib/__init__.py
+++ tests/integrations/stdlib/__init__.py
@@ -1,6 +1,6 @@
 import os
 import sys
 
-# Load `asyncpg_helpers` into the module search path to test query source path names relative to module. See
-# `test_query_source_with_module_in_search_path`
+# Load `httplib_helpers` into the module search path to test request source path names relative to module. See
+# `test_request_source_with_module_in_search_path`
 sys.path.insert(0, os.path.join(os.path.dirname(__file__)))
