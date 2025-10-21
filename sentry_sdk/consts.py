65d4e3ba1136129031d8bae456ac411b0b8968cd
Alexander Alderman Webb <alexander.webb@sentry.io>
2025-10-21 08:41:22 +0200

    feat: Enable HTTP request code origin by default

--- sentry_sdk/consts.py
+++ sentry_sdk/consts.py
@@ -909,7 +909,7 @@ class ClientConstructor:
         error_sampler=None,  # type: Optional[Callable[[Event, Hint], Union[float, bool]]]
         enable_db_query_source=True,  # type: bool
         db_query_source_threshold_ms=100,  # type: int
-        enable_http_request_source=False,  # type: bool
+        enable_http_request_source=True,  # type: bool
         http_request_source_threshold_ms=100,  # type: int
         spotlight=None,  # type: Optional[Union[bool, str]]
         cert_file=None,  # type: Optional[str]
