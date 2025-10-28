41fe2726260ebe4ede9b78842af571ec71fec9ee
Alexander Alderman Webb <alexander.webb@sentry.io>
2025-10-28 10:59:13 +0100

    test new behaviour

--- tests/integrations/django/test_cache_module.py
+++ tests/integrations/django/test_cache_module.py
@@ -509,6 +509,67 @@ def test_cache_spans_item_size(sentry_init, client, capture_events, use_django_c
     assert second_event["spans"][1]["data"]["cache.item_size"] == 58
 
 
+@pytest_mark_django_db_decorator()
+def test_cache_spans_get_custom_default(
+    sentry_init, capture_events, use_django_caching
+):
+    sentry_init(
+        integrations=[
+            DjangoIntegration(
+                cache_spans=True,
+                middleware_spans=False,
+                signals_spans=False,
+            )
+        ],
+        traces_sample_rate=1.0,
+    )
+    events = capture_events()
+
+    id = os.getpid()
+
+    from django.core.cache import cache
+
+    with sentry_sdk.start_transaction():
+        cache.set(f"S{id}", "Sensitive1")
+        cache.set(f"S{id + 1}", "")
+
+        cache.get(f"S{id}", "null")
+        cache.get(f"S{id}", default="null")
+
+        cache.get(f"S{id + 1}", "null")
+        cache.get(f"S{id + 1}", default="null")
+
+        cache.get(f"S{id + 2}", "null")
+        cache.get(f"S{id + 2}", default="null")
+
+    (transaction,) = events
+    assert len(transaction["spans"]) == 8
+
+    assert transaction["spans"][0]["op"] == "cache.put"
+    assert transaction["spans"][0]["description"] == f"S{id}"
+
+    assert transaction["spans"][1]["op"] == "cache.put"
+    assert transaction["spans"][1]["description"] == f"S{id + 1}"
+
+    for span in (transaction["spans"][2], transaction["spans"][3]):
+        assert span["op"] == "cache.get"
+        assert span["description"] == f"S{id}"
+        assert span["data"]["cache.hit"]
+        assert span["data"]["cache.item_size"] == 10
+
+    for span in (transaction["spans"][4], transaction["spans"][5]):
+        assert span["op"] == "cache.get"
+        assert span["description"] == f"S{id + 1}"
+        assert span["data"]["cache.hit"]
+        assert span["data"]["cache.item_size"] == 0
+
+    for span in (transaction["spans"][6], transaction["spans"][7]):
+        assert span["op"] == "cache.get"
+        assert span["description"] == f"S{id + 2}"
+        assert not span["data"]["cache.hit"]
+        assert "cache.item_size" not in span["data"]
+
+
 @pytest.mark.forked
 @pytest_mark_django_db_decorator()
 def test_cache_spans_get_many(sentry_init, capture_events, use_django_caching):
