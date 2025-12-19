633286fff78d14f2031486c9168eee8cd8b71002
Ivana Kellyer <ivana.kellyer@sentry.io>
2025-12-19 15:05:11 +0100

    love typing

--- sentry_sdk/_log_batcher.py
+++ sentry_sdk/_log_batcher.py
@@ -1,15 +1,15 @@
 from typing import TYPE_CHECKING
 
 from sentry_sdk._batcher import Batcher
-from sentry_sdk._types import Log
 from sentry_sdk.utils import serialize_attribute
 from sentry_sdk.envelope import Envelope, Item, PayloadRef
 
 if TYPE_CHECKING:
     from typing import Any
+    from sentry_sdk._types import Log
 
 
-class LogBatcher(Batcher[Log]):
+class LogBatcher(Batcher["Log"]):
     MAX_BEFORE_FLUSH = 100
     MAX_BEFORE_DROP = 1_000
     FLUSH_WAIT_TIME = 5.0
--- sentry_sdk/_metrics_batcher.py
+++ sentry_sdk/_metrics_batcher.py
@@ -1,15 +1,15 @@
 from typing import TYPE_CHECKING
 
 from sentry_sdk._batcher import Batcher
-from sentry_sdk._types import Metric
 from sentry_sdk.utils import serialize_attribute
 from sentry_sdk.envelope import Item
 
 if TYPE_CHECKING:
     from typing import Any
+    from sentry_sdk._types import Metric
 
 
-class MetricsBatcher(Batcher[Metric]):
+class MetricsBatcher(Batcher["Metric"]):
     MAX_BEFORE_FLUSH = 1000
     MAX_BEFORE_DROP = 10_000
     FLUSH_WAIT_TIME = 5.0
