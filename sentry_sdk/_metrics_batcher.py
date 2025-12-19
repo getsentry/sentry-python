3780eeddfc67acc05cdc32ea0d0c2c977966df86
Ivana Kellyer <ivana.kellyer@sentry.io>
2025-12-19 12:17:49 +0100

    fix mypy

--- sentry_sdk/_metrics_batcher.py
+++ sentry_sdk/_metrics_batcher.py
@@ -1,15 +1,15 @@
 from typing import TYPE_CHECKING
 
 from sentry_sdk._batcher import Batcher
+from sentry_sdk._types import Metric
 from sentry_sdk.utils import serialize_attribute
 from sentry_sdk.envelope import Item
 
 if TYPE_CHECKING:
     from typing import Any
-    from sentry_sdk._types import Metric
 
 
-class MetricsBatcher(Batcher):
+class MetricsBatcher(Batcher[Metric]):
     MAX_BEFORE_FLUSH = 1000
     MAX_BEFORE_DROP = 10_000
     FLUSH_WAIT_TIME = 5.0
@@ -38,7 +38,7 @@ class MetricsBatcher(Batcher):
 
         return res
 
-    def _record_lost(self):
+    def _record_lost(self, item: "Metric") -> None:
         self._record_lost_func(
             reason="queue_overflow",
             data_category="trace_metric",
