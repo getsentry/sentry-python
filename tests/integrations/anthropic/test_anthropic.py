003cb7356802588a9198c1af36d2739ff1495a71
Alex Alderman Webb <alexander.webb@sentry.io>
2026-01-28 10:42:25 +0100

    feat(ai): Add original input length meta attribute (#5375)

--- tests/integrations/anthropic/test_anthropic.py
+++ tests/integrations/anthropic/test_anthropic.py
@@ -1010,6 +1010,7 @@ def test_anthropic_message_truncation(sentry_init, capture_events):
     assert len(parsed_messages) == 1
     assert "small message 5" in str(parsed_messages[0])
 
+    assert chat_span["data"][SPANDATA.META_GEN_AI_ORIGINAL_INPUT_MESSAGES_LENGTH] == 5
     assert tx["_meta"]["spans"]["0"]["data"]["gen_ai.request.messages"][""]["len"] == 5
 
 
@@ -1061,6 +1062,7 @@ async def test_anthropic_message_truncation_async(sentry_init, capture_events):
     assert len(parsed_messages) == 1
     assert "small message 5" in str(parsed_messages[0])
 
+    assert chat_span["data"][SPANDATA.META_GEN_AI_ORIGINAL_INPUT_MESSAGES_LENGTH] == 5
     assert tx["_meta"]["spans"]["0"]["data"]["gen_ai.request.messages"][""]["len"] == 5
 
 
