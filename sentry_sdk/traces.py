09c886605c8cf79024a99ab1641745326257f3e5
Ivana Kellyer <ivana.kellyer@sentry.io>
2026-02-02 10:39:41 +0100

    feat(span-streaming): Add spans to telemetry pipeline, add span name and attributes (#5399)
    - Add streamed spans to telemetry pipeline
    - Add attributes and associated API to `StreamedSpan` since they're used
    in the pipeline
    - Add span name too, to round up the basic span API
    
    Chipping away at https://github.com/getsentry/sentry-python/pull/5317 to
    transform it into reviewable and mergeable PRs.

--- sentry_sdk/traces.py
+++ sentry_sdk/traces.py
@@ -8,8 +8,11 @@ sentry_sdk.init(_experiments={"trace_lifecycle": "stream"}).
 import uuid
 from typing import TYPE_CHECKING
 
+from sentry_sdk.utils import format_attribute
+
 if TYPE_CHECKING:
     from typing import Optional
+    from sentry_sdk._types import Attributes, AttributeValue
 
 
 class StreamedSpan:
@@ -22,14 +25,39 @@ class StreamedSpan:
     span implementation lives in tracing.Span.
     """
 
-    __slots__ = ("_trace_id",)
+    __slots__ = (
+        "name",
+        "_attributes",
+        "_trace_id",
+    )
 
     def __init__(
         self,
         *,
+        name: str,
+        attributes: "Optional[Attributes]" = None,
         trace_id: "Optional[str]" = None,
     ):
+        self.name: str = name
+        self._attributes: "Attributes" = attributes or {}
+
         self._trace_id = trace_id
+
+    def get_attributes(self) -> "Attributes":
+        return self._attributes
+
+    def set_attribute(self, key: str, value: "AttributeValue") -> None:
+        self._attributes[key] = format_attribute(value)
+
+    def set_attributes(self, attributes: "Attributes") -> None:
+        for key, value in attributes.items():
+            self.set_attribute(key, value)
+
+    def remove_attribute(self, key: str) -> None:
+        try:
+            del self._attributes[key]
+        except KeyError:
+            pass
 
     @property
     def trace_id(self) -> str:
