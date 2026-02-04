4768e99022d3eb419ac0a345a18394dd0cba25ce
Ivana Kellyer <ivana.kellyer@sentry.io>
2026-02-04 14:02:23 +0100

    add span_Id to batcher

--- sentry_sdk/_span_batcher.py
+++ sentry_sdk/_span_batcher.py
@@ -71,6 +71,7 @@ class SpanBatcher(Batcher["StreamedSpan"]):
     def _to_transport_format(item: "StreamedSpan") -> "Any":
         # TODO[span-first]
         res: "dict[str, Any]" = {
+            "span_id": item.span_id,
             "name": item._name,
             "status": item._status,
         }
