02af5df52ce3230041e188f0bd52460937bbe3a0
Alexander Alderman Webb <alexander.webb@sentry.io>
2026-02-11 11:29:06 +0100

    fix(openai-agents): Patch tool functions following library refactor

--- sentry_sdk/integrations/openai_agents/__init__.py
+++ sentry_sdk/integrations/openai_agents/__init__.py
@@ -1,8 +1,10 @@
 from sentry_sdk.integrations import DidNotEnable, Integration
+from sentry_sdk.utils import parse_version
 
 from .patches import (
     _create_get_model_wrapper,
-    _create_get_all_tools_wrapper,
+    _create_runner_get_all_tools_wrapper,
+    _create_run_loop_get_all_tools_wrapper,
     _create_run_wrapper,
     _create_run_streamed_wrapper,
     _patch_agent_run,
@@ -17,9 +19,19 @@ try:
     # after it, even if we don't use it.
     import agents
     from agents.run import DEFAULT_AGENT_RUNNER
+    from agents.version import __version__ as OPENAI_AGENTS_VERSION
 
 except ImportError:
     raise DidNotEnable("OpenAI Agents not installed")
+
+
+try:
+    # AgentRunner methods moved in v0.8
+    # https://github.com/openai/openai-agents-python/commit/3ce7c24d349b77bb750062b7e0e856d9ff48a5d5#diff-7470b3a5c5cbe2fcbb2703dc24f326f45a5819d853be2b1f395d122d278cd911
+    from agents.run_internal import run_loop, turn_preparation
+except ImportError:
+    run_loop = None
+    turn_preparation = None
 
 
 def _patch_runner() -> None:
@@ -45,9 +57,15 @@ def _patch_model() -> None:
     )
 
 
-def _patch_tools() -> None:
+def _patch_agent_runner_get_all_tools() -> None:
     agents.run.AgentRunner._get_all_tools = classmethod(
-        _create_get_all_tools_wrapper(agents.run.AgentRunner._get_all_tools),
+        _create_runner_get_all_tools_wrapper(agents.run.AgentRunner._get_all_tools),
+    )
+
+
+def _patch_run_get_all_tools() -> None:
+    agents.run.get_all_tools = _create_run_loop_get_all_tools_wrapper(
+        run_loop.get_all_tools
     )
 
 
@@ -57,6 +75,14 @@ class OpenAIAgentsIntegration(Integration):
     @staticmethod
     def setup_once() -> None:
         _patch_error_tracing()
-        _patch_tools()
         _patch_model()
         _patch_runner()
+
+        library_version = parse_version(OPENAI_AGENTS_VERSION)
+        if library_version is not None and library_version >= (
+            0,
+            8,
+        ):
+            _patch_run_get_all_tools()
+
+        _patch_agent_runner_get_all_tools()
