18dbeae37b579507ba085aa8a0c35a2b41054458
Alex Alderman Webb <alexander.webb@sentry.io>
2026-03-19 14:32:53 +0100

    fix: Do not raise from None (#5709)
    Stop raising exceptions `from None` in the ASGI and asyncio integrations.
    
    Closes https://github.com/getsentry/sentry-python/issues/5624

--- sentry_sdk/integrations/asyncio.py
+++ sentry_sdk/integrations/asyncio.py
@@ -173,8 +173,8 @@ def patch_asyncio() -> None:
                     with span_ctx if span_ctx else nullcontext():
                         try:
                             result = await coro
-                        except StopAsyncIteration as e:
-                            raise e from None
+                        except StopAsyncIteration:
+                            raise
                         except Exception:
                             reraise(*_capture_exception())
 
