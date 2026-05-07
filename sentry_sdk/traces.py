575edf9b65e7f6d395c642790289597dd89fa77c
Ivana Kellyer <ivana.kellyer@sentry.io>
2026-05-07 16:02:40 +0200

    ref: Rename `_timestamp` to `_end_timestamp` (#6235)
    The field capturing the end timestamp of a span used to be called
    `timestamp` in legacy mode (on the `Span` class), with `start_timestamp`
    as its counterpart.
    
    In span streaming, we originally followed the same convention on the new
    `StreamedSpan` class, but there's actually no reason to and it'd be
    better to use `end_timestamp` instead:
    - the field on the serialized span v2 is actually called `end_timestamp`
    - `end_timestamp` is a clearer name in general, and complements
    `start_timestamp` nicely

--- sentry_sdk/traces.py
+++ sentry_sdk/traces.py
@@ -245,7 +245,7 @@ class StreamedSpan:
         "_parent_sampled",
         "_start_timestamp",
         "_start_timestamp_monotonic_ns",
-        "_timestamp",
+        "_end_timestamp",
         "_status",
         "_scope",
         "_previous_span_on_scope",
@@ -292,7 +292,7 @@ class StreamedSpan:
         self._sample_rate = sample_rate
 
         self._start_timestamp = datetime.now(timezone.utc)
-        self._timestamp: "Optional[datetime]" = None
+        self._end_timestamp: "Optional[datetime]" = None
 
         # profiling depends on this value and requires that
         # it is measured in nanoseconds
@@ -328,7 +328,7 @@ class StreamedSpan:
     def __exit__(
         self, ty: "Optional[Any]", value: "Optional[Any]", tb: "Optional[Any]"
     ) -> None:
-        if self._timestamp is not None:
+        if self._end_timestamp is not None:
             # This span is already finished, ignore
             return
 
@@ -362,7 +362,7 @@ class StreamedSpan:
             self._previous_span_on_scope = old_span
 
     def _end(self, end_timestamp: "Optional[Union[float, datetime]]" = None) -> None:
-        if self._timestamp is not None:
+        if self._end_timestamp is not None:
             # This span is already finished, ignore.
             return
 
@@ -393,15 +393,15 @@ class StreamedSpan:
                     pass
 
             if isinstance(end_timestamp, datetime):
-                self._timestamp = end_timestamp
+                self._end_timestamp = end_timestamp
             else:
                 logger.debug(
                     "[Tracing] Failed to set end_timestamp. Using current time instead."
                 )
 
-        if self._timestamp is None:
+        if self._end_timestamp is None:
             elapsed = nanosecond_time() - self._start_timestamp_monotonic_ns
-            self._timestamp = self._start_timestamp + timedelta(
+            self._end_timestamp = self._start_timestamp + timedelta(
                 microseconds=elapsed / 1000
             )
 
@@ -480,8 +480,8 @@ class StreamedSpan:
         return self._start_timestamp
 
     @property
-    def timestamp(self) -> "Optional[datetime]":
-        return self._timestamp
+    def end_timestamp(self) -> "Optional[datetime]":
+        return self._end_timestamp
 
     def _is_segment(self) -> bool:
         return self._segment is self
@@ -700,7 +700,7 @@ class NoOpStreamedSpan(StreamedSpan):
         return None
 
     @property
-    def timestamp(self) -> "Optional[datetime]":
+    def end_timestamp(self) -> "Optional[datetime]":
         return None
 
 
