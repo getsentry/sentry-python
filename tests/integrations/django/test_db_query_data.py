480f1e60d73044c0e79dde5b9d12c1b7ae080ea9
Alexander Alderman Webb <alexander.webb@sentry.io>
2026-05-19 15:21:27 +0200

    test: Respect context manager lifecycles in fake_record_sql_queries

--- tests/integrations/django/test_db_query_data.py
+++ tests/integrations/django/test_db_query_data.py
@@ -346,17 +346,16 @@ def test_no_query_source_if_duration_too_short(sentry_init, client, capture_even
 
     class fake_record_sql_queries:  # noqa: N801
         def __init__(self, *args, **kwargs):
-            with record_sql_queries(*args, **kwargs) as span:
-                self.span = span
-
-            self.span.start_timestamp = datetime(2024, 1, 1, microsecond=0)
-            self.span.timestamp = datetime(2024, 1, 1, microsecond=99999)
+            self._ctx_mgr = record_sql_queries(*args, **kwargs)
 
         def __enter__(self):
+            self.span = self._ctx_mgr.__enter__()
+            self.span.start_timestamp = datetime(2024, 1, 1, microsecond=0)
             return self.span
 
         def __exit__(self, type, value, traceback):
-            pass
+            self._ctx_mgr.__exit__(type, value, traceback)
+            self.span.timestamp = datetime(2024, 1, 1, microsecond=99999)
 
     with mock.patch(
         "sentry_sdk.integrations.django.record_sql_queries",
@@ -404,17 +403,16 @@ def test_query_source_if_duration_over_threshold(sentry_init, client, capture_ev
 
     class fake_record_sql_queries:  # noqa: N801
         def __init__(self, *args, **kwargs):
-            with record_sql_queries(*args, **kwargs) as span:
-                self.span = span
-
-            self.span.start_timestamp = datetime(2024, 1, 1, microsecond=0)
-            self.span.timestamp = datetime(2024, 1, 1, microsecond=101000)
+            self._ctx_mgr = record_sql_queries(*args, **kwargs)
 
         def __enter__(self):
+            self.span = self._ctx_mgr.__enter__()
+            self.span.start_timestamp = datetime(2024, 1, 1, microsecond=0)
             return self.span
 
         def __exit__(self, type, value, traceback):
-            pass
+            self._ctx_mgr.__exit__(type, value, traceback)
+            self.span.timestamp = datetime(2024, 1, 1, microsecond=101000)
 
     with mock.patch(
         "sentry_sdk.integrations.django.record_sql_queries",
