e165b238522884d98569cae5af199b3f27d54955
Neel Shah <neel.shah@sentry.io>
2026-05-27 16:17:05 +0200

    port runtox

--- scripts/runtox.sh
+++ scripts/runtox.sh
@@ -5,21 +5,13 @@
 
 set -ex
 
-if [ -n "$TOXPATH" ]; then
-    true
-elif which tox &> /dev/null; then
-    TOXPATH=tox
-else
-    TOXPATH=./.venv/bin/tox
-fi
-
 searchstring="$1"
 
 # Filter out -latest environments unless explicitly requested
 if [[ "$searchstring" == *-latest* ]]; then
-    ENV="$($TOXPATH -l | grep -- "$searchstring" | tr $'\n' ',')"
+    ENV="$(uv run tox -l | grep -- "$searchstring" | tr $'\n' ',')"
 else
-    ENV="$($TOXPATH -l | grep -- "$searchstring" | grep -v -- '-latest$' | tr $'\n' ',')"
+    ENV="$(uv run tox -l | grep -- "$searchstring" | grep -v -- '-latest$' | tr $'\n' ',')"
 fi
 
 if [ -z "${ENV}" ]; then
@@ -30,7 +22,7 @@ fi
 # Django ASGI tests deadlock under tox's parallel env scheduler when multiple
 # Django envs share the same Postgres service container. Run those serially.
 if [[ "$searchstring" == *-django* ]]; then
-    exec $TOXPATH -e "$ENV" -- "${@:2}"
+    exec uv run tox -e "$ENV" -- "${@:2}"
 else
-    exec $TOXPATH -p auto -o -e "$ENV" -- "${@:2}"
+    exec uv run tox -p auto -o -e "$ENV" -- "${@:2}"
 fi
