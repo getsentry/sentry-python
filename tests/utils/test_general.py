bb324d77d5b9a7f3953116f82d92d30f83ec6be8
Erica Pisani <hey@ericapisani.dev>
2026-07-30 11:23:52 -0400

    test(utils): corner-set for safe_repr prefix x control-char grid (12 -> 4)

--- tests/utils/test_general.py
+++ tests/utils/test_general.py
@@ -38,8 +38,17 @@ def test_safe_repr_regressions():
     assert "лошадь" in safe_repr("лошадь")
 
 
-@pytest.mark.parametrize("prefix", ("", "abcd", "лошадь"))
-@pytest.mark.parametrize("character", "\x00\x07\x1b\n")
+@pytest.mark.parametrize(
+    "prefix,character",
+    [
+        # corner set of prefix x control char (same escape branch for all
+        # combinations)
+        ("", "\x00"),
+        ("abcd", "\n"),
+        ("лошадь", "\x1b"),
+        ("лошадь", "\x07"),
+    ],
+)
 def test_safe_repr_non_printable(prefix, character):
     """Check that non-printable characters are escaped"""
     string = prefix + character
