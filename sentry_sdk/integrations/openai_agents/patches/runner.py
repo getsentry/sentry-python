881ff72ebe63802c800f89ea7fd6643311f6371c
Alexander Alderman Webb <alexander.webb@sentry.io>
2026-08-03 12:38:37 +0200

    early return for pii

--- sentry_sdk/integrations/openai_agents/patches/runner.py
+++ sentry_sdk/integrations/openai_agents/patches/runner.py
@@ -4,6 +4,7 @@ from functools import wraps
 import sentry_sdk
 from sentry_sdk.consts import SPANDATA
 from sentry_sdk.integrations import DidNotEnable
+from sentry_sdk.scope import should_send_default_pii
 from sentry_sdk.traces import StreamedSpan
 from sentry_sdk.utils import capture_internal_exceptions, reraise
 
@@ -45,6 +46,9 @@ class _SentryRunHooks(RunHooks[TContext]):  # type: ignore[misc]
         span = execute_tool_span(tool, agent)
         span.__enter__()
         context.sentry_tool_span = span
+
+        if not should_send_default_pii():
+            return
 
         if isinstance(span, StreamedSpan):
             span.set_attribute(SPANDATA.GEN_AI_TOOL_INPUT, context.tool_arguments)
