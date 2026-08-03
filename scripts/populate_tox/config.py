6575ce8cc440cf3333e5a9ec84b93d06beb0d751
Ivana Kellyer <ivana.kellyer@sentry.io>
2026-08-03 12:43:59 +0200

    remove from config too

--- scripts/populate_tox/config.py
+++ scripts/populate_tox/config.py
@@ -157,13 +157,6 @@ TEST_SUITE_CONFIG = {
         "package": "flask",
         "deps": {
             "*": ["flask-login", "werkzeug", "blinker"],
-            # https://github.com/pallets/flask/issues/4455
-            "<2.0": [
-                "werkzeug<2.1.0",
-                "markupsafe<2.0.0",
-                "itsdangerous>=0.24,<2.0",
-                "jinja2<3.1.1",
-            ],
             "py3.7": ["setuptools<82"],  # Handled by importlib.metadata on Python 3.8+
         },
     },
