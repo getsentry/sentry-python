beb1080907a50badd9a442381db7c438332bc520
Alexander Alderman Webb <alexander.webb@sentry.io>
2026-08-06 15:28:10 +0200

    redo tox

--- scripts/populate_tox/config.py
+++ scripts/populate_tox/config.py
@@ -267,6 +267,7 @@ TEST_SUITE_CONFIG = {
             "*": ["pytest-asyncio", "openai", "langchain-openai"],
             "<=0.1": ["httpx<0.28.0"],
             ">=0.3": ["langchain-community"],
+            ">=1.0": ["langchain-classic"],
             ">=1.1.2": ["google-genai", "langchain-google-genai>=4"],
         },
         "python": {
