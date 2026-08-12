9ee8af5fcf669ac0ccb235a342b46493b7c75649
Ivana Kellyer <ivana.kellyer@sentry.io>
2026-08-12 13:53:34 +0200

    ref(aiohttp): Move crumbs to integration (#7135)
    Create HTTP client breadcrumbs directly in the aiohttp integration
    instead of using `maybe_create_breadcrumbs_from_span`.
    
    This turned out to be a bigger change than expected:
    - Breadcrumb data should be on par with what we're sending now. When we
    were taking it from the span, we had all the data readily available when
    we needed to create the breadcrumb. When moving the logic to the
    integration, we now need to remember information from the start request
    hook because it's not available in the finish request hook, where we
    create the breadcrumb now. (We need to create the breadcrumb at the end,
    because it contains stuff like HTTP status code.)
    - Breadcrumb data should be subject to PII filtering from
    `send_default_pii`/`data_collection`. So e.g. if parts of the query have
    been redacted on a span, they should not appear verbatim in the
    breadcrumb.
    - Since the PII redacting logic is different between transaction-based
    tracing and span streaming, the existing crumb tests needed to test both
    paths, as well as different PII filtering settings.
    
    Additionally:
    - Rename `span` on the aiohttp trace context to `_sentry_span`

--- sentry_sdk/tracing_utils.py
+++ sentry_sdk/tracing_utils.py
@@ -210,10 +210,25 @@ def record_sql_queries(
             yield span
 
 
+def add_http_breadcrumb(status_code: "Optional[int]", data: "dict[str, Any]") -> None:
+    level = None
+    if status_code:
+        if 500 <= status_code <= 599:
+            level = "error"
+        elif 400 <= status_code <= 499:
+            level = "warning"
+
+    kwargs: "dict[str, Any]" = {"type": "http", "category": "httplib", "data": data}
+    if level:
+        kwargs["level"] = level
+
+    sentry_sdk.add_breadcrumb(**kwargs)
+
+
 def maybe_create_breadcrumbs_from_span(
     scope: "sentry_sdk.Scope", span: "sentry_sdk.tracing.Span"
 ) -> None:
-    if span.op == OP.HTTP_CLIENT:
+    if span.op == OP.HTTP_CLIENT and span.origin not in ("auto.http.aiohttp",):
         level = None
         status_code = span._data.get(SPANDATA.HTTP_STATUS_CODE)
         if status_code:
