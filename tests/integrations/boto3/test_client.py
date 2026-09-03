e07aa3191ed00285e6f2a3c0c70d4f71436a4846
Pablo Deputter <pablo.deputter@sentry.io>
2026-09-03 15:21:55 +0200

    test(boto3): cover client call failures

--- tests/integrations/boto3/test_client.py
+++ tests/integrations/boto3/test_client.py
@@ -2,7 +2,7 @@ import boto3
 import pytest
 from botocore.awsrequest import AWSResponse
 from botocore.config import Config
-from botocore.exceptions import ClientError
+from botocore.exceptions import ClientError, EndpointConnectionError
 
 import sentry_sdk
 from sentry_sdk.consts import OP, SPANDATA
@@ -39,53 +39,68 @@ def client_factory(sentry_init, monkeypatch, span_streaming):
 
 
 def _mock_responses(client, status_codes):
-    requests_created = []
-    span_ids = []
+    request_span_ids = []
 
     def record_request(request, **kwargs):
-        requests_created.append(request)
         span = request.context.get("_sentrysdk_span")
         assert span is not None
-        span_ids.append(span.span_id)
+        request_span_ids.append(span.span_id)
 
     def respond(request, **kwargs):
         # `request_created` runs before `before_send`, so use zero-based index for current
         # attempt; `min(..., len(status_codes) - 1)` clamps to last status to avoid `IndexError`.
-        response_index = min(len(requests_created) - 1, len(status_codes) - 1)
+        response_index = min(len(request_span_ids) - 1, len(status_codes) - 1)
         return AWSResponse(request.url, status_codes[response_index], {}, Body(b""))
 
     client.meta.events.register("request-created", record_request)
     client.meta.events.register("before-send", respond)
-    return requests_created, span_ids
+    return request_span_ids
 
 
-def _capture_client_spans(call, capture_items, span_streaming):
+def _capture_boto3_spans_by_op(invoke_client_method, capture_items, span_streaming):
     items = capture_items()
 
     if span_streaming:
         with sentry_sdk.traces.start_span(name="parent"):  # type: ignore[attr-defined]
-            call()
+            invoke_client_method()
 
         sentry_sdk.flush()
-        return [
+        spans = [
             item.payload
             for item in items
             if item.type == "span"
-            and item.payload["attributes"].get(SPANDATA.SENTRY_OP) == OP.HTTP_CLIENT
             and item.payload["attributes"].get(SPANDATA.SENTRY_ORIGIN)
             == Boto3Integration.origin
         ]
+    else:
+        with sentry_sdk.start_transaction():
+            invoke_client_method()
 
-    with sentry_sdk.start_transaction():
-        call()
+        transaction = next(item.payload for item in items if item.type == "transaction")
+        spans = [
+            span
+            for span in transaction["spans"]
+            if span["origin"] == Boto3Integration.origin
+        ]
 
-    transaction = next(item.payload for item in items if item.type == "transaction")
-    return [
-        span
-        for span in transaction["spans"]
-        if span["op"] == OP.HTTP_CLIENT
-        and span["origin"] == Boto3Integration.origin
-    ]
+    spans_by_op = {}
+    for span in spans:
+        op = (
+            span["attributes"].get(SPANDATA.SENTRY_OP) if span_streaming else span["op"]
+        )
+        spans_by_op.setdefault(op, []).append(span)
+    return spans_by_op
+
+
+def _assert_span_finished(span, span_streaming):
+    finished_timestamp = "end_timestamp" if span_streaming else "timestamp"
+    assert span[finished_timestamp] is not None
+
+
+def _assert_one_failed_span(spans, span_streaming):
+    assert len(spans) == 1
+    assert spans[0]["status"] in ("error", "internal_error")
+    _assert_span_finished(spans[0], span_streaming)
 
 
 @pytest.mark.parametrize("attempt_count", [2, 3])
@@ -97,20 +112,18 @@ def test_retry_attempts_share_one_client_span(
     attempt_count,
 ):
     client = client_factory(attempt_count=attempt_count)
-    requests_created, span_ids = _mock_responses(
-        client, [500] * (attempt_count - 1) + [200]
-    )
+    request_span_ids = _mock_responses(client, [500] * (attempt_count - 1) + [200])
 
-    client_spans = _capture_client_spans(
+    spans_by_op = _capture_boto3_spans_by_op(
         lambda: client.head_object(Bucket="bucket", Key="foo"),
         capture_items,
         span_streaming,
     )
+    client_spans = spans_by_op.get(OP.HTTP_CLIENT, [])
 
-    assert len(requests_created) == attempt_count
-    assert len(span_ids) == attempt_count
+    assert len(request_span_ids) == attempt_count
     # all `AWSRequest` instances created during retries reference the same client span.
-    assert len(set(span_ids)) == 1
+    assert len(set(request_span_ids)) == 1
     assert len(client_spans) == 1
 
 
@@ -121,17 +134,99 @@ def test_retries_exhausted_has_one_failed_client_span(
     span_streaming,
 ):
     client = client_factory(attempt_count=2)
-    requests_created, span_ids = _mock_responses(client, [500])
+    request_span_ids = _mock_responses(client, [500])
 
     def attempt_failed_head_object_call():
         with pytest.raises(ClientError):
             client.head_object(Bucket="bucket", Key="foo.pdf")
 
-    client_spans = _capture_client_spans(
+    spans_by_op = _capture_boto3_spans_by_op(
         attempt_failed_head_object_call, capture_items, span_streaming
     )
+    client_spans = spans_by_op.get(OP.HTTP_CLIENT, [])
 
-    assert len(requests_created) == 2
-    assert len(span_ids) == 2
-    assert len(set(span_ids)) == 1
+    assert len(request_span_ids) == 2
+    assert len(set(request_span_ids)) == 1
+    _assert_one_failed_span(client_spans, span_streaming)
+
+
+@pytest.mark.parametrize(
+    "event_name",
+    [
+        pytest.param("before-parameter-build"),
+        pytest.param("before-send"),
+    ],
+)
+@pytest.mark.parametrize("span_streaming", [True, False])
+def test_client_call_exception_is_unchanged_and_finishes_span(
+    capture_items,
+    client_factory,
+    span_streaming,
+    event_name,
+):
+    client = client_factory()
+    if event_name == "before-send":
+        original_exception = EndpointConnectionError(
+            endpoint_url="https://s3.eu-north-1.amazonaws.com"
+        )
+    else:
+        original_exception = ValueError("parameter processing failed")
+
+    def raise_original_exception(**kwargs):
+        raise original_exception
+
+    client.meta.events.register(event_name, raise_original_exception)
+
+    def invoke_failing_client_method():
+        with pytest.raises(type(original_exception)) as exc_info:
+            client.head_object(Bucket="bucket", Key="foo")
+        assert exc_info.value is original_exception
+
+    spans_by_op = _capture_boto3_spans_by_op(
+        invoke_failing_client_method, capture_items, span_streaming
+    )
+    _assert_one_failed_span(spans_by_op.get(OP.HTTP_CLIENT, []), span_streaming)
+
+
+@pytest.mark.parametrize("span_streaming", [True, False])
+def test_streaming_body_read_failure_finishes_stream_span(
+    capture_items,
+    client_factory,
+    span_streaming,
+):
+    client = client_factory()
+    original_exception = OSError("stream read failed")
+
+    class _FailingBody(Body):
+        def __init__(self, exception):
+            super().__init__(b"")
+            self._exception = exception
+
+        def read(self, *args, **kwargs):
+            raise self._exception
+
+    def respond(request, **kwargs):
+        return AWSResponse(
+            request.url,
+            200,
+            {"content-length": "1"},
+            _FailingBody(original_exception),
+        )
+
+    client.meta.events.register("before-send", respond)
+
+    def invoke_client_method_and_read_body():
+        body = client.get_object(Bucket="bucket", Key="foo")["Body"]
+        with pytest.raises(OSError) as exc_info:
+            body.read()
+        assert exc_info.value is original_exception
+
+    spans_by_op = _capture_boto3_spans_by_op(
+        invoke_client_method_and_read_body, capture_items, span_streaming
+    )
+    client_spans = spans_by_op.get(OP.HTTP_CLIENT, [])
+    stream_spans = spans_by_op.get(OP.HTTP_CLIENT_STREAM, [])
+
     assert len(client_spans) == 1
+    assert len(stream_spans) == 1
+    _assert_span_finished(stream_spans[0], span_streaming)
