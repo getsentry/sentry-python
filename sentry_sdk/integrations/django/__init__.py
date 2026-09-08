485b779c1598028c804a03c4aa6c310bf8de4a3a
Alexander Alderman Webb <alexander.webb@sentry.io>
2026-09-08 14:34:00 +0200

    add try...except

--- sentry_sdk/integrations/django/__init__.py
+++ sentry_sdk/integrations/django/__init__.py
@@ -538,9 +538,17 @@ def _attempt_resolve_again(
 
     route_path = None
     if hasattr(request, "urlconf"):
-        route_path = LEGACY_RESOLVER.resolve(request.path_info, urlconf=request.urlconf)
+        try:
+            route_path = LEGACY_RESOLVER.resolve(
+                request.path_info, urlconf=request.urlconf
+            )
+        except Exception:
+            pass
     else:
-        route_path = LEGACY_RESOLVER.resolve(request.path_info)
+        try:
+            route_path = LEGACY_RESOLVER.resolve(request.path_info)
+        except Exception:
+            pass
 
     server_span = sentry_sdk.get_current_scope()._server_segment_span
     if server_span is not None and route_path is not None:
