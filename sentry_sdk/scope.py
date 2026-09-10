92e6f1eb9afebbb38ee7c2df8e19bbb569361301
Ivana Kellyer <ivana.kellyer@sentry.io>
2026-09-10 11:32:27 +0200

    chore: Adapt more tests, set transaction name on scope

--- sentry_sdk/scope.py
+++ sentry_sdk/scope.py
@@ -1212,6 +1212,8 @@ class Scope:
         if parent_span is None:
             propagation_context = self.get_active_propagation_context()
 
+            self._transaction = name
+
             if is_ignored_span(name, attributes):
                 return NoOpStreamedSpan(
                     name=name,
