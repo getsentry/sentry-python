314160578d69972d1efe07f2590287dbceb94ce6
Ivana Kellyer <ivana.kellyer@sentry.io>
2026-09-14 12:00:34 +0200

    unused const

--- sentry_sdk/utils.py
+++ sentry_sdk/utils.py
@@ -84,8 +84,6 @@ logger = logging.getLogger("sentry_sdk.errors")
 
 _installed_modules = None
 
-BASE64_ALPHABET = re.compile(r"^[a-zA-Z0-9/+=]*$")
-
 FALSY_ENV_VALUES = frozenset(("false", "f", "n", "no", "off", "0"))
 TRUTHY_ENV_VALUES = frozenset(("true", "t", "y", "yes", "on", "1"))
 _AWS_SIGV4_SIGNING_ALGORITHMS = frozenset(
