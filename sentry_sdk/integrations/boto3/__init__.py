bc72515dd50ef9825fb0517773c727b1c454b698
Pablo Deputter <pablo.deputter@sentry.io>
2026-09-15 11:44:12 +0200

    remove unused imports

--- sentry_sdk/integrations/boto3/__init__.py
+++ sentry_sdk/integrations/boto3/__init__.py
@@ -1,10 +1,4 @@
-from sentry_sdk.integrations import DidNotEnable, Integration, _check_minimum_version
-from sentry_sdk.utils import parse_version
-
-try:
-    from botocore import __version__ as BOTOCORE_VERSION
-except ImportError:
-    raise DidNotEnable("botocore is not installed")
+from sentry_sdk.integrations import Integration
 
 
 class Boto3Integration(Integration):
