9cc9bef158c53ecfb08cc88b7eae4713a74049f3
Alexander Alderman Webb <alexander.webb@sentry.io>
2026-09-21 10:30:22 +0200

    take out of auto-enabling

--- sentry_sdk/integrations/__init__.py
+++ sentry_sdk/integrations/__init__.py
@@ -93,7 +93,6 @@ _AUTO_ENABLING_INTEGRATIONS = [
     "sentry_sdk.integrations.litestar.LitestarIntegration",
     "sentry_sdk.integrations.loguru.LoguruIntegration",
     "sentry_sdk.integrations.mcp.MCPIntegration",
-    "sentry_sdk.integrations.mistral.MistralIntegration",
     "sentry_sdk.integrations.openai.OpenAIIntegration",
     "sentry_sdk.integrations.openai_agents.OpenAIAgentsIntegration",
     "sentry_sdk.integrations.pydantic_ai.PydanticAIIntegration",
