b5b96b8357d497cf1026775d790eaba286dc31eb
Ivana Kellyer <ivana.kellyer@sentry.io>
2026-09-24 13:03:36 +0200

    ref(graphene): Drop support for `send_default_pii` (#7697)
    Closes
    https://linear.app/getsentry/issue/PY-2823/remove-send-default-pii-from-graphene

--- tests/integrations/graphene/test_graphene.py
+++ tests/integrations/graphene/test_graphene.py
@@ -14,27 +14,13 @@ from sentry_sdk.integrations.starlette import StarletteIntegration
 DATA_COLLECTION_GRAPHQL_DOCUMENTS_PARAMS = [
     pytest.param(
         {"graphql": {"document": True}},
-        None,
         True,
         id="document_on_collects_graphql_data",
     ),
     pytest.param(
         {"graphql": {"document": False}},
-        None,
         False,
         id="document_off_omits_graphql_data",
-    ),
-    pytest.param(
-        {"graphql": {"document": False}},
-        True,
-        False,
-        id="data_collection_takes_precedence_over_send_default_pii_on",
-    ),
-    pytest.param(
-        {"graphql": {"document": True}},
-        False,
-        True,
-        id="data_collection_takes_precedence_over_send_default_pii_off",
     ),
 ]
 
@@ -50,146 +36,18 @@ class Query(ObjectType):
         raise RuntimeError("oh no!")
 
 
-def test_capture_request_if_available_and_send_pii_is_on_async(
-    sentry_init, capture_events
-):
-    sentry_init(
-        send_default_pii=True,
-        integrations=[
-            GrapheneIntegration(),
-            FastApiIntegration(),
-            StarletteIntegration(),
-        ],
-    )
-    events = capture_events()
-
-    schema = Schema(query=Query)
-
-    async_app = FastAPI()
-
-    @async_app.post("/graphql")
-    async def graphql_server_async(request: Request):
-        data = await request.json()
-        result = await schema.execute_async(data["query"])
-        return result.data
-
-    query = {"query": "query ErrorQuery {goodbye}"}
-    client = TestClient(async_app)
-    client.post("/graphql", json=query)
-
-    assert len(events) == 1
-
-    (event,) = events
-    assert event["exception"]["values"][0]["mechanism"]["type"] == "graphene"
-    assert event["request"]["api_target"] == "graphql"
-    assert event["request"]["data"] == query
-
-
-def test_capture_request_if_available_and_send_pii_is_on_sync(
-    sentry_init, capture_events
-):
-    sentry_init(
-        send_default_pii=True,
-        integrations=[GrapheneIntegration(), FlaskIntegration()],
-    )
-    events = capture_events()
-
-    schema = Schema(query=Query)
-
-    sync_app = Flask(__name__)
-
-    @sync_app.route("/graphql", methods=["POST"])
-    def graphql_server_sync():
-        data = request.get_json()
-        result = schema.execute(data["query"])
-        return jsonify(result.data), 200
-
-    query = {"query": "query ErrorQuery {goodbye}"}
-    client = sync_app.test_client()
-    client.post("/graphql", json=query)
-
-    assert len(events) == 1
-
-    (event,) = events
-    assert event["exception"]["values"][0]["mechanism"]["type"] == "graphene"
-    assert event["request"]["api_target"] == "graphql"
-    assert event["request"]["data"] == query
-
-
-def test_do_not_capture_request_if_send_pii_is_off_async(sentry_init, capture_events):
-    sentry_init(
-        integrations=[
-            GrapheneIntegration(),
-            FastApiIntegration(),
-            StarletteIntegration(),
-        ],
-    )
-    events = capture_events()
-
-    schema = Schema(query=Query)
-
-    async_app = FastAPI()
-
-    @async_app.post("/graphql")
-    async def graphql_server_async(request: Request):
-        data = await request.json()
-        result = await schema.execute_async(data["query"])
-        return result.data
-
-    query = {"query": "query ErrorQuery {goodbye}"}
-    client = TestClient(async_app)
-    client.post("/graphql", json=query)
-
-    assert len(events) == 1
-
-    (event,) = events
-    assert event["exception"]["values"][0]["mechanism"]["type"] == "graphene"
-    assert "data" not in event["request"]
-    assert "response" not in event["contexts"]
-
-
-def test_do_not_capture_request_if_send_pii_is_off_sync(sentry_init, capture_events):
-    sentry_init(
-        integrations=[GrapheneIntegration(), FlaskIntegration()],
-    )
-    events = capture_events()
-
-    schema = Schema(query=Query)
-
-    sync_app = Flask(__name__)
-
-    @sync_app.route("/graphql", methods=["POST"])
-    def graphql_server_sync():
-        data = request.get_json()
-        result = schema.execute(data["query"])
-        return jsonify(result.data), 200
-
-    query = {"query": "query ErrorQuery {goodbye}"}
-    client = sync_app.test_client()
-    client.post("/graphql", json=query)
-
-    assert len(events) == 1
-
-    (event,) = events
-    assert event["exception"]["values"][0]["mechanism"]["type"] == "graphene"
-    assert "data" not in event["request"]
-    assert "response" not in event["contexts"]
-
-
 @pytest.mark.parametrize(
-    "data_collection,send_default_pii,expect_api_target",
+    "data_collection,expect_api_target",
     DATA_COLLECTION_GRAPHQL_DOCUMENTS_PARAMS,
 )
-def test_event_processor_data_collection_sync(
-    sentry_init, capture_events, data_collection, send_default_pii, expect_api_target
+def test_document_data_collection_sync(
+    sentry_init, capture_events, data_collection, expect_api_target
 ):
-    init_kwargs = {
-        "integrations": [GrapheneIntegration(), FlaskIntegration()],
-        "data_collection": data_collection,
-    }
-    if send_default_pii is not None:
-        init_kwargs["send_default_pii"] = send_default_pii
-    sentry_init(**init_kwargs)
+    sentry_init(
+        integrations=[GrapheneIntegration(), FlaskIntegration()],
+        data_collection=data_collection,
+    )
+
     events = capture_events()
 
     schema = Schema(query=Query)
@@ -219,24 +77,20 @@ def test_event_processor_data_collection_sync(
 
 
 @pytest.mark.parametrize(
-    "data_collection,send_default_pii,expect_api_target",
+    "data_collection,expect_api_target",
     DATA_COLLECTION_GRAPHQL_DOCUMENTS_PARAMS,
 )
-def test_event_processor_data_collection_async(
-    sentry_init, capture_events, data_collection, send_default_pii, expect_api_target
+def test_document_data_collection_async(
+    sentry_init, capture_events, data_collection, expect_api_target
 ):
-    init_kwargs = {
-        "integrations": [
+    sentry_init(
+        integrations=[
             GrapheneIntegration(),
             FastApiIntegration(),
             StarletteIntegration(),
         ],
-        "data_collection": data_collection,
-    }
-
-    if send_default_pii is not None:
-        init_kwargs["send_default_pii"] = send_default_pii
-    sentry_init(**init_kwargs)
+        data_collection=data_collection,
+    )
 
     events = capture_events()
 
@@ -274,6 +128,7 @@ def test_no_event_if_no_errors_async(sentry_init, capture_events):
             FastApiIntegration(),
             StarletteIntegration(),
         ],
+        data_collection={},
     )
     events = capture_events()
 
@@ -302,6 +157,7 @@ def test_no_event_if_no_errors_sync(sentry_init, capture_events):
             GrapheneIntegration(),
             FlaskIntegration(),
         ],
+        data_collection={},
     )
     events = capture_events()
 
@@ -324,18 +180,12 @@ def test_no_event_if_no_errors_sync(sentry_init, capture_events):
     assert len(events) == 0
 
 
-@pytest.mark.parametrize(
-    "send_default_pii",
-    [True, False],
-)
-def test_graphql_span_holds_query_information(
-    sentry_init, capture_items, send_default_pii
-):
+def test_graphql_span_holds_query_information(sentry_init, capture_items):
     sentry_init(
         integrations=[GrapheneIntegration(), FlaskIntegration()],
         traces_sample_rate=1.0,
         default_integrations=False,
-        send_default_pii=send_default_pii,
+        data_collection={},
     )
     items = capture_items("span")
 
@@ -356,7 +206,7 @@ def test_graphql_span_holds_query_information(
     client = sync_app.test_client()
     client.post("/graphql", json=query)
 
-    sentry_sdk.get_client().flush()
+    sentry_sdk.flush()
 
     spans = [item.payload for item in items]
     assert len(spans) == 2
@@ -371,31 +221,26 @@ def test_graphql_span_holds_query_information(
     assert graphql_span["attributes"]["graphql.operation.type"] == "query"
     assert graphql_span["is_segment"] is False
 
-    if send_default_pii is True:
-        assert graphql_span["attributes"]["graphql.document"] == query["query"]
-    else:
-        assert "graphql.document" not in graphql_span["attributes"]
+    assert graphql_span["attributes"]["graphql.document"] == query["query"]
 
     assert flask_segment["is_segment"] is True
     assert graphql_span["parent_span_id"] == flask_segment["span_id"]
 
 
 @pytest.mark.parametrize(
-    "data_collection,send_default_pii,expect_document",
+    "data_collection,expect_document",
     DATA_COLLECTION_GRAPHQL_DOCUMENTS_PARAMS,
 )
-def test_graphql_span_data_collection(
-    sentry_init, capture_items, data_collection, send_default_pii, expect_document
+def test_graphql_span_data_collection_sync(
+    sentry_init, capture_items, data_collection, expect_document
 ):
-    init_kwargs = {
-        "integrations": [GrapheneIntegration(), FlaskIntegration()],
-        "traces_sample_rate": 1.0,
-        "default_integrations": False,
-        "data_collection": data_collection,
-    }
-    if send_default_pii is not None:
-        init_kwargs["send_default_pii"] = send_default_pii
-    sentry_init(**init_kwargs)
+    sentry_init(
+        integrations=[GrapheneIntegration(), FlaskIntegration()],
+        traces_sample_rate=1.0,
+        default_integrations=False,
+        data_collection=data_collection,
+    )
+
     items = capture_items("span")
 
     schema = Schema(query=Query)
@@ -415,7 +260,7 @@ def test_graphql_span_data_collection(
     client = sync_app.test_client()
     client.post("/graphql", json=query)
 
-    sentry_sdk.get_client().flush()
+    sentry_sdk.flush()
 
     spans = [item.payload for item in items]
     assert len(spans) == 2
@@ -439,12 +284,76 @@ def test_graphql_span_data_collection(
     assert graphql_span["parent_span_id"] == flask_segment["span_id"]
 
 
+@pytest.mark.parametrize(
+    "data_collection,expect_document",
+    DATA_COLLECTION_GRAPHQL_DOCUMENTS_PARAMS,
+)
+def test_graphql_span_data_collection_async(
+    sentry_init, capture_items, data_collection, expect_document
+):
+    sentry_init(
+        integrations=[
+            GrapheneIntegration(),
+            FastApiIntegration(),
+            StarletteIntegration(),
+        ],
+        traces_sample_rate=1.0,
+        default_integrations=False,
+        data_collection=data_collection,
+    )
+
+    items = capture_items("span")
+
+    schema = Schema(query=Query)
+
+    async_app = FastAPI()
+
+    @async_app.post("/graphql")
+    async def graphql_server_async(request: Request):
+        data = await request.json()
+        result = await schema.execute_async(
+            data["query"], operation_name=data.get("operationName")
+        )
+        return result.data
+
+    query = {
+        "query": "query GreetingQuery { hello }",
+        "operationName": "GreetingQuery",
+    }
+    client = TestClient(async_app)
+    client.post("/graphql", json=query)
+
+    sentry_sdk.flush()
+
+    spans = [item.payload for item in items]
+    assert len(spans) == 2
+
+    graphql_span, segment = spans
+
+    assert graphql_span["name"] == query["operationName"]
+    assert graphql_span["attributes"]["sentry.op"] == OP.GRAPHQL_QUERY
+    assert (
+        graphql_span["attributes"]["graphql.operation.name"] == query["operationName"]
+    )
+    assert graphql_span["attributes"]["graphql.operation.type"] == "query"
+    assert graphql_span["is_segment"] is False
+
+    if expect_document:
+        assert graphql_span["attributes"]["graphql.document"] == query["query"]
+    else:
+        assert "graphql.document" not in graphql_span["attributes"]
+
+    assert segment["is_segment"] is True
+    assert graphql_span["parent_span_id"] == segment["span_id"]
+
+
 def test_breadcrumbs_hold_query_information_on_error(sentry_init, capture_items):
     sentry_init(
         integrations=[
             GrapheneIntegration(),
         ],
         default_integrations=False,
+        data_collection={},
     )
     items = capture_items("span", "event")
 
@@ -465,7 +374,7 @@ def test_breadcrumbs_hold_query_information_on_error(sentry_init, capture_items)
     client = sync_app.test_client()
     client.post("/graphql", json=query)
 
-    sentry_sdk.get_client().flush()
+    sentry_sdk.flush()
 
     events = [item.payload for item in items if item.type == "event"]
     assert len(events) == 1
